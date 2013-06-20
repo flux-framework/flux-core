49f9345dc9fc0b2ea14c61590d5160f35411fb33
Jim Garlick <garlick.jim@gmail.com>
2013-06-20 08:53:22 -0700

    zmq-broker: add some comments in cmb.h

--- zmq-broker/cmb.h
+++ zmq-broker/cmb.h
@@ -4,30 +4,60 @@
 
 typedef struct cmb_struct *cmb_t;
 
+/* Create/destroy cmb context used in the other calls.
+ */
 cmb_t cmb_init (void);
 void cmb_fini (cmb_t c);
 
+/* Ping a particular plugin.
+ */
 int cmb_ping (cmb_t c, char *tag, int seq, int padding, char **tagp);
+
+/* Request statisticfs for a particular plugin.
+ */
 int cmb_stats (cmb_t c, char *name, int *req, int *rep, int *event);
 
+/* Watch traffic on the cmb sockets.
+ * Packets are converted to ascii and printed on stderr.
+ */
 int cmb_snoop (cmb_t c, bool enable);
 int cmb_snoop_one (cmb_t c);
 
+/* Subscribe, send, and receive events.
+ * Events are strings that begin with "event.".
+ * Subscriptions are substrings, e.g. the subscription "event.live"
+ * matches "event.live.up" and event.live.down".
+ */
 int cmb_event_subscribe (cmb_t c, char *subscription);
 int cmb_event_unsubscribe (cmb_t c, char *subscription);
 char *cmb_event_recv (cmb_t c);
 int cmb_event_send (cmb_t c, char *event);
 
+/* Execute the named barrier across the session.
+ * The barrier can be any size.
+ */
 int cmb_barrier (cmb_t c, char *name, int nprocs);
 
+/* Get/put key-value pairs.
+ * Errors from puts are deferred until commit.
+ */
 int cmb_kvs_put (cmb_t c, const char *key, const char *val);
 char *cmb_kvs_get (cmb_t c, const char *key);
 int cmb_kvs_commit (cmb_t c, int *errcountp, int *putcountp);
 
+/* Return state of all nodes in the session.
+ * Caller must free the two returned arrays (up and down).
+ * To only get one, set the other argument to NULL.
+ */
 int cmb_live_query (cmb_t c, int **up, int *ulp, int **dp, int *dlp, int *nnp);
 
+/* This is not working right now.
+ */
 int cmb_fd_open (cmb_t c, char *wname, char **np);
 
+/* Log messages.
+ * Subscriptions are not implemented yet (all messages are returned).
+ */
 int cmb_vlog (cmb_t c, const char *tag, const char *src,
               const char *fmt, va_list ap);
 int cmb_log (cmb_t c, const char *tag, const char *src, const char *fmt, ...)
@@ -36,6 +66,10 @@ int cmb_log_subscribe (cmb_t c, const char *sub);
 int cmb_log_unsubscribe (cmb_t c, const char *sub);
 char *cmb_log_recv (cmb_t c, char **tagp, struct timeval *tvp, char **fromp);
 
+/* Manipulate cmb routing tables.
+ * Add and del are fire and forget (no reply).
+ * Query returns JSON string, caller must free.
+ */
 int cmb_route_add (cmb_t c, int rank, int gw);
 int cmb_route_del (cmb_t c, int rank);
 char *cmb_route_query (cmb_t c);
