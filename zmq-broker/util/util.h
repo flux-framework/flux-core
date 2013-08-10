bd33a5999972b6162bb026b4daeaf9cf589d9d6b
Jim Garlick <garlick.jim@gmail.com>
2013-08-10 14:52:51 -0700

    util: add helpers to compute/verify SHA1 over serialized JSON object

--- zmq-broker/util/util.h
+++ zmq-broker/util/util.h
@@ -35,8 +35,12 @@ int env_getints (char *name, int **iap, int *lenp, int dflt_ia[], int dflt_len);
 char *argv_concat (int argc, char *argv[]);
 
 /* Fill 'href' with ASCII string representation of SHA1 hash of dat/len.
+ * The other two compute and verify same over serialized JSON object.
  */
 void compute_href (const void *dat, int len, href_t href);
+void compute_json_href (json_object *o, href_t href);
+bool verify_json_href (json_object *o, const href_t href);
+
 
 /* JSON helpers
  * N.B. for get_base64(): caller must free returned data if non-NULL.
