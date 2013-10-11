08fb755428a604a9da4d701e9e256f2194b6094d
Mark A. Grondona <mgrondona@llnl.gov>
2013-10-11 09:09:22 -0700

    mpi_hello: Print time for MPI_Init
    Print the time taken to complete MPI_Init in mpi_hello test.

--- zmq-broker/mpi_hello.c
+++ zmq-broker/mpi_hello.c
@@ -2,18 +2,47 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+
+static struct timespec diff (struct timespec start, struct timespec end)
+{
+    struct timespec temp;
+    if ((end.tv_nsec-start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
+}
+
+static double time_since (struct timespec t0)
+{
+    struct timespec ts, d;
+    clock_gettime (CLOCK_MONOTONIC, &ts);
+
+    d = diff (t0, ts);
+
+    return ((double) d.tv_sec * 1000 + (double) d.tv_nsec / 1000000);
+}
 
 int
 main(int argc, char *argv[])
 {
         int id, ntasks;
+	struct timespec ts0;
         char hostname[64];
+
+	clock_gettime (CLOCK_MONOTONIC, &ts0);
 
         MPI_Init(&argc, &argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &id);
         MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
         if (id == 0) {
-                printf("0: completed MPI_Init.  There are %d tasks\n", ntasks);
+                fprintf(stderr,
+		  "0: completed MPI_Init in %0.3fs.  There are %d tasks\n",
+		   time_since (ts0)/1000, ntasks);
                 fflush(stdout);
         }
 
