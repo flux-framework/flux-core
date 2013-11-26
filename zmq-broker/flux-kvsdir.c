043164b21ed81c3171b5b2b2e137371f2d2c9ace
Jim Garlick <garlick.jim@gmail.com>
2013-11-26 10:19:25 -0800

    flux-util: make flux-kvsdir options more ls-like

--- zmq-broker/flux-kvsdir.c
+++ zmq-broker/flux-kvsdir.c
@@ -10,19 +10,22 @@
 #include "util.h"
 #include "log.h"
 
-#define OPTIONS "hr"
+#define OPTIONS "hvRF"
 static const struct option longopts[] = {
     {"help",       no_argument,  0, 'h'},
-    {"recursive",  no_argument,  0, 'r'},
+    {"values",     no_argument,  0, 'v'},
+    {"recursive",  no_argument,  0, 'R'}, /* matches ls arg */
+    {"classify",   no_argument,  0, 'F'}, /* matches ls arg */
     { 0, 0, 0, 0 },
 };
 
-static void dump_kvs_dir (flux_t h, const char *path, bool ropt);
+static void dump_kvs_dir (flux_t h, const char *path,
+                          bool vopt, bool Ropt, bool Fopt);
 
 void usage (void)
 {
     fprintf (stderr, 
-"Usage: flux-kvsdir [--recursive] key\n"
+"Usage: flux-kvsdir [--values] [--classify] [--recursive] key\n"
 );
     exit (1);
 }
@@ -32,7 +35,9 @@ int main (int argc, char *argv[])
     flux_t h;
     int ch;
     char *key = NULL;
-    bool ropt = false;
+    bool vopt = false;
+    bool Ropt = false;
+    bool Fopt = false;
 
     log_init ("flux-kvsdir");
 
@@ -41,8 +46,14 @@ int main (int argc, char *argv[])
             case 'h': /* --help */
                 usage ();
                 break;
-            case 'r': /* --recursive */
-                ropt = true;
+            case 'v': /* --values */
+                vopt = true;
+                break;
+            case 'R': /* --recursive */
+                Ropt = true;
+                break;
+            case 'F': /* --classify */
+                Fopt = true;
                 break;
             default:
                 usage ();
@@ -57,14 +68,15 @@ int main (int argc, char *argv[])
     if (!(h = cmb_init ()))
         err_exit ("cmb_init");
 
-    dump_kvs_dir (h, key ? key : ".", ropt);
+    dump_kvs_dir (h, key ? key : ".", vopt, Ropt, Fopt);
 
     flux_handle_destroy (&h);
     log_fini ();
     return 0;
 }
 
-static void dump_kvs_dir (flux_t h, const char *path, bool ropt)
+static void dump_kvs_dir (flux_t h, const char *path,
+                          bool vopt, bool Ropt, bool Fopt)
 {
     kvsdir_t dir;
     kvsitr_t itr;
@@ -80,37 +92,43 @@ static void dump_kvs_dir (flux_t h, const char *path, bool ropt)
     while ((name = kvsitr_next (itr))) {
         key = kvsdir_key_at (dir, name);
         if (kvsdir_issymlink (dir, name)) {
-            char *link;
+            char *F = Fopt ? "@" : "";
 
-            if (kvs_get_symlink (h, key, &link) < 0) {
-                printf ("%s: %s\n", key, strerror (errno));
-                continue;
-            }
-            printf ("%s -> %s\n", key, link);
-            free (link);
-
+            if (vopt) {
+                char *link;
+                if (kvs_get_symlink (h, key, &link) < 0) {
+                    printf ("%s%s: %s\n", key, F, strerror (errno));
+                    continue;
+                }
+                printf ("%s%s -> %s\n", key, F, link);
+                free (link);
+            } else
+                printf ("%s%s", key, F);
         } else if (kvsdir_isdir (dir, name)) {
-            if (ropt)
-                dump_kvs_dir (h, key, ropt);
-            else
-                printf ("%s [dir]\n", key);
+            char *F = Fopt ? "." : "";
 
+            if (Ropt)
+                dump_kvs_dir (h, key, vopt, Ropt, Fopt);
+            else
+                printf ("%s%s\n", key, F);
         } else {
-            json_object *o;
-            int len, max;
-
-            if (kvs_get (h, key, &o) < 0) {
-                printf ("%s: %s\n", key, strerror (errno));
-                continue;
-            }
-            js = json_object_to_json_string_ext (o, JSON_C_TO_STRING_PLAIN);
-            len = strlen (js);
-            max = 80 - strlen (key) - 4;
-            if (len > max)
-                printf ("%s = %.*s ...\n", key, max - 4, js);
-            else
-                printf ("%s = %s\n", key, js);
-            json_object_put (o);
+            if (vopt) {
+                json_object *o;
+                int len, max;
+                if (kvs_get (h, key, &o) < 0) {
+                    printf ("%s: %s\n", key, strerror (errno));
+                    continue;
+                }
+                js = json_object_to_json_string_ext (o, JSON_C_TO_STRING_PLAIN);
+                len = strlen (js);
+                max = 80 - strlen (key) - 4;
+                if (len > max)
+                    printf ("%s = %.*s ...\n", key, max - 4, js);
+                else
+                    printf ("%s = %s\n", key, js);
+                json_object_put (o);
+            } else
+                printf ("%s\n", key);
         }
         free (key);
     }
