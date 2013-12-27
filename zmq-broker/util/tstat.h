e9012e9789c262535affe9b0ea34261a8913ee56
Jim Garlick <garlick.jim@gmail.com>
2013-12-27 12:18:26 -0800

    util: add tstat functions for gathering statistics

---
+++ zmq-broker/util/tstat.h
@@ -0,0 +1,24 @@
+#ifndef _HAVE_CMB_TSTAT_H
+#define _HAVE_CMB_TSTAT_H
+
+typedef struct {
+    double min, max;
+    int n;
+    double M, S, newM, newS;
+} tstat_t;
+
+void tstat_push (tstat_t *ts, double x);
+double tstat_mean (tstat_t *ts);
+double tstat_min (tstat_t *ts);
+double tstat_max (tstat_t *ts);
+double tstat_variance (tstat_t *ts);
+double tstat_stddev (tstat_t *ts);
+int tstat_count (tstat_t *ts);
+
+void util_json_object_add_tstat (json_object *o, const char *name,
+                                 tstat_t *ts, double scale);
+
+#endif /* !_HAVE_CMB_TSTAT_H */
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
