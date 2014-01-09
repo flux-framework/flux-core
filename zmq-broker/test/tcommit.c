9ae86dd7c7b8d2d28edcc4f8fc11b671a30a0722
Jim Garlick <garlick.jim@gmail.com>
2014-01-09 14:15:18 -0800

    test/tcommit: add --stats
    Get statistics on the individual put+commit/fence times and overall
    throughput.

--- zmq-broker/test/tcommit.c
+++ zmq-broker/test/tcommit.c
@@ -6,33 +6,45 @@
 #include <libgen.h>
 #include <pthread.h>
 #include <getopt.h>
+#include <czmq.h>
 
 #include "cmb.h"
 #include "util.h"
 #include "log.h"
+#include "tstat.h"
 
 typedef struct {
     pthread_t t;
     pthread_attr_t attr;
     int n;
     flux_t h;
+    zlist_t *perf;
 } thd_t;
 
 static int count = -1;
 static int nthreads = -1;
 static char *prefix = NULL;
 static bool fopt = false;
+static bool sopt = false;
 
-#define OPTIONS "f"
+#define OPTIONS "fs"
 static const struct option longopts[] = {
    {"fence",   no_argument,         0, 'f'},
+   {"stats",   no_argument,         0, 's'},
    {0, 0, 0, 0},
 };
 
 static void usage (void)
 {
-    fprintf (stderr, "Usage: tcommit [--fence] nthreads count prefix\n");
+    fprintf (stderr, "Usage: tcommit [--fence] [--stats] nthreads count prefix\n");
     exit (1);
+}
+
+double *ddup (double d)
+{
+    double *dcpy = xzmalloc (sizeof (double));
+    *dcpy = d;
+    return dcpy; 
 }
 
 void *thread (void *arg)
@@ -40,6 +52,7 @@ void *thread (void *arg)
     thd_t *t = arg;
     char *key, *fence = NULL;
     int i;
+    struct timespec t0;
 
     if (!(t->h = cmb_init ())) {
         err ("%d: cmb_init", t->n);
@@ -50,6 +63,8 @@ void *thread (void *arg)
             oom ();
         if (fopt && asprintf (&fence, "%s-%d", prefix, i) < 0)
             oom ();
+        if (sopt)
+            monotime (&t0);
         if (kvs_put_int (t->h, key, 42) < 0)
             err_exit ("%s", key);
         if (fopt) {
@@ -59,6 +74,8 @@ void *thread (void *arg)
             if (kvs_commit (t->h) < 0)
                 err_exit ("kvs_commit");
         }
+        if (sopt && zlist_append (t->perf, ddup (monotime_since (t0))) < 0)
+            oom ();
         free (key);
         if (fence)
             free (fence);
@@ -74,6 +91,8 @@ int main (int argc, char *argv[])
     thd_t *thd;
     int i, rc;
     int ch;
+    tstat_t ts;
+    struct timespec t0;
 
     log_init (basename (argv[0]));
 
@@ -81,6 +100,9 @@ int main (int argc, char *argv[])
         switch (ch) {
             case 'f':
                 fopt = true;
+                break;
+            case 's':
+                sopt = true;
                 break;
             default:
                 usage ();
@@ -93,10 +115,17 @@ int main (int argc, char *argv[])
     count = strtoul (argv[optind++], NULL, 10);
     prefix = argv[optind++];
 
+    memset (&ts, 0, sizeof (ts));
+
     thd = xzmalloc (sizeof (*thd) * nthreads);
+
+    if (sopt)
+        monotime (&t0);
 
     for (i = 0; i < nthreads; i++) {
         thd[i].n = i;
+        if (!(thd[i].perf = zlist_new ()))
+            oom ();
         if ((rc = pthread_attr_init (&thd[i].attr)))
             errn (rc, "pthread_attr_init");
         if ((rc = pthread_create (&thd[i].t, &thd[i].attr, thread, &thd[i])))
@@ -106,6 +135,23 @@ int main (int argc, char *argv[])
     for (i = 0; i < nthreads; i++) {
         if ((rc = pthread_join (thd[i].t, NULL)))
             errn (rc, "pthread_join");
+        if (sopt) {
+            double *e;
+            while ((e = zlist_pop (thd[i].perf))) {
+                tstat_push (&ts, *e);
+                free (e);
+            }
+        }
+        zlist_destroy (&thd[i].perf);
+    }
+
+    if (sopt) {
+        json_object *o = util_json_object_new_object ();
+        util_json_object_add_tstat (o, "put+commit times (sec)", &ts, 1E-3);
+        util_json_object_add_double (o, "put+commmit throughput (#/sec)",
+                          (double)(count*nthreads)/(monotime_since (t0)*1E-3));
+        printf ("%s\n", json_object_to_json_string_ext (o,JSON_C_TO_STRING_PRETTY));
+        json_object_put (o);
     }
 
     free (thd);
