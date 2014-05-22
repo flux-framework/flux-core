fff05bbc3695e4f418f3a77b5884479b42384ee8
Mark A. Grondona <mgrondona@llnl.gov>
2014-05-22 16:05:45 -0700

    rdl: Change ClusterOf to ListOf
    Rename ClusterOf to ListOf to avoid confusion with future real
    "Cluster" resources.

--- zmq-broker/conf/hype.lua
+++ zmq-broker/conf/hype.lua
@@ -1,11 +1,9 @@
 
 uses "Node"
-uses "ClusterOf"
 
 Hierarchy "default" {
     Resource{ "cluster", name = "hype",
-    children = ClusterOf{
-                  type = Node,
+    children = ListOf{ Node,
                   ids = "201-354",
                   args = { name = "hype", sockets = {"0-7", "8-15"} }
               }
