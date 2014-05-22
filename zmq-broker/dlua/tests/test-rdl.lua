26cbcf76b6cc6ff0b5364f8fb161d483707be89a
Mark A. Grondona <mgrondona@llnl.gov>
2014-05-22 15:57:29 -0700

    rdl: Add a sanity testsuite for RDL.lua

---
+++ zmq-broker/dlua/tests/test-rdl.lua
@@ -0,0 +1,197 @@
+#!/usr/bin/lua
+
+require 'lunit'
+
+local RDL = require 'RDL'
+
+module ("TestRDL", lunit.testcase, package.seeall)
+
+local function equals(t1, t2)
+   if t1 == t2 then
+       return true
+   end
+   if type(t1) ~= "table" or type(t2) ~= "table" then
+       return false
+   end
+   local v2
+   for k,v1 in pairs(t1) do
+       v2 = t2[k]
+       if v1 ~= v2 and not equals(v1, t2[k]) then
+           return false
+       end
+   end
+   for k in pairs(t2) do
+       if t1[k] == nil then
+           return false
+       end
+   end
+   return true
+end
+
+function test_rdl()
+    local rdl = RDL.eval([[
+Hierarchy "default" { Resource {"node", name="foo", id=0, tags = { "bar" } }}
+]])
+    assert_not_nil (rdl)
+    assert_true (is_table (rdl))
+    local r = rdl:resource("default")
+
+    assert_not_nil (r)
+    assert_equal ("foo0", r.name)
+    assert_equal ("node", r.type)
+    assert_equal (0,      r.id)
+    assert_equal ("default:/foo0", r.uri)
+    assert_equal ("/foo0", r.path)
+    assert_equal ("default", r.hierarchy_name)
+    assert(is_table(r.tags), "r.tags is "..type(r.tags))
+    assert (equals ({node = 1, bar = 1}, r.tags))
+    assert (equals ({node = 1, bar = 1}, r:aggregate()))
+
+    local t = r:tabulate()
+    --print (require 'inspect' (t))
+    assert_equal (r.type, t.type)
+    assert_equal (r.id,   t.id)
+    assert_equal (r.uuid, t.uuid)
+    assert_equal (r.path, t.hierarchy [r.hierarchy_name])
+
+    local uuid = r.uuid
+    assert_equal (36, uuid:len())
+    assert_equal (nil, uuid:find ("'[^a-f0-9-]"))
+end
+
+function test_has_socket()
+    local rdl = RDL.eval([[uses "Socket"]])
+    assert_not_nil (rdl)
+end
+
+function test_children()
+    local rdl = RDL.eval([[
+uses "Socket"
+Hierarchy "default" {
+  Resource {"node", name="foo", id=0,
+   children = {
+      Socket { id=0, cpus="0-3" },
+      Socket { id=1, cpus="4-7" }
+   }
+}}
+]])
+    assert_not_nil (rdl)
+    assert_true (is_table (rdl))
+    local a = {
+        node = 1,
+        socket = 2,
+        core = 8
+    }
+    local b = rdl:aggregate ('default')
+    local i = require 'inspect'
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+    local r = rdl:resource ('default:/foo0')
+    assert_not_nil (r)
+    assert_equal ("foo0", r.name)
+    assert_equal ("foo0", tostring (r))
+    assert_equal ("node", r.type)
+    assert_equal (0,      r.id)
+    assert_equal ("default:/foo0", r.uri)
+    assert_equal ("/foo0", r.path)
+    assert_equal ("default", r.hierarchy_name)
+
+    local b = r:aggregate()
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+    local s = r:next_child()
+    assert_not_nil (s)
+    assert_equal ("socket0", s.name)
+    assert_equal ("socket0", tostring (s))
+    assert_equal ("socket",  s.type)
+    assert_equal (0,         s.id)
+    assert_equal ("default:/foo0/socket0", s.uri)
+    assert_equal ("/foo0/socket0", s.path)
+    assert_equal ("default", s.hierarchy_name)
+
+    local a = { socket = 1, core = 4 }
+    local b = s:aggregate()
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+    local c = s:next_child()
+    assert_not_nil (c)
+    assert_equal ("core0", c.name)
+    assert_equal ("core0", tostring (c))
+    assert_equal ("core",  c.type)
+    assert_equal (0,       c.id)
+    assert_equal ("default:/foo0/socket0/core0", c.uri)
+    assert_equal ("/foo0/socket0/core0", c.path)
+    assert_equal ("default", c.hierarchy_name)
+
+    local a = { core = 1 }
+    local b = c:aggregate()
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+    -- Test unlink of child from socket0
+    --
+    assert (s:unlink ("core1"))
+
+    local a = { socket = 1, core = 3 }
+    local b = s:aggregate()
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+    -- Ensure next child is now core2 (we deleted core1)
+    local c = s:next_child()
+    assert_not_nil (c)
+    assert_equal ("core2", c.name)
+    assert_equal ("core2", tostring (c))
+    assert_equal ("core",  c.type)
+    assert_equal (2,       c.id)
+    assert_equal ("default:/foo0/socket0/core2", c.uri)
+    assert_equal ("/foo0/socket0/core2", c.path)
+    assert_equal ("default", c.hierarchy_name)
+
+    -- Top level aggregate should now be updated
+    local a = { node = 1, socket = 2, core = 7 }
+    local b = assert (rdl:aggregate ('default'))
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+  end
+
+function test_copy ()
+    local rdl = RDL.eval([[
+uses "Socket"
+Hierarchy "default" {
+  Resource {"node", name="foo", id=0,
+   children = {
+      Socket { id=0, cpus="0-3" },
+      Socket { id=1, cpus="4-7" }
+   }
+}}
+]])
+    assert_not_nil (rdl)
+    assert_true (is_table (rdl))
+    local a = {
+        node = 1,
+        socket = 2,
+        core = 8
+    }
+    local b = rdl:aggregate ('default')
+    local i = require 'inspect'
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+
+    -- Copy:
+    local slice = rdl:copy ("default:/foo0/socket1")
+
+    assert_not_nil (slice)
+    assert_true (is_table (slice))
+    local a = {
+        node = 1,
+        socket = 1,
+        core = 4
+    }
+    local b = slice:aggregate ('default')
+    assert (equals (a, b), "Expected ".. i(a) .. " got ".. i(b))
+
+    local r = assert (slice:resource ("default:/foo0/socket1"))
+    assert_equal ("socket1", r.name)
+
+end
+
+-- vi: ts=4 sw=4 expandtab
