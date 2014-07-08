b51100184e0743389a2e4902cd110e3ad2e1d18f
Jim Garlick <garlick.jim@gmail.com>
2014-07-08 16:02:48 -0700

    live: write conf.live.hello to KVS
    Write a nodelist to the KVS that represents all nodes that
    have reported in to their parent with a hello message, plus the root node.

--- zmq-broker/livesrv.c
+++ zmq-broker/livesrv.c
@@ -1,8 +1,8 @@
 /* livesrv.c - node liveness service */
 
 /* This module builds on the following services in the cmbd:
- * cmb.peers - get idle time (in heartbeats) for non-module peers
- * cmb.failover , cmb.recover - switch parent
+ *   cmb.peers - get idle time (in heartbeats) for non-module peers
+ *   cmb.failover , cmb.recover - switch parent
  * The cmbd expects failover/recovery to be driven externally (e.g. by us).
  * The cmbd maintains a hash of peers and their idle time, and also sends
  * a cmb.ping upstream on the heartbeat if nothing else has been sent in the
@@ -58,6 +58,8 @@
 #include "log.h"
 #include "plugin.h"
 #include "shortjson.h"
+#include "reduce.h"
+#include "hostlist.h"
 
 typedef enum { CS_OK, CS_SLOW, CS_FAIL } cstate_t;
 
@@ -84,6 +86,7 @@ typedef struct {
     zlist_t *parents;
     zhash_t *children;
     bool hb_subscribed;
+    red_t r;
     flux_t h;
 } ctx_t;
 
@@ -93,8 +96,13 @@ static int hello (ctx_t *ctx);
 static int goodbye (ctx_t *ctx, int parent_rank);
 static void manage_subscriptions (ctx_t *ctx);
 
+static void hello_sink (flux_t h, void *item, int batchnum, void *arg);
+static void hello_reduce (flux_t h, zlist_t *items, int batchnum, void *arg);
+
 static const int default_max_idle = 5;
 static const int default_slow_idle = 3;
+static const int reduce_timeout_master_msec = 100;
+static const int reduce_timeout_slave_msec = 10;
 
 static void freectx (ctx_t *ctx)
 {
@@ -104,6 +112,7 @@ static void freectx (ctx_t *ctx)
         parent_destroy (p);
     zlist_destroy (&ctx->parents);
     zhash_destroy (&ctx->children);
+    flux_red_destroy (ctx->r);
     free (ctx);
 }
 
@@ -121,6 +130,13 @@ static ctx_t *getctx (flux_t h)
             oom ();
         if (!(ctx->children = zhash_new ()))
             oom ();
+        ctx->r = flux_red_create (h, hello_sink, ctx);
+        flux_red_set_reduce_fn (ctx->r, hello_reduce);
+        flux_red_set_flags (ctx->r, FLUX_RED_TIMEDFLUSH);
+        if (ctx->master)
+            flux_red_set_timeout_msec (ctx->r, reduce_timeout_master_msec);
+        else
+            flux_red_set_timeout_msec (ctx->r, reduce_timeout_slave_msec);
         ctx->h = h;
         flux_aux_set (h, "livesrv", ctx, (FluxFreeFn)freectx);
     }
@@ -488,6 +504,101 @@ done:
     return rc;
 }
 
+static char *hl_string (hostlist_t hl)
+{
+    int len = 64;
+    char *s = xzmalloc (len);
+
+    hostlist_sort (hl);
+    while (hostlist_ranged_string (hl, len, s) < 0)
+        if (!(s = realloc (s, len *= 2)))
+            oom ();
+    len = strlen (s);
+    if (s[len - 1] == ']')
+        s[len - 1] = '\0';
+    if (*s == '[')
+        memmove (s, s + 1, len);
+    return s;
+}
+
+static void hello_sink (flux_t h, void *item, int batchnum, void *arg)
+{
+    ctx_t *ctx = arg;
+    hostlist_t b, a = item;
+    char *s;
+
+    if (ctx->master) { /* sink to KVS */
+        if (kvs_get_string (h, "conf.live.hello", &s) == 0) {
+            b = hostlist_create (s);
+            free (s);
+            hostlist_push_list (a, b);
+            hostlist_destroy (b);
+        }
+        s = hl_string (a);
+        if (kvs_put_string (h, "conf.live.hello", s) < 0 || kvs_commit (h) < 0)
+            flux_log (h, LOG_ERR, "%s: kvs_put_string: %s",
+                      __FUNCTION__, strerror (errno));
+        free (s);
+    } else {           /* push upstream */
+        s = hl_string (a);
+        JSON o = json_object_new_string (s);
+        if (flux_request_send (h, o, "live.push") < 0)
+            flux_log (h, LOG_ERR, "%s: flux_request_send: %s",
+                      __FUNCTION__, strerror (errno));
+        Jput (o);
+        free (s);
+    }
+}
+
+static void hello_reduce (flux_t h, zlist_t *items, int batchnum, void *arg)
+{
+    hostlist_t a, b;
+
+    if ((a = zlist_pop (items))) {
+        while ((b = zlist_pop (items))) {
+            hostlist_push_list (a, b);
+            hostlist_destroy (b);
+        }
+        zlist_append (items, a);
+    }
+}
+
+static int push_request_cb (flux_t h, int typemask, zmsg_t **zmsg, void *arg)
+{
+    ctx_t *ctx = arg;
+    JSON request = NULL;
+    const char *s;
+    hostlist_t a;
+
+    if (cmb_msg_decode (*zmsg, NULL, &request) < 0 || request == NULL
+             || !(s = json_object_get_string (request))
+             || !(a = hostlist_create (s))) {
+        flux_log (ctx->h, LOG_ERR, "%s: bad message", __FUNCTION__);
+        goto done;
+    }
+    if (flux_red_append (ctx->r, a, 0) < 0) {
+        flux_log (ctx->h, LOG_ERR, "%s: flux_red_append: %s",
+                  __FUNCTION__, strerror (errno));
+        hostlist_destroy (a);
+    }
+done:
+    Jput (request);
+    return 0;
+}
+
+static void hello_source (ctx_t *ctx, const char *rankstr)
+{
+    hostlist_t a;
+
+    if ((a = hostlist_create (rankstr))) {
+        if (flux_red_append (ctx->r, a, 0) < 0) {
+            flux_log (ctx->h, LOG_ERR, "%s: flux_red_append: %s",
+                      __FUNCTION__, strerror (errno));
+            hostlist_destroy (a);
+        }
+    }
+}
+
 /* hello: parents discover their children, and children discover their
  * grandparents which are potential failover candidates.
  */
@@ -505,13 +616,15 @@ static int hello_request_cb (flux_t h, int typemask, zmsg_t **zmsg, void *arg)
         goto done;
     }
     /* Create a record for this child, unless already seen.
+     * Also send rank upstream to be written (reduced) to conf.live.hello.
      */
     c = child_create (rank);
-    if (zhash_insert (ctx->children, c->rankstr, c) < 0)
-        child_destroy (c);
-    else
+    if (zhash_insert (ctx->children, c->rankstr, c) == 0) {
         zhash_freefn (ctx->children, c->rankstr,(zhash_free_fn *)child_destroy);
-    manage_subscriptions (ctx);
+        manage_subscriptions (ctx);
+        hello_source (ctx, c->rankstr);
+    } else
+        child_destroy (c);
     /* Construct response - built from our own hello response, if any.
      * We add ourselves temporarily, sans URI (see parents_fromjson()).
      */
@@ -552,6 +665,7 @@ static msghandler_t htab[] = {
     { FLUX_MSGTYPE_EVENT,       "live.cstate",         cstate_cb },
     { FLUX_MSGTYPE_REQUEST,     "live.hello",          hello_request_cb },
     { FLUX_MSGTYPE_REQUEST,     "live.goodbye",        goodbye_request_cb },
+    { FLUX_MSGTYPE_REQUEST,     "live.push",           push_request_cb },
 };
 const int htablen = sizeof (htab) / sizeof (htab[0]);
 
@@ -562,6 +676,12 @@ int mod_main (flux_t h, zhash_t *args)
     if (!ctx->master) {
         if (hello (ctx) < 0)
             return -1;
+    } else {
+        char *rankstr;
+        if (asprintf (&rankstr, "%d", ctx->rank) < 0)
+            oom ();
+        hello_source (ctx, rankstr);
+        free (rankstr);
     }
     if (kvs_watch_int (h, "conf.live.max-idle", max_idle_cb, ctx) < 0) {
         flux_log (h, LOG_ERR, "kvs_watch_int %s: %s", "conf.live.max-idle",
