3b6b83368e15de2c56ffa6568a9d4f2a4bccfa01
Jim Garlick <garlick.jim@gmail.com>
2014-08-04 15:01:11 -0700

    util: convert flux-host.c to use nodset_t

--- zmq-broker/flux-host.c
+++ zmq-broker/flux-host.c
@@ -5,8 +5,9 @@
 #include <json/json.h>
 #include <assert.h>
 #include <libgen.h>
+#include <stdbool.h>
 
-#include "hostlist.h"
+#include "nodeset.h"
 #include "cmb.h"
 #include "util.h"
 #include "log.h"
@@ -21,22 +22,23 @@ static const struct option longopts[] = {
 void usage (void)
 {
     fprintf (stderr, 
-"Usage: flux-host [--address] rank ...\n"
+"Usage: flux-host [--address] [nodeset] ...\n"
 );
     exit (1);
 }
 
-static char *rank2host (json_object *hosts, int rank);
-static char *rank2addr (json_object *hosts, int rank);
+static const char *rank2host (json_object *hosts, uint32_t rank);
+static const char *rank2addr (json_object *hosts, uint32_t rank);
 
 int main (int argc, char *argv[])
 {
     flux_t h;
     int ch;
     json_object *hosts;
-    hostlist_t hl;
-    hostlist_iterator_t itr;
-    char *host;
+    nodeset_t n = nodeset_new ();
+    nodeset_itr_t itr;
+    uint32_t r;
+    int size;
     bool aopt = false;
 
     log_init ("flux-host");
@@ -53,48 +55,35 @@ int main (int argc, char *argv[])
     }
     if (!(h = cmb_init ()))
         err_exit ("cmb_init");
+    if ((size = flux_size (h)) < 0)
+        err_exit ("flux_size");
     if (kvs_get (h, "hosts", &hosts) < 0)
         err_exit ("kvs_get hosts");
 
-    if (!(hl = hostlist_create ("")))
-        oom ();
     if (optind == argc) {
-        int size;
-        char *s;
-        if (flux_info (h, NULL, &size, NULL) < 0)
-            err_exit ("flux_info");
-        if (asprintf (&s, "0-%d", size - 1) < 0)
-            oom ();
-        if (!hostlist_push (hl, s))
-            err_exit ("hostlist_push: %s", s);
-        free (s);
+        nodeset_add_range (n, 0, size - 1);
     } else {
-        for (; optind < argc; optind++) {
-            if (!hostlist_push (hl, argv[optind]))
-                err_exit ("hostlist_push: %s", argv[optind]);
+        while (optind < argc) {
+            if (!nodeset_add_str (n, argv[optind++]))
+                err_exit ("error parsing nodeset argument");
         }
     }
-    hostlist_sort (hl);
-    hostlist_uniq (hl);
 
-    if (!(itr = hostlist_iterator_create (hl)))
-        oom ();
-    while ((host = hostlist_next (itr))) {
-        int rank = strtoul (host, NULL, 10);
-        char *s = aopt ? rank2addr (hosts, rank) : rank2host (hosts, rank);
-        printf ("%d:\t%s\n", rank, s);
-        free (s);
+    itr = nodeset_itr_new (n);
+    while ((r = nodeset_next (itr)) != NODESET_EOF) {
+        printf ("%u:\t%s\n", r, aopt ? rank2addr (hosts, r)
+                                     : rank2host (hosts, r));
     }
-    hostlist_iterator_destroy (itr);
+    nodeset_itr_destroy (itr);
+    nodeset_destroy (n);
 
-    hostlist_destroy (hl);
     json_object_put (hosts);
     flux_handle_destroy (&h);
     log_fini ();
     return 0;
 }
 
-static char *rank2host (json_object *hosts, int rank)
+static const char *rank2host (json_object *hosts, uint32_t rank)
 {
     json_object *o;
     const char *name;
@@ -103,10 +92,10 @@ static char *rank2host (json_object *hosts, int rank)
         msg_exit ("%s: rank %d not found", __FUNCTION__, rank);
     if (util_json_object_get_string (o, "name", &name) < 0)
         msg_exit ("%s: rank %d malformed hosts entry", __FUNCTION__, rank);
-    return xstrdup (name);
+    return name;
 }
 
-static char *rank2addr (json_object *hosts, int rank)
+static const char *rank2addr (json_object *hosts, uint32_t rank)
 {
     json_object *o, *a, *a0;
     const char *addr;
@@ -117,7 +106,7 @@ static char *rank2addr (json_object *hosts, int rank)
                 || !(a0 = json_object_array_get_idx (a, 0))
                 || !(addr = json_object_get_string (a0)))
         msg_exit ("%s: rank %d no addr", __FUNCTION__, rank);
-    return xstrdup (addr);
+    return addr;
 }
 
 /*
