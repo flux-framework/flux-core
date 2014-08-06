aacfd95c2959af7e231208dccc07189dfc28c304
Mark A. Grondona <mgrondona@llnl.gov>
2014-08-06 09:10:51 -0700

    conf/hype.lua: Configure memory to hype nodes

--- zmq-broker/conf/hype.lua
+++ zmq-broker/conf/hype.lua
@@ -5,7 +5,9 @@ Hierarchy "default" {
     Resource{ "cluster", name = "hype",
     children = { ListOf{ Node,
                   ids = "201-354",
-                  args = { name = "hype", sockets = {"0-7", "8-15"} }
+                  args = { name = "hype",
+                           sockets = {"0-7", "8-15"},
+                           memory_per_socket = 15000 }
                  },
                }
     }
