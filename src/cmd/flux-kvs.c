ddb7606403f993a48e3eee08f957492715db6d0d
Jim Garlick <garlick.jim@gmail.com>
2014-09-16 15:07:24 -0700

    flux-kvs: make into comprehensive kvs util

--- src/cmd/flux-kvs.c
+++ src/cmd/flux-kvs.c
@@ -37,50 +37,45 @@
 
 #include "xzmalloc.h"
 #include "log.h"
+#include "shortjson.h"
 
 #include "flux.h"
 #include "kvs.h"
 #include "api.h"
 
-#define OPTIONS "hdCDNqm:s:r:vV:"
+#define OPTIONS "h"
 static const struct option longopts[] = {
     {"help",       no_argument,  0, 'h'},
-    {"no-commit",  no_argument,  0, 'C'},
-    {"dropcache",  no_argument,  0, 'd'},
-    {"null-noerror", no_argument,  0, 'N'},
-    {"dropcache-all",  no_argument,  0, 'D'},
-    {"quiet",      no_argument,  0, 'q'},
-    {"mkdir",      required_argument,  0, 'm'},
-    {"symlink",    required_argument,  0, 's'},
-    {"readlink",   required_argument,  0, 'r'},
-    {"wait-version", required_argument,  0, 'V'},
-    {"get-version", no_argument,  0, 'v'},
     { 0, 0, 0, 0 },
 };
 
-void get (flux_t h, const char *key, bool null_noerror, bool quiet);
-void del (flux_t h, const char *key, bool quiet);
-void put (flux_t h, const char *key, const char *val, bool quiet);
-void commit (flux_t h);
+void cmd_get (flux_t h, int argc, char **argv);
+void cmd_put (flux_t h, int argc, char **argv);
+void cmd_unlink (flux_t h, int argc, char **argv);
+void cmd_link (flux_t h, int argc, char **argv);
+void cmd_readlink (flux_t h, int argc, char **argv);
+void cmd_mkdir (flux_t h, int argc, char **argv);
+void cmd_version (flux_t h, int argc, char **argv);
+void cmd_wait (flux_t h, int argc, char **argv);
+void cmd_watch (flux_t h, int argc, char **argv);
+void cmd_dropcache (flux_t h, int argc, char **argv);
+void cmd_dropcache_all (flux_t h, int argc, char **argv);
+
 
 void usage (void)
 {
-    fprintf (stderr, "Usage: flux-kvs key[=val] [key[=val]] [^] ...\n"
-"where the arguments are one or more of:\n"
-"    key         displays value of key\n"
-"    key=        unlinks key\n"
-"    key=val     sets value of key (with commit unless --no-commit)\n"
-"    ^           commit\n"
-"and 'val' has the form:\n"
-"    4           json int\n"
-"    4.2         json double\n"
-"    true|false  json boolean\n"
-"    [1,2,3]     json array (of int, but may be any type)\n"
-"    \"string\"    json string\n"
-"    {...}       json object\n"
-"remember to escape any characters that are interpted by your shell\n"
-"Use --dropcache to drop the local slave cache\n."
-"Use --dropcache-all to drop slave caches across the session\n."
+    fprintf (stderr,
+"Usage: flux-kvs get         key [key...]\n"
+"       flux-kvs put         key=val [key=val...]\n"
+"       flux-kvs unlink      key [key...]\n"
+"       flux-kvs link        target link_name\n"
+"       flux-kvs readlink    key\n"
+"       flux-kvs mkdir       key [key...]\n"
+"       flux-kvs watch       key\n"
+"       flux-kvs version\n"
+"       flux-kvs wait N\n"
+"       flux-kvs dropcache\n"
+"       flux-kvs dropcache-all\n"
 );
     exit (1);
 }
@@ -88,19 +83,8 @@ void usage (void)
 int main (int argc, char *argv[])
 {
     flux_t h;
-    bool need_commit = false;
-    int i, ch;
-    bool dopt = false;
-    bool Dopt = false;
-    bool Copt = false;
-    bool Nopt = false;
-    bool qopt = false;
-    char *mkdir_name = NULL;
-    char *symlink_name = NULL;
-    char *readlink_name = NULL;
-    bool vopt = false;
-    bool Vopt = false;
-    int version;
+    int ch;
+    char *cmd;
 
     log_init ("flux-kvs");
 
@@ -109,169 +93,201 @@ int main (int argc, char *argv[])
             case 'h': /* --help */
                 usage ();
                 break;
-            case 'C': /* --no-commit */
-                Copt = true;
-                break;
-            case 'd': /* --dropcache */
-                dopt = true;
-                break;
-            case 'D': /* --dropcache-all */
-                Dopt = true;
-                break;
-            case 'N': /* --null-noerror */
-                Nopt = true;
-                break;
-            case 'q': /* --quiet */
-                qopt = true;
-                break;
-            case 'm': /* --mkdir DIR */
-                mkdir_name = optarg;
-                break;
-            case 's': /* --symlink name=target */
-                symlink_name = optarg;
-                break;
-            case 'r': /* --readlink name */
-                readlink_name = optarg;
-                break;
-            case 'v': /* --get-version */
-                vopt = true;
-                break;
-            case 'V': /* --get-version */
-                Vopt = true;
-                version = strtoul (optarg, NULL, 10);
-                break;
             default:
                 usage ();
                 break;
         }
     }
-    if (optind == argc && !(dopt || Dopt || mkdir_name || symlink_name || readlink_name || vopt || Vopt))
+    if (optind == argc)
         usage ();
+    cmd = argv[optind++];
 
     if (!(h = flux_api_open ()))
         err_exit ("flux_api_open");
 
-    if (dopt) {
-        if (kvs_dropcache (h) < 0)
-            err_exit ("kvs_dropcache");
-    }
-    if (Dopt) {
-        if (flux_event_send (h, NULL, "event.kvs.dropcache") < 0)
-            err_exit ("flux_event_send");
-    }
-    if (mkdir_name) {
-        if (kvs_mkdir (h, mkdir_name) < 0)
-            err_exit ("flux_mkdir %s", mkdir_name);
-        if (!Copt)
-            need_commit = true;
-    }
-    if (symlink_name) {
-        char *cpy = xstrdup (symlink_name);
-        char *val = strchr (cpy, '=');
-        if (!val)
-            msg_exit ("--symlink requires a name=target argument");
-        *val++ = '\0';
-        if (kvs_symlink (h, cpy, val) < 0)
-            err_exit ("flux_symlink %s %s", cpy, val);
-        free (cpy);
-        if (!Copt)
-            need_commit = true;
-    }
-    if (readlink_name) {
-        char *val;
-        if (kvs_get_symlink (h, readlink_name, &val) < 0)
-            err_exit ("kvs_get_symlink %s", readlink_name);
-        if (qopt)
-            printf ("%s\n", val);
-        else
-            printf ("%s=%s\n", readlink_name, val);
-        free (val);
-    }
-    if (vopt) {
-        if (kvs_get_version (h, &version) < 0)
-            err_exit ("kvs_get_version");
-        printf ("%d\n", version);
-    }
-    if (Vopt) {
-        if (kvs_wait_version (h, version) < 0)
-            err_exit ("kvs_wait_version");
-    }
-
-    for (i = optind; i < argc; i++) {
-        char *key = xstrdup (argv[i]);
-        char *val = strchr (key, '=');
-
-        if (val) {
-            *val++ = '\0';
-            if (*val)
-                put (h, key, val, qopt);
-            else
-                del (h, key, qopt);
-            if (!Copt)
-                need_commit = true;
-        } else {
-            if (!strcmp (key, "^"))
-                commit (h);
-            else
-                get (h, key, Nopt, qopt);
-        }
-        free (key);
-    }
-    if (need_commit)
-        commit (h);
+    if (!strcmp (cmd, "get"))
+        cmd_get (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "put"))
+        cmd_put (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "unlink"))
+        cmd_unlink (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "link"))
+        cmd_link (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "readlink"))
+        cmd_readlink (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "mkdir"))
+        cmd_mkdir (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "version"))
+        cmd_version (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "wait"))
+        cmd_wait (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "watch"))
+        cmd_watch (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "dropcache"))
+        cmd_dropcache (h, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "dropcache-all"))
+        cmd_dropcache_all (h, argc - optind, argv + optind);
+    else
+        usage ();
 
     flux_api_close (h);
     log_fini ();
     return 0;
 }
 
-void get (flux_t h, const char *key, bool null_noerror, bool quiet)
+void cmd_get (flux_t h, int argc, char **argv)
 {
-    json_object *val;
+    JSON o;
+    int i;
 
-    if (kvs_get (h, key, &val) < 0) {
-        if (errno == ENOENT && null_noerror) {
-            if (quiet)
-                printf ("null\n");
-            else
-                printf ("%s=null\n", key);
-        } else
-            err_exit ("%s", key);
-    } else {
-        if (quiet)
-            printf ("%s\n", json_object_to_json_string (val));
+    if (argc == 0)
+        msg_exit ("get: specify one or more keys");
+    for (i = 0; i < argc; i++) {
+        if (kvs_get (h, argv[i], &o) < 0)
+            err_exit ("%s", argv[i]);
         else
-            printf ("%s=%s\n", key, json_object_to_json_string (val));
-        json_object_put (val);
+            printf ("%s\n", Jtostr (o));
+        Jput (o);
     }
 }
 
-void put (flux_t h, const char *key, const char *valstr, bool quiet)
+void cmd_put (flux_t h, int argc, char **argv)
 {
-    json_object *val = NULL;
+    int i;
 
-    assert (valstr != NULL);
-    val = json_tokener_parse (valstr);
-    if (kvs_put (h, key, val) < 0)
-        err_exit ("%s", key);
-    else if (!quiet)
-        printf ("%s=%s\n", key, json_object_to_json_string (val));
-    if (val)
-        json_object_put (val);
-}
-
-void del (flux_t h, const char *key, bool quiet)
-{
-    if (kvs_unlink (h, key) < 0)
-        err_exit ("%s", key);
-    else if (!quiet)
-        printf ("%s=\n", key);
-}
-
-void commit (flux_t h)
-{
+    if (argc == 0)
+        msg_exit ("put: specify one or more key=value pairs");
+    for (i = 0; i < argc; i++) {
+        char *key = xstrdup (argv[i]);
+        char *val = strchr (key, '=');
+        JSON o;
+        if (!val)
+            msg_exit ("put: you must specify a value as key=value");
+        *val++ = '\0';
+        if ((o = Jfromstr (val))) {
+            if (kvs_put (h, key, o) < 0)
+                err_exit ("%s", key);
+        } else {
+            if (kvs_put_string (h, key, val) < 0)
+                err_exit ("%s", key);
+        }
+        Jput (o);
+        free (key);
+    }
     if (kvs_commit (h) < 0)
         err_exit ("kvs_commit");
+}
+
+void cmd_unlink (flux_t h, int argc, char **argv)
+{
+    int i;
+
+    if (argc == 0)
+        msg_exit ("unlink: specify one or more keys");
+    for (i = 0; i < argc; i++) {
+        /* FIXME: unlink nonexistent silently fails */
+        /* FIXME: unlink directory silently succedes */
+        if (kvs_unlink (h, argv[i]) < 0)
+            err_exit ("%s", argv[i]);
+    }
+    if (kvs_commit (h) < 0)
+        err_exit ("kvs_commit");
+}
+
+void cmd_link (flux_t h, int argc, char **argv)
+{
+    if (argc != 2)
+        msg_exit ("link: specify target and link_name");
+    if (kvs_symlink (h, argv[1], argv[0]) < 0)
+        err_exit ("%s", argv[1]);
+    if (kvs_commit (h) < 0)
+        err_exit ("kvs_commit");
+}
+
+void cmd_readlink (flux_t h, int argc, char **argv)
+{
+    int i;
+    char *target;
+
+    if (argc == 0)
+        msg_exit ("readlink: specify one or more keys"); 
+    for (i = 0; i < argc; i++) {
+        if (kvs_get_symlink (h, argv[i], &target) < 0)
+            err_exit ("%s", argv[i]);
+        else
+            printf ("%s\n", target);
+        free (target);
+    }
+}
+
+void cmd_mkdir (flux_t h, int argc, char **argv)
+{
+    int i;
+
+    if (argc == 0)
+        msg_exit ("mkdir: specify one or more directories");
+    for (i = 0; i < argc; i++) {
+        if (kvs_mkdir (h, argv[i]) < 0)
+            err_exit ("%s", argv[i]);
+    }
+    if (kvs_commit (h) < 0)
+        err_exit ("kvs_commit");
+}
+
+void cmd_version (flux_t h, int argc, char **argv)
+{
+    int vers;
+    if (argc != 0)
+        msg_exit ("version: takes no arguments");
+    if (kvs_get_version (h, &vers) < 0)
+        err_exit ("kvs_get_version");
+    printf ("%d\n", vers);
+}
+
+void cmd_wait (flux_t h, int argc, char **argv)
+{
+    int vers;
+    if (argc != 1)
+        msg_exit ("wait: specify a version");
+    vers = strtoul (argv[0], NULL, 10);
+    if (kvs_wait_version (h, vers) < 0)
+        err_exit ("kvs_get_version");
+    printf ("%d\n", vers);
+}
+
+void cmd_watch (flux_t h, int argc, char **argv)
+{
+    JSON o = NULL;
+
+    if (argc != 1)
+        msg_exit ("watch: specify one key");
+    if (kvs_get (h, argv[0], &o) < 0 && errno != ENOENT) 
+        err_exit ("%s", argv[0]);
+    printf ("%s\n", o ? Jtostr (o) : "NULL");
+    for (;;) {
+        if (kvs_watch_once (h, argv[0], &o) < 0 && errno != ENOENT)
+            err_exit ("%s", argv[0]);
+        printf ("%s\n", o ? Jtostr (o) : "NULL");
+    }
+    /* FIXME: handle SIGINT? */
+    /* FIXME: handle directory */
+    Jput (o);
+}
+
+void cmd_dropcache (flux_t h, int argc, char **argv)
+{
+    if (argc != 0)
+        msg_exit ("dropcache: takes no arguments");
+    if (kvs_dropcache (h) < 0)
+        err_exit ("kvs_dropcache");
+}
+
+void cmd_dropcache_all (flux_t h, int argc, char **argv)
+{
+    if (argc != 0)
+        msg_exit ("dropcache-all: takes no arguments");
+    if (flux_event_send (h, NULL, "kvs.dropcache") < 0)
+        err_exit ("flux_event_send");
 }
 
 /*
