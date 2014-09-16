aeedef983a72c95a4dc6c05a167c0c33907e6fcf
Jim Garlick <garlick.jim@gmail.com>
2014-09-16 13:07:43 -0700

    flux driver: add --verbose option
    Dump FLUX_ environment vars if --verbose is given.

--- src/cmd/flux.c
+++ src/cmd/flux.c
@@ -46,15 +46,17 @@
 #include "setenvf.h"
 #include "argv.h"
 
-void exec_subcommand (const char *exec_path, char *argv[]);
+void dump_environment (void);
+void exec_subcommand (bool vopt, char *argv[]);
 char *dir_self (void);
 
-#define OPTIONS "+T:tx:hM:"
+#define OPTIONS "+T:tx:hM:v"
 static const struct option longopts[] = {
     {"tmpdir",          required_argument,  0, 'T'},
     {"trace-apisocket", no_argument,        0, 't'},
     {"exec-path",       required_argument,  0, 'x'},
     {"module-path",     required_argument,  0, 'M'},
+    {"verbose",         no_argument,        0, 'v'},
     {"help",            no_argument,        0, 'h'},
     {0, 0, 0, 0},
 };
@@ -67,6 +69,7 @@ static void usage (void)
 "    -M,--module-path PATH    set FLUX_MODULE_PATH\n"
 "    -T,--tmpdir PATH         set FLUX_TMPDIR\n"
 "    -t,--trace-apisock       set FLUX_TRACE_APISOCK=1\n"
+"    -v,--verbose             show environment\n"
 );
 }
 
@@ -106,8 +109,8 @@ int setup_lua_env (const char *exedir)
 int main (int argc, char *argv[])
 {
     int ch;
-    char *exec_path;
     bool hopt = false;
+    bool vopt = false;
     char *flux_exe_dir = dir_self ();
 
     log_init ("flux");
@@ -130,6 +133,9 @@ int main (int argc, char *argv[])
                 if (setenv ("FLUX_EXEC_PATH", optarg, 1) < 0)
                     err_exit ("setenv FLUX_EXEC_PATH=%s", optarg);
                 break;
+            case 'v': /* --exec-path */
+                vopt = true;
+                break;
             case 'h': /* --help  */
                 hopt = true;
                 break;
@@ -146,7 +152,6 @@ int main (int argc, char *argv[])
      * accordingly.
      */
     if (strcmp (flux_exe_dir, X_BINDIR) != 0) {
-        msg ("Configuring for execution from source tree");
         glob_t gl;
         char *modpath;
         if (!getenv ("FLUX_EXEC_PATH"))
@@ -162,15 +167,14 @@ int main (int argc, char *argv[])
 
     //setup_lua_env (flux_exe_dir);
 
-    if (!(exec_path = getenv ("FLUX_EXEC_PATH"))) {
-        exec_path = EXEC_PATH;
-        if (setenv ("FLUX_EXEC_PATH", exec_path, 1) < 0)
+    if (!getenv ("FLUX_EXEC_PATH")) {
+        if (setenv ("FLUX_EXEC_PATH", EXEC_PATH, 1) < 0)
             err_exit ("setenv FLUX_EXEC_PATH=%s", optarg);
     }
     if (hopt) {
         if (argc > 0) {
             char *av[] = { argv[0], "--help", NULL };
-            exec_subcommand (exec_path, av);
+            exec_subcommand (vopt, av);
         } else
             help ();
         exit (0);
@@ -179,14 +183,29 @@ int main (int argc, char *argv[])
         usage ();
         exit (1);
     }
-
-    exec_subcommand (exec_path, argv);
+    if (vopt)
+        dump_environment ();
+    exec_subcommand (vopt, argv);
 
     free (flux_exe_dir);
 
     log_fini ();
 
     return 0;
+}
+
+void dump_environment_one (const char *name)
+{
+    char *s = getenv (name);
+    msg ("%20s=%s", name, s ? s : "<unset>");
+}
+
+void dump_environment (void)
+{
+    dump_environment_one ("FLUX_EXEC_PATH");
+    dump_environment_one ("FLUX_MODULE_PATH");
+    dump_environment_one ("FLUX_TMPDIR");
+    dump_environment_one ("FLUX_TRACE_APISOCK");
 }
 
 /*  Return directory containing this executable.  Caller must free.
@@ -204,23 +223,27 @@ char *dir_self (void)
     return xstrdup (flux_exe_dir);
 }
 
-void exec_subcommand_dir (const char *dir, char *argv[], const char *prefix)
+void exec_subcommand_dir (bool vopt, const char *dir,char *argv[],
+                          const char *prefix)
 {
     char *path;
     if (asprintf (&path, "%s/%s%s", dir, prefix ? prefix : "", argv[0]) < 0)
         oom ();
+    if (vopt)
+        msg ("trying to exec %s", path);
     execvp (path, argv); /* no return if successful */
     free (path);
 }
 
-void exec_subcommand (const char *searchpath, char *argv[])
+void exec_subcommand (bool vopt, char *argv[])
 {
+    char *searchpath = getenv ("FLUX_EXEC_PATH"); // assert != NULL
     char *cpy = xstrdup (searchpath);
     char *dir, *saveptr = NULL, *a1 = cpy;
 
     while ((dir = strtok_r (a1, ":", &saveptr))) {
-        exec_subcommand_dir (dir, argv, "flux-");
-        exec_subcommand_dir (dir, argv, NULL); /* deprecated */
+        exec_subcommand_dir (vopt, dir, argv, "flux-");
+        exec_subcommand_dir (vopt, dir, argv, NULL); /* deprecated */
         a1 = NULL;
     }
     free (cpy);
