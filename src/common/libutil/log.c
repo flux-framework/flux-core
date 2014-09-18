8ffa55c5f4be0090d52225317ff8bcf2c70e9002
Jim Garlick <garlick.jim@gmail.com>
2014-09-18 09:13:15 -0700

    libutil: put a flux header on log.c

--- src/common/libutil/log.c
+++ src/common/libutil/log.c
@@ -1,27 +1,26 @@
-/*****************************************************************************
- *  Copyright (C) 2010 Lawrence Livermore National Security, LLC.
- *  Written by Jim Garlick <garlick@llnl.gov> LLNL-CODE-423279
- *  All Rights Reserved.
+/*****************************************************************************\
+ *  Copyright (c) 2014 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
  *
- *  This file is part of the Distributed I/O Daemon (diod).
- *  For details, see <http://code.google.com/p/diod/>.
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
  *
  *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License (as published by the
- *  Free Software Foundation) version 2, dated June 1991.
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the license, or (at your option)
+ *  any later version.
  *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software Foundation,
- *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA or see
- *  <http://www.gnu.org/licenses/>.
- *****************************************************************************/
-
-/* log.c - logging/error functions */
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+\*****************************************************************************/
 
 #if HAVE_CONFIG_H
 #include "config.h"
