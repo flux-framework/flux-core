19fbb4f381a00bce565889e753f4e703aeb3e957
Mark A. Grondona <mgrondona@llnl.gov>
2014-10-20 07:49:42 -0700

    lua: Add luaL_setfuncs if needed to lua utils code
    For 5.1/5.2 compatibility, add a luaL_setfuncs implementation that
    will work on 5.0/5.1.

--- src/bindings/lua/lutil.c
+++ src/bindings/lua/lutil.c
@@ -32,6 +32,8 @@
 #include <errno.h>
 #include <string.h>
 
+#include "lutil.h"
+
 int lua_pusherror (lua_State *L, char *fmt, ...)
 {
     int rc;
@@ -87,6 +89,24 @@ int l_format_args (lua_State *L, int index)
     lua_settop (L, index);
     return (1);
 }
+
+#if NEED_LUAL_SETFUNCS
+/*
+** Adapted from Lua 5.2.0
+*/
+void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
+    luaL_checkstack(L, nup+1, "too many upvalues");
+    for (; l->name != NULL; l++) {  /* fill the table with given functions */
+        int i;
+        lua_pushstring(L, l->name);
+        for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+            lua_pushvalue(L, -(nup+1));
+        lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+        lua_settable(L, -(nup + 3));
+    }
+    lua_pop(L, nup);  /* remove upvalues */
+}
+#endif
 
 
 /*
--- src/bindings/lua/lutil.h
+++ src/bindings/lua/lutil.h
@@ -1,5 +1,16 @@
 #ifndef HAVE_LUTIL_H
 #define HAVE_LUTIL_H
+
+#include <lua.h>
+#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM == 501
+#define NEED_LUAL_SETFUNCS 1
+/*  Lua 5.0/5.1 compatibility with Lua 5.2
+ *    http://lua-users.org/wiki/CompatibilityWithLuaFive
+ */
+# include <lauxlib.h>
+void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup);
+#endif /* LUA_VERSION_NUM <= 501 */
+
 int lua_pusherror (lua_State *L, char *fmt, ...);
 int l_pushresult (lua_State *L, int rc);
 int l_loadlibrary (lua_State *L, const char *name);
