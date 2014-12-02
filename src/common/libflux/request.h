e5670c1dfbd759331dc2d58ccd1567659b0e0669
Jim Garlick <garlick.jim@gmail.com>
2014-12-02 10:09:58 -0800

    libflux: clarify desc of flux_response_putmsg

--- src/common/libflux/request.h
+++ src/common/libflux/request.h
@@ -37,10 +37,11 @@ int flux_response_sendmsg (flux_t h, zmsg_t **zmsg);
  */
 zmsg_t *flux_response_recvmsg (flux_t h, bool nonblock);
 
-/* Give a response message back to the handle.  Responses put back in this
- * way are processed in FIFO order (via flux_response_recvmsg(), reactor
- * callbacks, or rpc calls)  before other incoming messages.  On success,
- * ownership of 'zmsg' is transferred to the handle.
+/* Put a response message in the handle's inbound message queue for processing
+ * in FIFO order, before other unprocessed messages.  The handle will become
+ * ready and the response will be returned by a call to flux_response_recvmsg()
+ * or similar.
+ * On success, ownership of 'zmsg' is transferred to the handle.
  * Returns 0 on success, or -1 on failure with errno set.
  */
 int flux_response_putmsg (flux_t h, zmsg_t **zmsg);
