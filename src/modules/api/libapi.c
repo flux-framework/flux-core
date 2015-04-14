aafca55f23fd2594fbafe8b42b2bd66e0c7fe7c0
Jim Garlick <garlick.jim@gmail.com>
2015-04-14 09:20:22 -0700

    api: rename symbols from cmb_ to api_
    This is purely a cosmetic change.

--- src/modules/api/libapi.c
+++ src/modules/api/libapi.c
@@ -49,7 +49,7 @@
 #include "src/common/libutil/ev_zmq.h"
 #include "src/common/libutil/ev_zlist.h"
 
-#define CMB_CTX_MAGIC   0xf434aaab
+#define API_CTX_MAGIC   0xf434aaab
 typedef struct {
     int magic;
     int fd;
@@ -70,7 +70,7 @@ typedef struct {
 
     zhash_t *watchers;
     int timer_seq;
-} cmb_t;
+} ctx_t;
 
 #define HASHKEY_LEN 80
 
@@ -79,34 +79,34 @@ typedef struct {
     FluxTmoutHandler cb;
     void *arg;
     int id;
-} cmb_timer_t;
+} atimer_t;
 
 typedef struct {
     ev_zmq w;
     FluxZsHandler cb;
     void *arg;
-} cmb_zs_t;
+} azs_t;
 
 typedef struct {
     ev_io w;
     FluxFdHandler cb;
     void *arg;
-} cmb_fd_t;
+} afd_t;
 
-static void cmb_reactor_stop (void *impl, int rc);
-static void sync_msg_watchers  (cmb_t *c);
+static void api_reactor_stop (void *impl, int rc);
+static void sync_msg_watchers  (ctx_t *c);
 
 
-static const struct flux_handle_ops cmb_ops;
+static const struct flux_handle_ops api_ops;
 
-static int cmb_sendmsg (void *impl, zmsg_t **zmsg)
+static int api_sendmsg (void *impl, zmsg_t **zmsg)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     return zfd_send (c->fd, zmsg);
 }
 
-static zmsg_t *cmb_recvmsg_putmsg (cmb_t *c)
+static zmsg_t *api_recvmsg_putmsg (ctx_t *c)
 {
     zmsg_t *zmsg = zlist_pop (c->putmsg);
     if (zmsg) {
@@ -116,21 +116,21 @@ static zmsg_t *cmb_recvmsg_putmsg (cmb_t *c)
     return zmsg;
 }
 
-static zmsg_t *cmb_recvmsg (void *impl, bool nonblock)
+static zmsg_t *api_recvmsg (void *impl, bool nonblock)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     zmsg_t *zmsg = NULL;
 
-    if (!(zmsg = cmb_recvmsg_putmsg (c)))
+    if (!(zmsg = api_recvmsg_putmsg (c)))
         zmsg = zfd_recv (c->fd, nonblock);
     return zmsg;
 }
 
-static int cmb_putmsg (void *impl, zmsg_t **zmsg)
+static int api_putmsg (void *impl, zmsg_t **zmsg)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     int oldcount = zlist_size (c->putmsg);
 
     if (zlist_append (c->putmsg, *zmsg) < 0)
@@ -141,10 +141,10 @@ static int cmb_putmsg (void *impl, zmsg_t **zmsg)
     return 0;
 }
 
-static int cmb_pushmsg (void *impl, zmsg_t **zmsg)
+static int api_pushmsg (void *impl, zmsg_t **zmsg)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     int oldcount = zlist_size (c->putmsg);
 
     if (zlist_push (c->putmsg, *zmsg) < 0)
@@ -155,10 +155,10 @@ static int cmb_pushmsg (void *impl, zmsg_t **zmsg)
     return 0;
 }
 
-static void cmb_purge (void *impl, flux_match_t match)
+static void api_purge (void *impl, flux_match_t match)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     zmsg_t *zmsg = zlist_first (c->putmsg);
 
     while (zmsg) {
@@ -170,26 +170,26 @@ static void cmb_purge (void *impl, flux_match_t match)
     }
 }
 
-static int cmb_event_subscribe (void *impl, const char *s)
+static int api_event_subscribe (void *impl, const char *s)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     flux_t h = c->h;
     return flux_request_send (h, NULL, "api.event.subscribe.%s", s ? s : "");
 }
 
-static int cmb_event_unsubscribe (void *impl, const char *s)
+static int api_event_unsubscribe (void *impl, const char *s)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     flux_t h = c->h;
     return flux_request_send (h, NULL, "api.event.unsubscribe.%s", s ? s : "");
 }
 
-static int cmb_rank (void *impl)
+static int api_rank (void *impl)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     if (c->rank == -1) {
         if (flux_info (c->h, &c->rank, NULL, NULL) < 0)
             return -1;
@@ -197,20 +197,20 @@ static int cmb_rank (void *impl)
     return c->rank;
 }
 
-static int cmb_reactor_start (void *impl)
+static int api_reactor_start (void *impl)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
 
     c->loop_rc = 0;
     ev_run (c->loop, 0);
     return c->loop_rc;
 }
 
-static void cmb_reactor_stop (void *impl, int rc)
+static void api_reactor_stop (void *impl, int rc)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
 
     c->loop_rc = rc;
     ev_break (c->loop, EVBREAK_ALL);
@@ -218,36 +218,36 @@ static void cmb_reactor_stop (void *impl, int rc)
 
 static void putmsg_cb (struct ev_loop *loop, ev_zlist *w, int revents)
 {
-    cmb_t *c = (cmb_t *)((char *)w - offsetof (cmb_t, putmsg_w));
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = (ctx_t *)((char *)w - offsetof (ctx_t, putmsg_w));
+    assert (c->magic == API_CTX_MAGIC);
 
     assert (zlist_size (c->putmsg) > 0);
     if (c->msg_cb) {
         if (c->msg_cb (c->h, c->msg_cb_arg) < 0)
-            cmb_reactor_stop (c, -1);
+            api_reactor_stop (c, -1);
     }
 }
 
 static void unix_cb (struct ev_loop *loop, ev_io *w, int revents)
 {
-    cmb_t *c = (cmb_t *)((char *)w - offsetof (cmb_t, unix_w));
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = (ctx_t *)((char *)w - offsetof (ctx_t, unix_w));
+    assert (c->magic == API_CTX_MAGIC);
 
     if (revents & EV_ERROR) {
-        cmb_reactor_stop (c, -1);
+        api_reactor_stop (c, -1);
     } else if (revents & EV_READ) {
         assert (zlist_size (c->putmsg) == 0);
         if (c->msg_cb) {
             if (c->msg_cb (c->h, c->msg_cb_arg) < 0)
-                cmb_reactor_stop (c, -1);
+                api_reactor_stop (c, -1);
         }
     }
 }
 
-static int cmb_reactor_msg_add (void *impl, flux_msg_f cb, void *arg)
+static int api_reactor_msg_add (void *impl, flux_msg_f cb, void *arg)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     int rc = -1;
 
     if (c->msg_cb != NULL) {
@@ -262,10 +262,10 @@ done:
     return rc;
 }
 
-static void cmb_reactor_msg_remove (void *impl)
+static void api_reactor_msg_remove (void *impl)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     c->msg_cb = NULL;
     c->msg_cb_arg = NULL;
     sync_msg_watchers (c);
@@ -273,22 +273,22 @@ static void cmb_reactor_msg_remove (void *impl)
 
 static void fd_cb (struct ev_loop *loop, ev_io *w, int revents)
 {
-    cmb_fd_t *f = (cmb_fd_t *)((char *)w - offsetof (cmb_fd_t, w));
-    cmb_t *c = w->data;
-    assert (c->magic == CMB_CTX_MAGIC);
+    afd_t *f = (afd_t *)((char *)w - offsetof (afd_t, w));
+    ctx_t *c = w->data;
+    assert (c->magic == API_CTX_MAGIC);
 
     if (f->cb (c->h, w->fd, etoz (revents), f->arg) < 0)
-        cmb_reactor_stop (c, -1);
+        api_reactor_stop (c, -1);
 }
 
-static int cmb_reactor_fd_add (void *impl, int fd, int events,
+static int api_reactor_fd_add (void *impl, int fd, int events,
                                FluxFdHandler cb, void *arg)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     char hashkey[HASHKEY_LEN];
 
-    cmb_fd_t *f = xzmalloc (sizeof (*f));
+    afd_t *f = xzmalloc (sizeof (*f));
     ev_io_init (&f->w, fd_cb, fd, ztoe (events));
     f->w.data = c;
     f->cb = cb;
@@ -303,14 +303,14 @@ static int cmb_reactor_fd_add (void *impl, int fd, int events,
     return 0;
 }
 
-static void cmb_reactor_fd_remove (void *impl, int fd, int events)
+static void api_reactor_fd_remove (void *impl, int fd, int events)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     char hashkey[HASHKEY_LEN];
 
     snprintf (hashkey, sizeof (hashkey), "fd:%d:%d", fd, events);
-    cmb_fd_t *f = zhash_lookup (c->watchers, hashkey);
+    afd_t *f = zhash_lookup (c->watchers, hashkey);
     if (f) {
         ev_io_stop (c->loop, &f->w);
         zhash_delete (c->watchers, hashkey);
@@ -319,23 +319,23 @@ static void cmb_reactor_fd_remove (void *impl, int fd, int events)
 
 static void zs_cb (struct ev_loop *loop, ev_zmq *w, int revents)
 {
-    cmb_zs_t *z = (cmb_zs_t *)((char *)w - offsetof (cmb_zs_t, w));
-    cmb_t *c = w->data;
-    assert (c->magic == CMB_CTX_MAGIC);
+    azs_t *z = (azs_t *)((char *)w - offsetof (azs_t, w));
+    ctx_t *c = w->data;
+    assert (c->magic == API_CTX_MAGIC);
 
     int rc = z->cb (c->h, w->zsock, etoz (revents), z->arg);
     if (rc < 0)
-        cmb_reactor_stop (c, -1);
+        api_reactor_stop (c, -1);
 }
 
-static int cmb_reactor_zs_add (void *impl, void *zs, int events,
+static int api_reactor_zs_add (void *impl, void *zs, int events,
                                FluxZsHandler cb, void *arg)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     char hashkey[HASHKEY_LEN];
 
-    cmb_zs_t *z = xzmalloc (sizeof (*z));
+    azs_t *z = xzmalloc (sizeof (*z));
     ev_zmq_init (&z->w, zs_cb, zs, ztoe (events));
     z->w.data = c;
     z->cb = cb;
@@ -349,14 +349,14 @@ static int cmb_reactor_zs_add (void *impl, void *zs, int events,
     return 0;
 }
 
-static void cmb_reactor_zs_remove (void *impl, void *zs, int events)
+static void api_reactor_zs_remove (void *impl, void *zs, int events)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     char hashkey[HASHKEY_LEN];
 
     snprintf (hashkey, sizeof (hashkey), "zsock:%p:%d", zs, events);
-    cmb_zs_t *z = zhash_lookup (c->watchers, hashkey);
+    azs_t *z = zhash_lookup (c->watchers, hashkey);
     if (z) {
         ev_zmq_stop (c->loop, &z->w);
         zhash_delete (c->watchers, hashkey);
@@ -365,24 +365,24 @@ static void cmb_reactor_zs_remove (void *impl, void *zs, int events)
 
 static void timer_cb (struct ev_loop *loop, ev_timer *w, int revents)
 {
-    cmb_timer_t *t = (cmb_timer_t *)((char *)w - offsetof (cmb_timer_t, w));
-    cmb_t *c = w->data;
-    assert (c->magic == CMB_CTX_MAGIC);
+    atimer_t *t = (atimer_t *)((char *)w - offsetof (atimer_t, w));
+    ctx_t *c = w->data;
+    assert (c->magic == API_CTX_MAGIC);
 
     if (t->cb (c->h, t->arg) < 0)
-         cmb_reactor_stop (c, -1);
+         api_reactor_stop (c, -1);
 }
 
-static int cmb_reactor_tmout_add (void *impl, unsigned long msec, bool oneshot,
+static int api_reactor_tmout_add (void *impl, unsigned long msec, bool oneshot,
                                   FluxTmoutHandler cb, void *arg)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     double after = (double)msec / 1000.0;
     double repeat = oneshot ? 0 : after;
     char hashkey[HASHKEY_LEN];
 
-    cmb_timer_t *t = xzmalloc (sizeof (*t));
+    atimer_t *t = xzmalloc (sizeof (*t));
     t->id = c->timer_seq++;
     t->w.data = c;
     ev_timer_init (&t->w, timer_cb, after, repeat);
@@ -398,24 +398,24 @@ static int cmb_reactor_tmout_add (void *impl, unsigned long msec, bool oneshot,
     return t->id;
 }
 
-static void cmb_reactor_tmout_remove (void *impl, int timer_id)
+static void api_reactor_tmout_remove (void *impl, int timer_id)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
     char hashkey[HASHKEY_LEN];
 
     snprintf (hashkey, sizeof (hashkey), "timer:%d", timer_id);
-    cmb_timer_t *t = zhash_lookup (c->watchers, hashkey);
+    atimer_t *t = zhash_lookup (c->watchers, hashkey);
     if (t) {
         ev_timer_stop (c->loop, &t->w);
         zhash_delete (c->watchers, hashkey);
     }
 }
 
-static void cmb_fini (void *impl)
+static void api_fini (void *impl)
 {
-    cmb_t *c = impl;
-    assert (c->magic == CMB_CTX_MAGIC);
+    ctx_t *c = impl;
+    assert (c->magic == API_CTX_MAGIC);
 
     if (c->fd >= 0)
         (void)close (c->fd);
@@ -428,7 +428,7 @@ static void cmb_fini (void *impl)
         zlist_destroy (&c->putmsg);
     }
     zhash_destroy (&c->watchers);
-    c->magic = ~CMB_CTX_MAGIC;
+    c->magic = ~API_CTX_MAGIC;
     free (c);
 }
 
@@ -452,7 +452,7 @@ done:
 /* Enable/disable the appropriate watchers to give putmsg priority
  * over the main unix socket.
  */
-static void sync_msg_watchers (cmb_t *c)
+static void sync_msg_watchers (ctx_t *c)
 {
     if (c->msg_cb == NULL) {
         ev_io_stop (c->loop, &c->unix_w);
@@ -468,12 +468,12 @@ static void sync_msg_watchers (cmb_t *c)
 
 flux_t flux_api_openpath (const char *path, int flags)
 {
-    cmb_t *c = NULL;
+    ctx_t *c = NULL;
     struct sockaddr_un addr;
     char pidfile[PATH_MAX + 1];
 
     c = xzmalloc (sizeof (*c));
-    c->magic = CMB_CTX_MAGIC;
+    c->magic = API_CTX_MAGIC;
     c->rank = -1;
 
     if (!(c->loop = ev_loop_new (EVFLAG_AUTO)))
@@ -504,11 +504,11 @@ flux_t flux_api_openpath (const char *path, int flags)
             break;
         usleep (100*1000);
     }
-    c->h = flux_handle_create (c, &cmb_ops, flags);
+    c->h = flux_handle_create (c, &api_ops, flags);
     return c->h;
 error:
     if (c)
-        cmb_fini (c);
+        api_fini (c);
     return NULL;
 }
 
@@ -539,26 +539,26 @@ void flux_api_close (flux_t h)
     flux_handle_destroy (&h);
 }
 
-static const struct flux_handle_ops cmb_ops = {
-    .sendmsg = cmb_sendmsg,
-    .recvmsg = cmb_recvmsg,
-    .putmsg = cmb_putmsg,
-    .pushmsg = cmb_pushmsg,
-    .purge = cmb_purge,
-    .event_subscribe = cmb_event_subscribe,
-    .event_unsubscribe = cmb_event_unsubscribe,
-    .rank = cmb_rank,
-    .reactor_stop = cmb_reactor_stop,
-    .reactor_start = cmb_reactor_start,
-    .reactor_fd_add = cmb_reactor_fd_add,
-    .reactor_fd_remove = cmb_reactor_fd_remove,
-    .reactor_zs_add = cmb_reactor_zs_add,
-    .reactor_zs_remove = cmb_reactor_zs_remove,
-    .reactor_tmout_add = cmb_reactor_tmout_add,
-    .reactor_tmout_remove = cmb_reactor_tmout_remove,
-    .reactor_msg_add = cmb_reactor_msg_add,
-    .reactor_msg_remove = cmb_reactor_msg_remove,
-    .impl_destroy = cmb_fini,
+static const struct flux_handle_ops api_ops = {
+    .sendmsg = api_sendmsg,
+    .recvmsg = api_recvmsg,
+    .putmsg = api_putmsg,
+    .pushmsg = api_pushmsg,
+    .purge = api_purge,
+    .event_subscribe = api_event_subscribe,
+    .event_unsubscribe = api_event_unsubscribe,
+    .rank = api_rank,
+    .reactor_stop = api_reactor_stop,
+    .reactor_start = api_reactor_start,
+    .reactor_fd_add = api_reactor_fd_add,
+    .reactor_fd_remove = api_reactor_fd_remove,
+    .reactor_zs_add = api_reactor_zs_add,
+    .reactor_zs_remove = api_reactor_zs_remove,
+    .reactor_tmout_add = api_reactor_tmout_add,
+    .reactor_tmout_remove = api_reactor_tmout_remove,
+    .reactor_msg_add = api_reactor_msg_add,
+    .reactor_msg_remove = api_reactor_msg_remove,
+    .impl_destroy = api_fini,
 };
 
 /*
