396b5dc6a609a8f3e3a8665318ad4be747789495
Mark A. Grondona <mgrondona@llnl.gov>
2015-06-13 08:29:05 -0700

    subprocess: test: add notes to subprocess testing
    To help diagnose issues in subprocess unit tests, add some notes
    between each set of tests using libtap's note() function.

--- src/modules/libsubprocess/test/subprocess.c
+++ src/modules/libsubprocess/test/subprocess.c
@@ -59,6 +59,7 @@ int main (int ac, char **av)
         BAIL_OUT ("Failed to create subprocess manager");
     ok (sm != NULL, "create subprocess manager");
 
+    note ("subprocess accessors tests");
     if (!(p = subprocess_create (sm)))
         BAIL_OUT ("Failed to create subprocess handle");
     ok (p != NULL, "create subprocess handle");
@@ -113,6 +114,7 @@ int main (int ac, char **av)
     subprocess_destroy (p);
 
     /* Test running an executable */
+    note ("test subprocess_manager_run");
     p = subprocess_manager_run (sm, 1, args3, NULL);
     ok (p != NULL, "subprocess_manager_run");
     ok (subprocess_pid (p) != (pid_t) -1, "process has valid pid");
@@ -127,6 +129,7 @@ int main (int ac, char **av)
     q = NULL;
 
     /*  Test failing program */
+    note ("test expected failure from subprocess_manager_run");
     args3[0] = "/bin/false";
     p = subprocess_manager_run (sm, 1, args3, NULL);
     if (p) {
@@ -142,6 +145,7 @@ int main (int ac, char **av)
         q = NULL;
     }
 
+    note ("Test signaled program");
 
     /* Test signaled program */
     p = subprocess_manager_run (sm, 2, args4, NULL);
@@ -161,6 +165,7 @@ int main (int ac, char **av)
 
     q = NULL;
 
+    note ("Test fork/exec interface");
     /* Test separate fork/exec interface */
     p = subprocess_create (sm);
     ok (p != NULL, "subprocess_create works");
@@ -188,6 +193,7 @@ int main (int ac, char **av)
     subprocess_destroy (p);
     q = NULL;
 
+    note ("Test exec failure");
     /* Test exec failure */
     p = subprocess_create (sm);
     ok (p != NULL, "subprocess create");
@@ -202,6 +208,7 @@ int main (int ac, char **av)
     is (subprocess_exit_string (p), "Exec Failure", "Exit state is Exec Failed");
     subprocess_destroy (p);
 
+    note ("Test set working directory");
     /* Test set working directory */
     p = subprocess_create (sm);
     ok (p != NULL, "subprocess create");
@@ -220,6 +227,7 @@ int main (int ac, char **av)
     ok (subprocess_exit_code (p) == 0, "subprocess successfully run in /tmp");
     subprocess_destroy (p);
 
+    note ("Test subprocess_reap interface");
     /* Test subprocess_reap */
     p = subprocess_create (sm);
     q = subprocess_create (sm);
@@ -242,6 +250,7 @@ int main (int ac, char **av)
     subprocess_destroy (p);
     subprocess_destroy (q);
 
+    note ("Test subprocess I/O");
     /* Test subprocess output */
     p = subprocess_create (sm);
     ok (p != NULL, "subprocess_create");
@@ -271,6 +280,7 @@ int main (int ac, char **av)
 
 
     /* Test subprocess input */
+    note ("test subprocess stdin");
     p = subprocess_create (sm);
     ok (p != NULL, "subprocess_create");
     ok (subprocess_argv_append (p, "/bin/cat") >= 0,  "subprocess_argv_append");
@@ -297,7 +307,6 @@ int main (int ac, char **av)
         free (buf);
     }
     subprocess_destroy (p);
-
     subprocess_manager_destroy (sm);
 
     done_testing ();
