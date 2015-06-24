b691cbf3f2241a05db598c2148cbb367532bade8
Mark A. Grondona <mgrondona@llnl.gov>
2015-06-24 16:23:08 -0700

    libutil: test: add very simple test for data encode/decode
    Add simple sanity check for util_json_object_add/get_data.

---
+++ src/common/libutil/test/encode.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include "src/common/libtap/tap.h"
+#include "src/common/libutil/jsonutil.h"
+
+static void *myfatal_h = NULL;
+
+void myfatal (void *h, int exit_code, const char *fmt, ...)
+{
+    myfatal_h = h;
+}
+
+static int test_encode ()
+{
+    int rlen;
+    char *r;
+    char p[] = "abcdefghijklmnop";
+    json_object *o = json_object_new_object ();
+
+    util_json_object_add_data (o, "data", (uint8_t *) p, strlen (p)+1);
+    note ("%s", json_object_to_json_string (o));
+    util_json_object_get_data (o, "data", (uint8_t **) &r, &rlen);
+
+    ok (strcmp (p, r) == 0, "'%s'='%s'", p, r);
+    ok (strlen(p)+1 == rlen, "lengths match");
+    free (r);
+    json_object_put (o);
+
+    return (0);
+}
+
+int main (int argc, char *argv[])
+{
+
+    plan (NO_PLAN);
+    test_encode ();
+    done_testing ();
+    return (0);
+}
+
+/*
+ * vi: ts=4 sw=4 expandtab
+ */
