f907eb6f3cff35966bd9c2041c312d312b6755b3
Jim Garlick <garlick.jim@gmail.com>
2015-07-10 14:20:18 -0700

    test: add api disconnect test
    Install a kvs watch then disconnect and see what happens to the
    kvs reporting of the number of watchers.  Do this on a single node
    using flux-kvs and flux comms-stats, and then use a specialized test
    to do it across a session.  If successful this shows that disconnect
    messages are generated for the same service on multiple nodes, and for
    FLUX_NODEID_ANY.
    
    Caveat: doesn't test FLUX_NODEID_UPSTREAM yet.

---
+++ src/test/tdisconnect.c
@@ -0,0 +1,87 @@
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <json.h>
+#include <flux/core.h>
+
+#include "src/common/libutil/shortjson.h"
+#include "src/common/libutil/log.h"
+#include "src/common/libutil/xzmalloc.h"
+
+/* Current kvs_watch() API doesn't accept a rank, so we cheat and
+ * build the minimal kvs.watch request message from scratch here.
+ * Read the first response and pretend we're done.  If this key actually
+ * changes then we are screwed here because we will have recycled the
+ * matchtags but more responses will be coming.
+ */
+void send_watch_requests (flux_t h, const char *key)
+{
+    JSON in = Jnew ();
+    flux_rpc_t r;
+    const char *json_str;
+
+    json_object_object_add (in, "noexist", NULL);
+    Jadd_bool (in, ".flag_first", true);
+    if (!(r = flux_rpc_multi (h, "kvs.watch", Jtostr (in), "all", 0)))
+        err_exit ("flux_rpc_multi kvs.watch");
+    while (!flux_rpc_completed (r)) {
+        if (flux_rpc_get (r, NULL, &json_str) < 0)
+            err_exit ("kvs.watch");
+    }
+    flux_rpc_destroy (r);
+    Jput (in);
+}
+
+/* Sum #watchers over all ranks.
+ */
+int count_watchers (flux_t h)
+{
+    JSON out;
+    const char *json_str;
+    int n, count = 0;
+    flux_rpc_t r;
+
+    if (!(r = flux_rpc_multi (h, "kvs.stats.get", NULL, "all", 0)))
+        err_exit ("flux_rpc_multi kvs.stats.get");
+    while (!flux_rpc_completed (r)) {
+        if (flux_rpc_get (r, NULL, &json_str) < 0)
+            err_exit ("kvs.stats.get");
+        if (!(out = Jfromstr (json_str)) || !Jget_int (out, "#watchers", &n))
+            msg_exit ("error decoding stats payload");
+        count += n;
+        Jput (out);
+    }
+    flux_rpc_destroy (r);
+    return count;
+}
+
+int main (int argc, char **argv)
+{
+    flux_t h;
+    int w0, w1, w2;
+
+    /* Install watchers on every rank, then disconnect.
+     * The number of watchers should return to the original count.
+     */
+    if (!(h = flux_open (NULL, 0)))
+        err_exit ("flux_open");
+    w0 = count_watchers (h);
+    send_watch_requests (h, "nonexist");
+    w1 = count_watchers (h) - w0;
+    msg ("test watchers: %d", w1);
+    flux_close (h);
+    msg ("disconnected");
+
+    if (!(h = flux_open (NULL, 0)))
+        err_exit ("flux_open");
+    w2 = count_watchers (h) - w0;
+    msg ("test watchers: %d", w2);
+    if (w2 != 0)
+        err_exit ("Test failure, watchers were not removed on disconnect");
+
+    return (0);
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ t/t1006-apidisconnect.t
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+
+test_description='Test api disconnect generation 
+'
+
+. `dirname $0`/sharness.sh
+SIZE=4
+test_under_flux ${SIZE}
+
+test_expect_success 'kvs watcher gets disconnected on client exit' '
+	before_watchers=`flux comms-stats --parse "#watchers" kvs` &&
+	echo "before: $before_watchers" &&
+	test_expect_code 142 run_timeout 1 flux kvs watch noexist &&
+	after_watchers=`flux comms-stats --parse "#watchers" kvs`
+	echo "after: $after_watchers" &&
+	test $before_watchers -eq $after_watchers
+'
+
+test_expect_success 'multi-node kvs watcher gets disconnected on client exit' '
+	${FLUX_BUILD_DIR}/src/test/tdisconnect
+'
+
+
+test_done
