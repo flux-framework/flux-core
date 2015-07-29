9548d85a654940b8acdfd2e83eb34f59acc713c5
Jim Garlick <garlick.jim@gmail.com>
2015-07-29 12:56:55 -0700

    doc/man3: add flux_reduce_create(3) and example
    Create a somewhat detailed manual page for flux_reduce_create().

---
+++ doc/man3/treduce.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <flux/core.h>
+#include "src/common/libutil/shortjson.h"
+#include "src/common/libutil/nodeset.h"
+#include "src/common/libutil/xzmalloc.h"
+
+struct context {
+    int batchnum;
+    flux_reduce_t *r;
+    char rankstr[16];
+    flux_t h;
+};
+
+int itemweight (void *item)
+{
+    int count = 1;
+    nodeset_t nodeset;
+
+    if ((nodeset = nodeset_new_str (item))) {
+        count = nodeset_count (nodeset);
+        nodeset_destroy (nodeset);
+    }
+    return count;
+}
+
+void sink (flux_reduce_t *r, int batchnum, void *arg)
+{
+    char *item;
+
+    while ((item = flux_reduce_pop (r))) {
+        fprintf (stderr, "%d: %s\n", batchnum, item);
+        free (item);
+    }
+}
+
+void forward (flux_reduce_t *r, int batchnum, void *arg)
+{
+    struct context *ctx = arg;
+    char *item;
+    flux_rpc_t *rpc;
+
+    while ((item = flux_reduce_pop (r))) {
+        JSON out = Jnew ();
+        Jadd_int (out, "batchnum", batchnum);
+        Jadd_str (out, "nodeset", item);
+        rpc = flux_rpc (ctx->h, "treduce.forward", Jtostr (out),
+                        FLUX_NODEID_UPSTREAM, FLUX_RPC_NORESPONSE);
+        flux_rpc_destroy (rpc);
+        Jput (out);
+        free (item);
+    }
+}
+
+void reduce (flux_reduce_t *r, int batchnum, void *arg)
+{
+    nodeset_t nodeset = NULL;
+    char *item;
+
+    if ((item = flux_reduce_pop (r))) {
+        nodeset = nodeset_new_str (item);
+        free (item);
+    }
+    if (nodeset) {
+        while ((item = flux_reduce_pop (r))) {
+            nodeset_add_str (nodeset, item);
+            free (item);
+        }
+        item = xstrdup (nodeset_str (nodeset));
+        if (flux_reduce_push (r, item) < 0)
+            free (item);
+        nodeset_destroy (nodeset);
+    }
+}
+
+void forward_cb (flux_t h, flux_msg_watcher_t *w,
+                 const flux_msg_t *msg, void *arg)
+{
+    struct context *ctx = arg;
+    const char *json_str, *nodeset_str;
+    JSON in = NULL;
+    int batchnum;
+    char *item;
+
+    if (flux_request_decode (msg, NULL, &json_str) < 0
+            || !(in = Jfromstr (json_str))
+            || !Jget_int (in, "batchnum", &batchnum)
+            || !Jget_str (in, "nodeset", &nodeset_str))
+        return;
+    item = xstrdup (nodeset_str);
+    if (flux_reduce_append (ctx->r, item, batchnum) < 0)
+        free (item);
+    Jput (in);
+}
+
+void heartbeat_cb (flux_t h, flux_msg_watcher_t *w,
+                   const flux_msg_t *msg, void *arg)
+{
+    struct context *ctx = arg;
+    char *item = xstrdup (ctx->rankstr);
+    if (flux_reduce_append (ctx->r, item, ctx->batchnum++) < 0)
+        free (item);
+}
+
+struct flux_msghandler htab[] = {
+    { FLUX_MSGTYPE_EVENT,     "hb",              heartbeat_cb },
+    { FLUX_MSGTYPE_REQUEST,   "treduce.forward", forward_cb },
+    FLUX_MSGHANDLER_TABLE_END,
+};
+
+struct flux_reduce_ops reduce_ops = {
+    .destroy = free,
+    .itemweight = itemweight,
+    .reduce = reduce,
+    .forward = forward,
+    .sink = sink,
+};
+
+int mod_main (flux_t h, int argc, char **argv)
+{
+    struct context ctx;
+    int rank = flux_rank (h);
+    double timeout = 0.;
+    int flags;
+
+    if (argc == 1) {
+        timeout = strtod (argv[0], NULL);
+        flags = FLUX_REDUCE_TIMEDFLUSH;
+    } else
+        flags = FLUX_REDUCE_HWMFLUSH;
+
+    ctx.batchnum = 0;
+    snprintf (ctx.rankstr, sizeof (ctx.rankstr), "%d", rank);
+    ctx.h = h;
+
+    if (!(ctx.r = flux_reduce_create (h, reduce_ops, timeout, &ctx, flags)))
+        return -1;
+    if (flux_event_subscribe (h, "hb") < 0)
+        return -1;
+    if (flux_msg_watcher_addvec (h, htab, &ctx) < 0)
+        return -1;
+    if (flux_reactor_start (h) < 0)
+        return -1;
+    flux_msg_watcher_delvec (h, htab);
+    return 0;
+}
+
+MOD_NAME ("treduce");
