20d567bc71b7b6699eca1a5830e0ffd790b4cda0
Jim Garlick <garlick.jim@gmail.com>
2015-08-13 15:29:49 -0700

    broker/overlay: avoid copies, update api usage
    Change the prototype of overlay_sendmsg_relay() to accept a
    (const flux_msg_t *) rather than a (zmsg_t *).  Instead of duplicating
    the message and calling zmsg_send(), call flux_msg_sendzsock() directly.
    This affects event messages forwarded to an "event relay" rank when
    there are multiple flux-broker's co-located on a node.
    
    In overlay_sendmsg_event(), event messages were being duplicated
    unconditionally and sent with zmsg_send().  Make duplication conditional
    on whether or not the message has to be encrypted.  Send with
    flux_msg_sendzsock().
    
    Change the prototype of overlay_sendmsg_event() to take a
    (const flux_msg_t *)
    
    Change prototype of overlay_mcast_child() to use a
    (const flux_msg_t *) since it duplicates the message before
    altering it.  Internally use the flux_msg_* functions rather
    than czmq ones.
    
    Have overlay_recvmsg_event() return a (flux_msg_t *) instead
    of a (zmsg_t *).  Use flux_msg_recvzsock().  Check the resturn
    value for error.
    
    Change overlay_sendmsg_right() to take a (const flux_msg_t *)
    instead of a (zmsg_t **).  Send with flux_msg_sendzsock().
    
    Use flux_msg_has_route() to check for cycles in the ring network
    rather than directly peeking in the route stack.
    
    Change overlay_sendmsg_child() to take a (const flux_msg_t *)
    instead of a (zmsg_t **).  Send with flux_msg_sendzsock().
    
    Change prototype of overlay_sendmsg_parent() to take a
    (const flux_msg_t *) instead of a (zmsg_t **).
    
    Rename overlay_set_reactor() to overlay_set_flux().

--- src/broker/overlay.h
+++ src/broker/overlay.h
@@ -12,7 +12,7 @@ void overlay_destroy (overlay_t *ov);
 void overlay_set_sec (overlay_t *ov, flux_sec_t sec);
 void overlay_set_zctx (overlay_t *ov, zctx_t *zctx);
 void overlay_set_rank (overlay_t *ov, uint32_t rank);
-void overlay_set_reactor (overlay_t *ov, flux_t h);
+void overlay_set_flux (overlay_t *ov, flux_t h);
 void overlay_set_heartbeat (overlay_t *ov, heartbeat_t *h);
 
 /* All ranks but rank 0 connect to a parent to form the main TBON.
@@ -29,8 +29,8 @@ void overlay_push_parent (overlay_t *ov, const char *fmt, ...);
 const char *overlay_get_parent (overlay_t *ov);
 void overlay_set_right (overlay_t *ov, const char *fmt, ...);
 void overlay_set_parent_cb (overlay_t *ov, overlay_cb_f cb, void *arg);
-int overlay_sendmsg_parent (overlay_t *ov, zmsg_t **zmsg);
-int overlay_sendmsg_right (overlay_t *ov, zmsg_t **zmsg);
+int overlay_sendmsg_parent (overlay_t *ov, const flux_msg_t *msg);
+int overlay_sendmsg_right (overlay_t *ov, const flux_msg_t *msg);
 int overlay_keepalive_parent (overlay_t *ov);
 
 /* The child is where other ranks connect to send requests.
@@ -39,14 +39,14 @@ int overlay_keepalive_parent (overlay_t *ov);
 void overlay_set_child (overlay_t *ov, const char *fmt, ...);
 const char *overlay_get_child (overlay_t *ov);
 void overlay_set_child_cb (overlay_t *ov, overlay_cb_f cb, void *arg);
-int overlay_sendmsg_child (overlay_t *ov, zmsg_t **zmsg);
+int overlay_sendmsg_child (overlay_t *ov, const flux_msg_t *msg);
 /* We can "multicast" events to all child peers using mcast_child().
  * It walks the 'children' hash, finding overlay peers that have not
- * yet been "muted", and routes them a copy of zmsg.  The broker Cc's
+ * yet been "muted", and routes them a copy of msg.  The broker Cc's
  * events over the TBON using this until peers indicate that they are
  * receiving duplicate seq numbers through the normal event socket.
  */
-int overlay_mcast_child (overlay_t *ov, zmsg_t *zmsg);
+int overlay_mcast_child (overlay_t *ov, const flux_msg_t *msg);
 void overlay_mute_child (overlay_t *ov, const char *uuid);
 
 /* Call when message is received from child 'uuid'.
@@ -63,8 +63,8 @@ json_object *overlay_lspeer_encode (overlay_t *ov);
 void overlay_set_event (overlay_t *ov, const char *fmt, ...);
 const char *overlay_get_event (overlay_t *ov);
 void overlay_set_event_cb (overlay_t *ov, overlay_cb_f cb, void *arg);
-int overlay_sendmsg_event (overlay_t *ov, zmsg_t *zmsg);
-zmsg_t *overlay_recvmsg_event (overlay_t *ov);
+int overlay_sendmsg_event (overlay_t *ov, const flux_msg_t *msg);
+flux_msg_t *overlay_recvmsg_event (overlay_t *ov);
 
 /* Since an epgm:// endpoint only allows one subscriber per node,
  * when there are multiple ranks per node, arrangements must be made
@@ -74,7 +74,7 @@ zmsg_t *overlay_recvmsg_event (overlay_t *ov);
  */
 void overlay_set_relay (overlay_t *ov, const char *fmt, ...);
 const char *overlay_get_relay (overlay_t *ov);
-int overlay_sendmsg_relay (overlay_t *ov, zmsg_t *zmsg);
+int overlay_sendmsg_relay (overlay_t *ov, const flux_msg_t *msg);
 
 /* Establish connections.
  * These functions are idempotent as the bind may need to be called
