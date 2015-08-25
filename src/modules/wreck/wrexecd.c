689761d7d1e6eb1b40ff48d7aed550bb8f5ca144
Mark Grondona <mark.grondona@gmail.com>
2015-08-25 17:22:12 +0000

    wreck: update names of some environment variables
    Update the names of some environment variables exported by
    wrexecd to make their purpose more clear. The changes are:
    
      FLUX_LWJ_TASK_ID ->       FLUX_TASK_RANK
      FLUX_LWJ_LOCAL_TASK_ID -> FLUX_TASK_LOCAL_ID
      FLUX_LWJ_ID ->            FLUX_JOB_ID
      FLUX_LWJ_NNODES ->        FLUX_JOB_NNODES
      FLUX_LWJ_NTASKS ->        FLUX_JOB_SIZE
      FLUX_LWJ_GTIDS ->         FLUX_LOCAL_RANKS

--- src/modules/wreck/wrexecd.c
+++ src/modules/wreck/wrexecd.c
@@ -1045,8 +1045,8 @@ int exec_command (struct prog_ctx *ctx, int i)
         prog_ctx_setenv  (ctx, "FLUX_TMPDIR", getenv ("FLUX_TMPDIR"));
         prog_ctx_setenvf (ctx, "MPIRUN_RANK",     1, "%d", t->globalid);
         prog_ctx_setenvf (ctx, "PMI_RANK", 1, "%d", t->globalid);
-        prog_ctx_setenvf (ctx, "FLUX_LWJ_TASK_ID", 1, "%d", t->globalid);
-        prog_ctx_setenvf (ctx, "FLUX_LWJ_LOCAL_TASK_ID", 1, "%d", i);
+        prog_ctx_setenvf (ctx, "FLUX_TASK_RANK", 1, "%d", t->globalid);
+        prog_ctx_setenvf (ctx, "FLUX_TASK_LOCAL_ID", 1, "%d", i);
 
         if (prog_ctx_getopt (ctx, "stop-children-in-exec")) {
             /* Stop process on exec with parent attached */
@@ -1397,14 +1397,14 @@ int exec_commands (struct prog_ctx *ctx)
 
     lua_stack_call (ctx->lua_stack, "rexecd_init");
 
-    prog_ctx_setenvf (ctx, "FLUX_LWJ_ID",    1, "%d", ctx->id);
-    prog_ctx_setenvf (ctx, "FLUX_LWJ_NNODES",1, "%d", ctx->nnodes);
+    prog_ctx_setenvf (ctx, "FLUX_JOB_ID",    1, "%d", ctx->id);
+    prog_ctx_setenvf (ctx, "FLUX_JOB_NNODES",1, "%d", ctx->nnodes);
     prog_ctx_setenvf (ctx, "FLUX_NODE_ID",   1, "%d", ctx->nodeid);
-    prog_ctx_setenvf (ctx, "FLUX_LWJ_NTASKS",1, "%d", ctx->total_ntasks);
+    prog_ctx_setenvf (ctx, "FLUX_JOB_SIZE",  1, "%d", ctx->total_ntasks);
     prog_ctx_setenvf (ctx, "MPIRUN_NPROCS", 1, "%d", ctx->total_ntasks);
     prog_ctx_setenvf (ctx, "PMI_SIZE", 1, "%d", ctx->total_ntasks);
     gtid_list_create (ctx, buf, sizeof (buf));
-    prog_ctx_setenvf (ctx, "FLUX_LWJ_GTIDS",  1, "%s", buf);
+    prog_ctx_setenvf (ctx, "FLUX_LOCAL_RANKS",  1, "%s", buf);
 
     for (i = 0; i < ctx->nprocs; i++)
         exec_command (ctx, i);
