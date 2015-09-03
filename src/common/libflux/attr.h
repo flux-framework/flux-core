ce5942c416a2612013616517a79371b174ffe472
Jim Garlick <garlick.jim@gmail.com>
2015-09-03 15:28:30 -0700

    libflux/attr: add new flux_attr class
    Add a user-facing interface for get/set/list broker attributes.
    
    Unlike flux_getattr(), these functions do not accept a nodeid argument;
    these functions always communicate with the local broker since
    the nodeid argument was not used much before and coupling these functions
    to one broker allows attributes to be cached in the handle
    
    Attributes can have flags, set by the broker, that influence what users
    can do with attributes.
     READONLY - attribute is not writeable by users
     IMMUTABLE - attribute is not writeable, and won't change on server
    
    If an attribute is immutable, its value be cached forever.
    
    Setting a nonexistent attribute creates it.  Flags cannot be set from
    this interface (only from the broker's).
    
    Setting an existing attribute to a NULL value deletes it.
    
    flux_attr_fake() be used to install phony cached attributes in
    the handle for testing.  It can replace the current method of faking
    rank and size using the handle aux cache.

---
+++ src/common/libflux/attr.h
@@ -0,0 +1,32 @@
+#ifndef _FLUX_CORE_ATTR_H
+#define _FLUX_CORE_ATTR_H
+
+#include <stdbool.h>
+
+#include "handle.h"
+
+/* Flags can only be set by the broker.
+ */
+enum {
+    FLUX_ATTRFLAG_IMMUTABLE = 1,    /* attribute is cacheable */
+    FLUX_ATTRFLAG_READONLY = 2,     /* attribute cannot be written */
+                                    /*   but may change on broker */
+    FLUX_ATTRFLAG_ACTIVE = 4,       /* attribute has get and/or set callbacks */
+};
+
+const char *flux_attr_get (flux_t h, const char *name, int *flags);
+
+int flux_attr_set (flux_t h, const char *name, const char *val);
+
+int flux_attr_fake (flux_t h, const char *name, const char *val, int flags);
+
+const char *flux_attr_first (flux_t h);
+
+const char *flux_attr_next (flux_t h);
+
+
+#endif /* !_FLUX_CORE_ATTR_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
