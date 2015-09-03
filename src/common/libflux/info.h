a1d742ac422b04c3e8ceeee913644870641f01df
Jim Garlick <garlick.jim@gmail.com>
2015-09-03 11:41:02 -0700

    doc: don't list attributes in flux/info.h
    Remove the outdated list of attributes from flux/info.h.

--- src/common/libflux/info.h
+++ src/common/libflux/info.h
@@ -11,10 +11,6 @@ int flux_size (flux_t h);
 
 /* flux_getattr is used to read misc. attributes internal to the broker.
  * The caller must dispose of the returned string with free ().
- * The following attributes are valid:
- *    snoop-uri   The name of the socket to be used by flux-snoop.
- *    parent-uri  The name of parent socket.
- *    request-uri The name of request socket.
  */
 char *flux_getattr (flux_t h, int rank, const char *name);
 
