d61ce2e7d461e039cdaca611b271d55cc7cc49c4
Mark Grondona <mark.grondona@gmail.com>
2015-09-21 23:51:59 +0000

    testsuite: add scripts/cpus-allowed.lua
    Add a convenience script for operating against the current cpumask
    for testing purposes. Can be used to generate cpu masks that intersect
    with the current, e.g.
    
     scripts/cpus-allowed.lua first
    
    returns the first CPU in the current mask, "last" returns the last, etc.

---
+++ t/scripts/cpus-allowed.lua
@@ -0,0 +1,28 @@
+#!/usr/bin/lua
+--
+--  Thin frontend to Lua affinity bindings, run cpu_set_t methods against
+--   the current cpumask and print the result.
+--
+local affinity = require 'flux.affinity'
+local cpuset = affinity.cpuset
+
+local function die (msg, ...)
+    io.stderr:write (string.format (msg, ...))
+    os.exit (1)
+end
+
+if arg[1] == "--help" or arg[1] == "-h"  then
+    print ([[Usage: cpus-allowed [COMMAND] [ARG]...]])
+end
+
+local currentmask = assert (affinity.getaffinity ())
+if #arg > 0 then
+    local method = currentmask[arg[1]] 
+    if type (method) ~= "function" then
+         die ("cpus-allowed: %s: not a method\n", arg[1])
+    end
+    local rc = method (currentmask, select (2, unpack (arg)))
+    if rc ~= nil then currentmask = rc end
+end
+print (currentmask)
+os.exit (0)
