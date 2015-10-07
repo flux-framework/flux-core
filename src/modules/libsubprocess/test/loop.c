0f6dce631598d73569b04ee8a0a04c2818c95735
Jim Garlick <garlick.jim@gmail.com>
2015-10-07 14:04:01 -0700

    test/subprocess: port loop test to flux reactor

--- src/modules/libsubprocess/test/loop.c
+++ src/modules/libsubprocess/test/loop.c
@@ -28,8 +28,9 @@
 #include <sys/signalfd.h>
 #include <json.h>
 #include <czmq.h>
+#include <flux/core.h>
 
-#include <src/modules/libzio/zio.h>
+#include "src/modules/libzio/zio.h"
 
 #include "tap.h"
 #include "subprocess.h"
@@ -69,23 +70,27 @@ static int init_signalfd ()
     return signalfd(-1, &mask, SFD_NONBLOCK | SFD_CLOEXEC);
 }
 
-static int signal_cb (zloop_t *zl, zmq_pollitem_t *item, void *arg)
+static void signal_cb (flux_reactor_t *r, flux_watcher_t *w,
+                       int revents, void *arg)
 {
+    int fd = flux_fd_watcher_get_fd (w);
     struct signalfd_siginfo fdsi;
     struct subprocess_manager *sm = arg;
 
-    if (read (item->fd, &fdsi, sizeof (fdsi)) < 0)
-        return (-1);
-
+    if (read (fd, &fdsi, sizeof (fdsi)) < 0) {
+        flux_reactor_stop (r);
+        return;
+    }
     diag ("signal_cb signo = %d", fdsi.ssi_signo);
-    if (fdsi.ssi_signo == SIGTERM)
-        return (-1);
+    if (fdsi.ssi_signo == SIGTERM) {
+        flux_reactor_stop (r);
+        return;
+    }
 
     ok (fdsi.ssi_signo == SIGCHLD, "got sigchld in signal_cb");
     ok (subprocess_manager_reap_all (sm) >= 0, "reap all children");
 
     //zloop_poller_end (zl, item);
-    return (0);
 }
 
 int main (int ac, char **av)
@@ -93,8 +98,9 @@ int main (int ac, char **av)
     int rc;
     struct subprocess_manager *sm;
     struct subprocess *p;
-    zloop_t *zloop;
-    zmq_pollitem_t zp = { .events = ZMQ_POLLIN, .revents = 0, .socket = NULL };
+    flux_reactor_t *r;
+    int sigfd;
+    flux_watcher_t *w;
 
     zsys_handler_set (NULL);
 
@@ -104,21 +110,22 @@ int main (int ac, char **av)
         BAIL_OUT ("Failed to create subprocess manager");
     ok (sm != NULL, "create subprocess manager");
 
-    if (!(zloop = zloop_new ()))
-        BAIL_OUT ("Failed to create a zloop");
+    if (!(r = flux_reactor_create()))
+        BAIL_OUT ("Failed to create a reactor");
 
-    zp.fd = init_signalfd ();
-    ok (zp.fd >= 0, "signalfd created");
+    sigfd = init_signalfd ();
+    ok (sigfd >= 0, "signalfd created");
 
-    ok (zloop_poller (zloop, &zp, (zloop_fn *) signal_cb, sm) >= 0,
-        "Created zloop poller for signalfd");
+    w = flux_fd_watcher_create (r, sigfd, FLUX_POLLIN, signal_cb, sm);
+    ok (w != NULL, "Created watcher for signalfd");
+    flux_watcher_start (w);
 
-    rc = subprocess_manager_set (sm, SM_ZLOOP, zloop);
-    ok (rc == 0, "set subprocess manager zloop (rc=%d, %s)", rc, strerror (errno));
+    rc = subprocess_manager_set (sm, SM_REACTOR, r);
+    ok (rc == 0, "set subprocess manager reactor (rc=%d, %s)", rc, strerror (errno));
 
     if (!(p = subprocess_create (sm)))
         BAIL_OUT ("Failed to create a subprocess object");
-    ok (subprocess_set_callback (p, exit_handler, zloop) >= 0,
+    ok (subprocess_set_callback (p, exit_handler, r) >= 0,
         "set subprocess exit handler");
     ok (subprocess_set_io_callback (p, io_cb) >= 0,
         "set subprocess io callback");
@@ -131,10 +138,11 @@ int main (int ac, char **av)
     ok (subprocess_fork (p) >= 0, "subprocess_fork");
     ok (subprocess_exec (p) >= 0, "subprocess_exec");
 
-    rc = zloop_start (zloop);
+    ok (flux_reactor_run (r, 0) == 0,
+        "reactor returned normally");
 
     subprocess_manager_destroy (sm);
-    zloop_destroy (&zloop);
+    flux_reactor_destroy (r);
 
     done_testing ();
 }
