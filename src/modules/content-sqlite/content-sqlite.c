9186cc9748fd2aada353ec6bc2dd89e60e866603
Jim Garlick <garlick.jim@gmail.com>
2016-01-14 10:34:00 -0800

    modules/content-sqlite: use blob-size-limit attr
    Use the value of the content-blob-size-limit broker attribute
    to establish the maximum blob size, rather than hardwiring
    the RFC 10 value.

--- src/modules/content-sqlite/content-sqlite.c
+++ src/modules/content-sqlite/content-sqlite.c
@@ -38,8 +38,6 @@
 #include "src/common/libutil/xzmalloc.h"
 #include "src/common/libutil/log.h"
 
-const uint32_t blob_size_limit = 1048576; /* RFC 10 */
-
 const bool debug_enabled = true;
 
 const char *sql_create_table = "CREATE TABLE objects("
@@ -60,6 +58,7 @@ typedef struct {
     flux_t h;
     bool broker_shutdown;
     const char *hashfun;
+    uint32_t blob_size_limit;
 } ctx_t;
 
 static void log_sqlite_error (ctx_t *ctx, const char *fmt, ...)
@@ -101,6 +100,7 @@ static ctx_t *getctx (flux_t h)
     ctx_t *ctx = (ctx_t *)flux_aux_get (h, "flux::content-sqlite");
     const char *scratchdir;
     const char *hashfun;
+    const char *tmp;
     int flags;
 
     if (!ctx) {
@@ -115,6 +115,11 @@ static ctx_t *getctx (flux_t h)
             flux_log_error (h, "content-hash: %s", hashfun);
             goto error;
         }
+        if (!(tmp = flux_attr_get (h, "content-blob-size-limit", NULL))) {
+            flux_log_error (h, "content-blob-size-limit");
+            goto error;
+        }
+        ctx->blob_size_limit = strtoul (tmp, NULL, 10);
         if (!(scratchdir = flux_attr_get (h, "scratch-directory", NULL))) {
             flux_log_error (h, "scratch-directory");
             goto error;
@@ -241,7 +246,7 @@ void store_cb (flux_t h, flux_msg_handler_t *w,
         flux_log_error (h, "store: request decode failed");
         goto done;
     }
-    if (size > blob_size_limit) {
+    if (size > ctx->blob_size_limit) {
         errno = EFBIG;
         goto done;
     }
