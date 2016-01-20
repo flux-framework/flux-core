54cd7aba6b3b3414fc67d4a5c9cafeb89b538b27
Mark A. Grondona <mgrondona@llnl.gov>
2016-01-20 09:53:58 -0800

    cmd/flux: use optparse for flux(1) subcommands
    Convert flux(1) to use optparse_t for main option processing and the
    handling of internal or "builtin" subcommands.
    
    All internal flux(1) subcommands are now registered as optparse
    subcommands. In order to ease the addition of new builtins, builtin
    subcommands are grouped separately in src/cmd/builtin/*.c and compiled
    into flux.c.
    
    Each builtin module now exports a single "registration" function
    named `subcommand_<name>_register`, where <name> is replaced with
    the basename of the C source file. The registration function is
    responsible for registering one or more new subcommands with flux.c
    optparse_t object which is passed to the function.
    
    A new script `builtin-cmds-list.sh` automatically generates a table of
    these functions in builtin-cmds.c for use during flux.c initialization.

---
+++ src/cmd/builtin/attr.c
@@ -0,0 +1,143 @@
+/*****************************************************************************\
+ *  Copyright (c) 2016 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
+ *
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the license, or (at your option)
+ *  any later version.
+ *
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+\*****************************************************************************/
+#include "builtin.h"
+
+static flux_t get_flux_handle (optparse_t *p)
+{
+    flux_t h = NULL;
+
+    if ((h = optparse_get_data (p, "flux_t")))
+        flux_incref (h);
+    else if ((h = flux_open (NULL, 0)) == NULL)
+        err_exit ("flux_open");
+    return h;
+}
+
+static struct optparse_option setattr_opts[] = {
+    { .name = "expunge", .key = 'e', .has_arg = 0,
+      .usage = "Unset the specified attribute",
+    },
+    OPTPARSE_TABLE_END
+};
+
+static int cmd_setattr (optparse_t *p, int ac, char *av[])
+{
+    int n;
+    const char *name = NULL, *val = NULL;
+    flux_t h;
+
+    n = optparse_optind (p);
+    if (optparse_hasopt (p, "expunge") && n == ac - 1) {
+        name = av[n];
+    } else if (!optparse_hasopt (p, "expunge") && n == ac - 2) {
+        name = av[n];
+        val = av[n + 1];
+    } else {
+        optparse_print_usage (p);
+        exit (1);
+    }
+
+    h = get_flux_handle (p);
+    if (flux_attr_set (h, name, val) < 0)
+        err_exit ("%s", av[1]);
+    flux_close (h);
+    return (0);
+}
+
+static struct optparse_option lsattr_opts[] = {
+    { .name = "values", .key = 'v', .has_arg = 0,
+      .usage = "List values with attributes",
+    },
+    OPTPARSE_TABLE_END
+};
+
+static int cmd_lsattr (optparse_t *p, int ac, char *av[])
+{
+    const char *name, *val;
+    flux_t h;
+    if (optparse_optind (p) != ac)
+        optparse_fatal_usage (p, 1, NULL);
+    h = get_flux_handle (p);
+    name = flux_attr_first (h);
+    while (name) {
+        if (optparse_hasopt (p, "values")) {
+            val = flux_attr_get (h, name, NULL);
+            printf ("%-40s%s\n", name, val ? val : "-");
+        } else {
+            printf ("%s\n", name);
+        }
+        name = flux_attr_next (h);
+    }
+    flux_close (h);
+    return (0);
+}
+
+static int cmd_getattr (optparse_t *p, int ac, char *av[])
+{
+    flux_t h = NULL;
+    const char *val;
+    int n, flags;
+
+    n = optparse_optind (p);
+    if (n != ac - 1)
+        optparse_fatal_usage (p, 1, NULL);
+
+    h = get_flux_handle (p);
+    if (!(val = flux_attr_get (h, av[n], &flags)))
+        err_exit ("%s", av[n]);
+    printf ("%s\n", val);
+    flux_close (h);
+    return (0);
+}
+
+int subcommand_attr_register (optparse_t *p)
+{
+    optparse_err_t e;
+    e = optparse_reg_subcommand (p, "setattr", cmd_setattr,
+        "name value",
+        "Get broker attribute",
+        setattr_opts);
+    if (e != OPTPARSE_SUCCESS)
+        return (-1);
+
+    e = optparse_reg_subcommand (p, "getattr", cmd_getattr,
+        "name",
+        "Get broker attribute",
+        NULL);
+    if (e != OPTPARSE_SUCCESS)
+        return (-1);
+
+    e = optparse_reg_subcommand (p, "lsattr", cmd_lsattr,
+        "[-v]",
+        "List broker attributes",
+        lsattr_opts);
+    if (e != OPTPARSE_SUCCESS)
+        return (-1);
+
+    return (0);
+}
+
+/*
+ * vi: ts=4 sw=4 expandtab
+ */
