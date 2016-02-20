894a9f6e3acce34d9638a72a68035eddd664adc1
Mark Grondona <mark.grondona@gmail.com>
2016-02-20 20:15:34 +0000

    testsuite: split env setup from flux-sharness.sh
    Split environment setup specific to flux-core from flux-sharness.sh,
    so that flux-sharness.sh holds only the flux test library functions.
    
    Add environment setup to new file in sharness.d/01-setup.sh.

--- t/sharness.d/flux-sharness.sh
+++ t/sharness.d/flux-sharness.sh
@@ -4,35 +4,6 @@
 #
 
 #
-#  Unset variables important to Flux
-#
-unset FLUX_CONFIG
-unset FLUX_MODULE_PATH
-unset FLUX_CMBD_PATH
-
-#
-#  FLUX_BUILD_DIR and FLUX_SOURCE_DIR are set to build and source paths
-#  (based on current directory)
-#
-if test -z "$FLUX_BUILD_DIR"; then
-    if test -z "${builddir}"; then
-        FLUX_BUILD_DIR="$(cd .. && pwd)"
-    else
-        FLUX_BUILD_DIR="$(cd ${builddir}/.. && pwd))"
-    fi
-    export FLUX_BUILD_DIR
-fi
-if test -z "$FLUX_SOURCE_DIR"; then
-    if test -z "${srcdir}"; then
-        FLUX_SOURCE_DIR="$(cd .. && pwd)"
-    else
-        FLUX_SOURCE_DIR="$(cd ${srcdir}/.. && pwd)"
-    fi
-    export FLUX_SOURCE_DIR
-fi
-
-
-#
 #  Extra functions for Flux testsuite
 #
 run_timeout() {
@@ -104,28 +75,6 @@ test_on_rank() {
     ranks=$1; shift;
     flux exec --rank=${ranks} "$@"
 }
-
-
-#
-#  Export some extra variables to test scripts specific to Flux
-#   testsuite
-#
-#  Add path to flux(1) command to PATH
-#
-if test -n "$FLUX_TEST_INSTALLED_PATH"; then
-    PATH=$FLUX_TEST_INSTALLED_PATH:$PATH
-    fluxbin=$FLUX_TEST_INSTALLED_PATH/flux
-else # normal case, use ${top_builddir}/src/cmd/flux
-    PATH=$FLUX_BUILD_DIR/src/cmd:$PATH
-    fluxbin=$FLUX_BUILD_DIR/src/cmd/flux
-fi
-export PATH
-
-if ! test -x ${fluxbin}; then
-    echo >&2 "Failed to find a flux binary in ${fluxbin}."
-    echo >&2 "Do you need to run make?"
-    return 1
-fi
 
 #  Export a shorter name for this test
 TEST_NAME=$SHARNESS_TEST_NAME
