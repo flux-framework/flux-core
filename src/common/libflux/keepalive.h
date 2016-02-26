da5565a35e810011a78d865937f8ab89eb58111a
Jim Garlick <garlick.jim@gmail.com>
2016-02-26 11:18:50 -0800

    libflux/keepalive: add keepalive class

---
+++ src/common/libflux/keepalive.h
@@ -0,0 +1,12 @@
+#ifndef _FLUX_CORE_KEEPALIVE_H
+#define _FLUX_CORE_KEEPALIVE_H 1
+
+flux_msg_t *flux_keepalive_encode (int errnum, int status);
+
+int flux_keepalive_decode (const flux_msg_t *msg, int *errnum, int *status);
+
+#endif /* !_FLUX_CORE_KEEPALIVE_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
