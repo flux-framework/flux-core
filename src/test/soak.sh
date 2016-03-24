b7b0ddead7629c516277e59fa84e4a294178e2fa
Jim Garlick <garlick.jim@gmail.com>
2016-03-24 10:34:16 -0700

    test/soak: add command line options
    Improve soak.sh's command line option handling, and add a mode
    which allows data to be replotted.  Adjust default axes.

--- src/test/soak.sh
+++ src/test/soak.sh
@@ -1,47 +1,105 @@
 #!/bin/bash -e
 
-# Usage: soak.sh [NJOBS] [NNODES]
+declare top_srcdir=$(pwd)/../..
 
-LOGFILE=${TMPDIR-/tmp}/soak-$$.log
-DATFILE=${TMPDIR-/tmp}/soak-$$.dat
-PNGFILE=${TMPDIR-/tmp}/soak-$$.png
-NJOBS=${1-100}
-NNODES=${2-8}
+declare NJOBS=100
+declare NNODES=8
+declare GITDESC="flux-core $(git describe))"
 
-if test ${NJOBS} -lt 1000; then
-    YMAX=1
-    Y2MAX=1
-elif test ${NJOBS} -lt 10000; then
-    YMAX=2
-    Y2MAX=8
-elif test ${NJOBS} -lt 20000; then
-    YMAX=4
-    Y2MAX=16
+declare prefix=${TMPDIR-/tmp}/soak-$$
+
+declare LOGFILE=${prefix}.log
+declare DATFILE=${prefix}.dat
+declare PNGFILE=${prefix}.png
+
+declare existing_data=""
+declare sec_max=""
+declare gb_max=""
+
+usage() {
+    echo "Usage: soak.sh [OPTIONS]" >&2
+    echo "  -j NJOBS      set the number of jobs to run (default ${NJOBS})" >&2
+    echo "  -n NNODES     run on NNODES (default ${NNODES})" >&2
+    echo "  -s SEC_MAX    set range of Y axes (default calculated)" >&2
+    echo "  -g GB_MAX     set range of Y2 axes (default calculated)" >&2
+    echo "  -d FILE       replot data from FILE - skips running the test" >&2
+    echo "If replotting, NJOBS is read from FILE and -n is ignored" >&2
+    exit 1
+}
+
+die() {
+    echo $* >&2
+    exit 1
+}
+
+while getopts "?hj:n:d:g:s:" opt; do
+    case ${opt} in
+        h|\?) usage ;;
+        j) NJOBS=${OPTARG} ;;
+        n) NNODES=${OPTARG} ;;
+        d) existing_data=${OPTARG} ;;
+        g) gb_max=${OPTARG} ;;
+        s) sec_max=${OPTARG} ;;
+        *) die "bad option: ${opt}" ;;
+    esac
+done
+shift $((${OPTIND} - 1))
+[ $# = 0 ] || usage
+[ $NJOBS -gt 0 ] || usage
+[ $NNODES -gt 0 ] || usage
+if [ -n "$existing_data" ]; then
+    [ -r $existing_data ] || usage
+    DATFILE=$existing_data
+    PNGFILE=$(mktemp).png
+    NJOBS=$(tail -1 ${DATFILE} | cut -f1 -d' ')
 fi
 
-top_srcdir=$(pwd)/../..
+# Try for consistent Y axes tics across runs
+if [ -z "$sec_max" ]; then
+    if test ${NJOBS} -le 2000; then
+        sec_max=1
+    elif test ${NJOBS} -le 10000; then
+        sec_max=2
+    else
+        sec_max=4
+    fi
+fi
 
-echo Running ${NJOBS} jobs, logging to ${LOGFILE}
-${top_srcdir}/src/cmd/flux start -s${NNODES} -o,-S,log-filename=${LOGFILE} ./soak-workload.sh ${NJOBS}
+# Try for consistent Y2 axes tics across runs
+if [ -z "$gb_max" ]; then
+    if test ${NJOBS} -le 2000; then
+        gb_max=2
+    elif test ${NJOBS} -le 10000; then
+        gb_max=4
+    else
+        gb_max=8
+    fi
+fi
 
-echo Extracting ${DATFILE} from ${LOGFILE}
-grep soak ${LOGFILE} | sed -e 's/[^:]*: //' >${DATFILE}
+if [ -z "$existing_data" ]; then
+    echo Running ${NJOBS} jobs, logging to ${LOGFILE}
+    ${top_srcdir}/src/cmd/flux start -s${NNODES} -o,-S,log-filename=${LOGFILE} \
+        ./soak-workload.sh ${NJOBS}
+
+    echo Extracting ${DATFILE} from ${LOGFILE}
+    grep soak ${LOGFILE} | sed -e 's/[^:]*: //' >${DATFILE}
+fi
 
 echo Plotting to ${PNGFILE}
 gnuplot <<EOT
 set terminal png font arial 12
 set output "${PNGFILE}"
 
-set title "soak test - ${NNODES}-node jobs back to back (flux-core $(git describe))"
+set title "soak test - ${NNODES}-node jobs back to back (${GITDESC})"
 
 set key left top
 set xlabel "jobid"
 set xrange [0:${NJOBS}]
 set ylabel "seconds"
-set yrange [0:${YMAX}]
+set yrange [0:${sec_max}]
 set y2label "gigabytes"
 set y2tics
-set y2range [0:${Y2MAX}]
+set y2range [0:${gb_max}]
 plot \
   "${DATFILE}" using 1:2 with linespoints title 'job runtime',\
   "${DATFILE}" using 1:(\$3/1000000) with linespoints title 'broker rss' axes x1y2,\
@@ -50,3 +108,5 @@ EOT
 
 echo Displaying ${PNGFILE}
 eog ${PNGFILE}
+
+# vi:tabstop=4 shiftwidth=4 expandtab
