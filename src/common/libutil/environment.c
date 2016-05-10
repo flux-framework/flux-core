662c7a691b19c9155de4ebb0c7436da3b3bf30e3
Jim Garlick <garlick.jim@gmail.com>
2016-05-10 14:42:10 -0700

    libutil/environment: extract class from flux_conf
    The environment class can stand alone and does not need
    to be tied to the beleaguered flux_conf_t class.  It also
    doesn't need to be exposed in the Flux API.

---
+++ src/common/libutil/environment.c
@@ -0,0 +1,265 @@
+/*****************************************************************************\
+ *  Copyright (c) 2014 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
+ *
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the license, or (at your option)
+ *  any later version.
+ *
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+\*****************************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <sys/types.h>
+#include <pwd.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <czmq.h>
+#include <argz.h>
+
+#include "src/common/libutil/log.h"
+#include "src/common/libutil/xzmalloc.h"
+#include "src/common/libutil/iterators.h"
+
+
+#include "environment.h"
+
+struct environment {
+    zhash_t *environment;
+};
+
+void environment_destroy (struct environment *e)
+{
+    if (e) {
+        zhash_destroy (&e->environment);
+        free (e);
+    }
+}
+
+struct environment *environment_create (void)
+{
+    struct environment *e = xzmalloc (sizeof(*e));
+    if (!(e->environment = zhash_new ()))
+        oom ();
+    return e;
+}
+
+struct env_item {
+    char *argz;
+    size_t argz_len;
+    char sep;
+    _Bool clean;
+    _Bool unset;
+    char *str_cache;
+};
+
+struct env_item * new_env_item(){
+    return calloc (1, sizeof(struct env_item));
+}
+void free_env_item(struct env_item *i)
+{
+    free (i->argz);
+    free (i->str_cache);
+    free (i);
+}
+
+char *find_env_item(struct env_item *i, const char *s)
+{
+    char *entry = 0;
+    while ((entry = argz_next (i->argz, i->argz_len, entry))) {
+        if (!strcmp(entry, s))
+            return entry;
+    }
+    return NULL;
+}
+
+static const char *stringify_env_item (struct env_item *item)
+{
+    if (!item)
+        return NULL;
+    if (!item->argz)
+        return "";
+    if (item->clean)
+        return item->str_cache;
+
+    free(item->str_cache);
+    item->str_cache = malloc(item->argz_len);
+    memcpy(item->str_cache, item->argz, item->argz_len);
+    argz_stringify(item->str_cache, item->argz_len, item->sep);
+
+    return item->str_cache;
+}
+
+const char *environment_get (struct environment *e, const char *key)
+{
+    struct env_item *item = zhash_lookup (e->environment, key);
+    return stringify_env_item(item);
+}
+
+static void environment_set_inner (struct environment *e,
+                                   const char *key,
+                                   const char *value,
+                                   char separator)
+{
+    struct env_item *item = new_env_item();
+    item->clean = false;
+    item->unset = (value == NULL);
+    item->sep = separator;
+    zhash_update (e->environment, key, (void *)item);
+    zhash_freefn (e->environment, key, (zhash_free_fn *)free_env_item);
+
+    environment_push_back(e, key, value);
+}
+
+void environment_set (struct environment *e,
+                      const char *key,
+                      const char *value,
+                      char separator)
+{
+    environment_set_inner (e, key, value, separator);
+}
+
+void environment_unset (struct environment *e, const char *key)
+{
+    environment_set_inner (e, key, NULL, 0);
+}
+
+static void environment_push_inner (struct environment *e,
+                                    const char *key,
+                                    const char *value,
+                                    bool before,
+                                    bool split)
+{
+    if (!value || strlen (value) == 0)
+        return;
+
+    struct env_item *item = zhash_lookup (e->environment, key);
+    if (!item) {
+        item = new_env_item();
+        zhash_update (e->environment, key, (void *)item);
+        zhash_freefn (e->environment, key, (zhash_free_fn *)free_env_item);
+    }
+
+    if (split) {
+        char *split_value = NULL;
+        size_t split_value_len = 0;
+        argz_create_sep (value, item->sep, &split_value, &split_value_len);
+        char *entry = 0;
+        while((entry = argz_next (split_value, split_value_len, entry))) {
+            if ((!strlen(entry)) || find_env_item(item, entry))
+                continue;
+            if (before) {
+                argz_insert (&item->argz, &item->argz_len, item->argz, entry);
+            } else {
+                argz_add(&item->argz, &item->argz_len, entry);
+            }
+        }
+        free(split_value);
+    } else {
+        if (before) {
+            argz_insert (&item->argz, &item->argz_len, item->argz, value);
+        } else {
+            argz_add (&item->argz, &item->argz_len, value);
+        }
+    }
+}
+
+void environment_push (struct environment *e,
+                       const char *key,
+                       const char *value)
+{
+    environment_push_inner (e, key, value, true, true);
+}
+
+void environment_push_back (struct environment *e,
+                            const char *key,
+                            const char *value)
+{
+    environment_push_inner (e, key, value, false, true);
+}
+
+void environment_no_dedup_push (struct environment *e,
+                                const char *key,
+                                const char *value)
+{
+    environment_push_inner (e, key, value, true, false);
+}
+
+void environment_no_dedup_push_back (struct environment *e,
+                                     const char *key,
+                                     const char *value)
+{
+    environment_push_inner (e, key, value, false, false);
+}
+
+void environment_from_env (struct environment *e,
+                           const char *key,
+                           const char *default_base,
+                           char separator)
+{
+    const char *env = getenv (key);
+    if (!env)
+        env = default_base;
+    environment_set (e, key, env, separator);
+}
+
+void environment_set_separator (struct environment *e,
+                                const char *key,
+                                char separator)
+{
+    struct env_item *item = zhash_lookup (e->environment, key);
+    if (item)
+        item->sep = separator;
+}
+
+const char *environment_first (struct environment *e)
+{
+    return stringify_env_item (zhash_first (e->environment));
+}
+
+const char *environment_next (struct environment *e)
+{
+    return stringify_env_item (zhash_next (e->environment));
+}
+
+const char *environment_cursor (struct environment *e)
+{
+    return zhash_cursor (e->environment);
+}
+
+void environment_apply (struct environment *e)
+{
+    const char *key;
+    struct env_item *item;
+    FOREACH_ZHASH (e->environment, key, item)
+    {
+        if (item->unset) {
+            if (unsetenv (key))
+                err_exit ("unsetenv: %s", key);
+        } else {
+            const char *value = stringify_env_item (item);
+            if (setenv (key, value, 1) < 0)
+                err_exit ("setenv: %s=%s", key, value);
+        }
+    }
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ src/common/libutil/environment.h
@@ -0,0 +1,176 @@
+#ifndef _UTIL_ENVIRONMENT_H
+#define _UTIL_ENVIRONMENT_H
+
+#include <stdbool.h>
+#include <stdarg.h>
+
+/* Create/destroy environment
+ */
+struct environment *environment_create (void);
+void environment_destroy (struct environment *e);
+
+/**
+ * @brief Set the iteration cursor for the environment to the first element
+ *
+ * @param e the environment to iterate over
+ *
+ * @return the key of the first element or null if the environment is empty
+ */
+const char *environment_first (struct environment *e);
+
+/**
+ * @brief Get the next key in the environment
+ *
+ * @param e the environment to continue iterating over
+ *
+ * @return the key of the next element or null if iteration is complete or the
+ * environment is empty
+ */
+const char *environment_next (struct environment *e);
+
+/**
+ * @brief Get the value of the current cursor element, matching the key from
+ * either *_first or *_next
+ *
+ * @param e the envirnoment to operate on
+ *
+ * @return The value of the current environment element
+ */
+const char *environment_cursor (struct environment *e);
+
+/**
+ * @brief Apply the changes encoded in this environment to the environment of
+ * the current process.
+ *
+ * All keys with values of non-zero length are set to those values, keys whose
+ * values are strings of length 0 are explicitly unset.  Other keys in the
+ * environment remain unchanged.
+ *
+ * @param e the environment that should be applied
+ */
+void environment_apply (struct environment *e);
+
+/**
+ * @brief Split, deduplicate, and push a new value onto the front of the
+ * environment variable whose key is "key."
+ *
+ * @param e the environment to add this element to
+ * @param key the environment variable name
+ * @param value The value to use, this will be split based on the separator
+ * defined for the key if one is set, otherwise it is prepended whole
+ */
+void environment_push (struct environment *e,
+                       const char *key,
+                       const char *value);
+
+/**
+ * @brief Split, deduplicate, and push a new value onto the back of the
+ * environment variable whose key is "key."
+ *
+ * @param e the environment to add this element to
+ * @param key the environment variable name
+ * @param value The value to use, this will be split based on the separator
+ * defined for the key if one is set, otherwise it is appended whole
+ */
+void environment_push_back (struct environment *e,
+                            const char *key,
+                            const char *value);
+
+/**
+ * @brief Add the specified value to the front of the target key without
+ * de-duplication, it will still be separated from the rest of the value by sep,
+ * if a sep has been set for this key.
+ *
+ * Generally, the de-duplicating functions are preferred to these, but if a
+ * value you must include contains separator characters, use these.
+ *
+ * @param e The environment to operate on
+ * @param key The key to target
+ * @param value The value to prepend
+ */
+void environment_no_dedup_push (struct environment *e,
+                                const char *key,
+                                const char *value);
+
+/**
+ * @brief Add the specified value to the target key without de-duplication, it
+ * will still be separated from the rest of the value by sep, if a sep has
+ * been set for this key.
+ *
+ * @param e The environment to operate on
+ * @param key The key to target
+ * @param value The value to append
+ */
+void environment_no_dedup_push_back (struct environment *e,
+                                     const char *key,
+                                     const char *value);
+
+/**
+ * @brief Set the environment value "key" to "value," where elements are
+ * separated by "sep," overwrites whatever value is currently set.
+ *
+ * @param e the environment to act on
+ * @param key the key to target
+ * @param value the value to set
+ * @param separator the separator used to divide elements in the value, ":"
+ * for unix paths for example, or ";" for LUA_PATH
+ */
+void environment_set (struct environment *e,
+                      const char *key,
+                      const char *value,
+                      char separator);
+
+/**
+ * @brief Explicitly unset the environment variable "key," this will cause the
+ * variable to be cleared when this environment spec is applied.
+ *
+ * @param e the environment to act on
+ * @param key the key which should be unset on application
+ */
+void environment_unset (struct environment *e, const char *key);
+
+/**
+ * @brief Initialize the environment variable "key" with the value from the
+ * enclosing environment if it is set, otherwise set it to "default_base."
+ *
+ * @param e the environment to act on
+ * @param key the key to initialize
+ * @param default_base the default value to use if the environment variable is
+ * unset
+ * @param separator the separator between tokens in the value
+ */
+void environment_from_env (struct environment *e,
+                           const char *key,
+                           const char *default_base,
+                           char separator);
+
+/**
+ * @brief Set the separator for a given environment variable.
+ *
+ * @param e the environment to act on
+ * @param key the environment variable to update
+ * @param separator the separator to use to join components of this object and
+ * split and dedup inputs
+ */
+void environment_set_separator (struct environment *e,
+                                const char *key,
+                                char separator);
+
+/**
+ * @brief Get the value of a given environment variable by name.
+ *
+ * This does *not* check the environment of the current process, but the
+ * configured environment specified by 'e'.
+ *
+ * @param e the environment to act on
+ * @param key key of the environment variable to read
+ *
+ * @return A reference to the value of the key, or null if that key is not set
+ */
+const char *environment_get (struct environment *e, const char *key);
+
+#endif /* !_UTIL_ENVIRONMENT */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
