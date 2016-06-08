3d1a7d7e287eb8905881ce0d5452c0f32ed56928
Jim Garlick <garlick.jim@gmail.com>
2016-06-08 14:29:30 -0700

    doc/flux_get_rank(3): drop flux_get_arity()

--- doc/man3/tinfo.c
+++ doc/man3/tinfo.c
@@ -3,16 +3,10 @@
 #include <inttypes.h>
 #include "src/common/libutil/log.h"
 
-int tree_height (uint32_t n, int k)
-{
-    return (int)floor (log (n) / log (k));
-}
-
 int main (int argc, char **argv)
 {
     flux_t h;
     uint32_t rank, n;
-    int k;
 
     if (!(h = flux_open (NULL, 0)))
         err_exit ("flux_open");
@@ -20,12 +14,6 @@ int main (int argc, char **argv)
         err_exit ("flux_get_rank");
     if (flux_get_size (h, &n) < 0)
         err_exit ("flux_get_size");
-    if (flux_get_arity (h, &k) < 0)
-        err_exit ("flux_get_arity");
-    printf ("height of %d-ary tree of size %" PRIu32 ": %d\n",
-            k, n, tree_height (n, k));
-    printf ("height of %d-ary at rank %" PRIu32 ": %d\n",
-            k, rank, tree_height (rank + 1, k));
     flux_close (h);
     return (0);
 }
