7712c4bfcb1b149c4ff7288bc5a646c18693d365
Jim Garlick <garlick.jim@gmail.com>
2016-06-12 19:00:11 +0000

    libutil/log: drop syslog support

--- src/common/libutil/log.h
+++ src/common/libutil/log.h
@@ -7,11 +7,8 @@
 
 #include "macros.h"
 
-void log_init (char *p);
+void log_init (char *cmd_name);
 void log_fini (void);
-void log_set_dest (char *dest);
-char *log_get_dest (void);
-void log_msg (const char *fmt, va_list ap);
 
 void err_exit (const char *fmt, ...)
         __attribute__ ((format (printf, 1, 2), noreturn));
@@ -26,33 +23,9 @@ void msg_exit (const char *fmt, ...)
 void msg (const char *fmt, ...)
         __attribute__ ((format (printf, 1, 2)));
 
-int check_int (int res,
-               const char *fmt,
-               ...  )
-__attribute__ ((format (printf, 2, 3)));
-void *check_ptr (void *res,
-                 const char *fmt,
-                 ... )
-__attribute__ ((format (printf, 2, 3)));
-
-#define LOG_POSITION __FILE__ ":" STRINGIFY (__LINE__)
-
-// NOTE: FMT string *MUST* be a string literal
-#define CHECK_INT(X, ...) check_int ((X), LOG_POSITION \
-                                          ":negative integer from:" \
-                                          STRINGIFY (X) ":" \
-                                          __VA_ARGS__)
-#define CHECK_PTR(X, ...) check_ptr ((X), LOG_POSITION \
-                                          ":null pointer from:" \
-                                          STRINGIFY (X) ":" \
-                                          __VA_ARGS__)
-
 #define oom() do { \
   errn_exit (ENOMEM, "%s::%s(), line %d", __FILE__, __FUNCTION__, __LINE__); \
 } while (0)
-
-const char *log_leveltostr (int level);
-int log_strtolevel (const char *s);
 
 #endif /* !_UTIL_LOG_H */
 
