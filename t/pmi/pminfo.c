635298c7951755d50e7e4d626fd209f0ccaf342c
Jim Garlick <garlick.jim@gmail.com>
2016-07-01 13:28:41 -0700

    t/pmi/pminfo: use PMI-1 API

--- t/pmi/pminfo.c
+++ t/pmi/pminfo.c
@@ -9,7 +9,8 @@
 
 #include "src/common/libutil/log.h"
 #include "src/common/libutil/xzmalloc.h"
-#include "src/common/libpmi/client.h"
+#include "src/common/libpmi/pmi.h"
+#include "src/common/libpmi/pmi_strerror.h"
 
 #define OPTIONS "l:"
 static const struct option longopts[] = {
@@ -24,7 +25,6 @@ int main(int argc, char *argv[])
     char *kvsname;
     int ch;
     char *library = NULL;
-    pmi_t *pmi;
 
     while ((ch = getopt_long (argc, argv, OPTIONS, longopts, NULL)) != -1) {
         switch (ch) {
@@ -33,57 +33,54 @@ int main(int argc, char *argv[])
                 break;
         }
     }
-    if (library)
-        pmi = pmi_create_dlopen (library);
-    else
-        pmi = pmi_create_guess ();
-    if (!pmi)
-        log_err_exit ("pmi_create");
-    e = pmi_init (pmi, &spawned);
+    if (library) {
+        setenv ("PMI_LIBRARY", library, 1);
+        unsetenv ("PMI_FD");
+    }
+    e = PMI_Init (&spawned);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("pmi_init: %s", pmi_strerror (e));
-    e = pmi_initialized (pmi, &initialized);
+        log_msg_exit ("PMI_Init: %s", pmi_strerror (e));
+    e = PMI_Initialized (&initialized);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("pmi_initialized: %s", pmi_strerror (e));
+        log_msg_exit ("PMI_Initialized: %s", pmi_strerror (e));
     if (initialized == 0)
-        log_msg_exit ("pmi_initialized says nope!");
-    e = pmi_get_rank (pmi, &rank);
+        log_msg_exit ("PMI_Initialized says nope!");
+    e = PMI_Get_rank (&rank);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("pmi_get_rank: %s", pmi_strerror (e));
-    e = pmi_get_size (pmi, &size);
+        log_msg_exit ("PMI_Get_rank: %s", pmi_strerror (e));
+    e = PMI_Get_size (&size);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("%d: pmi_get_size: %s",
-                rank, pmi_strerror (e));
-    e = pmi_kvs_get_name_length_max (pmi, &kvsname_len);
+        log_msg_exit ("%d: PMI_Get_size: %s",
+                      rank, pmi_strerror (e));
+    e = PMI_KVS_Get_name_length_max (&kvsname_len);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("%d: pmi_kvs_get_name_length_max: %s",
-                rank, pmi_strerror (e));
-    e = pmi_kvs_get_key_length_max (pmi, &key_len);
+        log_msg_exit ("%d: PMI_KVS_Get_name_length_max: %s",
+                      rank, pmi_strerror (e));
+    e = PMI_KVS_Get_key_length_max (&key_len);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("%d: pmi_kvs_get_key_length_max: %s",
-                rank, pmi_strerror (e));
-    e = pmi_kvs_get_value_length_max (pmi, &val_len);
+        log_msg_exit ("%d: PMI_KVS_Get_key_length_max: %s",
+                      rank, pmi_strerror (e));
+    e = PMI_KVS_Get_value_length_max (&val_len);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("%d: pmi_kvs_get_value_length_max: %s",
-                rank, pmi_strerror (e));
-    e = pmi_get_appnum (pmi, &appnum);
+        log_msg_exit ("%d: PMI_KVS_Get_value_length_max: %s",
+                      rank, pmi_strerror (e));
+    e = PMI_Get_appnum (&appnum);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("pmi_get_appnum: %s", pmi_strerror (e));
+        log_msg_exit ("PMI_Get_appnum: %s", pmi_strerror (e));
 
     kvsname = xzmalloc (kvsname_len);
-    e = pmi_kvs_get_my_name (pmi, kvsname, kvsname_len);
+    e = PMI_KVS_Get_my_name (kvsname, kvsname_len);
     if (e != PMI_SUCCESS)
-        log_msg_exit ("%d: pmi_kvs_get_my_name: %s", rank, pmi_strerror (e));
+        log_msg_exit ("%d: PMI_KVS_Get_my_name: %s", rank, pmi_strerror (e));
 
     printf ("%d: size=%d appnum=%d maxes=%d:%d:%d kvsname=%s\n",
             rank, size, appnum, kvsname_len, key_len, val_len, kvsname);
 
-    e = pmi_finalize (pmi);
+    e = PMI_Finalize ();
     if (e != PMI_SUCCESS)
-        log_msg_exit ("%d: pmi_finalize: %s", rank, pmi_strerror (e));
+        log_msg_exit ("%d: PMI_Finalize: %s", rank, pmi_strerror (e));
 
     free (kvsname);
-    pmi_destroy (pmi);
 
     return 0;
 }
