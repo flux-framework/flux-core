3a9fcd66d8c817fa84f5f861bb515f19de62c5d7
Jim Garlick <garlick.jim@gmail.com>
2016-08-04 13:55:43 -0700

    lua.d/intel_mpi.lua: revert SLURM_ env settings
    Now that wrexecd provides PMI-1 wire protocol, these SLURM_
    environment variables (discussed in #630) are no longer required.

--- src/modules/wreck/lua.d/intel_mpi.lua
+++ src/modules/wreck/lua.d/intel_mpi.lua
@@ -7,20 +7,6 @@ function rexecd_init ()
     local libpmi = f:getattr ('conf.pmi_library_path')
     env['I_MPI_PMI_LIBRARY'] = libpmi
 
-    -- XXX: Unfortunately, Intel MPI doesn't seem to want to correctly
-    --  bootsrap using Flux PMI library unless SLURM_NPROCS and
-    --  SLURM_PROCID are correctly set. When Flux supports PMI-1 wire
-    --  protocol via PMI_FD, these lines should be able to go away.
-    env['SLURM_NPROCS'] = wreck.kvsdir.ntasks
 end
-
-function rexecd_task_init ()
-    --
-    -- See note above about requirement for these SLURM_* environment
-    --  variables for Intel MPI PMI-bootstrap functionality
-    --
-    wreck.environ ['SLURM_PROCID'] = wreck.globalid
-end
-
 
 -- vi: ts=4 sw=4 expandtab
