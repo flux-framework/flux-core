58f3f750a6e2f926a11b6ddc7dc9a2c1c74198ae
Jim Garlick <garlick.jim@gmail.com>
2016-09-21 11:23:47 -0700

    modules/kvs: move dirent desc. to json_dirent.h

--- src/modules/kvs/kvs.c
+++ src/modules/kvs/kvs.c
@@ -22,40 +22,6 @@
  *  See also:  http://www.gnu.org/licenses/
 \*****************************************************************************/
 
-/* JSON directory object:
- * list of key-value pairs where key is a name, value is a dirent
- *
- * JSON dirent objects:
- * object containing one key-value pair where key is one of
- * "FILEREF", "DIRREF", "FILEVAL", "DIRVAL", "LINKVAL", and value is a SHA1
- * hash key into ctx->store (FILEREF, DIRREF), an actual directory, file
- * (value), or link target JSON object (FILEVAL, DIRVAL, LINKVAL).
- *
- * For example, consider KVS containing:
- * a="foo"
- * b="bar"
- * c.d="baz"
- * X -> c.d
- *
- * Root directory:
- * {"a":{"FILEREF":"f1d2d2f924e986ac86fdf7b36c94bcdf32beec15"},
- *  "b":{"FILEREF","8714e0ef31edb00e33683f575274379955b3526c"},
- *  "c":{"DIRREF","6eadd3a778e410597c85d74c287a57ad66071a45"},
- *  "X":{"LINKVAL","c.d"}}
- *
- * Deep copy of root directory:
- * {"a":{"FILEVAL":"foo"},
- *  "b":{"FILEVAL","bar"},
- *  "c":{"DIRVAL",{"d":{"FILEVAL":"baz"}}},
- *  "X":{"LINKVAL","c.d"}}
- *
- * On LINKVAL's:
- * - target is always fully qualified key name
- * - links are always followed in path traversal of intermediate directories
- * - for kvs_get, terminal links are only followed if 'readlink' flag is set
- * - for kvs_put, terminal links are never followed
- */
-
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
