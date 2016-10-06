0f3351f2f7b9fd512028bca06dc83b8ac9cefa7c
Jim Garlick <garlick.jim@gmail.com>
2016-10-06 09:13:03 -0700

    build: drop Makefile.inc
    The installed Makefile.inc for flux-core has been replaced with
    flux-core.pc, and is no longer used by flux-sched.  Drop it and
    its sharness tests.

--- t/build/hello_czmq.c
+++
@@ -1,9 +0,0 @@
-#include <czmq.h>
-
-int main (int argc, char **argv)
-{
-	zlist_t *zlist = zlist_new ();
-	zlist_destroy (&zlist);
-
-	return 0;
-}
--- t/build/hello_flux_internal.c
+++
@@ -1,9 +0,0 @@
-#include "src/common/libutil/shortjson.h"
-
-int main (int argc, char **argv)
-{
-	json_object *o = Jnew();
-	Jput (o);
-
-	return 0;
-}
