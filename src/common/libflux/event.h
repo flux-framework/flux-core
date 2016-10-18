6d53fa9af5dd77f447900ab48dc238a0562b933b
Jim Garlick <garlick.jim@gmail.com>
2016-10-18 04:06:11 +0000

    libflux/event: add json payload accessors
    Add new event functions that allow json payload to be encoded/
    decoded with jansson pack/unpack.
    
    flux_event_encodef()
    flux_event_decodef()

--- src/common/libflux/event.h
+++ src/common/libflux/event.h
@@ -15,11 +15,24 @@
 int flux_event_decode (const flux_msg_t *msg, const char **topic,
                        const char **json_str);
 
+/* Decode an event message with json payload.  These functions use
+ * jansson unpack style variable arguments for decoding the JSON object
+ * payload directly.  Returns 0 on success, or -1 on failure with errno set.
+ */
+int flux_event_decodef (const flux_msg_t *msg, const char **topic,
+                        const char *fmt, ...);
+
 /* Encode an event message.
  * If json_str is non-NULL, it is copied to the message payload.
  * Returns message or NULL on failure with errno set.
  */
 flux_msg_t *flux_event_encode (const char *topic, const char *json_str);
+
+/* Encode an event message with json payload.  These functions use
+ * jansson pack style variable arguments for encoding the JSON object
+ * payload directly.  Returns message or NULL on failure with errno set.
+ */
+flux_msg_t *flux_event_encodef (const char *topic, const char *fmt, ...);
 
 #endif /* !FLUX_CORE_EVENT_H */
 
