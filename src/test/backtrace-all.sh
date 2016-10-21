1476b25e6a98ef75077504f71e3438448978e26f
Mark Grondona <mark.grondona@gmail.com>
2016-10-21 15:16:52 +0000

    test: backtrace-all.sh: fix backtrace for exec in PATH
    When executable causing backtrace is in PATH, and thus doesn't have
    a relative or absolute path recorded in the corefile, the
    backtrace-all script would fail to list a backtrace because
    `test -x $exe` was failing. Precede the check with a `which $exe`
    to translate an executable name to a path.
    
    Also, quote around '*.core' in case there are one or more corefiles
    in the current directory.

--- src/test/backtrace-all.sh
+++ src/test/backtrace-all.sh
@@ -6,13 +6,14 @@
 say () { printf "\033[91m%s\033[39m\n" "$@" >&2; }
 die () { say "$@"; exit 1; }
 
-find . -name *.core -o -name core | while read -r line; do
+find . -name '*.core' -o -name core -type f | while read -r line; do
     d=$(dirname $line)
     f=$(basename $line)
     exe=$(file $line | sed "s/.*from '\([^ \t]*\).*'.*/\1/")
     ( cd $d &&
-      test -x $exe || die "Failed to find executable at $exe" &&
+      exe=$(which $exe || echo $exe)
       say "Found corefile for $exe" &&
+      test -x $exe || die "Failed to find executable at $exe" &&
       gdb --batch --quiet -ex "thread apply all bt full" $exe $f
     )
 done
