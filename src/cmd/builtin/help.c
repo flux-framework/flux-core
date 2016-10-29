e8f81cce033e75e96cbff0d04e7e9a88bff559aa
Albert Chu <chu11@llnl.gov>
2016-10-28 17:49:51 -0700

    Output appropriate error message on help failure
    If flux-core is configured and built with --disable-docs, the "flux
    help" command may still pick up manpages generated outside of the
    current build (such as a version of flux installed on the system).
    
    Install a simple ".nodocs" file when not generating documents.
    When user attempts to user "flux help", this file can be detected
    and an appropriate error message will be output to the user.
    
    Fixes #874

--- src/cmd/builtin/help.c
+++ src/cmd/builtin/help.c
@@ -26,6 +26,15 @@
 #include "src/common/libutil/setenvf.h"
 #include "builtin.h"
 
+static int no_docs_set (optparse_t *p)
+{
+    int *flags = optparse_get_data (p, "conf_flags");
+    const char *no_docs_path = flux_conf_get ("no_docs_path", *flags);
+    struct stat sb;
+
+    return (!stat (no_docs_path, &sb));
+}
+
 static int cmd_help (optparse_t *p, int ac, char *av[])
 {
     int n = optparse_optind (p);
@@ -34,6 +43,9 @@ static int cmd_help (optparse_t *p, int ac, char *av[])
     if (n < ac) {
         const char *topic = av [n];
         int rc;
+
+        if (no_docs_set (p))
+            log_msg_exit ("flux manual pages not built");
 
         /* N.B. Flux doc dir already prepended to MANPATH if needed.
          */
