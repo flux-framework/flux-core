9d3b7303a80af239744e122dc08be3a82759cbc7
Jim Garlick <garlick.jim@gmail.com>
2016-12-15 12:04:48 -0800

    wreck/openmpi.lua: drop LD_LIBRARY_PATH, set orte_tmpdir
    The new openmpi Flux component uses FLUX_PMI_LIBRARY_PATH
    at runtime to dlopen our PMI library, so LD_LIBRARY_PATH
    does not need to be set for it to work right in the
    default configuration.  Therefore, don't change it
    in the openmpi lua script.
    
    Do set OMPI_MCA_orte_tmpdir_base to the rank-specific
    directory so that shared memory segment names do not
    collide when there are multiple Flux ranks on a (real)
    node launching an MPI program with multiple MPI ranks
    on a (flux) node.

--- src/modules/wreck/lua.d/openmpi.lua
+++ src/modules/wreck/lua.d/openmpi.lua
@@ -1,31 +1,16 @@
 -- Set environment specific to openmpi
 --
--- If configured for SLURM, OpenMPI links with libpmi.so or libpmi2.so
--- (depending on options used).  Flux provides alternate versions of these
--- libraries in a non-default location, and we must set LD_LIBRARY_PATH to
--- point there.
---
--- Some versions of OpenMPI, 1.10.2 for example, set an rpath for their
--- PMI plugins that includes /usr/lib64.  That is bug and those versions
--- need to be patched patched to work with Flux.
--- 
--- If Flux supports the PMI-2 wire protocol, the SLURM libpmi.so might
--- work with Flux.  It currently doesn't, and in fact ignores the protocol
--- version handshake entirely.   (See flux-framework/flux-core#746)
-
 
 local dirname = require 'flux.posix'.dirname
 
 function rexecd_init ()
     local env = wreck.environ
     local f = wreck.flux
-    local libpmi = f:getattr ('conf.pmi_library_path')
-    local ldpath = dirname (libpmi)
+    local rankdir = f:getattr ('scratch-directory-rank')
 
-    if (env['LD_LIBRARY_PATH'] ~= nil) then
-        ldpath = ldpath..':'..env['LD_LIBRARY_PATH']
-    end
-    env['LD_LIBRARY_PATH'] = ldpath
+    -- Avoid shared memory segment name collisions
+    -- when flux instance runs >1 broker per node.
+    env['OMPI_MCA_orte_tmpdir_base'] = rankdir
 end
 
 -- vi: ts=4 sw=4 expandtab
