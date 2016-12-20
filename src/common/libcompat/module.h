2ce1bbc3aec2675b656a26f35c43600627d8f0e2
Mark A. Grondona <mgrondona@llnl.gov>
2016-12-19 16:04:08 -0800

    libcompat: add flux_modfind and flux_modname
    Add compat versions of flux_modfind and flux_modname.
    These use an internal version of flux extensor instead of direct
    dlopen() to find and name modules.

---
+++ src/common/libcompat/module.h
@@ -0,0 +1,11 @@
+#ifndef _FLUX_MODFIND_H
+#define _FLUX_MODFIND_H
+
+char *flux_modname (const char *path);
+char *flux_modfind (const char *searchpath, const char *modname);
+
+#endif /* !_FLUX_MODFIND_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
