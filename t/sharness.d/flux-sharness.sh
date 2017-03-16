0a9ea4f6946b3df5f478e5174633d7dffd1b2631
Jim Garlick <garlick.jim@gmail.com>
2017-03-16 16:06:11 -0700

    test/sharness: add rc3 "personalities"
    Problem: rc1 files for different test environments
    should have corresponding rc3 files for proper teardown.
    
    Edit flux-sharness.sh to require a personality to have
    both rc1 and rc3 scripts, and add rc3 scripts for the three
    test personalities already configured with rc1 scripts.

--- t/sharness.d/flux-sharness.sh
+++ t/sharness.d/flux-sharness.sh
@@ -57,8 +57,9 @@ test_under_flux() {
         export FLUX_RC3_PATH=""
     elif test "$personality" != "full"; then
         export FLUX_RC1_PATH=$FLUX_SOURCE_DIR/t/rc/rc1-$personality
-        export FLUX_RC3_PATH=""
-        test -x $FLUX_RC1_PATH || error "$FLUX_RC1_PATH"
+        export FLUX_RC3_PATH=$FLUX_SOURCE_DIR/t/rc/rc3-$personality
+        test -x $FLUX_RC1_PATH || error "cannot execute $FLUX_RC1_PATH"
+        test -x $FLUX_RC3_PATH || error "cannot execute $FLUX_RC3_PATH"
     else
         unset FLUX_RC1_PATH
         unset FLUX_RC3_PATH
