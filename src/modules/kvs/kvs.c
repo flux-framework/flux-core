9a7e9244f5ad9560d7f7b2fe23ecb2df1534f89a
Albert Chu <chu11@llnl.gov>
2017-05-01 17:21:28 -0700

    modules/kvs: Make private functions static

--- src/modules/kvs/kvs.c
+++ src/modules/kvs/kvs.c
@@ -107,10 +107,10 @@ typedef struct {
 
 static int setroot_event_send (kvs_ctx_t *ctx, json_object *names);
 static int error_event_send (kvs_ctx_t *ctx, json_object *names, int errnum);
-void commit_prep_cb (flux_reactor_t *r, flux_watcher_t *w,
-                     int revents, void *arg);
-void commit_check_cb (flux_reactor_t *r, flux_watcher_t *w,
-                      int revents, void *arg);
+static void commit_prep_cb (flux_reactor_t *r, flux_watcher_t *w,
+                            int revents, void *arg);
+static void commit_check_cb (flux_reactor_t *r, flux_watcher_t *w,
+                             int revents, void *arg);
 
 static void freectx (void *arg)
 {
@@ -593,8 +593,8 @@ stall:
     return;
 }
 
-void commit_prep_cb (flux_reactor_t *r, flux_watcher_t *w,
-                     int revents, void *arg)
+static void commit_prep_cb (flux_reactor_t *r, flux_watcher_t *w,
+                            int revents, void *arg)
 {
     kvs_ctx_t *ctx = arg;
     fence_t *f;
@@ -619,7 +619,7 @@ void commit_prep_cb (flux_reactor_t *r, flux_watcher_t *w,
  * If we were to merge commit #1 and commit #3, A=2 would be set after
  * A=3.
  */
-void commit_merge_all (kvs_ctx_t *ctx)
+static void commit_merge_all (kvs_ctx_t *ctx)
 {
     fence_t *f = zlist_first (ctx->ready);
 
@@ -660,8 +660,8 @@ void commit_merge_all (kvs_ctx_t *ctx)
     }
 }
 
-void commit_check_cb (flux_reactor_t *r, flux_watcher_t *w,
-                      int revents, void *arg)
+static void commit_check_cb (flux_reactor_t *r, flux_watcher_t *w,
+                             int revents, void *arg)
 {
     kvs_ctx_t *ctx = arg;
     fence_t *f;
