2d3a521684dd0004bf219fc05d21dc8b20b81859
Albert Chu <chu11@llnl.gov>
2017-05-15 17:18:28 -0700

    libutil/test: Fix dirwalk test corner case
    Base directory in test comparison must also be "REALPATH".

--- src/common/libutil/test/dirwalk.c
+++ src/common/libutil/test/dirwalk.c
@@ -182,7 +182,7 @@ static int d_unlinkat (dirwalk_t *d, void *arg)
 
 int main(int argc, char** argv)
 {
-    char *s, *tmp = NULL, *tmp2 = NULL;
+    char *s, *rpath, *tmp = NULL, *tmp2 = NULL;
     int n;
 
     plan (NO_PLAN);
@@ -293,9 +293,12 @@ int main(int argc, char** argv)
     l = dirwalk_find (tmp, flags, "*", 0, find_dir, NULL);
     ok (l && zlist_size (l) >  0, "dirwalk works with DIRWALK_REALPATH");
 
-    ok (l && check_zlist_order (l, tmp, expect_breadth),
+    /* tmp base for comparison must also be realpath-ed */
+    rpath = realpath (tmp, NULL);
+    ok (l && check_zlist_order (l, rpath, expect_breadth),
         "breadth-first visited directories with DIRWALK_REALPATH works");
     zlist_destroy (&l);
+    free (rpath);
 
     if (chdir (cwd) < 0)
         BAIL_OUT ("chdir (%s)", cwd);
