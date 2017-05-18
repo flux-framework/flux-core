f6dcca7f0dd9c25596a1aa73dfb01395491f2b20
Albert Chu <chu11@llnl.gov>
2017-05-18 15:36:26 -0700

    soak: Update for recent flux changes
    Update content dir from scratch-directory to broker.rundir.
    
    Use flux module stats instead of flux comms-stats.
    
    Fixes #1070

--- src/test/soak-workload.sh
+++ src/test/soak-workload.sh
@@ -3,7 +3,7 @@
 size=$(flux getattr size)
 
 contentdir=$(flux getattr persist-directory 2>/dev/null) \
-	|| contentdir=$(flux getattr scratch-directory)
+	|| contentdir=$(flux getattr broker.rundir)
 
 declare heap_prefix=""
 declare njobs=10
@@ -24,7 +24,7 @@ fi
 
 for i in `seq 1 $njobs`; do
     elapsed=$(/usr/bin/time -f "%e" flux wreckrun --ntasks $size /bin/true 2>&1)
-    broker_rss=$(flux comms-stats --rusage cmb --parse maxrss)
+    broker_rss=$(flux module stats --parse maxrss --rusage cmb)
     content_k=$(du -sk $contentdir | cut -f1)
     flux logger --severity=info --appname=soak $i $elapsed $broker_rss $content_k
     if [ -n "${heap_prefix}" ]; then
