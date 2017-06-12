3db0aa33ad5148ff43f7c1af84c991e876222ff6
Jim Garlick <garlick.jim@gmail.com>
2017-06-12 16:45:58 -0700

    doc/flux_rpc(3): rework man page and examples
    Consolidate RPC functions in one page flux_rpc(3),
    adding references to flux_future_then(3) etc for
    synchronization portion.
    
    Rework example code to be more clear.

--- doc/man3/trpc.c
+++ doc/man3/trpc.c
@@ -1,37 +1,25 @@
 #include <flux/core.h>
-#include "src/common/libutil/shortjson.h"
 #include "src/common/libutil/log.h"
-
-void get_rank (flux_future_t *f)
-{
-    const char *json_str;
-    json_object *o;
-    const char *rank;
-
-    if (flux_rpc_get (f, &json_str) < 0)
-        log_err_exit ("flux_rpc_get");
-    if (!json_str
-        || !(o = Jfromstr (json_str))
-        || !Jget_str (o, "value", &rank))
-        log_msg_exit ("response protocol error");
-    printf ("rank is %s\n", rank);
-    Jput (o);
-}
 
 int main (int argc, char **argv)
 {
     flux_t *h;
     flux_future_t *f;
-    json_object *o = Jnew();
+    const char *rankstr;
 
     if (!(h = flux_open (NULL, 0)))
         log_err_exit ("flux_open");
-    Jadd_str (o, "name", "rank");
-    if (!(f = flux_rpc (h, "attr.get", Jtostr (o), FLUX_NODEID_ANY, 0)))
-        log_err_exit ("flux_rpc");
-    get_rank (f);
+
+    if (!(f = flux_rpcf (h, "attr.get", FLUX_NODEID_ANY, 0,
+		        "{s:s}", "name", "rank")))
+        log_err_exit ("flux_rpcf");
+
+    if (flux_rpc_getf (f, "{s:s}", "value", &rankstr) < 0)
+        log_err_exit ("flux_rpc_getf");
+
+    printf ("rank is %s\n", rankstr);
     flux_future_destroy (f);
+
     flux_close (h);
-    Jput (o);
     return (0);
 }
--- doc/man3/trpc_then.c
+++ doc/man3/trpc_then.c
@@ -1,21 +1,14 @@
 #include <flux/core.h>
-#include "src/common/libutil/shortjson.h"
 #include "src/common/libutil/log.h"
 
-void get_rank (flux_future_t *f, void *arg)
+void continuation (flux_future_t *f, void *arg)
 {
-    const char *json_str;
-    json_object *o;
-    const char *rank;
+    const char *rankstr;
 
-    if (flux_rpc_get (f, &json_str) < 0)
-        log_err_exit ("flux_rpc_get");
-    if (!json_str
-        || !(o = Jfromstr (json_str))
-        || !Jget_str (o, "value", &rank))
-        log_msg_exit ("response protocol error");
-    printf ("rank is %s\n", rank);
-    Jput (o);
+    if (flux_rpc_getf (f, "{s:s}", "value", &rankstr) < 0)
+        log_err_exit ("flux_rpc_getf");
+
+    printf ("rank is %s\n", rankstr);
     flux_future_destroy (f);
 }
 
@@ -23,21 +16,20 @@ int main (int argc, char **argv)
 {
     flux_t *h;
     flux_future_t *f;
-    json_object *o = Jnew ();
 
-    Jadd_str (o, "name", "rank");
     if (!(h = flux_open (NULL, 0)))
         log_err_exit ("flux_open");
-    if (!(f = flux_rpc (h, "attr.get", Jtostr (o), FLUX_NODEID_ANY, 0)))
-        log_err_exit ("flux_rpc");
-    if (flux_future_wait_for (f, 0.) == 0)
-        get_rank (f, NULL);
-    else if (flux_future_then (f, -1., get_rank, NULL))
+
+    if (!(f = flux_rpcf (h, "attr.get", FLUX_NODEID_ANY, 0,
+		        "{s:s}", "name", "rank")))
+        log_err_exit ("flux_rpcf");
+
+    if (flux_future_then (f, -1., continuation, NULL) < 0)
         log_err_exit ("flux_future_then");
+
     if (flux_reactor_run (flux_get_reactor (h), 0) < 0)
         log_err_exit ("flux_reactor_run");
 
     flux_close (h);
-    Jput (o);
     return (0);
 }
