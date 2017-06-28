84dc0b2c9507e64927f9bf7b16307704d757c3a5
Albert Chu <chu11@llnl.gov>
2017-06-27 17:04:51 -0700

    modules/kvs: New convenience function json_hash()

--- src/modules/kvs/json_util.h
+++ src/modules/kvs/json_util.h
@@ -3,6 +3,7 @@
 
 #include "src/common/libutil/tstat.h"
 #include "waitqueue.h"
+#include "types.h"
 
 /* Copy element wise a json directory object into a new json object.
  */
@@ -11,6 +12,10 @@ json_object *json_object_copydir (json_object *dir);
 /* Compare two json objects, return true if same, false if not
  */
 bool json_compare (json_object *o1, json_object *o2);
+
+/* Calculate hash of a json object
+ */
+int json_hash (const char *hash_name, json_object *o, href_t ref);
 
 #endif  /* !_FLUX_KVS_JSON_UTIL_H */
 
