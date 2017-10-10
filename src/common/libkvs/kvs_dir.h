4a24b8c4dd9ca8471f15942034f1d603d102c725
Jim Garlick <garlick.jim@gmail.com>
2017-10-10 11:48:54 -0700

    libkvs/dir: add some inline documentation
    Problem: There is no documentation for flux_kvsdir_t.
    
    The flux_kvsdir_t probably won't be deprecated although it may
    be slimmed down in the future.  Meanwhile there are still users.
    Start with some inline documentation in the header.

--- src/common/libkvs/kvs_dir.h
+++ src/common/libkvs/kvs_dir.h
@@ -5,48 +5,76 @@
 extern "C" {
 #endif
 
+/* The flux_kvsdir_t represents an unordered list of directory entries.
+ * It is also overloaded as a container for a flux_t handle, a snapshot
+ * reference, and a namespace placeholder, in support of legacy
+ * flux_kvsdir_get() and flux_kvsdir_put() series of functions in
+ * kvs_classic.h.  Those features may be deprecated in the future.
+ */
+
 typedef struct flux_kvsdir flux_kvsdir_t;
 typedef struct flux_kvsitr flux_kvsitr_t;
 
+/* Create/destroy/copy a flux_kvsdir_t.
+ *
+ * flux_kvsdir_create() creates a new object from 'json_str', an encoded
+ * RFC 11 dir object.  'key' is the full key path associated with the
+ * directory.  Optional: 'handle', is a flux_t handle, and 'rootref' is
+ * a snapshot reference.  The optional arguments are  used to support the
+ * legacy flux_kvsdir_get() and flux_kvsdir_put() series of functions.
+ *
+ * flux_kvsdir_incref() increments an internal reference count that is
+ * decremented by each call to flux_kvsdir_destroy().  Resources
+ * are not freed until the reference count reaches zero.  The object is
+ * initially created with a reference count of one.
+ */
 flux_kvsdir_t *flux_kvsdir_create (flux_t *handle, const char *rootref,
                                    const char *key, const char *json_str);
-flux_kvsdir_t *flux_kvsdir_copy (const flux_kvsdir_t *dir);
 void flux_kvsdir_destroy (flux_kvsdir_t *dir);
-void flux_kvsdir_incref (flux_kvsdir_t *dir);
-const char *flux_kvsdir_tostring (const flux_kvsdir_t *dir);
 
-/* An iterator interface for walking the list of names in a flux_kvsdir_t
- * returned by kvs_get_dir().  flux_kvsitr_create() always succeeds.
- * flux_kvsitr_next() returns NULL when the last item is reached.
+flux_kvsdir_t *flux_kvsdir_copy (const flux_kvsdir_t *dir);
+void flux_kvsdir_incref (flux_kvsdir_t *dir);
+
+
+/* flux_kvsitr_t is an iterator for walking the list of names in a
+ * flux_kvsdir_t.  flux_kvsitr_next() returns NULL when the last
+ * item is reached.
  */
 flux_kvsitr_t *flux_kvsitr_create (const flux_kvsdir_t *dir);
 void flux_kvsitr_destroy (flux_kvsitr_t *itr);
 const char *flux_kvsitr_next (flux_kvsitr_t *itr);
 void flux_kvsitr_rewind (flux_kvsitr_t *itr);
 
-/* Test attributes of 'name', relative to kvsdir object.
- * Intended for testing names returned by flux_kvsitr_next (no recursion).
- * Symlinks are not dereferenced, i.e. symlink pointing to dir will read
- * issymlink=true, isdir=false.
- */
-bool flux_kvsdir_exists (const flux_kvsdir_t *dir, const char *name);
-bool flux_kvsdir_isdir (const flux_kvsdir_t *dir, const char *name);
-bool flux_kvsdir_issymlink (const flux_kvsdir_t *dir, const char *name);
-
-/* Get key associated with a directory or directory entry.
- * Both functions always succeed.
- */
-const char *flux_kvsdir_key (const flux_kvsdir_t *dir);
-
-/* caller frees result */
-char *flux_kvsdir_key_at (const flux_kvsdir_t *dir, const char *key);
-
-void *flux_kvsdir_handle (const flux_kvsdir_t *dir);
-const char *flux_kvsdir_rootref (const flux_kvsdir_t *dir);
-
 /* Get the number of keys in a directory.
  */
 int flux_kvsdir_get_size (const flux_kvsdir_t *dir);
+
+/* Test whether 'name' exists in 'dir'.
+ * It is expected to be a single name, not fully qualified key path.
+ */
+bool flux_kvsdir_exists (const flux_kvsdir_t *dir, const char *name);
+
+/* Test whether 'name' exists in 'dir' and is itself a directory.
+ */
+bool flux_kvsdir_isdir (const flux_kvsdir_t *dir, const char *name);
+
+/* Test whether 'name' exists in 'dir' and is a symbolic link.
+ */
+bool flux_kvsdir_issymlink (const flux_kvsdir_t *dir, const char *name);
+
+/* Access the original 'key', 'json_str', 'handle', and 'rootref' parameters
+ * passed to flux_kvsdir_create ()
+ */
+const char *flux_kvsdir_key (const flux_kvsdir_t *dir);
+const char *flux_kvsdir_tostring (const flux_kvsdir_t *dir);
+void *flux_kvsdir_handle (const flux_kvsdir_t *dir);
+const char *flux_kvsdir_rootref (const flux_kvsdir_t *dir);
+
+/* Construct a fully-qualified key from flux_kvsdir_key() + "." + key.
+ * Caller must free.
+ */
+char *flux_kvsdir_key_at (const flux_kvsdir_t *dir, const char *key);
+
 
 #ifdef __cplusplus
 }
