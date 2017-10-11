3a3a1d875680021a19c5690323b1a4570acdeca0
Jim Garlick <garlick.jim@gmail.com>
2017-10-11 11:16:35 -0700

    libkvs/classic: deprecate all functions
    Add __attribute__ ((deprecated)) to all kvs_classic functions.

--- src/common/libkvs/kvs_classic.h
+++ src/common/libkvs/kvs_classic.h
@@ -5,8 +5,8 @@
 extern "C" {
 #endif
 
-/* These functions are soon to be deprecated and probably
- * should not be used in new code.
+/* These functions are from an earlier era and should not be used in
+ * new code.
  */
 
 /* flux_kvs_get() and flux_kvs_get_dir() combine a flux_kvs_lookup()
@@ -18,9 +18,10 @@ extern "C" {
  * constructing the key argument on the fly.
  */
 
-int flux_kvs_get (flux_t *h, const char *key, char **json_str);
+int flux_kvs_get (flux_t *h, const char *key, char **json_str)
+                    __attribute__ ((deprecated));
 int flux_kvs_get_dir (flux_t *h, flux_kvsdir_t **dirp, const char *fmt, ...)
-                      __attribute__ ((format (printf, 3, 4)));
+                    __attribute__ ((format (printf, 3, 4), deprecated));
 
 /* These functions are like their counterparts in "kvs_txn.h" except
  * they append operations to an "anonymous" transaction.
@@ -29,10 +30,14 @@ int flux_kvs_get_dir (flux_t *h, flux_kvsdir_t **dirp, const char *fmt, ...)
  * transaction.
  */
 
-int flux_kvs_put (flux_t *h, const char *key, const char *json_str);
-int flux_kvs_unlink (flux_t *h, const char *key);
-int flux_kvs_symlink (flux_t *h, const char *key, const char *target);
-int flux_kvs_mkdir (flux_t *h, const char *key);
+int flux_kvs_put (flux_t *h, const char *key, const char *json_str)
+                    __attribute__ ((deprecated));
+int flux_kvs_unlink (flux_t *h, const char *key)
+                    __attribute__ ((deprecated));
+int flux_kvs_symlink (flux_t *h, const char *key, const char *target)
+                    __attribute__ ((deprecated));
+int flux_kvs_mkdir (flux_t *h, const char *key)
+                    __attribute__ ((deprecated));
 
 /* flux_kvs_commit_anon() and flux_kvs_fence_anon() combine a flux_kvs_commit()
  * and a flux_future_get() call in one function, thus they block the calling
@@ -40,8 +45,10 @@ int flux_kvs_mkdir (flux_t *h, const char *key);
  * anonymous transaction (see above).
  */
 
-int flux_kvs_commit_anon (flux_t *h, int flags);
-int flux_kvs_fence_anon (flux_t *h, const char *name, int nprocs, int flags);
+int flux_kvs_commit_anon (flux_t *h, int flags)
+                    __attribute__ ((deprecated));
+int flux_kvs_fence_anon (flux_t *h, const char *name, int nprocs, int flags)
+                    __attribute__ ((deprecated));
 
 /* flux_kvsdir_get() and flux_kvsdir_get_dir() combine a flux_kvs_lookup()
  * and flux_kvs_get() call in one function, thus they block the calling
@@ -63,10 +70,11 @@ int flux_kvs_fence_anon (flux_t *h, const char *name, int nprocs, int flags);
  */
 
 int flux_kvsdir_get (const flux_kvsdir_t *dir,
-                    const char *key, char **json_str);
+                    const char *key, char **json_str)
+                    __attribute__ ((deprecated));
 int flux_kvsdir_get_dir (const flux_kvsdir_t *dir, flux_kvsdir_t **dirp,
                     const char *fmt, ...)
-                    __attribute__ ((format (printf, 3, 4)));
+                    __attribute__ ((format (printf, 3, 4), deprecated));
 
 /* These functions are like their counterparts in "kvs_txn.h" except
  * they append operations to an "anonymous" transaction, and construct
@@ -79,11 +87,15 @@ int flux_kvsdir_get_dir (const flux_kvsdir_t *dir, flux_kvsdir_t **dirp,
  */
 
 int flux_kvsdir_put (const flux_kvsdir_t *dir, const char *key,
-                    const char *json_str);
+                    const char *json_str)
+                    __attribute__ ((deprecated));
 int flux_kvsdir_pack (const flux_kvsdir_t *dir, const char *key,
-                    const char *fmt, ...);
-int flux_kvsdir_unlink (const flux_kvsdir_t *dir, const char *key);
-int flux_kvsdir_mkdir (const flux_kvsdir_t *dir, const char *key);
+                    const char *fmt, ...)
+                    __attribute__ ((deprecated));
+int flux_kvsdir_unlink (const flux_kvsdir_t *dir, const char *key)
+                    __attribute__ ((deprecated));
+int flux_kvsdir_mkdir (const flux_kvsdir_t *dir, const char *key)
+                    __attribute__ ((deprecated));
 
 #ifdef __cplusplus
 }
