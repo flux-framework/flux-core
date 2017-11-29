0b2ffeeb66f44a8764ab3d61c3220440201b0c27
Albert Chu <chu11@llnl.gov>
2017-11-28 16:53:20 -0800

    common/libkvs: Add new unit tests
    Add new unit tests for common/libkvs/kvs.c functions.

---
+++ src/common/libkvs/test/kvs.c
@@ -0,0 +1,35 @@
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string.h>
+#include <errno.h>
+
+#include "src/common/libflux/flux.h"
+#include "kvs.h"
+#include "src/common/libtap/tap.h"
+
+void errors (void)
+{
+    /* check simple error cases */
+
+    errno = 0;
+    ok (flux_kvs_namespace_create (NULL, NULL, 5) == NULL && errno == EINVAL,
+        "flux_kvs_namespace_create fails on bad input");
+}
+
+int main (int argc, char *argv[])
+{
+
+    plan (NO_PLAN);
+
+    errors ();
+
+    done_testing();
+    return (0);
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
+
