32396fec17809fad47586158ffc6171f517eadee
Albert Chu <chu11@llnl.gov>
2018-01-12 16:31:14 -0800

    modules/kvs: Update parameter name for consistency

--- src/modules/kvs/waitqueue.h
+++ src/modules/kvs/waitqueue.h
@@ -55,7 +55,7 @@ int wait_runqueue (waitqueue_t *q);
  * The message handler will be reinvoked once the wait_t usecount reaches zero.
  * Message will be copied and destroyed with the wait_t.
  */
-wait_t *wait_create_msg_handler (flux_t *h, flux_msg_handler_t *w,
+wait_t *wait_create_msg_handler (flux_t *h, flux_msg_handler_t *mh,
                                  const flux_msg_t *msg, void *arg,
                                  flux_msg_handler_f cb);
 
