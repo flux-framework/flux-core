a887d1c93df55dccbcdae709cfd6c1a0806ec567
Mark A. Grondona <mark.grondona@gmail.com>
2018-04-28 08:02:46 -0700

    t/t2000-wreck-dummy-sched: add tests using dummy sched module
    Add a couple tests that use the dummy sched module in order to
    test the events generated by job.submit and job.submit-nocreate.
    
    These tests are isolated in their own test file because the wreck
    job module caches the `sched_loaded` boolean, so the dummy sched
    module can't be unloaded to revert wreck/job to its previous behavior,
    which may confuse future tests (e.g. wreckrun would start blocking)
    
    Also, subsequent tests could be confused by the tests in this file
    leaving jobs in a submitted state.

--- t/wreck/sched-dummy.c
+++ t/wreck/sched-dummy.c
@@ -2,7 +2,6 @@
 #include "config.h"
 #endif
 #include <flux/core.h>
-
 /*
  *  Dummy sched module, do nothing but answer pings
  */
@@ -12,11 +11,7 @@ int mod_main (flux_t *h, int argc, char *argv[])
         return (-1);
     return (0);
 }
-
-/* This dso extends a comms module named "parent".
- */
 MOD_NAME ("sched");
-
 /*
  * vi:tabstop=4 shiftwidth=4 expandtab
  */
