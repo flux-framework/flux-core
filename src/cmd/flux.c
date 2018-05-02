711d665a7c667e60045e4637b08c28ff932e9d23
Jim Garlick <garlick.jim@gmail.com>
2018-05-02 13:51:09 -0700

    cmd/flux: drop shortjson.h
    Problem: cmd/flux includes shortjson.h but doesn't use it.
    
    Drop the extraneous include.

--- src/cmd/flux.c
+++ src/cmd/flux.c
@@ -40,7 +40,6 @@
 
 #include "src/common/libutil/log.h"
 #include "src/common/libutil/xzmalloc.h"
-#include "src/common/libutil/shortjson.h"
 #include "src/common/libutil/environment.h"
 
 #include "cmdhelp.h"
