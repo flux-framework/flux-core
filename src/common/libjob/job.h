09429d128c317f827671c147df05f03c7a0dd062
Jim Garlick <garlick.jim@gmail.com>
2018-05-23 12:22:19 -0700

    libjob: add flux job library
    Add a library will provides an API for job creation,
    monitoring, and control.  For now it contains only
    an interface for job submission.

---
+++ src/common/libjob/job.h
@@ -0,0 +1,36 @@
+#ifndef _FLUX_CORE_JOB_H
+#define _FLUX_CORE_JOB_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <flux/core.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef uint64_t flux_jobid_t;
+
+/* Submit a job to the system.
+ * J should be RFC 14 jobspec signed by flux_sign_wrap(), provided
+ * flux was built --with-flux-security.  If not, then J should be bare jobspec.
+ * Currently the 'flags' parameter must be set to 0.
+ * The system assigns a jobid and returns it in the response.
+ */
+flux_future_t *flux_job_submit (flux_t *h, const char *J, int flags);
+
+/* Parse jobid from response to flux_job_submit() request.
+ * Returns 0 on success, -1 on failure with errno set - and an extended
+ * error message may be available with flux_future_error_string().
+ */
+int flux_job_submit_get_id (flux_future_t *f, flux_jobid_t *id);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_FLUX_CORE_JOB_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
