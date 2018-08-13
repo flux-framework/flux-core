ec61b4d6aec40c9d2ade1831beec21a6e8a51bf9
Albert Chu <chu11@llnl.gov>
2018-08-13 11:03:18 -0700

    common/subprocess: Add subprocess unit tests

---
+++ src/common/subprocess/test/cmd.c
@@ -0,0 +1,227 @@
+
+#include <assert.h>
+#include <jansson.h>
+
+#include "src/common/libtap/tap.h"
+#include "src/common/subprocess/command.h"
+
+/*
+ *  Check basic flux_cmd_create () with args
+ */
+void check_basic_create ()
+{
+    char **av;
+    char * argv[] = {
+        "test",
+        "--option=foo",
+        "bar",
+        NULL
+    };
+    int argc = (sizeof (argv)/sizeof (argv[0])) - 1;
+    char * env[] = {
+        "FOO=bar",
+        "PATH=/bin",
+        NULL
+    };
+    flux_cmd_t *cmd;
+
+    diag ("simple flux_cmd_create (argc, argv, env)");
+    cmd = flux_cmd_create (argc, argv, env);
+    ok (cmd != NULL, "flux_cmd_create ()");
+    av = flux_cmd_argv_expand (cmd);
+    ok (av != NULL, "flux_cmd_argv_expand ()");
+    is (av[0], "test", "av[0] == test");
+    is (av[1], "--option=foo", "av[1] == --option=foo");
+    is (av[2], "bar", "av[2] == bar");
+    ok (av[3] == NULL, "av[3] == NULL");
+    free (av);
+    is (flux_cmd_getenv (cmd, "FOO"), "bar", "flux_cmd_getenv");
+    is (flux_cmd_getenv (cmd, "PATH"), "/bin", "flux_cmd_getenv");
+
+    flux_cmd_destroy (cmd);
+}
+
+
+void check_empty_cmd_attributes (flux_cmd_t *cmd)
+{
+    char **argv, **env;
+
+    ok (flux_cmd_argc (cmd) == 0, "flux_cmd_argc");
+
+    argv = flux_cmd_argv_expand (cmd);
+    ok (argv != NULL, "flux_cmd_argv_expand returned an argv");
+    ok (argv[0] == NULL, "argv is properly NULL terminated");
+    free (argv);
+
+    env = flux_cmd_env_expand (cmd);
+    ok (env != NULL, "flux_cmd_env_expand works");
+    ok (env[0] == NULL, "flux_cmd_env_expand properly terminates env");
+    free (env);
+
+    ok (flux_cmd_getcwd (cmd) == NULL,
+        "flux_cmd_getcwd returns NULL");
+}
+
+/*
+ * Set some basic known cmd attributes for testing
+ */
+void set_cmd_attributes (flux_cmd_t *cmd)
+{
+    assert (flux_cmd_argc (cmd) == 0);
+
+    // Append to argv
+    ok (flux_cmd_argv_append (cmd, "command") >= 0,
+        "flux_cmd_argv_append");
+    ok (flux_cmd_argv_append (cmd, "foo") >= 0,
+        "flux_cmd_argv_append");
+    ok (flux_cmd_argv_append (cmd, "bar") >= 0,
+        "flux_cmd_argv_append");
+
+    // Test setenvf
+    ok (flux_cmd_setenvf (cmd, 0, "PATH", "/bin:/usr/bin") >= 0,
+        "flux_cmd_setenvf (PATH)");
+
+    ok (flux_cmd_setcwd (cmd, "/tmp") >= 0,
+        "flux_cmd_setcwd (/tmp)");
+    ok (flux_cmd_add_channel (cmd, "MY_FD") >= 0,
+        "flux_cmd_add_channel");
+    ok (flux_cmd_setopt (cmd, "OPTION", "VALUE") >= 0,
+        "flux_cmd_setopt");
+}
+
+/* set alternate way, to ensure alternate ways also work */
+void set_cmd_attributes2 (flux_cmd_t *cmd)
+{
+    char *env[] = { "PATH=/bin:/usr/bin", NULL };
+
+    ok (flux_cmd_set_env (cmd, env) == 0,
+        "flux_cmd_set_env");
+}
+
+void check_cmd_attributes (flux_cmd_t *cmd)
+{
+    char **argv, **env;
+    const char *arg = NULL;
+
+    ok (flux_cmd_argc (cmd) == 3, "flux_cmd_argc");
+
+    argv = flux_cmd_argv_expand (cmd);
+    ok (argv != NULL, "flux_cmd_argv_expand returned an argv");
+    ok (argv[3] == NULL, "argv is properly NULL terminated");
+    is (argv[0], "command", "argv[0] is correct");
+    is (argv[1], "foo", "argv[1] is correct");
+    is (argv[2], "bar", "argv[2] is correct");
+    free (argv);
+
+    ok (flux_cmd_arg (cmd, 3) == NULL
+        && errno == EINVAL,
+        "flux_cmd_arg returns EINVAL on bad range");
+    arg = flux_cmd_arg (cmd, 0);
+    ok (arg != NULL
+        && !strcmp (arg, "command"),
+        "flux_cmd_arg returns correct argv[0]");
+    arg = flux_cmd_arg (cmd, 1);
+    ok (arg != NULL
+        && !strcmp (arg, "foo"),
+        "flux_cmd_arg returns correct argv[1]");
+    arg = flux_cmd_arg (cmd, 2);
+    ok (arg != NULL
+        && !strcmp (arg, "bar"),
+        "flux_cmd_arg returns correct argv[2]");
+
+    is (flux_cmd_getenv (cmd, "PATH"), "/bin:/usr/bin",
+        "flux_cmd_getenv");
+
+    env = flux_cmd_env_expand (cmd);
+    ok (env != NULL, "flux_cmd_env_expand works");
+    ok (env[1] == NULL, "flux_cmd_env_expand properly terminates env");
+    is (env[0], "PATH=/bin:/usr/bin",
+        "first entry of env is as expected");
+    free (env);
+
+    is (flux_cmd_getcwd (cmd), "/tmp",
+        "flux_cmd_getcwd");
+    is (flux_cmd_getopt (cmd, "OPTION"), "VALUE",
+        "flux_cmd_getopt (cmd, 'OPTION') == VALUE");
+}
+
+int main (int argc, char *argv[])
+{
+    char *s;
+    flux_cmd_t *cmd, *copy;
+
+    plan (NO_PLAN);
+
+    diag ("Basic flux_cmd_create");
+    check_basic_create ();
+
+    diag ("Create a flux_cmd_t and fill it with known values");
+    // Create an empty command then fill it with nonsense:
+    cmd = flux_cmd_create (0, NULL, NULL);
+    ok (cmd != NULL, "flux_cmd_create (0, NULL, NULL)");
+    check_empty_cmd_attributes (cmd);
+    set_cmd_attributes (cmd);
+
+    diag ("Ensure flux_cmd_t contains expected values and test interfaces");
+    // Check the nonsense
+    check_cmd_attributes (cmd);
+
+    set_cmd_attributes2 (cmd);
+
+    diag ("Ensure flux_cmd_t contains expected values again");
+    check_cmd_attributes (cmd);
+
+    // Test unsetenv with throwaway var
+    diag ("Test setenv/getenv/unsetenv");
+    ok (flux_cmd_setenvf (cmd, 1, "FOO", "%d", 42) >= 0,
+        "flux_cmd_setenvf (FOO=42)");
+    is (flux_cmd_getenv (cmd, "FOO"), "42",
+        "flux_cmd_getenv (FOO) == 42");
+    flux_cmd_unsetenv (cmd, "FOO");
+    ok (flux_cmd_getenv (cmd, "FOO") == NULL,
+        "flux_cmd_unsetenv works");
+
+    // Test env overwrite
+    ok (flux_cmd_setenvf (cmd, 0, "FOO", "%d", 42) >= 0,
+        "flux_cmd_setenvf (FOO=42)");
+    is (flux_cmd_getenv (cmd, "FOO"), "42",
+        "flux_cmd_getenv (FOO) == 42");
+    ok (flux_cmd_setenvf (cmd, 0, "FOO", "%d", 24) < 0,
+        "flux_cmd_setenvf (FOO=24) no overwrite fails");
+    ok (flux_cmd_setenvf (cmd, 1, "FOO", "%d", 24) >= 0,
+        "flux_cmd_setenvf (FOO=24, overwrite=true)");
+    is (flux_cmd_getenv (cmd, "FOO"), "24",
+        "flux_cmd_getenv (FOO) == 24");
+    flux_cmd_unsetenv (cmd, "FOO");
+
+    diag ("Copy a flux_cmd_t and and ensure it matches source cmd");
+    copy = flux_cmd_copy (cmd);
+    ok (copy != NULL, "flux_cmd_copy");
+    check_cmd_attributes (copy);
+    flux_cmd_destroy (copy);
+
+    diag ("Convert flux_cmd_t to/from JSON");
+    s = flux_cmd_tojson (cmd);
+    ok (s != NULL, "flux_cmd_tojson (%d bytes)", strlen (s));
+    if (s) {
+        json_error_t error;
+        diag (s);
+        copy = flux_cmd_fromjson (s, &error);
+        free (s);
+        ok (copy != NULL, "flux_cmd_fromjson returned a new cmd");
+        if (copy) {
+            check_cmd_attributes (copy);
+            flux_cmd_destroy (copy);
+        }
+        else
+            diag ("%d:%d: %s", error.line, error.column, error.text);
+    }
+    flux_cmd_destroy (cmd);
+
+    done_testing ();
+    return 0;
+}
+
+/*
+ * vi: ts=4 sw=4 expandtab
+ */
---
+++ src/common/subprocess/test/test_echo.c
@@ -0,0 +1,148 @@
+/* simple tool that outputs args to stdout/stderr or both depending on
+ * options
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+
+int out = 0;
+int err = 0;
+int channel_out = 0;
+int no_newline = 0;
+char *channel_name;
+int channel = 0;
+int fd = STDIN_FILENO;
+int prefix = 0;
+
+void output (const char *str)
+{
+    if (channel && channel_out) {
+        char prefixbuf[1024];
+        int plen;
+
+        plen = sprintf (prefixbuf,
+                        "%s%s%s",
+                        prefix ? channel_name : "",
+                        prefix ? ":" : "",
+                        str);
+        if (write (fd, prefixbuf, plen) < 0) {
+            perror ("write");
+            exit (1);
+        }
+    }
+    if (out) {
+        fprintf (stdout,
+                 "%s%s",
+                 prefix ? "STDOUT:" : "",
+                 str);
+        fflush (stdout);
+    }
+    if (err) {
+        fprintf (stderr,
+                 "%s%s",
+                 prefix ? "STDERR:" : "",
+                 str);
+        fflush (stderr);
+    }
+}
+
+int
+main (int argc, char *argv[])
+{
+    int bytes = 0;
+
+    while (1) {
+        int c = getopt (argc, argv, "c:COEnPb:");
+        if (c < 0)
+            break;
+
+        switch (c) {
+        case 'O':
+            out++;
+            break;
+        case 'E':
+            err++;
+            break;
+        case 'n':
+            no_newline++;
+            break;
+        case 'c':
+            channel++;
+            channel_name = optarg;
+            break;
+        case 'C':
+            channel_out++;
+            break;
+        case 'P':
+            prefix++;
+            break;
+        case 'b':
+            bytes = atoi (optarg);
+            break;
+        }
+    }
+
+    if ((out + err) == 0
+        && (!channel && !channel_out)) {
+        fprintf (stderr, "must specify a way to output");
+        exit (1);
+    }
+
+    if (channel) {
+        const char *fdstr;
+        char channelstr[1024];
+
+        sprintf (channelstr, "%s_FD", channel_name);
+
+        if (!(fdstr = getenv (channelstr))) {
+            perror ("getenv");
+            exit (1);
+        }
+
+        fd = atoi (fdstr);
+    }
+
+    if (optind != argc) {
+        while (optind < argc) {
+            char outbuf[1024];
+
+            sprintf (outbuf,
+                     "%s%s",
+                     argv[optind],
+                     no_newline ? "" : "\n");
+
+            output (outbuf);
+            optind++;
+        }
+    }
+    else {
+        char buf[1024];
+        int total = 0;
+        int len;
+
+        memset (buf, '\0', 1024);
+        while ((len = read (fd, buf, 1024)) > 0) {
+            char outbuf[1024];
+
+            sprintf (outbuf,
+                     "%s%s",
+                     buf,
+                     no_newline ? "" : "\n");
+
+            output (outbuf);
+
+            total += len;
+
+            if (bytes && total >= bytes)
+                break;
+
+            memset (buf, '\0', 1024);
+        }
+    }
+
+    close (STDOUT_FILENO);
+    close (STDERR_FILENO);
+    exit (0);
+}
