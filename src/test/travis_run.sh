b53fc4f8b3bb473a484b1299b7841a62670b7f2e
Tom Scogland <scogland1@llnl.gov>
2018-09-14 15:23:07 -0700

    refactor travis.yml and travis_run script
    This is a relatively large refactor.  All coverage handling and so-forth
    happens in the script now. There may be more environment variables that
    can be removed from the docker command, but some have come out.  Basic
    summary:
    
    * coverity has been removed from .travis.yml
    * flux/core.h is placed in two different paths in /usr and /usr/local
    during setup
    * coveralls is downloaded into the docker image by the script during
    coverage runs
    * coveralls is uploaded from inside the container
    * caliper should work, added by previous commit

--- src/test/travis_run.sh
+++ src/test/travis_run.sh
@@ -6,9 +6,17 @@ test "${TRAVIS_BRANCH}" != 'coverity_scan' || exit 0
 # Force git to update the shallow clone and include tags so git-describe works
 git fetch --unshallow --tags || true
 ulimit -c unlimited
+
+
+# make ccache safe for PRs and clang
+test "$TRAVIS_PULL_REQUEST" == "false" || export CCACHE_READONLY=1
+if test "$CC" = "clang"; then
+  export CCACHE_CPP2=1
+fi
+
 sudo /usr/sbin/update-ccache-symlinks
 export PATH=/usr/lib/ccache:$PATH
-export MAKECMDS="make distcheck"
+
 # Ensure travis builds libev such that libfaketime will work:
 # (force libev to *not* use syscall interface for clock_gettime())
 export CPPFLAGS="$CPPFLAGS -DEV_USE_CLOCK_SYSCALL=0 -DEV_USE_MONOTONIC=1"
@@ -21,19 +29,46 @@ export FLUX_TEST_SIZE_MAX=5
 # Invoke MPI tests
 export TEST_MPI=t
 
-# Enable coverage for $CC-coverage build
-# We can't use distcheck here, it doesn't play well with coverage testing:
-if test "$COVERAGE" = "t" ; then ARGS="$ARGS --enable-code-coverage"; MAKECMDS="make -j 2 && make -j 2 check-code-coverage && lcov -l flux*-coverage.info"; fi
-# Use make install for T_INSTALL:
-if test "$T_INSTALL" = "t" ; then ARGS="$ARGS --prefix=/tmp/flux"; MAKECMDS="make && make install && /tmp/flux/bin/flux keygen && FLUX_TEST_INSTALLED_PATH=/tmp/flux/bin make -j 2 check"; fi
-# Use src/test/cppcheck.sh instead of make?:
-if test "$CPPCHECK" = "t" ; then MAKECMDS="sh -x src/test/cppcheck.sh && make"; fi
-
 export FLUX_TESTS_LOGFILE=t
 export DISTCHECK_CONFIGURE_FLAGS=${ARGS}
 
+# Ensure we always use internal <flux/core.h> by placing a dummy file
+#  in the same path as ZMQ_FLAGS:
+sudo mkdir -p /usr/include/flux /usr/local/include/flux
+sudo sh -c "echo '#error Non-build-tree flux/core.h!' > /usr/include/flux/core.h"
+sudo sh -c "echo '#error Non-build-tree flux/core.h!' > /usr/local/include/flux/core.h"
+
+# set up build directory and autogen
 ./autogen.sh
 mkdir -p travis-build
 cd travis-build
-../configure ${ARGS}
-eval $MAKECMDS
+CONF=../configure $ARGS
+
+if test "$COVERAGE" = "t" ; then
+  # Enable coverage for $CC-coverage build
+  # We can't use distcheck here, it doesn't play well with coverage testing:
+  # coveralls-lcov required only for coveralls upload:
+  sudo gem install coveralls-lcov
+  $CONF --enable-code-coverage
+
+  make -j 2
+  make -j 2 check-code-coverage
+  lcov -l flux*-coverage.info
+  # Upload results to coveralls.io for first job of N
+  coveralls-lcov flux*-coverage.info
+  bash <(curl -s https://codecov.io/bash)
+elif test "$T_INSTALL" = "t" ; then
+  # Use make install for T_INSTALL:
+  $CONF --prefix=/tmp/flux
+  make
+  make install
+  /tmp/flux/bin/flux keygen
+  FLUX_TEST_INSTALLED_PATH=/tmp/flux/bin make -j 2 check
+elif test "$CPPCHECK" = "t" ; then
+  $CONF
+  # Use src/test/cppcheck.sh instead of make?:
+  sh -x src/test/cppcheck.sh && make
+else
+  $CONF
+  make distcheck
+fi
