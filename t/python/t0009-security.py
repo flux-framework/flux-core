018a040e83c07a7b554780df356ed13b8fa59998
Stephen Herbein <stephen272@gmail.com>
2018-10-10 17:05:53 -0700

    python: add bindings for flux-security
        - include simple test of wrap/unwrap and security error messaging
        - test python3 with flux-security in at least one travis builder
    
    Closes #1709

---
+++ t/python/t0009-security.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import unittest
+from tempfile import NamedTemporaryFile
+
+import flux.core as core
+from flux.security import SecurityContext
+
+def __flux_size():
+    return 1
+
+class TestKVS(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        conf = b"""[sign]
+max-ttl = 30
+default-type = "none"
+allowed-types = [ "none" ]
+"""
+        with NamedTemporaryFile() as tmpfile:
+            tmpfile.file.write(conf)
+            tmpfile.file.flush()
+            os.fsync(tmpfile.file)
+            self.context = SecurityContext(tmpfile.name)
+
+    def test_00_sign_none(self):
+        unsigned_str = u"hello world"
+        signed_str = self.context.sign_wrap(unsigned_str, mech_type="none")
+        unwrapped_payload, wrapping_user = self.context.sign_unwrap(signed_str)
+        unwrapped_str = unwrapped_payload.decode('utf-8')
+
+        self.assertEqual(unsigned_str, unwrapped_str)
+        self.assertEqual(wrapping_user, os.getuid())
+
+    def test_01_security_error(self):
+        with self.assertRaisesRegexp(EnvironmentError, "sign-unwrap:.*"):
+            unwrapped_payload, wrapping_user = self.context.sign_unwrap(b"foo")
+
+if __name__ == '__main__':
+    from subflux import rerun_under_flux
+    if rerun_under_flux(__flux_size()):
+        from pycotap import TAPTestRunner
+        unittest.main(testRunner=TAPTestRunner())
