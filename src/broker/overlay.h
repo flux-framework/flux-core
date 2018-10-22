8c27d909d2263fb8b3639f8da6d327bf6e823430
Jim Garlick <garlick.jim@gmail.com>
2018-10-22 16:57:55 -0700

    broker/overlay: drop event socket support
    Drop the following functions that are no longer used:
      overlay_mute_child ()
      overlay_set_event ()
      overlay_get_event ()
      overlay_set_event_cb ()
      overlay_sendmsg_event ()
      overlay_recvmsg_event ()
      overlay_set_relay ()
      overlay_get_relay ()
      overlay_sendmsg_relay ()

--- src/broker/overlay.h
+++ src/broker/overlay.h
@@ -36,13 +36,9 @@ const char *overlay_get_child (overlay_t *ov);
 void overlay_set_child_cb (overlay_t *ov, overlay_cb_f cb, void *arg);
 int overlay_sendmsg_child (overlay_t *ov, const flux_msg_t *msg);
 /* We can "multicast" events to all child peers using mcast_child().
- * It walks the 'children' hash, finding overlay peers that have not
- * yet been "muted", and routes them a copy of msg.  The broker Cc's
- * events over the TBON using this until peers indicate that they are
- * receiving duplicate seq numbers through the normal event socket.
+ * It walks the 'children' hash, finding peers and routeing them a copy of msg.
  */
 int overlay_mcast_child (overlay_t *ov, const flux_msg_t *msg);
-void overlay_mute_child (overlay_t *ov, const char *uuid);
 
 /* Call when message is received from child 'uuid'.
  */
@@ -51,25 +47,6 @@ void overlay_checkin_child (overlay_t *ov, const char *uuid);
 /* Encode cmb.lspeer response payload.
  */
 char *overlay_lspeer_encode (overlay_t *ov);
-
-/* The event socket is SUB for ranks > 0, and PUB for rank 0.
- * Internally, all events are routed to rank 0 before being published.
- */
-void overlay_set_event (overlay_t *ov, const char *fmt, ...);
-const char *overlay_get_event (overlay_t *ov);
-void overlay_set_event_cb (overlay_t *ov, overlay_cb_f cb, void *arg);
-int overlay_sendmsg_event (overlay_t *ov, const flux_msg_t *msg);
-flux_msg_t *overlay_recvmsg_event (overlay_t *ov);
-
-/* Since an epgm:// endpoint only allows one subscriber per node,
- * when there are multiple ranks per node, arrangements must be made
- * to forward events within a clique.  Only the relay itself has this
- * socket; other clique members would subscribe to the relay's URI
- * via their main event socket.  The PMI bootstrap sets this up if needed.
- */
-void overlay_set_relay (overlay_t *ov, const char *fmt, ...);
-const char *overlay_get_relay (overlay_t *ov);
-int overlay_sendmsg_relay (overlay_t *ov, const flux_msg_t *msg);
 
 /* Establish connections.
  * These functions are idempotent as the bind may need to be called
@@ -84,13 +61,16 @@ int overlay_connect (overlay_t *ov);
  */
 int overlay_reparent (overlay_t *ov, const char *uri, bool *recycled);
 
-/* Add attributes to 'attrs' to reveal information about the overlay
- * network.  Two of the attributes directly retrieve information from
- * "overlay" through callbacks registered with 'attrs': "tbon.parent-endpoint"
- * and "mcast.relay-endpoint".  The rest are simple non-active attributes:
- * "rank", "size", "tbon.arity", "tbon.level", "tbon.maxlevel", and
- * "tbon.descendants".
- *
+/* Add attributes to 'attrs' to reveal information about the overlay network.
+ * Active attrs:
+ *   tbon.parent-endpoint
+ * Passive attrs:
+ *   rank
+ *   size
+ *   tbon.arity
+ *   tbon.level
+ *   tbon.maxlevel
+ *   tbon.descendants
  * Returns 0 on success, -1 on error.
  */
 int overlay_register_attrs (overlay_t *overlay, attr_t *attrs);
