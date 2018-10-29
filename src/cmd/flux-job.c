5ba305493287c2d0b08644d820d81c2c7d1cd61c
Jim Garlick <garlick.jim@gmail.com>
2018-10-29 15:49:26 -0700

    cmd/flux-job: update license boiler plate to GPLv2+

--- src/cmd/flux-job.c
+++ src/cmd/flux-job.c
@@ -9,10 +9,7 @@
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the Free
  *  Software Foundation; either version 2 of the license, or (at your option)
- *  any later version.  Additionally, the libflux-core library may be
- *  redistributed under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation, either version 2 of the license,
- *  or (at your option) any later version.
+ *  any later version.
  *
  *  Flux is distributed in the hope that it will be useful, but WITHOUT
  *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
