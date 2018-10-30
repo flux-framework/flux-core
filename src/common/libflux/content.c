d5f85ba7d07157dba9d187fa4f403de8ac66eb1b
Jim Garlick <garlick.jim@gmail.com>
2018-10-30 10:32:19 -0700

    libflux: change license boiler plate to LGPLv2.1+

--- src/common/libflux/content.c
+++ src/common/libflux/content.c
@@ -7,20 +7,20 @@
  *  For details, see https://github.com/flux-framework.
  *
  *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the license, or (at your option)
- *  any later version.
+ *  under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the license,
+ *  or (at your option) any later version.
  *
  *  Flux is distributed in the hope that it will be useful, but WITHOUT
  *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
  *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  *  See also:  http://www.gnu.org/licenses/
- \*****************************************************************************/
+\*****************************************************************************/
 
 #if HAVE_CONFIG_H
 #include "config.h"
