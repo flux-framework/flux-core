b199ad16bd7518a17c36d28d0819fb13b6dc34c4
Jim Garlick <garlick.jim@gmail.com>
2018-11-03 16:10:49 -0400

    testsuite: add handle unit test
    Cover the aux_get/aux_set functions that were just modified
    for (locally handled) invalid arguments.

---
+++ src/common/libflux/test/handle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <flux/core.h>
+
+#include "src/common/libtap/tap.h"
+
+int main (int argc, char *argv[])
+{
+    plan (NO_PLAN);
+
+    errno = 0;
+    ok (flux_aux_set (NULL, "foo", "bar", NULL) < 0 && errno == EINVAL,
+        "flux_aux_set h=NULL fails with EINVAL");
+    errno = 0;
+    ok (flux_aux_get (NULL, "foo") == NULL && errno == EINVAL,
+        "flux_aux_get h=NULL fails with EINVAL");
+
+    done_testing();
+
+    return (0);
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
+
