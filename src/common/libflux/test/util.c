25c2796d3c62d852da1c37c02474a3ec201c5015
Jim Garlick <garlick.jim@gmail.com>
2018-11-09 09:40:25 -0800

    testsuite: add loopback_create()
    Problem: libflux unit tests are dependent on the loop://
    connector and thus cannot be located within the libflux
    directory.
    
    Add loopback_create() to the test server helper.
    This creates a flux_t handle with loopback behavior
    identical to the loop:// connector, except since it is
    only used in unit tests, it can BAIL_OUT() on fatal
    errors.
    
    Also cleanup:  if test server is not provided with a
    server callback, provide an internal one that logs all
    messages received by the server.  Simplify some error
    handling (use BAIL_OUT for unlikely errors).

--- src/common/libflux/test/util.c
+++ src/common/libflux/test/util.c
@@ -8,12 +8,13 @@
 #include "util.h"
 
 #include "src/common/libtap/tap.h"
-#include "src/common/libutil/setenvf.h"
+#include "src/common/libutil/msglist.h"
 
 struct test_server {
     flux_t *c;
     flux_t *s;
-    flux_msg_handler_t *mh;
+    flux_msg_handler_t *shutdown_mh;
+    flux_msg_handler_t *diag_mh;
     test_server_f cb;
     void *arg;
     pthread_t thread;
@@ -23,6 +24,7 @@ struct test_server {
 
 static flux_t *test_connector_create (const char *shmem_name,
                                       bool server, int flags);
+
 
 void shutdown_cb (flux_t *h, flux_msg_handler_t *mh,
                   const flux_msg_t *msg, void *arg)
@@ -49,32 +51,54 @@ int test_server_stop (flux_t *c)
     int rc = -1;
     struct test_server *a = flux_aux_get (c, "test_server");
 
-    if (!a) {
-        diag ("%s: flux_aux_get failed\n", __FUNCTION__);
-        errno = EINVAL;
-        goto done;
-    }
+    if (!a)
+        BAIL_OUT ("flux_aux_get test_server");
     if (!(msg = flux_request_encode ("shutdown", NULL)))
-        goto done;
-    if (flux_send (a->c, msg, 0) < 0) {
-        diag ("%s: flux_send: %s\n", __FUNCTION__, flux_strerror (errno));
-        goto done;
-    }
-    if ((e = pthread_join (a->thread, NULL)) != 0) {
-        errno = e;
-        diag ("%s: pthread_join: %s\n", __FUNCTION__, strerror (errno));
-        goto done;
-    }
+        BAIL_OUT ("flux_request_encode");
+    if (flux_send (a->c, msg, 0) < 0)
+        BAIL_OUT ("flux_send");
+    if ((e = pthread_join (a->thread, NULL)) != 0)
+        BAIL_OUT ("pthread_join");
     rc = a->rc;
-done:
     flux_msg_destroy (msg);
     return rc;
+}
+
+static void diag_cb (flux_t *h, flux_msg_handler_t *mh,
+                     const flux_msg_t *msg, void *arg)
+{
+    int msgtype;
+    const char *topic = NULL;
+
+    if (flux_msg_get_type (msg, &msgtype) < 0)
+        goto badmsg;
+    if (msgtype != FLUX_MSGTYPE_KEEPALIVE) {
+        if (flux_msg_get_topic (msg, &topic) < 0)
+            goto badmsg;
+    }
+    diag ("server: < %s%s%s", flux_msg_typestr (msgtype),
+           topic ? " " : "", topic ? topic : "");
+    return;
+badmsg:
+    diag ("server: malformed message:", flux_strerror (errno));
+}
+
+static int diag_server (flux_t *h, void *arg)
+{
+    diag ("server: starting");
+    if (flux_reactor_run (flux_get_reactor (h), 0) < 0) {
+        diag ("flux_reactor_run failed");
+        return -1;
+    }
+    diag ("server: ending");
+    return 0;
 }
 
 static void test_server_destroy (struct test_server *a)
 {
     if (a) {
-        flux_msg_handler_destroy (a->mh);
+        flux_msg_handler_destroy (a->shutdown_mh);
+        flux_msg_handler_destroy (a->diag_mh);
         flux_close (a->s);
         zuuid_destroy (&a->uuid);
         free (a);
@@ -85,49 +109,55 @@ flux_t *test_server_create (test_server_f cb, void *arg)
 {
     int e;
     struct test_server *a;
+    int cflags = 0; // client connector flags
+    int sflags = 0; // server connector flags
 
     if (!(a = calloc (1, sizeof (*a))))
         BAIL_OUT ("calloc");
-    if (!cb)
-        BAIL_OUT ("test_server created called without server callback");
     a->cb = cb;
     a->arg = arg;
 
     if (!(a->uuid = zuuid_new ()))
         BAIL_OUT ("zuuid_new failed");
 
+    if (getenv ("FLUX_HANDLE_TRACE"))
+        cflags |= FLUX_O_TRACE;
+
     /* Create back-to-back wired flux_t handles
      */
-    if (!(a->s = test_connector_create (zuuid_str (a->uuid), true, 0)))
+    if (!(a->s = test_connector_create (zuuid_str (a->uuid), true, sflags)))
         BAIL_OUT ("test_connector_create server");
-    if (!(a->c = test_connector_create (zuuid_str (a->uuid), false, 0)))
+    if (!(a->c = test_connector_create (zuuid_str (a->uuid), false, cflags)))
         BAIL_OUT ("test_connector_create client");
+
+    /* If no callback, register watcher for all messages so we can log them.
+     * N.B. this has to go in before shutdown else shutdown will be masked.
+     */
+    if (!a->cb) {
+        if (!(a->diag_mh = flux_msg_handler_create (a->s, FLUX_MATCH_ANY,
+                                                    diag_cb, a)))
+            BAIL_OUT ("flux_msg_handler_create");
+        flux_msg_handler_start (a->diag_mh);
+        a->cb = diag_server;
+    }
 
     /* Register watcher for "shutdown" request on server side
      */
     struct flux_match match = FLUX_MATCH_REQUEST;
     match.topic_glob = "shutdown";
-    if (!(a->mh = flux_msg_handler_create (a->s, match, shutdown_cb, a))) {
-        diag ("%s: flux_msg_handler_create: %s\n",
-             __FUNCTION__, flux_strerror (errno));
-        goto error;
-    }
-    flux_msg_handler_start (a->mh);
+    if (!(a->shutdown_mh = flux_msg_handler_create (a->s, match,
+                                                    shutdown_cb, a)))
+        BAIL_OUT ("flux_msg_handler_create");
+    flux_msg_handler_start (a->shutdown_mh);
 
     /* Start server thread
      */
-    if ((e = pthread_create (&a->thread, NULL, thread_wrapper, a)) != 0) {
-        errno = e;
-        diag ("%s: pthread_create: %s\n",
-              __FUNCTION__, strerror (errno));
-        goto error;
-    }
-    flux_aux_set (a->c, "test_server", a,
-                  (flux_free_f)test_server_destroy);
+    if ((e = pthread_create (&a->thread, NULL, thread_wrapper, a)) != 0)
+        BAIL_OUT ("pthread_create");
+    if (flux_aux_set (a->c, "test_server", a,
+                      (flux_free_f)test_server_destroy) < 0)
+        BAIL_OUT ("flux_aux_set");
     return a->c;
-error:
-    test_server_destroy (a);
-    return NULL;
 }
 
 void test_server_environment_init (const char *test_name)
@@ -259,6 +289,169 @@ static flux_t *test_connector_create (const char *shmem_name,
     return tcon->h;
 }
 
+/* Loopback connector implementation
+ */
+
+struct loopback_connector {
+    msglist_t *queue;
+    flux_t *h;
+    int pollfd;
+    int pollevents;
+    uint32_t userid;
+    uint32_t rolemask;
+};
+
+static int loopback_connector_pollevents (void *impl)
+{
+    struct loopback_connector *lcon = impl;
+    int e;
+    int revents = 0;
+
+    if ((e = msglist_pollevents (lcon->queue)) < 0)
+        return -1;
+    if (e & POLLIN)
+        revents |= FLUX_POLLIN;
+    if (e & POLLOUT)
+        revents |= FLUX_POLLOUT;
+    if (e & POLLERR)
+        revents |= FLUX_POLLERR;
+
+    return revents;
+}
+
+static int loopback_connector_pollfd (void *impl)
+{
+    struct loopback_connector *lcon = impl;
+
+    return msglist_pollfd (lcon->queue);
+}
+
+static int loopback_connector_send (void *impl, const flux_msg_t *msg,
+                                    int flags)
+{
+    struct loopback_connector *lcon = impl;
+    uint32_t userid;
+    uint32_t rolemask;
+    flux_msg_t *cpy;
+
+    if (flux_msg_get_userid (msg, &userid) < 0)
+        return -1;
+    if (flux_msg_get_rolemask (msg, &rolemask) < 0)
+        return -1;
+    if (!(cpy = flux_msg_copy (msg, true)))
+        return -1;
+    if (userid == FLUX_USERID_UNKNOWN) {
+        if (flux_msg_set_userid (cpy, lcon->userid) < 0)
+            goto error;
+    }
+    if (rolemask == FLUX_ROLE_NONE) {
+        if (flux_msg_set_rolemask (cpy, lcon->rolemask) < 0)
+            goto error;
+    }
+    if (msglist_append (lcon->queue, cpy) < 0) // steals 'cpy'
+        goto error;
+    return 0;
+error:
+    flux_msg_destroy (cpy);
+    return -1;
+}
+
+static flux_msg_t *loopback_connector_recv (void *impl, int flags)
+{
+    struct loopback_connector *lcon = impl;
+    flux_msg_t *msg;
+
+    if (!(msg = msglist_pop (lcon->queue))) {
+        errno = EWOULDBLOCK;
+        return NULL;
+    }
+    return msg;
+}
+
+static int loopback_connector_getopt (void *impl, const char *option,
+                                      void *val, size_t size)
+{
+    struct loopback_connector *lcon = impl;
+
+    if (option && !strcmp (option, FLUX_OPT_TESTING_USERID)) {
+        if (size != sizeof (lcon->userid))
+            goto error;
+        memcpy (val, &lcon->userid, size);
+    }
+    else if (option && !strcmp (option, FLUX_OPT_TESTING_ROLEMASK)) {
+        if (size != sizeof (lcon->rolemask))
+            goto error;
+        memcpy (val, &lcon->rolemask, size);
+    }
+    else
+        goto error;
+    return 0;
+error:
+    errno = EINVAL;
+    return -1;
+}
+
+static int loopback_connector_setopt (void *impl, const char *option,
+                                      const void *val, size_t size)
+{
+    struct loopback_connector *lcon = impl;
+    size_t val_size;
+
+    if (option && !strcmp (option, FLUX_OPT_TESTING_USERID)) {
+        val_size = sizeof (lcon->userid);
+        if (size != val_size)
+            goto error;
+        memcpy (&lcon->userid, val, val_size);
+    }
+    else if (option && !strcmp (option, FLUX_OPT_TESTING_ROLEMASK)) {
+        val_size = sizeof (lcon->rolemask);
+        if (size != val_size)
+            goto error;
+        memcpy (&lcon->rolemask, val, val_size);
+    }
+    else
+        goto error;
+    return 0;
+error:
+    errno = EINVAL;
+    return -1;
+}
+
+static void loopback_connector_fini (void *impl)
+{
+    struct loopback_connector *lcon = impl;
+
+    msglist_destroy (lcon->queue);
+    free (lcon);
+}
+
+static const struct flux_handle_ops loopback_ops = {
+    .pollfd = loopback_connector_pollfd,
+    .pollevents = loopback_connector_pollevents,
+    .send = loopback_connector_send,
+    .recv = loopback_connector_recv,
+    .getopt = loopback_connector_getopt,
+    .setopt = loopback_connector_setopt,
+    .event_subscribe = NULL,
+    .event_unsubscribe = NULL,
+    .impl_destroy = loopback_connector_fini,
+};
+
+flux_t *loopback_create (int flags)
+{
+    struct loopback_connector *lcon;
+
+    if (!(lcon = calloc (1, sizeof (*lcon))))
+        BAIL_OUT ("calloc");
+    lcon->userid = geteuid ();
+    lcon->rolemask = FLUX_ROLE_OWNER;
+    if (!(lcon->queue = msglist_create ((msglist_free_f)flux_msg_destroy)))
+        BAIL_OUT ("msglist_create");
+
+    if (!(lcon->h = flux_handle_create (lcon, &loopback_ops, flags)))
+        BAIL_OUT ("flux_handle_create");
+    return lcon->h;
+}
 
 /*
  * vi:tabstop=4 shiftwidth=4 expandtab
--- src/common/libflux/test/util.h
+++ src/common/libflux/test/util.h
@@ -2,7 +2,8 @@
  *
  * Start a thread running the user-supplied function 'cb' which
  * is connected back to back to a flux_t handle returned by the
- * create function.  Run until test_server_stop() is called.
+ * create function.  To finalize, call test_server_stop(),
+ * followed by flux_close().
  *
  * Caveats:
  * 1) subscribe/unsubscribe requests are not supported
@@ -10,8 +11,12 @@
  * 3) broker attributes (such as rank and size) are unavailable
  * 4) message nodeid is ignored
  *
- * Unit tests that use the test server should probably call
- * test_server_environment_init() once to initialize czmq's runtime.
+ * Unit tests that use the test server should call
+ * test_server_environment_init() once prior to creating the first server
+ * to initialize czmq's runtime.
+ *
+ * If callback is NULL, a default callback is run that logs each
+ * message received with diag().
  */
 typedef int (*test_server_f)(flux_t *h, void *arg);
 
@@ -20,3 +25,17 @@ flux_t *test_server_create (test_server_f cb, void *arg);
 int test_server_stop (flux_t *c);
 
 void test_server_environment_init (const char *test_name);
+
+
+/* Create a loopback connector for testing.
+ * The net effect is much the same as flux_open("local://") except
+ * the implementation is self contained here.  Close with flux_close().
+ *
+ * Like loop://, this support test manipulation of credentials:
+ *   flux_opt_set (h, FLUX_OPT_TESTING_USERID, &userid, sizeof (userid);
+ *   flux_opt_set (h, FLUX_OPT_TESTING_ROLEMASK, &rolemask, sizeof (rolemask))
+ *
+ * N.B. No need to call test_server_environment_init() if this is the
+ * only component used from this module.
+ */
+flux_t *loopback_create (int flags);
