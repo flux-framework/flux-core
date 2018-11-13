73dc534f6812a6a26749d0277c7591db42df89a8
Jim Garlick <garlick.jim@gmail.com>
2018-11-13 14:27:01 -0800

    libflux/panic: clean up flux_panic()
    Problem: flux_panic() triggers assertions in czmq atexit handler.
    
    Change the cmb.panic message handler to call _exit(1) after
    logging its message to stderr.
    
    Cleanup:
    - protocol now requires 'reason' string (renamed from 'msg')
    - add flags to proto and function (reserved for future use)
    - don't send response on protocol error, just log it
    - take uint32_t nodeid not integer rank with -1 conversion
      to FLUX_NODEID_ANY
    
    Update flux-comms panic command.

--- src/common/libflux/panic.h
+++ src/common/libflux/panic.h
@@ -25,13 +25,18 @@
 #ifndef _FLUX_CORE_PANIC_H
 #define _FLUX_CORE_PANIC_H
 
+#include <stdint.h>
 #include "handle.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int flux_panic (flux_t *h, int rank, const char *msg);
+/* Tell broker on 'nodeid' to call _exit() after displaying 'reason'
+ * on stderr.  'nodeid' may be FLUX_NODEID_ANY to select the local
+ * broker.  Currently flags should be set to zero.
+ */
+int flux_panic (flux_t *h, uint32_t nodeid, int flags, const char *reason);
 
 #ifdef __cplusplus
 }
