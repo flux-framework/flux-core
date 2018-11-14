8c2360384e3e61bd92b43c5e0c1f36503add55af
Albert Chu <chu11@llnl.gov>
2018-11-14 15:06:53 -0800

    modules/kvs: Use zhashx_t in cache
    Replace use of zhash_t with zhashx_t in cache.  By doing so,
    we avoid duplicating the hash key/blobref that is already duplicated
    in each cache entry.  The keys are also not duplicated when
    cache entries are checked for expiration.
    
    A global destructor for cache entries is also set, removing the need
    to set a free function on every item in the hash.

--- src/modules/kvs/cache.c
+++ src/modules/kvs/cache.c
@@ -66,7 +66,7 @@ struct cache_entry {
 };
 
 struct cache {
-    zhash_t *zh;
+    zhashx_t *zhx;
 };
 
 struct cache_entry *cache_entry_create (const char *ref)
@@ -303,7 +303,7 @@ int cache_entry_wait_valid (struct cache_entry *entry, wait_t *wait)
 struct cache_entry *cache_lookup (struct cache *cache, const char *ref,
                                   int current_epoch)
 {
-    struct cache_entry *entry = zhash_lookup (cache->zh, ref);
+    struct cache_entry *entry = zhashx_lookup (cache->zhx, ref);
     if (entry && current_epoch > entry->lastuse_epoch)
         entry->lastuse_epoch = current_epoch;
     return entry;
@@ -314,17 +314,15 @@ int cache_insert (struct cache *cache, struct cache_entry *entry)
     int rc;
 
     if (cache && entry) {
-        rc = zhash_insert (cache->zh, entry->blobref, entry);
+        rc = zhashx_insert (cache->zhx, entry->blobref, entry);
         assert (rc == 0);
-
-        zhash_freefn (cache->zh, entry->blobref, cache_entry_destroy);
     }
     return 0;
 }
 
 int cache_remove_entry (struct cache *cache, const char *ref)
 {
-    struct cache_entry *entry = zhash_lookup (cache->zh, ref);
+    struct cache_entry *entry = zhashx_lookup (cache->zhx, ref);
 
     if (entry
         && !entry->dirty
@@ -332,7 +330,7 @@ int cache_remove_entry (struct cache *cache, const char *ref)
             || !wait_queue_length (entry->waitlist_notdirty))
         && (!entry->waitlist_valid
             || !wait_queue_length (entry->waitlist_valid))) {
-        zhash_delete (cache->zh, ref);
+        zhashx_delete (cache->zhx, ref);
         return 1;
     }
     return 0;
@@ -340,7 +338,7 @@ int cache_remove_entry (struct cache *cache, const char *ref)
 
 int cache_count_entries (struct cache *cache)
 {
-    return zhash_size (cache->zh);
+    return zhashx_size (cache->zhx);
 }
 
 static int cache_entry_age (struct cache_entry *entry, int current_epoch)
@@ -354,29 +352,30 @@ static int cache_entry_age (struct cache_entry *entry, int current_epoch)
 
 int cache_expire_entries (struct cache *cache, int current_epoch, int thresh)
 {
-    zlist_t *keys;
+    zlistx_t *keys;
     char *ref;
     struct cache_entry *entry;
     int count = 0;
 
-    /* Do not use zhash_first()/zhash_next() or FOREACH_ZHASH, as
-     * zhash_delete() call below modifies hash */
-    if (!(keys = zhash_keys (cache->zh))) {
+    /* Do not use zhashx_first()/zhashx_next() or FOREACH_ZHASHX, as
+     * zhashx_delete() call below modifies hash */
+    if (!(keys = zhashx_keys (cache->zhx))) {
         errno = ENOMEM;
         return -1;
     }
-    while ((ref = zlist_pop (keys))) {
-        if ((entry = zhash_lookup (cache->zh, ref))
+    ref = zlistx_first (keys);
+    while (ref) {
+        if ((entry = zhashx_lookup (cache->zhx, ref))
             && !cache_entry_get_dirty (entry)
             && cache_entry_get_valid (entry)
             && (thresh == 0
                 || cache_entry_age (entry, current_epoch) > thresh)) {
-                zhash_delete (cache->zh, ref);
+                zhashx_delete (cache->zhx, ref);
                 count++;
         }
-        free (ref);
+        ref = zlistx_next (keys);
     }
-    zlist_destroy (&keys);
+    zlistx_destroy (&keys);
     return count;
 }
 
@@ -389,7 +388,7 @@ int cache_get_stats (struct cache *cache, tstat_t *ts, int *sizep,
     int incomplete = 0;
     int dirty = 0;
 
-    FOREACH_ZHASH (cache->zh, key, entry) {
+    FOREACH_ZHASHX (cache->zhx, key, entry) {
         if (cache_entry_get_valid (entry)) {
             int obj_size = 0;
 
@@ -419,7 +418,7 @@ int cache_wait_destroy_msg (struct cache *cache, wait_test_msg_f cb, void *arg)
     int n, count = 0;
     int rc = -1;
 
-    FOREACH_ZHASH (cache->zh, key, entry) {
+    FOREACH_ZHASHX (cache->zhx, key, entry) {
         if (entry->waitlist_valid) {
             if ((n = wait_destroy_msg (entry->waitlist_valid, cb, arg)) < 0)
                 goto done;
@@ -441,6 +440,13 @@ const char *cache_entry_get_blobref (struct cache_entry *entry)
     return entry ? entry->blobref : NULL;
 }
 
+static void cache_entry_destroy_wrapper (void **arg)
+{
+    struct cache_entry **entry = (struct cache_entry **)arg;
+    if (entry)
+        cache_entry_destroy (*entry);
+}
+
 struct cache *cache_create (void)
 {
     struct cache *cache = calloc (1, sizeof (*cache));
@@ -448,18 +454,22 @@ struct cache *cache_create (void)
         errno = ENOMEM;
         return NULL;
     }
-    if (!(cache->zh = zhash_new ())) {
+    if (!(cache->zhx = zhashx_new ())) {
         free (cache);
         errno = ENOMEM;
         return NULL;
     }
+    /* do not duplicate hash keys, use blobrefs stored in cache entry */
+    zhashx_set_key_destructor (cache->zhx, NULL);
+    zhashx_set_key_duplicator (cache->zhx, NULL);
+    zhashx_set_destructor (cache->zhx, cache_entry_destroy_wrapper);
     return cache;
 }
 
 void cache_destroy (struct cache *cache)
 {
     if (cache) {
-        zhash_destroy (&cache->zh);
+        zhashx_destroy (&cache->zhx);
         free (cache);
     }
 }
