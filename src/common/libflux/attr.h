781f29ca34ddb13b18af1193630a9267e91ca17b
Jim Garlick <garlick.jim@gmail.com>
2018-11-17 15:31:52 -0800

    libflux/attr: add header documentation

--- src/common/libflux/attr.h
+++ src/common/libflux/attr.h
@@ -25,14 +25,39 @@
 #ifndef _FLUX_CORE_ATTR_H
 #define _FLUX_CORE_ATTR_H
 
+/* broker attributes
+ *
+ * Brokers have configuration attributes.
+ * Values are local to a particular broker rank.
+ * Some may be overridden on the broker command line wtih -Sattr=val.
+ * The following commands are available for manipulating attributes
+ * on the running system:
+ *   flux lsattr [-v]
+ *   flux setattr name value
+ *   flux getattr name
+ * In additon, the following functions may be used to get/set broker
+ * attributes programmatically.
+ */
+
 #include "handle.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/* Get the value for attribute 'name' from the local broker.
+ * Returns value on success, NULL on failure with errno set.
+ * This function performs a synchronous RPC to the broker if the
+ * attribute is not found in cache, thus may block for the round-trip
+ * communication.
+ */
 const char *flux_attr_get (flux_t *h, const char *name);
 
+/* Set the value for attribute 'name' from the local broker.
+ * Returns value on success, NULL on failure with errno set.
+ * This function performs a synchronous RPC to the broker,
+ * thus blocks for the round-trip communication.
+ */
 int flux_attr_set (flux_t *h, const char *name, const char *val);
 
 #ifdef __cplusplus
