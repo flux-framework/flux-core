a8dee1e784d7655222356885eb4d19c2c8c00efe
Jim Garlick <garlick.jim@gmail.com>
2018-11-27 13:18:01 -0800

    testsuite: add module unit test

---
+++ src/common/libflux/test/module_fake1.c
@@ -0,0 +1,7 @@
+#include "src/common/libflux/module.h"
+
+int mod_main (int argc, char **argv)
+{
+     return 0;
+}
+MOD_NAME ("fake1");
---
+++ src/common/libflux/test/module_fake2.c
@@ -0,0 +1,8 @@
+#include "src/common/libflux/module.h"
+
+int mod_main (int argc, char **argv)
+{
+     return 0;
+}
+// leave undefined for unit test
+//MOD_NAME ("fake2");
