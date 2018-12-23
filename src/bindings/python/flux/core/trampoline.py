31d0cf32adf5d64a5080081cec3a5d218a6ff90b
Stephen Herbein <stephen272@gmail.com>
2018-12-22 16:33:16 -0800

    python/trampoline: cleanup debug prints

--- src/bindings/python/flux/core/trampoline.py
+++ src/bindings/python/flux/core/trampoline.py
@@ -4,11 +4,8 @@ from flux.core.handle import Flux
 
 
 def mod_main_trampoline(name, int_handle, args):
-    # print "trampoline entered"
     # generate a flux wrapper class instance from the handle
     flux_instance = Flux(handle=lib.unpack_long(int_handle))
-    # print "flux instance retrieved, loading:", name
-    # impo__import__('flux.modules.' + name)rt the user's module dynamically
     user_mod = None
     try:
         user_mod = importlib.import_module(
@@ -16,7 +13,6 @@ def mod_main_trampoline(name, int_handle, args):
     except ImportError:  # check user paths for the module
         user_mod = importlib.import_module(name)
 
-    # print "user module loaded:", name
     # call into mod_main with a flux class instance and the argument dict
     # it might be more pythonic to unpack the args as keyword/positional
     # arguments to this function, but I think this is cleaner for now
