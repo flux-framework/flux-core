690f6f6d2d2486721d52779b5a30dd6c6b9ad1f7
Jim Garlick <garlick.jim@gmail.com>
2019-01-02 11:46:33 -0800

    libidset: change license to LGPLv3

--- src/common/libidset/idset_private.h
+++ src/common/libidset/idset_private.h
@@ -1,3 +1,13 @@
+/************************************************************\
+ * Copyright 2018 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
 /* Implemented as a Van Emde Boas tree using code.google.com/p/libveb.
  * T.D is data; T.M is size
  * All ops are O(log m), for key bitsize m: 2^m == T.M.
