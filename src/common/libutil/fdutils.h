de947131b14e02f3e650b917c84c1c9b646c15b5
Jim Garlick <garlick.jim@gmail.com>
2019-01-02 16:02:51 -0800

    libutil: change license to LGPLv3
    Chagne all flux-original files (not vendored dependencies).

--- src/common/libutil/fdutils.h
+++ src/common/libutil/fdutils.h
@@ -1,3 +1,13 @@
+/************************************************************\
+ * Copyright 2018 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
 #ifndef _UTIL_FDUTILS_H
 #define _UTIL_FDUTILS_H 1
 
