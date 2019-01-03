79717a4df49a5bff83866f4912c1bde0aa6cb573
Jim Garlick <garlick.jim@gmail.com>
2019-01-02 16:52:00 -0800

    testsuite: change license to LGPLv3
    In many cases, add headers to source code
    that had none.

--- src/common/libsubprocess/test/test_fork_sleep.c
+++ src/common/libsubprocess/test/test_fork_sleep.c
@@ -1,3 +1,13 @@
+/************************************************************\
+ * Copyright 2014 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- t/scripts/cpus-allowed.lua
+++ t/scripts/cpus-allowed.lua
@@ -1,4 +1,15 @@
 #!/usr/bin/env lua
+
+-------------------------------------------------------------
+-- Copyright 2014 Lawrence Livermore National Security, LLC
+-- (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+--
+-- This file is part of the Flux resource manager framework.
+-- For details, see https://github.com/flux-framework.
+--
+-- SPDX-License-Identifier: LGPL-3.0
+-------------------------------------------------------------
+
 --
 --  Thin frontend to Lua affinity bindings, run cpu_set_t methods against
 --   the current cpumask and print the result.
