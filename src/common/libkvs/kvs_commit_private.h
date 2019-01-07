04cbf2f558f00c98670c9e7951795fbfd599702e
Albert Chu <chu11@llnl.gov>
2019-01-07 14:03:37 -0800

    common/libkvs: Add headers for private functions
    Add headers to access flux_kvs_lookup_ns() and flux_kvs_commit_ns().

---
+++ src/common/libkvs/kvs_commit_private.h
@@ -0,0 +1,23 @@
+/************************************************************\
+ * Copyright 2019 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef _KVS_COMMIT_PRIVATE_H
+#define _KVS_COMMIT_PRIVATE_H
+
+flux_future_t *flux_kvs_commit_ns (flux_t *h,
+                                   const char *namespace,
+                                   int flags,
+                                   flux_kvs_txn_t *txn);
+
+#endif /* !_KVS_COMMIT_PRIVATE_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
