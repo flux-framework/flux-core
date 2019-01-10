945781d54e5c1c355a819306709c20e2d45457a3
Albert Chu <chu11@llnl.gov>
2019-01-10 10:55:13 -0800

    common/libkvs: Remove kvs_namespace_prefix()

--- src/common/libkvs/test/kvs_util.c
+++ src/common/libkvs/test/kvs_util.c
@@ -64,84 +64,12 @@ void kvs_util_normalize_key_path_tests (void)
     free (s);
 }
 
-void kvs_namespace_prefix_tests (void)
-{
-    char *namespace_prefix, *key_suffix;
-
-    ok (kvs_namespace_prefix (NULL, NULL, NULL) == 0,
-        "kvs_namespace_prefix returns 0 on all NULL inputs");
-
-    ok (kvs_namespace_prefix ("foo", NULL, NULL) == 0,
-        "kvs_namespace_prefix returns 0 on just a keyname");
-
-    ok (kvs_namespace_prefix ("foo/bar", NULL, NULL) == 0,
-        "kvs_namespace_prefix returns 0 on key with just a slash");
-
-    ok (kvs_namespace_prefix ("ns:foo", NULL, NULL) < 0
-        && errno == EINVAL,
-        "kvs_namespace_prefix returns -1 on key with just a ns: and no slash");
-
-    ok (kvs_namespace_prefix ("ns:foo/", NULL, NULL) < 0
-        && errno == EINVAL,
-        "kvs_namespace_prefix returns -1 on key with only a namespace");
-
-    ok (kvs_namespace_prefix ("ns:/bar", NULL, NULL) < 0
-        && errno == EINVAL,
-        "kvs_namespace_prefix returns -1 on key without namespace between ns: and /");
-
-    ok (kvs_namespace_prefix ("ns:foo/ns:bar", NULL, NULL) < 0
-        && errno == EINVAL,
-        "kvs_namespace_prefix returns -1 on key with chained namespaces");
-
-    ok (kvs_namespace_prefix ("ns:foo/bar", &namespace_prefix, &key_suffix) == 1,
-        "kvs_namespace_prefix returns 1 on key with a namespace prefix");
-
-    ok (!strcmp (namespace_prefix, "foo"),
-        "kvs_namespace_prefix returned correct namespace");
-
-    ok (!strcmp (key_suffix, "bar"),
-        "kvs_key_suffix_parse returned correct key_suffix");
-
-    free (namespace_prefix);
-    free (key_suffix);
-
-    ok (kvs_namespace_prefix ("ns:baz/boo", &namespace_prefix, NULL) == 1,
-        "kvs_namespace_prefix returns 1, pass in only prefix pointer");
-
-    ok (!strcmp (namespace_prefix, "baz"),
-        "kvs_namespace_prefix returned correct namespace");
-
-    free (namespace_prefix);
-
-    ok (kvs_namespace_prefix ("ns:baz/boo", NULL, &key_suffix) == 1,
-        "kvs_namespace_prefix returns 1, pass in only prefix pointer");
-
-    ok (!strcmp (key_suffix, "boo"),
-        "kvs_key_suffix_parse returned correct key_suffix");
-
-    free (key_suffix);
-
-    ok (kvs_namespace_prefix ("ns:foo/.", &namespace_prefix, &key_suffix) == 1,
-        "kvs_namespace_prefix returns 1 on key with a namespace prefix and . suffix");
-
-    ok (!strcmp (namespace_prefix, "foo"),
-        "kvs_namespace_prefix returned correct namespace");
-
-    ok (!strcmp (key_suffix, "."),
-        "kvs_key_suffix_parse returned correct key_suffix");
-
-    free (namespace_prefix);
-    free (key_suffix);
-}
-
-
 int main (int argc, char *argv[])
 {
 
     plan (NO_PLAN);
 
     kvs_util_normalize_key_path_tests ();
-    kvs_namespace_prefix_tests ();
 
     done_testing ();
     return (0);
