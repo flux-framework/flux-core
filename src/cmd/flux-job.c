d60f31e4f037cb2bc3a228563fdaa464574fac05
Jim Garlick <garlick.jim@gmail.com>
2019-01-11 10:17:37 -0800

    testsuite: split submitbench out to test prog
    Problem: the "submitbench" subcommand of flux job
    does not really belong in a user-facing program.
    
    Create a standalone, noinst program that contains
    the ingest throughput test, then rename flux job
    submitbench to flux job submit and simplify it so
    that it only handles one job submission at a time.
    
    In flux job submit, if no jobspec argument is present,
    presume that it should be read from stdin, so
    "flux job submit" is equivalent to  "flux job submit -".
    
    Convert sharness tests to call either submitbench
    or flux job submit as appropriate.
    
    Cleanup:  now that travis-ci is starting the munge
    daemon to support the default signing mech in
    flux-security, there is no need to force all jobs
    submitted in test to use --sign-type=none when
    configured with --flux-security.

--- src/cmd/flux-job.c
+++ src/cmd/flux-job.c
@@ -8,6 +8,8 @@
  * SPDX-License-Identifier: LGPL-3.0
 \************************************************************/
 
+/* "plumbing" commands (see git(1)) for Flux job management */
+
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -19,6 +21,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <assert.h>
 #include <jansson.h>
 #include <flux/core.h>
 #include <flux/optparse.h>
@@ -31,7 +34,7 @@
 #include "src/common/libutil/read_all.h"
 
 int cmd_list (optparse_t *p, int argc, char **argv);
-int cmd_submitbench (optparse_t *p, int argc, char **argv);
+int cmd_submit (optparse_t *p, int argc, char **argv);
 int cmd_id (optparse_t *p, int argc, char **argv);
 int cmd_purge (optparse_t *p, int argc, char **argv);
 int cmd_priority (optparse_t *p, int argc, char **argv);
@@ -50,20 +53,11 @@ static struct optparse_option list_opts[] =  {
     OPTPARSE_TABLE_END
 };
 
-static struct optparse_option submitbench_opts[] =  {
-    { .name = "repeat", .key = 'r', .has_arg = 1, .arginfo = "N",
-      .usage = "Run N instances of jobspec",
-    },
-    { .name = "fanout", .key = 'f', .has_arg = 1, .arginfo = "N",
-      .usage = "Run at most N RPCs in parallel",
-    },
+static struct optparse_option submit_opts[] =  {
     { .name = "priority", .key = 'p', .has_arg = 1, .arginfo = "N",
       .usage = "Set job priority (0-31, default=16)",
     },
 #if HAVE_FLUX_SECURITY
-    { .name = "reuse-signature", .key = 'R', .has_arg = 0,
-      .usage = "Sign jobspec once and reuse the result for multiple RPCs",
-    },
     { .name = "security-config", .key = 'c', .has_arg = 1, .arginfo = "pattern",
       .usage = "Use non-default security config glob",
     },
@@ -108,12 +102,12 @@ static struct optparse_subcommand subcommands[] = {
       0,
       NULL,
     },
-    { "submitbench",
-      "[OPTIONS] jobspec",
-      "Run job(s)",
-      cmd_submitbench,
+    { "submit",
+      "[OPTIONS] [jobspec]",
+      "Run job",
+      cmd_submit,
       0,
-      submitbench_opts
+      submit_opts
     },
     { "id",
       "[OPTIONS] [id ...]",
@@ -342,27 +336,6 @@ int cmd_list (optparse_t *p, int argc, char **argv)
    return (0);
 }
 
-struct submitbench_ctx {
-    flux_t *h;
-#if HAVE_FLUX_SECURITY
-    flux_security_t *sec;
-    const char *sign_type;
-#endif
-    int flags;
-    flux_watcher_t *prep;
-    flux_watcher_t *check;
-    flux_watcher_t *idle;
-    int txcount;
-    int rxcount;
-    int totcount;
-    int max_queue_depth;
-    optparse_t *p;
-    void *jobspec;
-    int jobspecsz;
-    const char *J;
-    int priority;
-};
-
 /* Read entire file 'name' ("-" for stdin).  Exit program on error.
  */
 size_t read_jobspec (const char *name, void **bufp)
@@ -385,16 +358,60 @@ size_t read_jobspec (const char *name, void **bufp)
     return size;
 }
 
-/* handle RPC response
- * Once all responses are received, stop prep/check watchers
- * so reactor will stop.
- */
-void submitbench_continuation (flux_future_t *f, void *arg)
+int cmd_submit (optparse_t *p, int argc, char **argv)
 {
-    struct submitbench_ctx *ctx = arg;
+    flux_t *h;
+#if HAVE_FLUX_SECURITY
+    flux_security_t *sec = NULL;
+    const char *sec_config;
+    const char *sign_type;
+#endif
+    int flags = 0;
+    void *jobspec;
+    int jobspecsz;
+    const char *J = NULL;
+    int priority;
+    int optindex = optparse_option_index (p);
+    flux_future_t *f;
     flux_jobid_t id;
     const char *errmsg;
+    const char *input = "-";
 
+    if (optindex != argc - 1 && optindex != argc) {
+        optparse_print_usage (p);
+        exit (1);
+    }
+    if (optindex < argc)
+        input = argv[optindex++];
+#if HAVE_FLUX_SECURITY
+    /* If any non-default security options are specified, create security
+     * context so jobspec can be pre-signed before submission.
+     */
+    if (optparse_hasopt (p, "security-config")
+                            || optparse_hasopt (p, "sign-type")) {
+        sec_config = optparse_get_str (p, "security-config", NULL);
+        if (!(sec = flux_security_create (0)))
+            log_err_exit ("security");
+        if (flux_security_configure (sec, sec_config) < 0)
+            log_err_exit ("security config %s", flux_security_last_error (sec));
+        sign_type = optparse_get_str (p, "sign-type", NULL);
+    }
+#endif
+    if (!(h = flux_open (NULL, 0)))
+        log_err_exit ("flux_open");
+    jobspecsz = read_jobspec (input, &jobspec);
+    assert (((char *)jobspec)[jobspecsz] == '\0');
+    priority = optparse_get_int (p, "priority", FLUX_JOB_PRIORITY_DEFAULT);
+
+#if HAVE_FLUX_SECURITY
+    if (sec) {
+        if (!(J = flux_sign_wrap (sec, jobspec, jobspecsz, sign_type, 0)))
+            log_err_exit ("flux_sign_wrap: %s", flux_security_last_error (sec));
+        flags |= FLUX_JOB_PRE_SIGNED;
+    }
+#endif
+    if (!(f = flux_job_submit (h, J ? J : jobspec, priority, flags)))
+        log_err_exit ("flux_job_submit");
     if (flux_job_submit_get_id (f, &id) < 0) {
         if ((errmsg = flux_future_error_string (f)))
             log_msg_exit ("submit: %s", errmsg);
@@ -405,116 +422,11 @@ void submitbench_continuation (flux_future_t *f, void *arg)
     }
     printf ("%llu\n", (unsigned long long)id);
     flux_future_destroy (f);
-
-    ctx->rxcount++;
-}
-
-/* prep - called before event loop would block
- * Prevent loop from blocking if 'check' could send RPCs.
- * Stop the prep/check watchers if RPCs have all been sent,
- * so that, once responses are received, the reactor will exit naturally.
- */
-void submitbench_prep (flux_reactor_t *r, flux_watcher_t *w,
-                       int revents, void *arg)
-{
-    struct submitbench_ctx *ctx = arg;
-
-    if (ctx->txcount == ctx->totcount) {
-        flux_watcher_stop (ctx->prep);
-        flux_watcher_stop (ctx->check);
-    }
-    else if ((ctx->txcount - ctx->rxcount) < ctx->max_queue_depth)
-        flux_watcher_start (ctx->idle); // keeps loop from blocking
-}
-
-/* check - called after event loop unblocks
- * If there are RPCs to send, send one.
- */
-void submitbench_check (flux_reactor_t *r, flux_watcher_t *w,
-                     int revents, void *arg)
-{
-    struct submitbench_ctx *ctx = arg;
-    int flags = ctx->flags;
-
-    flux_watcher_stop (ctx->idle);
-    if (ctx->txcount < ctx->totcount
-                    && (ctx->txcount - ctx->rxcount) < ctx->max_queue_depth) {
-        flux_future_t *f;
 #if HAVE_FLUX_SECURITY
-        if (ctx->sec) {
-            if (!ctx->J || !optparse_hasopt (ctx->p, "reuse-signature")) {
-                if (!(ctx->J = flux_sign_wrap (ctx->sec, ctx->jobspec,
-                                               ctx->jobspecsz,
-                                               ctx->sign_type, 0)))
-                    log_err_exit ("flux_sign_wrap: %s",
-                                  flux_security_last_error (ctx->sec));
-            }
-            flags |= FLUX_JOB_PRE_SIGNED;
-        }
+    flux_security_destroy (sec); // invalidates J
 #endif
-        if (!(f = flux_job_submit (ctx->h, ctx->J ? ctx->J : ctx->jobspec,
-                                   ctx->priority, flags)))
-            log_err_exit ("flux_job_submit");
-        if (flux_future_then (f, -1., submitbench_continuation, ctx) < 0)
-            log_err_exit ("flux_future_then");
-        ctx->txcount++;
-    }
-}
-
-int cmd_submitbench (optparse_t *p, int argc, char **argv)
-{
-    flux_reactor_t *r;
-    int optindex = optparse_option_index (p);
-    struct submitbench_ctx ctx;
-
-    memset (&ctx, 0, sizeof (ctx));
-
-    if (optindex != argc - 1) {
-        optparse_print_usage (p);
-        exit (1);
-    }
-#if HAVE_FLUX_SECURITY
-    /* If any non-default security options are specified, create security
-     * context so jobspec can be pre-signed before submission.
-     */
-    if (optparse_hasopt (p, "security-config")
-                            || optparse_hasopt (p, "reuse-signature")
-                            || optparse_hasopt (p, "sign-type")) {
-        const char *sec_config = optparse_get_str (p, "security-config", NULL);
-        if (!(ctx.sec = flux_security_create (0)))
-            log_err_exit ("security");
-        if (flux_security_configure (ctx.sec, sec_config) < 0)
-            log_err_exit ("security config %s", flux_security_last_error (ctx.sec));
-        ctx.sign_type = optparse_get_str (p, "sign-type", NULL);
-    }
-#endif
-    if (!(ctx.h = flux_open (NULL, 0)))
-        log_err_exit ("flux_open");
-    r = flux_get_reactor (ctx.h);
-    ctx.p = p;
-    ctx.max_queue_depth = optparse_get_int (p, "fanout", 256);
-    ctx.totcount = optparse_get_int (p, "repeat", 1);
-    ctx.jobspecsz = read_jobspec (argv[optindex++], &ctx.jobspec);
-    ctx.priority = optparse_get_int (p, "priority", FLUX_JOB_PRIORITY_DEFAULT);
-
-    /* Prep/check/idle watchers perform flow control, keeping
-     * at most ctx.max_queue_depth RPCs outstanding.
-     */
-    ctx.prep = flux_prepare_watcher_create (r, submitbench_prep, &ctx);
-    ctx.check = flux_check_watcher_create (r, submitbench_check, &ctx);
-    ctx.idle = flux_idle_watcher_create (r, NULL, NULL);
-    if (!ctx.prep || !ctx.check || !ctx.idle)
-        log_err_exit ("flux_watcher_create");
-    flux_watcher_start (ctx.prep);
-    flux_watcher_start (ctx.check);
-
-    if (flux_reactor_run (r, 0) < 0)
-        log_err_exit ("flux_reactor_run");
-#if HAVE_FLUX_SECURITY
-    flux_security_destroy (ctx.sec); // invalidates ctx.J
-#endif
-    flux_close (ctx.h);
-    free (ctx.jobspec);
+    flux_close (h);
+    free (jobspec);
     return 0;
 }
 
