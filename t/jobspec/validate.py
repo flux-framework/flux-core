b0bfc4b5d4be95f7233d5fe61d55b88f0c3e23a5
Stephen Herbein <stephen272@gmail.com>
2019-01-14 10:10:09 -0600

    validate: support reading jobspec from stdin
    split `validate_input` into `validate_input` and `validate_inputfile` to
    support validating both files on the filesystem and file-like
    objects (i.e., stdin)
    
    Two minor refactors include:
    
    Use `.format` to ensure that the string representations of the objects
    are formatted and printed properly. Previously was erroring out with:
    `TypeError: cannot concatenate 'str' and 'exceptions.ValueError'
    objects`.
    
    ensure files are closed by using a context manager

--- t/jobspec/validate.py
+++ t/jobspec/validate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Usage: validate.py --schema=jobspec.json data.json [data.json ...]
+# Usage: cat data.json | validate.py --schema=jobspec.json
 
 import sys
 import argparse
@@ -9,43 +10,56 @@ import yaml
 import jsonschema
 
 
-def validate_input(filename, schema):
-    f = open(filename)
-    data = yaml.safe_load(f)
-    jsonschema.validate(data, schema)
+def validate_input(jobspec_stream, schema, label=""):
+    errors = 0
+    try:
+        data = yaml.safe_load(jobspec_stream)
+        jsonschema.validate(data, schema)
+    except (yaml.YAMLError) as e:
+        print("{}: {}".format(label, e.problem))
+        errors = errors + 1
+    except (Exception) as e:
+        print("{}: {}".format(label, e.message))
+        errors = errors + 1
+    except:
+        print("{}: unknown error".format(label))
+        errors = errors + 1
+    else:
+        print("{}: ok".format(label))
+    return errors
+
+
+def validate_inputfile(infile, schema):
+    errors = 0
+    try:
+        with open(infile) as fd:
+            errors += validate_input(fd, schema, label=infile)
+    except (OSError, IOError) as e:
+        print("{}: {}".format(infile, e.strerror))
+        errors = errors + 1
+    return errors
 
 
 # parse command line args
 parser = argparse.ArgumentParser()
 parser.add_argument("--schema", "-s", type=str, required=True)
-args, unknown = parser.parse_known_args()
+parser.add_argument("jobspecs", nargs="*")
+args = parser.parse_args()
 
 # Parse json-schema file (JSON format)
 try:
     schema = json.load(open(args.schema))
 except (OSError, IOError) as e:
-    sys.exit(args.schema + ": " + e.strerror)
+    sys.exit("{}: {}".format(args.schema, e.strerror))
 except:
-    sys.exit(args.schema + ": unknown error")
+    sys.exit("{}: unknown error".format(args.schema))
 
 # Validate each file on command line
 errors = 0
-for infile in unknown:
-    try:
-        validate_input(infile, schema)
-    except (OSError, IOError) as e:
-        print(infile + ": " + e.strerror)
-        errors = errors + 1
-    except (yaml.YAMLError) as e:
-        print(infile + ": " + e.problem)
-        errors = errors + 1
-    except (Exception) as e:
-        print(infile + ": " + e.message)
-        errors = errors + 1
-    except:
-        print(infile + ": unknown error")
-        errors = errors + 1
-    else:
-        print(infile + ": ok")
+if args.jobspecs:
+    for infile in args.jobspecs:
+        errors += validate_inputfile(infile, schema)
+else:
+    errors += validate_input(sys.stdin, schema, label="stdin")
 
 sys.exit(errors)
