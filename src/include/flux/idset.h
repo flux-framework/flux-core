b600a05c0bd3702e9ad94aa88321bb8148663875
Mark A. Grondona <mark.grondona@gmail.com>
2019-02-13 19:44:43 -0800

    build: add missing header wrapper <flux/idset.h>
    Problem: Source can't include idset.h header the same way they would
    out-of-tree, i.e. <flux/idset>
    
    Add the idset.h "wrapper" in src/include/flux/idset.h to match what
    is done for optparse.h.
    
    Fixes #2007

---
+++ src/include/flux/idset.h
@@ -0,0 +1,14 @@
+/************************************************************\
+ * Copyright 2014 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+/* Allow in-tree programs to #include <flux/idset.h> like out-of-tree would.
+ */
+
+#include "src/common/libidset/idset.h"
