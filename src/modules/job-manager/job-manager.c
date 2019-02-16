ab06c925b0a72ee6412c1a3a6d2f57b64267ac27
Jim Garlick <garlick.jim@gmail.com>
2019-02-15 16:11:19 -0800

    job-manager: handle partial submit failure
    Problem: jobs are submitted in batches to the job manager.
    If one job fails to be added, all the jobs fail.  However,
    when there is a failure, some jobs may be left in the queue
    after their users were told they failed.
    
    Keep a list of newly added jobs.  On failure, walk the
    list and dequeue the jobs.

--- src/modules/job-manager/job-manager.c
+++ src/modules/job-manager/job-manager.c
@@ -11,6 +11,7 @@
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include <czmq.h>
 #include <jansson.h>
 #include <flux/core.h>
 
@@ -29,6 +30,59 @@ struct job_manager_ctx {
     struct queue *queue;
 };
 
+/* Enqueue jobs from 'jobs' array in queue.
+ * On success, return a list of struct job's.
+ * On failure, return NULL with errno set (no jobs enqueued).
+ */
+zlist_t *enqueue_jobs (struct queue *queue, json_t *jobs)
+{
+    size_t index;
+    json_t *el;
+    zlist_t *newjobs;
+    struct job *job;
+    int saved_errno;
+
+    if (!(newjobs = zlist_new ()))
+        goto error;
+    json_array_foreach (jobs, index, el) {
+        flux_jobid_t id;
+        uint32_t userid;
+        int priority;
+        double t_submit;
+
+        if (json_unpack (el, "{s:I s:i s:i s:f}", "id", &id,
+                                                  "priority", &priority,
+                                                  "userid", &userid,
+                                                  "t_submit", &t_submit) < 0) {
+            goto error;
+        }
+        if (!(job = job_create (id, priority, userid, t_submit)))
+            goto error;
+        if (queue_insert (queue, job, &job->queue_handle) < 0) {
+            job_decref (job);
+            if (errno == EEXIST)
+                continue; // don't report error - might be a race with restart
+            goto error;
+        }
+        if (zlist_push (newjobs, job) < 0) {
+            queue_delete (queue, job, job->queue_handle);
+            job_decref (job);
+            errno = ENOMEM;
+            goto error;
+        }
+    }
+    return newjobs;
+error:
+    saved_errno = errno;
+    while ((job = zlist_pop (newjobs))) {
+        queue_delete (queue, job, job->queue_handle);
+        job_decref (job);
+    }
+    zlist_destroy (&newjobs);
+    errno = saved_errno;
+    return NULL;
+}
+
 /* handle submit request (from job-ingest module)
  * This is a batched request for one or more jobs already validated
  * by the ingest module, and already instantiated in the KVS.
@@ -39,46 +93,29 @@ static void submit_cb (flux_t *h, flux_msg_handler_t *mh,
 {
     struct job_manager_ctx *ctx = arg;
     json_t *jobs;
-    size_t index;
-    json_t *el;
+    zlist_t *newjobs;
+    struct job *job;
 
     if (flux_request_unpack (msg, NULL, "{s:o}", "jobs", &jobs) < 0) {
         flux_log_error (h, "%s", __FUNCTION__);
         goto error;
     }
-    json_array_foreach (jobs, index, el) {
-        flux_jobid_t id;
-        uint32_t userid;
-        int priority;
-        double t_submit;
-        struct job *job = NULL;
-
-        if (json_unpack (el, "{s:I s:i s:i s:f}", "id", &id,
-                                                  "priority", &priority,
-                                                  "userid", &userid,
-                                                  "t_submit", &t_submit) < 0) {
-            flux_log (h, LOG_ERR, "%s: error decoding job index %u",
-                      __FUNCTION__, (unsigned int)index);
-            goto error;
-        }
-        if (!(job = job_create (id, priority, userid, t_submit)))
-            goto error;
-        /* N.B. ignore EEXIST, in case restart_from_kvs() loaded a job
-         * while its submit request was in still flight.
-         */
-        if (queue_insert (ctx->queue, job, &job->queue_handle) < 0
-                                                        && errno != EEXIST) {
-            flux_log_error (h, "%s: queue_insert %llu",
-                            __FUNCTION__, (unsigned long long)id);
-            job_decref (job);
-            goto error;
-        }
-        job_decref (job);
+    if (!(newjobs = enqueue_jobs (ctx->queue, jobs))) {
+        flux_log_error (h, "%s: error enqueuing batch", __FUNCTION__);
+        goto error;
     }
     if (flux_respond (h, msg, 0, NULL) < 0)
         flux_log_error (h, "%s: flux_respond", __FUNCTION__);
-    flux_log (h, LOG_DEBUG, "%s: added %u jobs", __FUNCTION__,
-                            (unsigned int)index);
+    flux_log (h, LOG_DEBUG, "%s: added %d jobs", __FUNCTION__,
+                            (int)zlist_size (newjobs));
+    /* Submitting user is being responded to with jobid's.
+     * Now walk the list of new jobs and advance their state.
+     */
+    while ((job = zlist_pop (newjobs))) {
+        /* FILL IN */
+        job_decref (job);
+    }
+    zlist_destroy (&newjobs);
     return;
 error:
     if (flux_respond_error (h, msg, errno, NULL) < 0)
