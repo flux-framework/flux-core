d2b04419f406ad11941bfbd116b85bcaca9dcb77
Jim Garlick <garlick.jim@gmail.com>
2019-02-17 16:02:48 -0800

    modules/job-manager: restart using new job constructor
    Change the restart logic to use the newly added
    flux_job_create_from_eventlog() constructor to
    recreate in-memory job state (soley) from the job's
    eventlog in the KVS.
    
    A side benefit fo this cleanup is dropping two synchronous
    KVS lookups per job to fetch priority and userid, which
    which should speed up restart.
    
    It also allows the one-off context parsing code in
    restart.c to be dropped.

--- src/modules/job-manager/restart.c
+++ src/modules/job-manager/restart.c
@@ -22,36 +22,7 @@
 
 #include "job.h"
 #include "restart.h"
-
-/* Parse severity (0-7) from exception event context.
- * Returns severity on success, -1 on failure.
- */
-int restart_decode_exception_severity (const char *s)
-{
-    char *argz = NULL;
-    size_t argz_len = 0;
-    char *sevstr;
-    int severity;
-    char *endptr;
-
-    if (argz_create_sep (s, ' ', &argz, &argz_len) != 0)
-        return -1;
-    if (!(sevstr = envz_get (argz, argz_len, "severity")))
-        goto error;
-    errno = 0;
-    severity = strtol (sevstr, &endptr, 10);
-    if (errno != 0)
-        goto error;
-    if (*endptr != '\0')
-        goto error;
-    if (severity < 0 || severity > 7)
-        goto error;
-    free (argz);
-    return severity;
-error:
-    free (argz);
-    return -1;
-}
+#include "util.h"
 
 int restart_count_char (const char *s, char c)
 {
@@ -61,45 +32,6 @@ int restart_count_char (const char *s, char c)
             count++;
     }
     return count;
-}
-
-int restart_replay_eventlog (const char *s, double *t_submit,
-                             int *flagsp, int *statep)
-{
-    struct flux_kvs_eventlog *eventlog;
-    const char *event;
-    char name[FLUX_KVS_MAX_EVENT_NAME + 1];
-    char context[FLUX_KVS_MAX_EVENT_CONTEXT + 1];
-    double t;
-    int flags = 0;
-    int state = FLUX_JOB_NEW;
-
-    if (!(eventlog = flux_kvs_eventlog_decode (s)))
-        return -1;
-    if (!(event = flux_kvs_eventlog_first (eventlog)))
-        goto error_inval;
-    if (flux_kvs_event_decode (event, &t, name, sizeof (name), NULL, 0) < 0)
-        goto error;
-    if (strcmp (name, "submit") != 0)
-        goto error_inval;
-    while ((event = flux_kvs_eventlog_next (eventlog))) {
-        if (flux_kvs_event_decode (event, NULL, name, sizeof (name),
-                                   context, sizeof (context)) < 0)
-            goto error;
-        if (!strcmp (name, "exception")
-                    && restart_decode_exception_severity (context) == 0)
-            state = FLUX_JOB_CLEANUP;
-    }
-    *t_submit = t;
-    *flagsp = flags;
-    *statep = state;
-    flux_kvs_eventlog_destroy (eventlog);
-    return 0;
-error_inval:
-    errno = EINVAL;
-error:
-    flux_kvs_eventlog_destroy (eventlog);
-    return -1;
 }
 
 static flux_future_t *lookup_job_attr (flux_t *h, const char *jobdir,
@@ -118,14 +50,10 @@ static int depthfirst_map_one (flux_t *h, const char *key, int dirskip,
                                restart_map_f cb, void *arg)
 {
     flux_jobid_t id;
-    flux_future_t *f = NULL;
-    uint32_t userid;
-    int priority;
+    flux_future_t *f;
     const char *eventlog;
-    struct job *job;
-    double t_submit;
-    int flags;
-    int state;
+    struct job *job = NULL;
+    int rc = -1;
 
     if (strlen (key) <= dirskip) {
         errno = EINVAL;
@@ -133,43 +61,19 @@ static int depthfirst_map_one (flux_t *h, const char *key, int dirskip,
     }
     if (fluid_decode (key + dirskip + 1, &id, FLUID_STRING_DOTHEX) < 0)
         return -1;
-    /* userid */
-    if (!(f = lookup_job_attr (h, key, "userid")))
-        goto error;
-    if (flux_kvs_lookup_get_unpack (f, "i", &userid) < 0)
-        goto error_future;
-    flux_future_destroy (f);
-
-    /* priority */
-    if (!(f = lookup_job_attr (h, key, "priority")))
-        goto error;
-    if (flux_kvs_lookup_get_unpack (f, "i", &priority) < 0)
-        goto error_future;
-    flux_future_destroy (f);
-
-    /* get t_submit, flags, state from eventlog) */
     if (!(f = lookup_job_attr (h, key, "eventlog")))
-        goto error;
+        goto done;
     if (flux_kvs_lookup_get (f, &eventlog) < 0)
-        goto error_future;
-    if (restart_replay_eventlog (eventlog, &t_submit, &flags, &state) < 0)
-        goto error_future;
+        goto done;
+    if (!(job = job_create_from_eventlog (id, eventlog)))
+        goto done;
+    if (cb (job, arg) < 0)
+        goto done;
+    rc = 1;
+done:
     flux_future_destroy (f);
-
-    /* make callback */
-    if (!(job = job_create (id, priority, userid, t_submit, flags)))
-        goto error;
-    job->state = state;
-    if (cb (job, arg) < 0) {
-        job_decref (job);
-        goto error;
-    }
     job_decref (job);
-    return 1;
-error_future:
-    flux_future_destroy (f);
-error:
-    return -1;
+    return rc;
 }
 
 static int depthfirst_map (flux_t *h, const char *key,
