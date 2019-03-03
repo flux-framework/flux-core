cacde0b7437f87b7715d663e340038714a40e95c
Jim Garlick <garlick.jim@gmail.com>
2019-03-02 16:20:07 -0800

    job-manager: add event batcher
    Problem: several subsystems in the job-manager independently
    commit events to the job KVS eventlog, duplicating code and
    missing an opportunity to batch temporally close commits
    together for reduced overhead.
    
    Add an new eventlog update mechanism, similar to the KVS update
    batching in job-ingest.  Accept updates via two functions:
    event_log(), and a varargs version, event_log_fmt(). These
    functions add to a KVS transaction that grows in memory until
    a timer expires, then the transaction is committed in one RPC.
    
    Callbacks can optionally be registered with each event.
    The callbacks are called upon completion of the transaction,
    with access to the future used for the commit, so they can
    determine whether it succeeded or failed.
    
    A hardwired tunable, initially set to 10ms, determines how long
    a batched transaction waits until it is flushed.  This value
    may be tuned as we gain experience with the system.
    
    If job-manager is trying to unload with commits in flight, block
    until those complete, and invoke any registered callbacks before
    tearing down.

--- src/modules/job-manager/job-manager.c
+++ src/modules/job-manager/job-manager.c
@@ -23,6 +23,7 @@
 #include "list.h"
 #include "priority.h"
 #include "alloc.h"
+#include "event.h"
 
 
 struct job_manager_ctx {
@@ -30,6 +31,7 @@ struct job_manager_ctx {
     flux_msg_handler_t **handlers;
     struct queue *queue;
     struct alloc_ctx *alloc_ctx;
+    struct event_ctx *event_ctx;
 };
 
 /* Enqueue jobs from 'jobs' array in queue.
@@ -216,6 +218,10 @@ int mod_main (flux_t *h, int argc, char **argv)
         flux_log_error (h, "error creating scheduler interface");
         goto done;
     }
+    if (!(ctx.event_ctx = event_ctx_create (h))) {
+        flux_log_error (h, "error creating event batcher");
+        goto done;
+    }
     if (flux_msg_handler_addvec (h, htab, &ctx, &ctx.handlers) < 0) {
         flux_log_error (h, "flux_msghandler_add");
         goto done;
@@ -232,6 +238,7 @@ int mod_main (flux_t *h, int argc, char **argv)
 done:
     flux_msg_handler_delvec (ctx.handlers);
     alloc_ctx_destroy (ctx.alloc_ctx);
+    event_ctx_destroy (ctx.event_ctx);
     queue_destroy (ctx.queue);
     return rc;
 }
