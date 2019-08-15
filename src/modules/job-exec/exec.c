9773dde88244893577151e0809ad237c69269ffa
Mark A. Grondona <mark.grondona@gmail.com>
2019-08-15 13:33:23 -0700

    job-exec: add a timeout for exec_kill
    Add a timeout of 3s for the future returned by bulk_exec_kill.
    
    This prevents leaking future if all kill requests do not receive
    a response for some reason. Additionally, this will generate a
    timeout error for an exceptional condition.

--- src/modules/job-exec/exec.c
+++ src/modules/job-exec/exec.c
@@ -273,7 +273,7 @@ static int exec_kill (struct jobinfo *job, int signum)
               signum);
 
     jobinfo_incref (job);
-    if (flux_future_then (f, -1., exec_kill_cb, job) < 0) {
+    if (flux_future_then (f, 3., exec_kill_cb, job) < 0) {
         flux_log_error (job->h, "%ju: exec_kill: flux_future_then", job->id);
         flux_future_destroy (f);
         return -1;
