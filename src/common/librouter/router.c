7b528559140874c0e3b3f010ad7ae58b4902f933
Jim Garlick <garlick.jim@gmail.com>
2019-08-31 09:43:47 -0700

    librouter/router: add router class
    The router class represents a set of connected clients.  It maintains
    a routing table (hash by UUID) that tracks clients, including their
    subscriptions, registered services, and used services.  Each client
    has its own callback for sending messages to it, to support the possibility
    of one router handling multiple connector types.
    
    Messages from clients enter the router via the router_entry_recv()
    function, and are forwarded to the broker as appropriate.
    In addition, the router directly handles the following requests
    from clients:
    
    local.sub, local.unsub
        The local connector (client side) sends this request
        in its implementations of the connector op->event_subscribe()
        and op->event_unsubscribe().  A subhash is used to manage
        event subscriptions in the router.
    
    service.add, service.remove
        Dynamic service registration is performed on behalf of a client.
        On success, a request to the service name will be forwarded to
        the client that requested the registration.
    
    The router establishes message handlers on its broker-facing
    flux_t handle for events, requests, and responses, and performs routing
    to clients based on event subscriptions, dynamically registered services,
    and matching client UUIDs to the top of a response route stack,
    respectively, as described in RFC 3.
    
    A disconnect hash triggers the sending of disconnect messages upon
    client disconnect, as described in RFC 6.

---
+++ src/common/librouter/router.c
@@ -0,0 +1,498 @@
+/************************************************************\
+ * Copyright 2019 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdlib.h>
+#include <czmq.h>
+#include <flux/core.h>
+
+#include "src/common/libutil/errno_safe.h"
+
+#include "router.h"
+#include "subhash.h"
+#include "servhash.h"
+#include "disconnect.h"
+
+struct router_entry {
+    char *uuid;
+    router_entry_send_f send;
+    void *arg;
+    struct router *rtr;
+    struct subhash *subscriptions;  // client's subscriber hash
+    struct disconnect *dcon;
+};
+
+struct router {
+    flux_t *h;
+    zhashx_t *routes;               // uuid => 'struct router_entry'
+    void *arg;
+    struct subhash *subscriptions;  // router's subscriber hash
+    struct servhash *services;
+    flux_msg_handler_t **handlers;
+    bool mute;
+};
+
+/* Generate internal response and 'msg' (success or failure only,
+ * no payload) and send it to the client represented by 'entry'.
+ */
+static void router_entry_respond (struct router_entry *entry,
+                                  const flux_msg_t *msg,
+                                  int errnum)
+{
+    struct router *rtr = entry->rtr;
+    const char *topic;
+    flux_msg_t *rmsg;
+    uint32_t matchtag;
+
+    if (flux_msg_get_topic (msg, &topic) < 0)
+        return;
+    if (flux_msg_get_matchtag (msg, &matchtag) < 0)
+        return;
+    if (errnum)
+        rmsg = flux_response_encode_error (topic, errnum, NULL);
+    else
+        rmsg = flux_response_encode (topic, NULL);
+    if (!rmsg)
+        goto done;
+    if (flux_msg_set_rolemask (rmsg, FLUX_ROLE_OWNER) < 0)
+        goto done;
+    if (flux_msg_set_matchtag (rmsg, matchtag) < 0)
+        goto done;
+    if (entry->send (rmsg, entry->arg) < 0) {
+        if (errno != EPIPE && errnum != ECONNRESET) {
+            flux_log_error (rtr->h,
+                            "router: response > client=%.5s",
+                            entry->uuid);
+        }
+        goto done;
+    }
+done:
+    flux_msg_destroy (rmsg);
+}
+
+/* servhash respond_f footprint */
+static void router_entry_respond_byuuid (const flux_msg_t *msg,
+                                         const char *uuid,
+                                         int errnum,
+                                         void *arg)
+{
+    struct router *rtr = arg;
+    struct router_entry *entry;
+
+    if ((entry = zhashx_lookup (rtr->routes, uuid)))
+        router_entry_respond (entry, msg, errnum);
+}
+
+/* Handle internal local.sub request.
+ */
+static void local_sub_request (struct router_entry *entry, flux_msg_t *msg)
+{
+    const char *topic;
+
+    if (flux_request_unpack (msg, NULL, "{s:s}", "topic", &topic) < 0)
+        goto error;
+    if (subhash_subscribe (entry->subscriptions, topic) < 0)
+        goto error;
+    router_entry_respond (entry, msg, 0);
+    return;
+error:
+    router_entry_respond (entry, msg, errno);
+}
+
+/* Handle internal local.unsub request.
+ */
+static void local_unsub_request (struct router_entry *entry, flux_msg_t *msg)
+{
+    const char *topic;
+
+    if (flux_request_unpack (msg, NULL, "{s:s}", "topic", &topic) < 0)
+        goto error;
+    if (subhash_unsubscribe (entry->subscriptions, topic) < 0)
+        goto error;
+    router_entry_respond (entry, msg, 0);
+    return;
+error:
+    router_entry_respond (entry, msg, errno);
+}
+
+/* Handle internal service.add request.
+ */
+static void service_add_request (struct router_entry *entry, flux_msg_t *msg)
+{
+    struct router *rtr = entry->rtr;
+    const char *name;
+
+    if (flux_request_unpack (msg, NULL, "{s:s}", "service", &name) < 0)
+        goto error;
+    if (servhash_add (rtr->services, name, entry->uuid, msg) < 0)
+        goto error;
+    return;
+error:
+    router_entry_respond (entry, msg, errno);
+}
+
+/* Handle internal service.remove request.
+ */
+static void service_remove_request (struct router_entry *entry, flux_msg_t *msg)
+{
+    struct router *rtr = entry->rtr;
+    const char *name;
+
+    if (flux_request_unpack (msg, NULL, "{s:s}", "service", &name) < 0)
+        goto error;
+    if (servhash_remove (rtr->services, name, entry->uuid, msg) < 0)
+        goto error;
+    return;
+error:
+    router_entry_respond (entry, msg, errno);
+}
+
+/* Receive a message from a client represented by 'entry'.
+ * Most messages will be forwarded to the broker as is, but some
+ * require conditioning, and some requests are handled internally.
+ */
+void router_entry_recv (struct router_entry *entry, flux_msg_t *msg)
+{
+    struct router *rtr = entry->rtr;
+    int type;
+    const char *topic;
+
+    if (flux_msg_get_type (msg, &type) < 0)
+        return;
+    if (flux_msg_get_topic (msg, &topic) < 0)
+        return;
+    switch (type) {
+        case FLUX_MSGTYPE_REQUEST:
+            if (!strcmp (topic, "local.sub")) {
+                local_sub_request (entry, msg);
+                break;
+            }
+            if (!strcmp (topic, "local.unsub")) {
+                local_unsub_request (entry, msg);
+                break;
+            }
+            if (!strcmp (topic, "service.add")) {
+                service_add_request (entry, msg);
+                break;
+            }
+            if (!strcmp (topic, "service.remove")) {
+                service_remove_request (entry, msg);
+                break;
+            }
+            if (flux_msg_enable_route (msg) < 0)
+                return;
+            if (flux_msg_push_route (msg, entry->uuid) < 0)
+                return;
+            if (disconnect_arm (entry->dcon, msg) < 0)
+                return;
+            /* fall through */
+        case FLUX_MSGTYPE_EVENT:
+        case FLUX_MSGTYPE_RESPONSE:
+            if (flux_send (rtr->h, msg, 0) < 0) {
+                flux_log_error (rtr->h,
+                                "router: client=%.5s %s %s > broker",
+                                entry->uuid,
+                                flux_msg_typestr (type),
+                                topic);
+                return;
+            }
+    }
+}
+
+static int broker_subscribe (const char *topic, void *arg)
+{
+    struct router *rtr = arg;
+
+    if (flux_event_subscribe (rtr->h, topic) < 0)
+        return -1;
+
+    /* N.B. t/t1008-proxy.t looks for this log message */
+    flux_log (rtr->h, LOG_DEBUG, "subscribe %s", topic);
+
+    return 0;
+}
+
+static int broker_unsubscribe (const char *topic, void *arg)
+{
+    struct router *rtr = arg;
+
+    if (!rtr->mute && flux_event_unsubscribe (rtr->h, topic) < 0)
+        return -1;
+
+    /* N.B. t/t1008-proxy.t looks for this log message */
+    flux_log (rtr->h, LOG_DEBUG, "unsubscribe %s", topic);
+
+    return 0;
+}
+
+/* A client asks the router to subscribe.
+ * This might generate a broker_subscribe() or just usecount++.
+ */
+static int router_subscribe (const char *topic, void *arg)
+{
+    struct router *rtr = arg;
+
+    return subhash_subscribe (rtr->subscriptions, topic);
+}
+
+/* A client asks the router to unsubscribe.
+ * This might generate a broker_unsubscribe() or just usecount--.
+ */
+static int router_unsubscribe (const char *topic, void *arg)
+{
+    struct router *rtr = arg;
+
+    return subhash_unsubscribe (rtr->subscriptions, topic);
+}
+
+static void disconnect_cb (const flux_msg_t *msg, void *arg)
+{
+    struct router_entry *entry = arg;
+    struct router *rtr = entry->rtr;
+
+    if (rtr && rtr->h) {
+        if (flux_send (rtr->h, msg, 0) < 0) {
+            flux_log_error (rtr->h,
+                            "router: disconnect < client=%.5s",
+                            entry->uuid);
+        }
+    }
+}
+
+static void router_entry_destroy (struct router_entry *entry)
+{
+    if (entry) {
+        struct router *rtr = entry->rtr;
+
+        disconnect_destroy (entry->dcon);
+        servhash_disconnect (rtr->services, entry->uuid);
+        subhash_destroy (entry->subscriptions);
+        ERRNO_SAFE_WRAP (free, entry->uuid);
+        ERRNO_SAFE_WRAP (free, entry);
+    }
+}
+
+// zhashx_destructor_fn footprint (wrapper)
+static void router_entry_destructor (void **item)
+{
+    if (item) {
+        router_entry_destroy (*item);
+        *item = NULL;
+    }
+}
+
+static struct router_entry *router_entry_create (const char *uuid,
+                                                 router_entry_send_f cb,
+                                                 void *arg)
+{
+    struct router_entry *entry;
+
+    if (!uuid || !cb) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (!(entry = calloc (1, sizeof (*entry))))
+        return NULL;
+    if (!(entry->uuid = strdup (uuid)))
+        goto error;
+    if (!(entry->subscriptions = subhash_create ()))
+        goto error;
+
+    if (!(entry->dcon = disconnect_create (disconnect_cb, entry)))
+        goto error;
+    entry->send = cb;
+    entry->arg = arg;
+    return entry;
+error:
+    router_entry_destroy (entry);
+    return NULL;
+}
+
+struct router_entry *router_entry_add (struct router *rtr,
+                                       const char *uuid,
+                                       router_entry_send_f cb,
+                                       void *arg)
+{
+    struct router_entry *entry;
+
+    if (!(entry = router_entry_create (uuid, cb, arg)))
+        return NULL;
+
+    subhash_set_subscribe (entry->subscriptions, router_subscribe, rtr);
+    subhash_set_unsubscribe (entry->subscriptions, router_unsubscribe, rtr);
+
+    if (zhashx_insert (rtr->routes, uuid, entry) < 0) {
+        router_entry_destroy (entry);
+        errno = EEXIST;
+        return NULL;
+    }
+    entry->rtr = rtr;
+    return entry;
+}
+
+void router_entry_delete (struct router_entry *entry)
+{
+    if (entry && entry->rtr && entry->rtr->routes)
+        zhashx_delete (entry->rtr->routes, entry->uuid);
+}
+
+/* Receive request from broker.
+ * Forward to client with registered service or respond with ENOSYS.
+ */
+static void request_cb (flux_t *h,
+                        flux_msg_handler_t *mh,
+                        const flux_msg_t *msg,
+                        void *arg)
+{
+    struct router *rtr = arg;
+    struct router_entry *entry;
+    const char *uuid;
+
+    if (servhash_match (rtr->services, msg, &uuid) < 0)
+        goto error;
+    if (!(entry = zhashx_lookup (rtr->routes, uuid)))
+        goto error;
+    if (entry->send (msg, entry->arg) < 0) {
+        if (errno != EPIPE && errno != EWOULDBLOCK)
+            flux_log_error (h, "router: request > client=%.5s", entry->uuid);
+    }
+    return;
+error:
+    if (flux_respond_error (h, msg, ENOSYS, NULL) < 0)
+        flux_log_error (h, "router: request > client");
+}
+
+/* Receive response from broker.
+ * Pop uuid off route stack, lookup uuid key in rtr->routes hash,
+ * and send to resulting client.
+ */
+static void response_cb (flux_t *h,
+                         flux_msg_handler_t *mh,
+                         const flux_msg_t *msg,
+                         void *arg)
+{
+    struct router *rtr = arg;
+    struct router_entry *entry = NULL;
+    flux_msg_t *cpy;
+    char *uuid = NULL;
+
+    if (!(cpy = flux_msg_copy (msg, true)))
+        goto error;
+    if (flux_msg_pop_route (cpy, &uuid) < 0) // may set uuid=NULL on success
+        goto error;
+    if (!uuid) {
+        errno = EINVAL;
+        goto error;
+    }
+    if (!(entry = zhashx_lookup (rtr->routes, uuid))) {
+        errno = EHOSTUNREACH;
+        goto error;
+    }
+    if (entry->send (cpy, entry->arg) < 0) {
+        flux_log_error (h, "router: response > client=%.5s", entry->uuid);
+        goto error;
+    }
+    free (uuid);
+    flux_msg_destroy (cpy);
+    return;
+error:
+    ERRNO_SAFE_WRAP (free, uuid);
+    flux_msg_destroy (cpy);
+}
+
+/* Receive event from broker.
+ * Distribute to all router entries with matching subscriptions.
+ */
+static void event_cb (flux_t *h,
+                      flux_msg_handler_t *mh,
+                      const flux_msg_t *msg,
+                      void *arg)
+{
+    struct router *rtr = arg;
+    struct router_entry *entry;
+    const char *topic;
+
+    if (flux_msg_get_topic (msg, &topic) < 0) {
+        flux_log_error (h, "router: event > client");
+        return;
+    }
+
+    entry = zhashx_first (rtr->routes);
+    while ((entry)) {
+        if (subhash_topic_match (entry->subscriptions, topic)) {
+            if (entry->send (msg, entry->arg) < 0)
+                flux_log_error (h, "router: event > client=%.5s", entry->uuid);
+        }
+        entry = zhashx_next (rtr->routes);
+    }
+}
+
+static const struct flux_msg_handler_spec htab[] = {
+    { FLUX_MSGTYPE_EVENT,     NULL, event_cb,    FLUX_ROLE_ALL },
+    { FLUX_MSGTYPE_RESPONSE,  NULL, response_cb, FLUX_ROLE_ALL },
+    { FLUX_MSGTYPE_REQUEST,   NULL, request_cb,  FLUX_ROLE_ALL },
+    FLUX_MSGHANDLER_TABLE_END
+};
+
+struct router *router_create (flux_t *h)
+{
+    struct router *rtr;
+
+    if (!h) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (!(rtr = calloc (1, sizeof (*rtr))))
+        return NULL;
+    rtr->h = h;
+
+    if (!(rtr->routes = zhashx_new ()))
+        goto error;
+    zhashx_set_destructor (rtr->routes, router_entry_destructor);
+
+    if (!(rtr->subscriptions = subhash_create ()))
+        goto error;
+    subhash_set_subscribe (rtr->subscriptions, broker_subscribe, rtr);
+    subhash_set_unsubscribe (rtr->subscriptions, broker_unsubscribe, rtr);
+
+    if (!(rtr->services = servhash_create (h)))
+        goto error;
+    servhash_set_respond (rtr->services, router_entry_respond_byuuid, rtr);
+
+    if (flux_msg_handler_addvec (h, htab, rtr, &rtr->handlers) < 0)
+        goto error;
+    return rtr;
+error:
+    router_destroy (rtr);
+    return NULL;
+}
+
+void router_destroy (struct router *rtr)
+{
+    if (rtr) {
+        flux_msg_handler_delvec (rtr->handlers);
+        subhash_destroy (rtr->subscriptions);
+        servhash_destroy (rtr->services);
+        ERRNO_SAFE_WRAP (zhashx_destroy, &rtr->routes);
+        ERRNO_SAFE_WRAP (free, rtr);
+    }
+}
+
+void router_mute (struct router *rtr)
+{
+    if (rtr)
+        rtr->mute = true;
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ src/common/librouter/router.h
@@ -0,0 +1,53 @@
+/************************************************************\
+ * Copyright 2019 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef _ROUTER_ROUTER_H
+#define _ROUTER_ROUTER_H
+
+#include "subhash.h"
+
+struct router;
+struct router_entry;
+
+/* router > client
+ *  e.g. calls usock_conn_send()
+ */
+typedef int (*router_entry_send_f)(const flux_msg_t *msg, void *arg);
+
+/* router < client
+ *   e.g. usock_conn_recv_f calls this
+ */
+void router_entry_recv (struct router_entry *entry, flux_msg_t *msg);
+
+/* Upon client connect, add route entry.
+ * Returned entry can be used with router_entry_recv() and should be
+ * destroyed when client disconnects (e.g. store in usock_conn aux hash).
+ */
+struct router_entry *router_entry_add (struct router *rtr,
+                                       const char *uuid,
+                                       router_entry_send_f cb,
+                                       void *arg);
+void router_entry_delete (struct router_entry *entry);
+
+/* Create/destroy router.  'h' is the "upstream broker connection.
+ */
+struct router *router_create (flux_t *h);
+void router_destroy (struct router *rtr);
+
+/* Avoid unsubscribe deadlock during broker shutdown - issue #1025
+ */
+void router_mute (struct router *rtr);
+
+
+#endif /* !_ROUTER_ROUTER_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
