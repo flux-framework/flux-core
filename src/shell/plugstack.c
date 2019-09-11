9579a4b827a5d2535fc3f900d81d4cb3552c1cc8
Mark A. Grondona <mark.grondona@gmail.com>
2019-09-11 15:29:12 -0700

    shell: update plugstack implementation for dso loading
    Add plugstack_load and plugstack_loadall functions to load a DSO
    plugin into the shell's plugin stack or load from a search path
    respectively.
    
    Allow a builtin searchpath with getter/setter, and an optional
    set of plugstack "aux" items which are propagated to all plugins
    loaded via the plugin loading functions of plugstack.

--- src/shell/plugstack.c
+++ src/shell/plugstack.c
@@ -18,12 +18,16 @@
 #include <dlfcn.h>
 #include <czmq.h>
 #include <stdarg.h>
+#include <flux/core.h>
 
 #include "src/common/libutil/log.h"
+#include "src/common/libutil/iterators.h"
 
 #include "plugstack.h"
 
 struct plugstack {
+    char *searchpath;   /* If set, search path for plugstack_load()        */
+    zhashx_t *aux;      /* aux items to propagate to loaded plugins        */
     zlistx_t *plugins;  /* Ordered list of loaded plugins                  */
     zhashx_t *names;    /* Hash for lookup of plugins by name              */
 };
@@ -35,6 +39,40 @@ void plugstack_unload_name (struct plugstack *st, const char *name)
         zlistx_delete (st->plugins, item);
         zhashx_delete (st->names, name);
     }
+}
+
+int plugstack_set_searchpath (struct plugstack *st, const char *path)
+{
+    if (!st) {
+        errno = EINVAL;
+        return -1;
+    }
+    free (st->searchpath);
+    st->searchpath = NULL;
+    if (path && !(st->searchpath = strdup (path)))
+        return -1;
+    return 0;
+}
+
+const char *plugstack_get_searchpath (struct plugstack *st)
+{
+    if (!st) {
+        errno = EINVAL;
+        return NULL;
+    }
+    return st->searchpath;
+}
+
+int plugstack_plugin_aux_set (struct plugstack *st,
+                              const char *name,
+                              void *data)
+{
+    if (!st || !name) {
+        errno = EINVAL;
+        return -1;
+    }
+    zhashx_update (st->aux, name, data);
+    return 0;
 }
 
 int plugstack_push (struct plugstack *st, flux_plugin_t *p)
@@ -63,6 +101,8 @@ void plugstack_destroy (struct plugstack *st)
         int saved_errno = errno;
         zlistx_destroy (&st->plugins);
         zhashx_destroy (&st->names);
+        zhashx_destroy (&st->aux);
+        free (st->searchpath);
         free (st);
         errno = saved_errno;
     }
@@ -79,7 +119,8 @@ struct plugstack * plugstack_create (void)
     struct plugstack *st = calloc (1, sizeof (*st));
     if (!st
         || !(st->plugins = zlistx_new ())
-        || !(st->names = zhashx_new ())) {
+        || !(st->names = zhashx_new ())
+        || !(st->aux = zhashx_new ())) {
         plugstack_destroy (st);
         return NULL;
     }
@@ -98,6 +139,169 @@ int plugstack_call (struct plugstack *st,
         p = zlistx_next (st->plugins);
     }
     return 0;
+}
+
+static int plugin_aux_from_zhashx (flux_plugin_t *p, zhashx_t *aux)
+{
+    const char *key;
+    void *val;
+    FOREACH_ZHASHX (aux, key, val) {
+        if (flux_plugin_aux_set (p, key, val, NULL) < 0)
+            return -1;
+    }
+    return 0;
+}
+
+static int load_plugin (struct plugstack *st,
+                        const char *path,
+                        const char *conf)
+{
+    flux_plugin_t *p = flux_plugin_create ();
+    if (!p)
+        return -1;
+    if (conf && flux_plugin_set_conf (p, conf) < 0) {
+        log_msg ("set_conf: %s: %s", path, flux_plugin_strerror (p));
+        goto error;
+    }
+    if (plugin_aux_from_zhashx (p, st->aux) < 0) {
+        log_msg ("%s: failed to set aux items", path);
+    }
+    if (flux_plugin_load_dso (p, path) < 0) {
+        log_msg ("%s", flux_plugin_strerror (p));
+        goto error;
+    }
+    if (plugstack_push (st, p) < 0) {
+        log_err ("plugstack_push (%s)", path);
+        goto error;
+    }
+    return 0;
+error:
+    flux_plugin_destroy (p);
+    return -1;
+}
+
+static int load_from_glob (struct plugstack *st, glob_t *gl, const char *conf)
+{
+    int n = 0;
+    size_t i;
+    for (i = 0; i < gl->gl_pathc; i++) {
+        if (load_plugin (st, gl->gl_pathv[i], conf) < 0)
+            return -1;
+        n++;
+    }
+    return n;
+}
+
+static int plugstack_glob (struct plugstack *st,
+                           const char *pattern,
+                           const char *conf)
+{
+    glob_t gl;
+    int rc = -1;
+
+    rc = glob (pattern, GLOB_TILDE_CHECK, NULL, &gl);
+    switch (rc) {
+        case 0:
+            rc = load_from_glob (st, &gl, conf);
+            break;
+        case GLOB_NOMATCH:
+            rc = 0;
+            break;
+        case GLOB_NOSPACE:
+            log_msg ("glob: Out of memory");
+            break;
+        case GLOB_ABORTED:
+            //log_err ("glob: failed to read %s", pattern);
+            break;
+        default:
+            log_msg ("glob: unknown rc = %d", rc);
+    }
+    globfree (&gl);
+    return rc;
+}
+
+/*  Return 1 if either searchpath is NULL, or pattern starts with '/' or '~'.
+ *  Also, pattern starting with './' to explicitly bypass searchpath.
+ */
+static int no_searchpath (const char *searchpath, const char *pattern)
+{
+    return (!searchpath
+            || pattern[0] == '/'
+            || pattern[0] == '~'
+            || (pattern[0] == '.' && pattern[1] == '/'));
+}
+
+static void item_free (void **item)
+{
+    if (*item) {
+        free (*item);
+        *item = NULL;
+    }
+}
+
+static zlistx_t * list_o_patterns (const char *searchpath, const char *pattern)
+{
+    char *copy;
+    char *str;
+    char *dir;
+    char *path;
+    char *sp = NULL;
+    zlistx_t *l = zlistx_new ();
+
+    if (!l || !(copy = strdup (searchpath)))
+        return NULL;
+    str = copy;
+
+    zlistx_set_destructor (l, item_free);
+
+    while ((dir = strtok_r (str, ":", &sp))) {
+        if (asprintf (&path, "%s/%s", dir, pattern) < 0)
+            goto error;
+        zlistx_add_end (l, path);
+        str = NULL;
+    }
+    free (copy);
+    return l;
+error:
+    free (copy);
+    zlistx_destroy (&l);
+    return NULL;
+}
+
+int plugstack_load (struct plugstack *st,
+                    const char *pattern,
+                    const char *conf)
+{
+    zlistx_t *l;
+    char *path;
+    int rc = 0;
+
+    if (!st || !pattern) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (no_searchpath (st->searchpath, pattern))
+        return plugstack_glob (st, pattern, conf);
+
+    if (!(l = list_o_patterns (st->searchpath, pattern)))
+        return -1;
+    /*
+     *  NB: traverse searchpath list in *reverse* order. this is because
+     *   of the way plugstack works, the _last_ loaded plugin takes
+     *   precedence. So to preserve the idiom of "searchpath" we iterate
+     *   the search in reverse order.
+     */
+    path = zlistx_last (l);
+    while (path) {
+        int n;
+        if ((n = plugstack_glob (st, path, conf)) < 0)
+            return -1;
+        rc += n;
+        path = zlistx_prev (l);
+    }
+    zlistx_destroy (&l);
+    return rc;
 }
 
 /* vi: ts=4 sw=4 expandtab
--- src/shell/plugstack.h
+++ src/shell/plugstack.h
@@ -17,7 +17,45 @@ struct plugstack * plugstack_create (void);
 
 void plugstack_destroy (struct plugstack *st);
 
+/*  Set aux items to be propagated to any plugins loaded by
+ *   plugstack_load().
+ */
+int plugstack_plugin_aux_set (struct plugstack *st,
+                              const char *name,
+                              void *val);
+
 int plugstack_push (struct plugstack *st, flux_plugin_t *p);
+
+/*  Set a colon-delimited set of PATHs to use when searching for
+ *   a filename to load with plugstack_load().
+ *  Replaces any previously set path.
+ */
+int plugstack_set_searchpath (struct plugstack *st, const char *searchpath);
+
+/*  Get current plugstack searchpath
+ */
+const char *plugstack_get_searchpath (struct plugstack *st);
+
+/*  Load all plugins matching glob pattern in all directories in
+ *   searchpatch inst the plugin stack 'st', providing optional
+ *   load configuration conf (a JSON encoded string).
+ *
+ *  If pattern starts with a '/' or '~' then searchpath is ignored.
+ *  If searchpath is NULL then works like plugstack_load() below.
+ *
+ * Returns the number of plugins loaded or -1 on error.
+ */
+int plugstack_loadall (struct plugstack *st,
+                       const char *searchpath,
+                       const char *pattern,
+                       const char *conf);
+
+/*  Load all plugins matching a glob pattern, passing optional
+ *   configuration conf (a JSON-encoded string).
+ */
+int plugstack_load (struct plugstack *st,
+                    const char *pattern,
+                    const char *conf);
 
 int plugstack_call (struct plugstack *st,
                     const char *name,
