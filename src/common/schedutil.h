6f17439f188d5ad6e01b607bd819647fab523f21
Stephen Herbein <herbein1@llnl.gov>
2019-09-15 21:17:56 -0700

    libschedutil: export the library for use by flux-sched
    Install the library as libflux-schedutil.so, with a flux-schedutil.pc
    file for pkgconfig.

---
+++ src/common/schedutil.h
@@ -0,0 +1,33 @@
+/************************************************************\
+ * Copyright 2019 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef _FLUX_SCHEDUTIL_H
+#define _FLUX_SCHEDUTIL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "schedutil/init.h"
+#include "schedutil/hello.h"
+#include "schedutil/ready.h"
+#include "schedutil/alloc.h"
+#include "schedutil/free.h"
+#include "schedutil/ops.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_FLUX_SCHEDUTIL_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
