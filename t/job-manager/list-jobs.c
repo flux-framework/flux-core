364bf24d999bbd48fbcc355ad64bf51d6d7dd878
Albert Chu <chu11@llnl.gov>
2019-10-18 14:56:01 -0700

    t/job-manager: Add list-jobs testing tool
    Create new testing tool list-jobs to test job-manager.list functionality.
    
    This tool is created to ensure that job-manager.list functionality
    is still properly covered in testing, regardless of how the command
    `flux job list` may be changed in the future.

---
+++ t/job-manager/list-jobs.c
@@ -0,0 +1,127 @@
+/************************************************************\
+ * Copyright 2018 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <czmq.h>
+#include <flux/core.h>
+#include <flux/optparse.h>
+#include <jansson.h>
+#if HAVE_FLUX_SECURITY
+#include <flux/security/sign.h>
+#endif
+#include "src/common/libutil/log.h"
+
+static struct optparse_option list_opts[] =  {
+    { .name = "count", .key = 'c', .has_arg = 1, .arginfo = "N",
+      .usage = "Limit output to N jobs",
+    },
+    OPTPARSE_TABLE_END
+};
+
+/* convert floating point timestamp (UNIX epoch, UTC) to ISO 8601 string,
+ * with second precision
+ */
+static int iso_timestr (double timestamp, char *buf, size_t size)
+{
+    time_t sec = timestamp;
+    struct tm tm;
+
+    if (!gmtime_r (&sec, &tm))
+        return -1;
+    if (strftime (buf, size, "%FT%TZ", &tm) == 0)
+        return -1;
+    return 0;
+}
+
+int main (int argc, char *argv[])
+{
+    flux_t *h;
+    optparse_t *opts;
+    int max_entries;
+    int optindex;
+    char *attrs = "[\"id\",\"userid\",\"priority\",\"t_submit\",\"state\"]";
+    flux_future_t *f;
+    json_t *jobs;
+    size_t index;
+    json_t *value;
+    json_t *attrs_array;
+
+    log_init ("list-jobs");
+
+    opts = optparse_create ("list-jobs");
+    if (optparse_add_option_table (opts, list_opts) != OPTPARSE_SUCCESS)
+        log_msg_exit ("optparse_add_option_table");
+    if ((optindex = optparse_parse_args (opts, argc, argv)) < 0)
+        exit (1);
+
+    max_entries = optparse_get_int (opts, "count", 0);
+
+    if (optindex != argc) {
+        optparse_print_usage (opts);
+        exit (1);
+    }
+
+    if (!(h = flux_open (NULL, 0)))
+        log_err_exit ("flux_open");
+
+    if (!(attrs_array = json_loads (attrs, 0, NULL)))
+        log_err_exit ("json_loads");
+
+    if (!(f = flux_rpc_pack (h, "job-manager.list", FLUX_NODEID_ANY, 0,
+                             "{s:i s:O}",
+                             "max_entries", max_entries,
+                             "attrs", attrs_array)))
+        log_err_exit ("flux_rpc_pack");
+
+    if (flux_rpc_get_unpack (f, "{s:o}", "jobs", &jobs) < 0)
+        log_err_exit ("flux_rpc_get_unpack");
+
+    json_array_foreach (jobs, index, value) {
+        flux_jobid_t id;
+        int priority;
+        uint32_t userid;
+        double t_submit;
+        char timestr[80];
+        flux_job_state_t state;
+
+        if (json_unpack (value, "{s:I s:i s:i s:f s:i}",
+                                "id", &id,
+                                "priority", &priority,
+                                "userid", &userid,
+                                "t_submit", &t_submit,
+                                "state", &state) < 0)
+            log_msg_exit ("error parsing job data");
+        if (iso_timestr (t_submit, timestr, sizeof (timestr)) < 0)
+            log_err_exit ("time conversion error");
+        printf ("%llu\t%s\t%lu\t%d\t%s\n",
+                (unsigned long long)id,
+                flux_job_statetostr (state, true),
+                (unsigned long)userid,
+                priority,
+                timestr);
+    }
+
+    json_decref (attrs_array);
+    flux_future_destroy (f);
+    flux_close (h);
+    log_fini ();
+
+    return 0;
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
