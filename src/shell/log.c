a2a73dd9817912c4ad976f68192a3316dd8d2a7d
Mark A. Grondona <mark.grondona@gmail.com>
2019-10-19 11:37:10 -0700

    shell: log: add shell internal logging facility
    Add a flexible shell logging facility for use by internal shell
    components and plugins. The logging facility sets up 6 logging levels
    from FLUX_SHELL_FATAL to FLUX_SHELL_TRACE. All messages at or below
    the current internal logging level are sent to stderr in a default
    output format. Messages are additionally formatted as plugin arguments
    and sent to all "shell.log" callbacks. This allows multiple logging
    destinations to be registered via shell plugins.
    
    For convenience a set of macros are provided which automatically
    collect file and line information from the call site. These are named
    from shell_die() for fatal messages to shell_trace() for logging at
    FLUX_SHELL_TRACE level.
    
    When shell_die() or flux_shell_fatal() is called, the shell will
    terminate all local tasks and generate an "exec" exception, then return
    to normal operation to collect task exit status and terminate cleanly.

---
+++ src/shell/log.c
@@ -0,0 +1,358 @@
+/************************************************************\
+ * Copyright 2019 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+/* shell log facility
+ *
+ * Implements logging facility for shell components and plugins
+ * (see shell.h for public interfaces)
+ *
+ * By default all messages at logger.level or below are logged
+ * to stderr. If the shell plugin stack has been initialized
+ * logging messages are additionally sent to any "shell.log"
+ * callbacks, allowing other logging implementations to be loaded
+ * in the shell at runtime.
+ *
+ */
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <jansson.h>
+#include <flux/core.h>
+#include <flux/shell.h>
+
+#include "info.h"
+#include "internal.h"
+#include "log.h"
+
+static struct {
+    FILE *fp;
+    char *prog;
+    int level;
+    int fp_level;
+    int rank;
+    unsigned int active:1;
+    unsigned int exception_logged:1;
+    flux_shell_t *shell;
+} logger;
+
+const char *levelstr[] = {
+    "FATAL", "FATAL", "FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE"
+};
+
+/*  Format a log message in standard form to stream fp */
+static void log_fprintf (FILE *fp,
+                         int level,
+                         const char *component,
+                         const char *file,
+                         int line,
+                         const char *msg)
+{
+    const char *prefix = levelstr [level];
+
+    fprintf (logger.fp, "%s: ", logger.prog);
+    if (prefix)
+        fprintf (logger.fp, "%5s: ", prefix);
+    if (component)
+        fprintf (logger.fp, "%s: ", component);
+
+    /* Log file:line information if level >= FLUX_SHELL_DEBUG */
+    if (logger.level > FLUX_SHELL_NOTICE && file && line > 0)
+        fprintf (logger.fp, "%s:%d: ", file, line);
+
+    fprintf (logger.fp, "%s\n", msg);
+}
+
+static flux_plugin_arg_t *log_msg_args (int level,
+                                        int rank,
+                                        const char *component,
+                                        const char *file,
+                                        int line,
+                                        const char *msg)
+{
+    int rc = -1;
+    int flags = FLUX_PLUGIN_ARG_IN | FLUX_PLUGIN_ARG_UPDATE;
+    flux_plugin_arg_t *args = flux_plugin_arg_create ();
+
+    if (!args)
+        return NULL;
+    if (flux_plugin_arg_pack (args, flags,
+                              "{ s:i s:i s:s }",
+                              "rank", rank,
+                              "level", level,
+                              "message", msg) < 0)
+        goto out;
+    if (component && flux_plugin_arg_pack (args, flags,
+                                           "{ s:s }",
+                                           "component", component) < 0)
+        goto out;
+    if (file && flux_plugin_arg_pack (args, flags,
+                                      "{ s:s s:i }",
+                                      "file", file,
+                                      "line", line) < 0)
+        goto out;
+    rc = 0;
+out:
+    if (rc < 0) {
+        flux_plugin_arg_destroy (args);
+        return NULL;
+    }
+    return args;
+}
+
+
+static int log_event (int level,
+                      int rank,
+                      const char *component,
+                      const char *file,
+                      int line,
+                      const char *msg)
+{
+    int rc = 0;
+    flux_plugin_arg_t *args;
+
+    /*  If plugin stack is not initialized yet, or log level is at or
+     *   below our threshold, log the message to stderr
+     */
+    if (!logger.shell->plugstack || level <= logger.fp_level)
+        log_fprintf (stderr, level, component, file, line, msg);
+
+    /*  If plugin stack is initialized, and we're not actively processing
+     *   another log message then format log message args and call all
+     *   "shell.log" callbacks.
+     */
+    if (logger.shell->plugstack && !logger.active) {
+        logger.active = 1;
+        if (!(args = log_msg_args (level, rank, component, file, line, msg)))
+            return -1;
+        rc = flux_shell_plugstack_call (logger.shell, "shell.log", args);
+        flux_plugin_arg_destroy (args);
+        logger.active = 0;
+    }
+    return rc;
+}
+
+static void send_logmsg (const char *buf, int level, const char *file, int line)
+{
+    const char *component = plugstack_current_name (logger.shell->plugstack);
+    if (logger.rank < 0 && logger.shell->info)
+        logger.rank = logger.shell->info->shell_rank;
+    if (log_event (level, logger.rank, component, file, line, buf) < 0)
+        fprintf (stderr, "%s: log failure: %s\n", logger.prog, buf);
+}
+
+static int errorcat (int errnum, int start, char *buf, size_t len)
+{
+    char *p;
+    int n, size;
+
+    if (errnum == 0)
+        return 0;
+
+    size = len - start;
+    p = buf + start;
+    if ((n = snprintf (p, size, ": %s", strerror (errnum))) >= size)
+        return -1;
+    return n;
+}
+
+/*  Format message, printing an error to stderr on failure
+ *  If errnum > 0, then append result of strerror (errnum).
+ */
+static int msgfmt (char *buf,
+                   size_t len,
+                   int errnum,
+                   const char *fmt,
+                   va_list ap)
+{
+    int rc = vsnprintf (buf, len, fmt, ap);
+    if ((rc < 0 || rc >= len) || errorcat (errnum, rc, buf, len) < 0) {
+        fprintf (stderr,
+                 "%s: unable to format log msg (%s): %s\n",
+                 logger.prog,
+                 fmt,
+                 strerror (errno));
+        return -1;
+    }
+    /*  Clean up trailing newline, pointless here
+     */
+    if (buf[rc-1] == '\n')
+        buf[rc-1] = '\0';
+    return 0;
+}
+
+void flux_shell_log (int level,
+                     const char *file,
+                     int line,
+                     const char *fmt, ...)
+{
+    char buf [4096];
+    va_list ap;
+    va_start (ap, fmt);
+    if (msgfmt (buf, sizeof (buf), 0, fmt, ap) == 0)
+        send_logmsg (buf, level, file, line);
+    va_end (ap);
+}
+
+int flux_shell_err (const char *file,
+                   int line,
+                   int errnum,
+                   const char *fmt, ...)
+{
+    char buf [4096];
+    va_list ap;
+    va_start (ap, fmt);
+    if (msgfmt (buf, sizeof (buf), errnum, fmt, ap) == 0)
+        send_logmsg (buf, FLUX_SHELL_ERROR, file, line);
+    va_end (ap);
+    errno = errnum;
+    return -1;
+}
+
+void flux_shell_fatal (const char *file,
+                       int line,
+                       int errnum,
+                       const char *fmt, ...)
+{
+    flux_shell_t *shell = logger.shell;
+    flux_future_t *f = NULL;
+    char buf [4096];
+    va_list ap;
+
+    va_start (ap, fmt);
+    if (msgfmt (buf, sizeof (buf), errnum, fmt, ap) < 0)
+        sprintf (buf, "flux-shell: fatal error");
+    else
+        send_logmsg (buf, FLUX_SHELL_FATAL, file, line);
+    va_end (ap);
+
+    /*  Attempt to kill any running tasks
+     */
+    flux_shell_killall (shell, SIGKILL);
+
+    /*
+     *  Only need to generate an exception if we have a broker connection
+     *   and are not in standalone mode. O/w, exits immediately below.
+     */
+    if (shell->h && !shell->standalone && !logger.exception_logged) {
+        /*  Raise an exec exception.
+         *  Wait synchronously for response to ensure exception is
+         *   received by job-manager before shell potentially exits.
+         */
+        if (!(f = flux_job_raise (shell->h,
+                                  shell->info->jobid,
+                                  "exec",
+                                  0,
+                                  buf))
+           || flux_future_get (f, NULL) < 0) {
+            fprintf (stderr,
+                     "flux-shell: failed to raise job exception: %s\n",
+                     flux_future_error_string (f));
+        }
+        else
+            shell_log_set_exception_logged ();
+    }
+    exit (1);
+}
+
+void shell_log_set_exception_logged (void)
+{
+    logger.exception_logged = 1;
+}
+
+static int log_setlevel (flux_shell_t *shell, const char *dest, int level)
+{
+    int rc = -1;
+    flux_plugin_arg_t *args;
+
+    if (!shell || !dest) {
+        errno = EINVAL;
+        return -1;
+    }
+    if (!shell->plugstack) {
+        errno = EAGAIN;
+        return -1;
+    }
+    if (!(args = flux_plugin_arg_create ()))
+        return -1;
+    if (flux_plugin_arg_pack (args, FLUX_PLUGIN_ARG_IN,
+                              "{ s:s s:i }",
+                              "dest", dest,
+                              "level", level) < 0)
+        goto out;
+    rc = flux_shell_plugstack_call (shell, "shell.log-setlevel", args);
+out:
+    flux_plugin_arg_destroy (args);
+    return rc;
+}
+
+int flux_shell_log_setlevel (int level, const char *dest)
+{
+    if (level < FLUX_SHELL_FATAL || level > FLUX_SHELL_TRACE) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    /*  Always set internal dispatch level.
+     *  In standalone mode, also set stderr level by default
+     */
+    if (level > logger.level)
+        logger.level = level;
+    if (logger.shell->standalone)
+        logger.fp_level = level;
+
+    if (dest == NULL)
+        return 0;
+
+    /*  If dest is set, then attempt to notify any loaded loggers of
+     *   the severity level change.
+     */
+    if (dest != NULL) {
+        if (strcmp (dest, "stderr") == 0)
+            logger.fp_level = level;
+        else
+            return log_setlevel (logger.shell, dest, level);
+    }
+    return 0;
+}
+
+int shell_log_init (flux_shell_t *shell, const char *progname)
+{
+    logger.shell = shell;
+    logger.level = FLUX_SHELL_NOTICE;
+    logger.fp_level = FLUX_SHELL_NOTICE;
+    logger.active = 0;
+    logger.fp = stderr;
+    logger.rank = -1;
+    if (progname && !(logger.prog = strdup (progname)))
+        return -1;
+    return 0;
+}
+
+int shell_log_reinit (flux_shell_t *shell)
+{
+   if (shell->verbose > 2) {
+        shell_warn ("Ignoring shell verbosity > 2");
+        shell->verbose = 2;
+    }
+    if (flux_shell_log_setlevel (FLUX_SHELL_NOTICE + shell->verbose, "any") < 0)
+        shell_die ("failed to set log level");
+    return 0;
+}
+
+void shell_log_fini (void)
+{
+    logger.shell = NULL;
+    free (logger.prog);
+    fclose (logger.fp);
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
