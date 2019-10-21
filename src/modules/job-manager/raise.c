13d0a6ea630c8f3669cafc79182104211e04f074
Mark A. Grondona <mark.grondona@gmail.com>
2019-10-21 10:17:39 -0700

    job-manager: relax restriction on exception note contents
    The job-manager rejects exceptions in which the "note" context key
    contains "=", however this is no longer a requirement since context
    is now encoded JSON.
    
    Relax the requirement and allow exceptions with "=" characters in their
    "note" strings.

--- src/modules/job-manager/raise.c
+++ src/modules/job-manager/raise.c
@@ -103,11 +103,6 @@ void raise_handle_request (flux_t *h, struct queue *queue,
         errno = EPERM;
         goto error;
     }
-    if (note && strchr (note, '=')) {
-        errstr = "exception note may not contain key=value attributes";
-        errno = EPROTO;
-        goto error;
-    }
     if (event_job_post_pack (event_ctx, job,
                              "exception",
                              "{ s:s s:i s:i s:s }",
