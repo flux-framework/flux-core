a3036aefe3e4644f232c98c6216063691bbf0150
Albert Chu <chu11@llnl.gov>
2019-11-18 18:24:26 -0800

    cmd/flux-job: Add new stats subcommand
    The new stats subcommand will send an RPC to job-info.job-stats to
    get current job statistics.  At the moment the job statistics simply
    are the number of jobs in each job state and the total number of
    jobs.
    
    For the time being, simply output the job stats json object in raw
    form the stdout.  No special formatting is done at the moment.

--- src/cmd/flux-job.c
+++ src/cmd/flux-job.c
@@ -53,6 +53,7 @@ int cmd_priority (optparse_t *p, int argc, char **argv);
 int cmd_eventlog (optparse_t *p, int argc, char **argv);
 int cmd_wait_event (optparse_t *p, int argc, char **argv);
 int cmd_info (optparse_t *p, int argc, char **argv);
+int cmd_stats (optparse_t *p, int argc, char **argv);
 int cmd_drain (optparse_t *p, int argc, char **argv);
 int cmd_undrain (optparse_t *p, int argc, char **argv);
 
@@ -272,6 +273,13 @@ static struct optparse_subcommand subcommands[] = {
       "id key ...",
       "Display info for a job",
       cmd_info,
+      0,
+      NULL
+    },
+    { "stats",
+      NULL,
+      "Get current job stats",
+      cmd_stats,
       0,
       NULL
     },
@@ -1919,6 +1927,27 @@ int cmd_info (optparse_t *p, int argc, char **argv)
         log_err_exit ("flux_reactor_run");
 
     json_decref (ctx.keys);
+    flux_close (h);
+    return (0);
+}
+
+int cmd_stats (optparse_t *p, int argc, char **argv)
+{
+    flux_t *h;
+    flux_future_t *f;
+    const char *topic = "job-info.job-stats";
+    const char *s;
+
+    if (!(h = flux_open (NULL, 0)))
+        log_err_exit ("flux_open");
+
+    if (!(f = flux_rpc (h, topic, NULL, FLUX_NODEID_ANY, 0)))
+        log_err_exit ("flux_rpc");
+    if (flux_rpc_get (f, &s) < 0)
+        log_msg_exit ("stats: %s", future_strerror (f, errno));
+
+    /* for time being, just output json object for result */
+    printf ("%s\n", s);
     flux_close (h);
     return (0);
 }
