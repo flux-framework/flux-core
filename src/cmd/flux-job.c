3a135e68eb47ff4b9bfdc2154b5a0b1da854854d
Albert Chu <chu11@llnl.gov>
2019-12-12 09:43:13 -0800

    cmd/flux-job: Get job state timings in job list
    In the flux job list command, get all additional job state timings
    when requesting job list information.  Specifically, get the
    t_depend, t_sched, t_run, t_cleanup, and t_inactive attributes.

--- src/cmd/flux-job.c
+++ src/cmd/flux-job.c
@@ -628,7 +628,8 @@ int cmd_list (optparse_t *p, int argc, char **argv)
 {
     int optindex = optparse_option_index (p);
     int max_entries = optparse_get_int (p, "count", 0);
-    char *attrs = "[\"userid\",\"priority\",\"t_submit\",\"state\",\"job-name\"]";
+    char *attrs = "[\"userid\",\"priority\",\"t_submit\",\"state\",\"job-name\"," \
+                  "\"t_depend\",\"t_sched\",\"t_run\",\"t_cleanup\",\"t_inactive\"]";
     flux_t *h;
     flux_future_t *f;
     json_t *jobs;
