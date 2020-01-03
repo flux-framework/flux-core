9c2e251597a610b288cbda3f476c5fa3aeb346fe
Mark A. Grondona <mark.grondona@gmail.com>
2020-01-03 13:34:04 -0800

    libutil/unlink_recursive: add missing header guard
    Add missing header guard to src/common/libutil/unlink_recursive.h
    as detected by lgtm scan.

--- src/common/libutil/unlink_recursive.h
+++ src/common/libutil/unlink_recursive.h
@@ -8,10 +8,14 @@
  * SPDX-License-Identifier: LGPL-3.0
 \************************************************************/
 
+#ifndef HAVE_LIBUTIL_UNLINK_RECURSIVE_H
+#define HAVE_LIBUTIL_UNLINK_RECURSIVE_H 1
+
 /* Return number of files/directories unlinked or -1 on error.
  */
 int unlink_recursive (const char *dirpath);
 
+#endif /* !HAVE_LIBUTIL_UNLINK_RECURSIVE_H */
 /*
  * vi:tabstop=4 shiftwidth=4 expandtab
  */
