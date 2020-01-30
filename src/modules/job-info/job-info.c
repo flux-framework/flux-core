08fe5a7f61fc8e4a3dde65ea4813a7299add6b8b
Albert Chu <chu11@llnl.gov>
2020-01-29 16:40:32 -0800

    modules/job-info: Add job-info.list-id service
    Add a new listing service that retrieve job info for a specific
    job id.

--- src/modules/job-info/job-info.c
+++ src/modules/job-info/job-info.c
@@ -128,6 +128,11 @@ static const struct flux_msg_handler_spec htab[] = {
       .rolemask     = FLUX_ROLE_USER
     },
     { .typemask     = FLUX_MSGTYPE_REQUEST,
+      .topic_glob   = "job-info.list-id",
+      .cb           = list_id_cb,
+      .rolemask     = FLUX_ROLE_USER
+    },
+    { .typemask     = FLUX_MSGTYPE_REQUEST,
       .topic_glob   = "job-info.list-attrs",
       .cb           = list_attrs_cb,
       .rolemask     = FLUX_ROLE_USER
