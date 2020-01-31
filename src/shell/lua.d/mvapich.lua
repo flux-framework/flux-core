285fba233c3981aa3e5fdfcd900dddce6163b70c
Mark A. Grondona <mark.grondona@gmail.com>
2020-01-31 03:45:55 +0000

    shell: add copyright header to shell Lua scripts
    Add missing copyright header to shell lua.d/* rc scripts.

--- src/shell/lua.d/mvapich.lua
+++ src/shell/lua.d/mvapich.lua
@@ -1,3 +1,13 @@
+-------------------------------------------------------------
+-- Copyright 2020 Lawrence Livermore National Security, LLC
+-- (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+--
+-- This file is part of the Flux resource manager framework.
+-- For details, see https://github.com/flux-framework.
+--
+-- SPDX-License-Identifier: LGPL-3.0
+-------------------------------------------------------------
+
 local dirname = require 'flux.posix'.dirname
 
 local f, err = require 'flux'.new ()
