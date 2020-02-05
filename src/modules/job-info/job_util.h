aa3b6483140769b2a673f12bfc4bf774e80c0ff0
Albert Chu <chu11@llnl.gov>
2020-02-04 16:44:34 -0800

    modules/job-info: Move job_to_json to new file
    Create new job_util.[ch] utility files and move job_to_json() into
    it.

---
+++ src/modules/job-info/job_util.h
@@ -0,0 +1,24 @@
+/************************************************************\
+ * Copyright 2018 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef _FLUX_JOB_INFO_JOB_UTIL_H
+#define _FLUX_JOB_INFO_JOB_UTIL_H
+
+#include <flux/core.h>
+
+#include "job_state.h"
+
+json_t *job_to_json (struct job *job, json_t *attrs);
+
+#endif /* ! _FLUX_JOB_INFO_JOB_UTIL_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
