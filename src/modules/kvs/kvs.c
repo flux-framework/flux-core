ab0008ec04133163d88638b741afe78fd6397092
Albert Chu <chu11@llnl.gov>
2020-02-21 15:16:56 -0800

    modules/kvs: Unsubscribe namespace-removed event
    When a namespace is removed, unsubscribe from the
    kvs.namespace-removed-<NS> event.
    
    Fixes #2762

--- src/modules/kvs/kvs.c
+++ src/modules/kvs/kvs.c
@@ -240,6 +240,7 @@ static int event_unsubscribe (kvs_ctx_t *ctx, const char *ns)
 {
     char *setroot_topic = NULL;
     char *error_topic = NULL;
+    char *removed_topic = NULL;
     int rc = -1;
 
     if (asprintf (&setroot_topic, "kvs.setroot-%s", ns) < 0) {
@@ -262,10 +263,21 @@ static int event_unsubscribe (kvs_ctx_t *ctx, const char *ns)
         goto cleanup;
     }
 
+    if (asprintf (&removed_topic, "kvs.namespace-removed-%s", ns) < 0) {
+        errno = ENOMEM;
+        goto cleanup;
+    }
+
+    if (flux_event_subscribe (ctx->h, removed_topic) < 0) {
+        flux_log_error (ctx->h, "flux_event_subscribe");
+        goto cleanup;
+    }
+
     rc = 0;
 cleanup:
     free (setroot_topic);
     free (error_topic);
+    free (removed_topic);
     return rc;
 }
 
