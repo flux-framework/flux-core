c58bb72bedfd8d3f3429098a71a1603b4d62d7da
Albert Chu <chu11@llnl.gov>
2020-07-01 12:22:43 -0700

    t/job-manager/sched-helper: Rename functions
    Rename functions to make it clear they are getting / checking
    via the job manager.
    
    Update callers appropriately.

--- t/job-manager/sched-helper.sh
+++ t/job-manager/sched-helper.sh
@@ -3,51 +3,51 @@
 
 # job-manager sched helper functions
 
-LIST_JOBS=${FLUX_BUILD_DIR}/t/job-manager/list-jobs
+JMGR_JOB_LIST=${FLUX_BUILD_DIR}/t/job-manager/list-jobs
 
-# internal function to get job state
+# internal function to get job state via job manager
 #
 # if job is not found by list-jobs, but the clean event exists
 # in the job's eventlog, return state as inactive
 #
 # arg1 - jobid
-_get_state() {
+_jmgr_get_state() {
         local id=$1
-        local state=$(${LIST_JOBS} | awk '$1 == "'${id}'" { print $2; }')
+        local state=$(${JMGR_JOB_LIST} | awk '$1 == "'${id}'" { print $2; }')
         test -z "$state" \
                 && flux job wait-event --timeout=5 ${id} clean >/dev/null \
                 && state=I
         echo $state
 }
 
-# verify if job is in specific state
+# verify if job is in specific state through job manager
 #
 # function will loop for up to 5 seconds in case state change is slow
 #
 # arg1 - jobid
 # arg2 - single character expected state (e.g. R = running)
-check_state() {
+jmgr_check_state() {
         local id=$1
         local wantstate=$2
         for try in $(seq 1 10); do
-                test $(_get_state $id) = $wantstate && return 0
+                test $(_jmgr_get_state $id) = $wantstate && return 0
                 sleep 0.5
         done
         return 1
 }
 
-# internal function to get job annotation key value
+# internal function to get job annotation key value via job manager
 #
 # arg1 - jobid
 # arg2 - key
-_get_annotation() {
+_jmgr_get_annotation() {
         local id=$1
         local key=$2
-        local note="$(${LIST_JOBS} | grep ${id} | cut -f 6- | jq .\"${key}\")"
+        local note="$(${JMGR_JOB_LIST} | grep ${id} | cut -f 6- | jq .\"${key}\")"
         echo $note
 }
 
-# verify if job contains specific annotation key & value
+# verify if job contains specific annotation key & value through job manager
 #
 # function will loop for up to 5 seconds in case annotation update
 # arrives slowly
@@ -57,34 +57,34 @@ _get_annotation() {
 # arg3 - value of key in annotation
 #
 # callers should set HAVE_JQ requirement
-check_annotation() {
+jmgr_check_annotation() {
         local id=$1
         local key=$2
         local value="$3"
         for try in $(seq 1 10); do
-                test "$(_get_annotation $id $key)" = "${value}" && return 0
+                test "$(_jmgr_get_annotation $id $key)" = "${value}" && return 0
                 sleep 0.5
         done
         return 1
 }
 
-# verify if job contains specific annotation key
+# verify if job contains specific annotation key through job manager
 #
 # arg1 - jobid
 # arg2 - key in annotation
 #
 # callers should set HAVE_JQ requirement
-check_annotation_exists() {
+jmgr_check_annotation_exists() {
         local id=$1
         local key=$2
-        ${LIST_JOBS} | grep ${id} | cut -f 6- | jq -e .\"${key}\" > /dev/null
+        ${JMGR_JOB_LIST} | grep ${id} | cut -f 6- | jq -e .\"${key}\" > /dev/null
 }
 
-# verify that job contains no annotations
+# verify that job contains no annotations through job manager
 #
 # arg1 - jobid
-check_no_annotations() {
+jmgr_check_no_annotations() {
         local id=$1
-        test -z "$(${LIST_JOBS} | grep ${id} | cut -f 6-)" && return 0
+        test -z "$(${JMGR_JOB_LIST} | grep ${id} | cut -f 6-)" && return 0
         return 1
 }
