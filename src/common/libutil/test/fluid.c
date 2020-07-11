82137f790e9b4bbece25edab0241ed4fb1beb563
Mark A. Grondona <mark.grondona@gmail.com>
2020-07-11 00:21:55 +0000

    test: libutil: add tests for FLUID_STRING_F58
    Add unit tests for fluid_encode/decode with F58 strings.

--- src/common/libutil/test/fluid.c
+++ src/common/libutil/test/fluid.c
@@ -8,10 +8,99 @@
  * SPDX-License-Identifier: LGPL-3.0
 \************************************************************/
 
+#include <errno.h>
+
 #include "src/common/libtap/tap.h"
 #include "src/common/libutil/fluid.h"
 
-int main (int argc, char *argv[])
+struct f58_test {
+    fluid_t id;
+    const char *f58;
+};
+
+struct f58_test f58_tests [] = {
+    { 0, "ƒ1" },
+    { 1, "ƒ2" },
+    { 57, "ƒz" },
+    { 1234, "ƒNH" },
+    { 1888, "ƒZZ" },
+    { 3363, "ƒzz" },
+    { 3364, "ƒ211" },
+    { 4369, "ƒ2JL" },
+    { 65535, "ƒLUv" },
+    { 4294967295, "ƒ7YXq9G" },
+    { 633528662, "ƒxyzzy" },
+    { 6731191091817518LL, "ƒuZZybuNNy" },
+    { 18446744073709551614UL, "ƒjpXCZedGfVP" },
+    { 18446744073709551615UL, "ƒjpXCZedGfVQ" },
+    { 0, NULL },
+};
+
+struct f58_test f58_alt_tests [] = {
+    { 0, "f1" },
+    { 0, "f111" },
+    { 1, "f2" },
+    { 57, "fz" },
+    { 1234, "fNH" },
+    { 1888, "fZZ" },
+    { 3363, "fzz" },
+    { 3364, "f211" },
+    { 4369, "f2JL" },
+    { 65535, "fLUv" },
+    { 4294967295, "f7YXq9G" },
+    { 633528662, "fxyzzy" },
+    { 6731191091817518LL, "fuZZybuNNy" },
+    { 18446744073709551614UL, "fjpXCZedGfVP" },
+    { 18446744073709551615UL, "fjpXCZedGfVQ" },
+    { 0, NULL },
+};
+
+void test_f58 (void)
+{
+    fluid_string_type_t type = FLUID_STRING_F58;
+    char buf[16];
+    fluid_t id;
+    struct f58_test *tp = f58_tests;
+    while (tp->f58 != NULL) {
+        ok (fluid_encode (buf, sizeof(buf), tp->id, type) == 0,
+            "f58_encode (%ju)", tp->id);
+        is (buf, tp->f58,
+            "f58_encode %ju -> %s", tp->id, buf);
+        ok (fluid_decode (tp->f58, &id, type) == 0,
+            "f58_decode (%s)", tp->f58);
+        ok (id == tp->id,
+            "%s -> %ju", tp->f58, (uintmax_t) id);
+        tp++;
+    }
+    tp = f58_alt_tests;
+    while (tp->f58 != NULL) {
+        ok (fluid_decode (tp->f58, &id, type) == 0,
+            "f58_decode (%s)", tp->f58);
+        ok (id == tp->id,
+            "%s -> %ju", tp->f58, (uintmax_t) id);
+        tp++;
+    }
+
+    ok (fluid_encode (buf, 1, 1, type) < 0 && errno == EOVERFLOW,
+        "fluid_encode (buf, 1, 1, F58) returns EOVERFLOW");
+    ok (fluid_encode (buf, 5, 65535, type) < 0 && errno == EOVERFLOW,
+        "fluid_encode (buf, 5, 65535, F58) returns EOVERFLOW");
+
+    ok (fluid_decode ("1234", &id, type) < 0 && errno == EINVAL,
+        "fluid_decode ('aaa', FLUID_STRING_F58) returns EINVAL");
+    ok (fluid_decode ("aaa", &id, type) < 0 && errno == EINVAL,
+        "fluid_decode ('aaa', FLUID_STRING_F58) returns EINVAL");
+    ok (fluid_decode ("f", &id, type) < 0 && errno == EINVAL,
+        "fluid_decode ('f', FLUID_STRING_F58) returns EINVAL");
+    ok (fluid_decode ("flux", &id, type) < 0 && errno == EINVAL,
+        "fluid_decode ('flux', FLUID_STRING_F58) returns EINVAL");
+    ok (fluid_decode ("f1230", &id, type) < 0 && errno == EINVAL,
+        "fluid_decode ('f1230', FLUID_STRING_F58) returns EINVAL");
+    ok (fluid_decode ("x1", &id, type) < 0 && errno == EINVAL,
+        "fluid_decode ('x1', FLUID_STRING_F58) returns EINVAL");
+}
+
+void test_basic (void)
 {
     struct fluid_generator gen;
     fluid_t id, id2;
@@ -20,8 +109,6 @@ int main (int argc, char *argv[])
     int generate_errors;
     int encode_errors;
     int decode_errors;
-
-    plan (NO_PLAN);
 
     ok (fluid_init (&gen, 0, 0) == 0,
         "fluid_init id=0 timestamp=0 works");
@@ -136,6 +223,14 @@ int main (int argc, char *argv[])
         "fluid_decode type=MNEMONIC fails on input=bogus");
     ok (fluid_decode ("a-a-a--a-a-a", &id, FLUID_STRING_MNEMONIC) < 0,
         "fluid_decode type=MNEMONIC fails on unknown words xx-xx-xx--xx-xx-xx");
+}
+
+int main (int argc, char *argv[])
+{
+    plan (NO_PLAN);
+
+    test_basic ();
+    test_f58 ();
 
     done_testing ();
     return 0;
