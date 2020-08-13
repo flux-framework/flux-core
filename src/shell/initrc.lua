3f0c51aff01bcc0468b7d765c2a07d8eadad88b2
Mark A. Grondona <mark.grondona@gmail.com>
2020-08-13 01:27:43 +0000

    shell: initrc.lua: support "userrc" shell option
    Problem: Users that want to add additional functionality to a
    custom shell initrc, but still want to call the default system
    initrc.lua have to load the system initrc manually, e.g. via
    
     source ("/etc/flux/shell/initrc.lua")
    
    This is an onerous requirement for casual users.
    
    Instead, add support in the default system initrc.lua for a "userrc"
    shell option, which, if set, will load the specified file after
    loading all system plugins.

--- src/shell/initrc.lua
+++ src/shell/initrc.lua
@@ -5,6 +5,9 @@ plugin.load { file = "*.so", conf = {} }
 -- Source all rc files under shell.rcpath/lua.d/*.lua of shell rcpath:
 source (shell.rcpath .. "/lua.d/*.lua")
 
+-- If userrc is set in shell options, then load the user supplied initrc here:
+if shell.options.userrc then source (shell.options.userrc) end
+
 -- Uncomment to source user initrc.lua if it exists
 -- source_if_exists "~/.flux/shell/initrc.lua"
 
