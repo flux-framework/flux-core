9d9ac44aa8ac29c04ce5c22a974975b1f3d47b28
Jim Garlick <garlick.jim@gmail.com>
2020-08-22 08:47:01 -0700

    broker/state_machine: remove unused func prototypes
    Problem: two function prototypes in the state_machine.h header
    file have no longer have an implementation.
    
    Drop orphaned prototypes.

--- src/broker/state_machine.h
+++ src/broker/state_machine.h
@@ -28,10 +28,6 @@ void state_machine_destroy (struct state_machine *s);
 
 void state_machine_post (struct state_machine *s, const char *event);
 
-broker_state_t state_machine_get_state (struct state_machine *s);
-
-void state_machine_shutdown (struct state_machine *s);
-
 void state_machine_kill (struct state_machine *s, int signum);
 
 #endif /* !_BROKER_STATE_MACHINE_H */
