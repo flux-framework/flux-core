b453c8fedd369523f715683b7a033f3c66130167
Jim Garlick <garlick.jim@gmail.com>
2020-08-27 09:05:24 -0700

    sharness: stop using invalid invalid init.rc2_timeout attr
    Problem: test_under_flux still sets init.rc2_timeout, but
    this timeout was removed in the conversino to 'runat'.
    
    Drop this timeout setting.

--- t/sharness.d/flux-sharness.sh
+++ t/sharness.d/flux-sharness.sh
@@ -94,12 +94,6 @@ test_under_flux() {
     if test -n "$SHARNESS_TEST_DIRECTORY"; then
         cd $SHARNESS_TEST_DIRECTORY
     fi
-    timeout="-o -Sinit.rc2_timeout=300"
-    if test -n "$FLUX_TEST_DISABLE_TIMEOUT"; then
-        timeout=""
-    elif test_have_prereq LONGTEST; then
-        timeout="-o,-Sinit.rc2_timeout=900"
-    fi
 
     if test "$personality" = "minimal"; then
         RC1_PATH=""
@@ -125,7 +119,6 @@ test_under_flux() {
     # Extend timeouts when running under AddressSanitizer
     if test_have_prereq ASAN; then
         gracetime="-o,-g,10"
-        timeout="-o -Sinit.rc2_timeout=800"
         # Set log_path for ASan o/w errors from broker may be lost
         ASAN_OPTIONS=${ASAN_OPTIONS}:log_path=${TEST_NAME}.asan
     fi
@@ -136,7 +129,6 @@ test_under_flux() {
                       ${RC1_PATH+-o -Sbroker.rc1_path=${RC1_PATH}} \
                       ${RC3_PATH+-o -Sbroker.rc3_path=${RC3_PATH}} \
                       ${logopts} \
-                      ${timeout} \
                       ${gracetime} \
                       ${valgrind} \
                      "sh $0 ${flags}"
