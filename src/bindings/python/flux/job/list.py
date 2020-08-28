c1d5eeee364b34f36d9b217a74a8f14c2e634a0f
Mark A. Grondona <mark.grondona@gmail.com>
2020-08-28 23:30:59 +0000

    python: add JobList class for fetching filtered list of all jobs
    Problem: Special code to construct "results" and "states" filters,
    userid filtering, etc is only in flux-jobs.py and not available
    for use by other Python API users.
    
    Add a new flux.job.list.JobList class to the Python API to facilitate
    "filtered" job list queries. By default, all jobs are returned by the
    class, with all "attrs", but filtering by states, results, and userid
    is supported similar to the use in flux-jobs(1).
    
    Fixes #3032

--- src/bindings/python/flux/job/list.py
+++ src/bindings/python/flux/job/list.py
@@ -8,9 +8,36 @@
 # SPDX-License-Identifier: LGPL-3.0
 ###############################################################
 import os
+import pwd
 
+import flux.constants
 from flux.job.info import JobInfo
 from flux.rpc import RPC
+
+
+VALID_ATTRS = [
+    "userid",
+    "priority",
+    "t_submit",
+    "t_depend",
+    "t_sched",
+    "t_run",
+    "t_cleanup",
+    "t_inactive",
+    "state",
+    "name",
+    "ntasks",
+    "nnodes",
+    "ranks",
+    "success",
+    "exception_occurred",
+    "exception_type",
+    "exception_severity",
+    "exception_note",
+    "result",
+    "expiration",
+    "annotations",
+]
 
 
 class JobListRPC(RPC):
@@ -63,3 +90,85 @@ class JobListIdRPC(RPC):
 def job_list_id(flux_handle, jobid, attrs=[]):
     payload = {"id": int(jobid), "attrs": attrs}
     return JobListIdRPC(flux_handle, "job-info.list-id", payload)
+
+
+class JobList:
+    STATES = {
+        "depend": flux.constants.FLUX_JOB_DEPEND,
+        "sched": flux.constants.FLUX_JOB_SCHED,
+        "run": flux.constants.FLUX_JOB_RUN,
+        "cleanup": flux.constants.FLUX_JOB_CLEANUP,
+        "inactive": flux.constants.FLUX_JOB_INACTIVE,
+        "pending": flux.constants.FLUX_JOB_PENDING,
+        "running": flux.constants.FLUX_JOB_RUNNING,
+        "active": flux.constants.FLUX_JOB_ACTIVE,
+    }
+    RESULTS = {
+        "completed": flux.constants.FLUX_JOB_RESULT_COMPLETED,
+        "failed": flux.constants.FLUX_JOB_RESULT_FAILED,
+        "cancelled": flux.constants.FLUX_JOB_RESULT_CANCELLED,
+        "timeout": flux.constants.FLUX_JOB_RESULT_TIMEOUT,
+    }
+
+    def __init__(
+        self,
+        flux_handle,
+        attrs=VALID_ATTRS,
+        filters=[],
+        user=None,
+        max_entries=1000,
+        ids=None,
+    ):
+        self.handle = flux_handle
+        self.attrs = list(attrs)
+        self.states = 0
+        self.results = 0
+        self.max_entries = max_entries
+        self.ids = ids
+        for fname in filters:
+            for name in fname.split(","):
+                self.add_filter(name)
+        self.set_user(user)
+
+    def set_user(self, user):
+        if user is None:
+            self.userid = os.getuid()
+        elif user == "all":
+            self.userid = flux.constants.FLUX_USERID_UNKNOWN
+        else:
+            try:
+                self.userid = pwd.getpwnam(user).pw_uid
+            except KeyError:
+                self.userid = int(user)
+            except ValueError:
+                raise ValueError(f"Invalid user {user} specified")
+
+    def add_filter(self, fname):
+        fname = fname.lower()
+        if fname == "all":
+            self.states |= self.STATES["pending"]
+            self.states |= self.STATES["running"]
+            return
+
+        if fname in self.STATES:
+            self.states |= self.STATES[fname]
+        elif fname in self.RESULTS:
+            # Must specify "inactive" to get results:
+            self.states |= self.STATES["inactive"]
+            self.results |= self.RESULTS[fname]
+        else:
+            raise ValueError(f"Invalid filter specified: {fname}")
+
+    def fetch_jobs(self):
+        return job_list(
+            self.handle,
+            max_entries=self.max_entries,
+            attrs=self.attrs,
+            userid=self.userid,
+            states=self.states,
+            results=self.results,
+        )
+
+    def jobs(self):
+        for job in self.fetch_jobs().get_jobs():
+            yield JobInfo(job)
