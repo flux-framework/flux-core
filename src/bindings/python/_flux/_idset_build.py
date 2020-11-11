e101b7896582325b2c4e7e646234475ff184b89f
Mark A. Grondona <mark.grondona@gmail.com>
2020-11-11 13:59:42 -0800

    python: add flux.idset binding
    Problem: There is no Python interface for RFC22 Idset encoding.
    
    Add a simple Python IDset class which wraps libidset so that Python
    programs can encode/decode and manipulate idsets with full compatibility
    with the C library.

---
+++ src/bindings/python/_flux/_idset_build.py
@@ -0,0 +1,28 @@
+from cffi import FFI
+
+ffi = FFI()
+
+ffi.set_source(
+    "_flux._idset",
+    """
+#include <flux/idset.h>
+
+
+// TODO: remove this when we can use cffi 1.10
+#ifdef __GNUC__
+#pragma GCC visibility push(default)
+#endif
+            """,
+)
+
+cdefs = """
+static const unsigned int IDSET_INVALID_ID;
+void free (void *);
+"""
+
+with open("_idset_preproc.h") as h:
+    cdefs = cdefs + h.read()
+
+ffi.cdef(cdefs)
+if __name__ == "__main__":
+    ffi.emit_c_code("_idset.c")
