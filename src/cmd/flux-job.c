42aede44f82a7bb1d8821410f7601c0b372f4c5c
Albert Chu <chu11@llnl.gov>
2020-11-17 15:04:05 -0800

    cmd/flux-job: Retrieve t_priority in flux job list

--- src/cmd/flux-job.c
+++ src/cmd/flux-job.c
@@ -1033,8 +1033,8 @@ static const char *list_attrs =
     "\"name\",\"ntasks\",\"nnodes\",\"ranks\",\"nodelist\",\"expiration\"," \
     "\"success\",\"exception_occurred\",\"exception_severity\"," \
     "\"exception_type\",\"exception_note\",\"result\","  \
-    "\"t_depend\",\"t_sched\",\"t_run\",\"t_cleanup\",\"t_inactive\"," \
-    "\"annotations\"]";
+    "\"t_depend\",\"t_priority\",\"t_sched\",\"t_run\",\"t_cleanup\"," \
+    "\"t_inactive\"," "\"annotations\"]";
 
 int cmd_list (optparse_t *p, int argc, char **argv)
 {
