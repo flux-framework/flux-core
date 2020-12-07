8f1e3db0704cb0c1c70d4e223edb42109349bb6a
Albert Chu <chu11@llnl.gov>
2020-12-07 15:22:34 -0800

    job-info: Add comment on flux-restart coverage
    Add comment noting that a flux-restart path is extremely hard to cover
    given how job-info and job-manager are currently implemented.

--- src/modules/job-info/job_state.c
+++ src/modules/job-info/job_state.c
@@ -1687,6 +1687,12 @@ static int journal_process_event (struct job_state_ctx *jsctx, json_t *event)
             return -1;
     }
     else if (!strcmp (name, "flux-restart")) {
+        /* Presently, job-info depends on job-manager.events-journal
+         * service.  So if job-manager reloads, job-info must be
+         * reloaded, making the probability of reaching this
+         * `flux-restart` path very low.  Code added for completeness
+         * and in case dependency removed in the future.
+         */
         if (journal_revert_job (jsctx,
                                 id,
                                 eventlog_seq,
