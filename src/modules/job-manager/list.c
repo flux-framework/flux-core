f4493001b51ff264d90c600bd76a7112dde75b7d
Mark A. Grondona <mark.grondona@gmail.com>
2021-03-25 10:47:04 -0700

    job-manager: add any dependencies to built-in list RPC
    Problem: Job dependencies are missing from job-manager internal
    list RPC.
    
    If any dependencies are set for a job, add them to the list RPC response.

--- src/modules/job-manager/list.c
+++ src/modules/job-manager/list.c
@@ -64,6 +64,14 @@ int list_append_job (json_t *jobs, struct job *job)
             return -1;
         }
     }
+    if (job->dependencies) {
+        json_t *deps = grudgeset_tojson (job->dependencies);
+        if (json_object_set (o, "dependencies", deps) < 0) {
+            json_decref (o);
+            errno = ENOMEM;
+            return -1;
+        }
+    }
     if (json_array_append_new (jobs, o) < 0) {
         json_decref (o);
         errno = ENOMEM;
