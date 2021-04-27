c1e800006f94db78e48fc3cd8ef2d2afe99fc2ee
Mark A. Grondona <mark.grondona@gmail.com>
2021-04-27 14:40:55 -0700

    testsuite: use bulksubmit for inception tests
    Problem: The CI "inception" tests use prove(1) to run each sharness
    test as a Flux job, but prove is meant for console use and is thus
    unwieldy in CI.
    
    Add a script, t/test-inception.sh which runs the tests using
    `flux mini bulksubmit` instead of prove(1). Dump the output of any
    failing tests to <test>.log so that the checks-annotate.sh script
    can find output. Exit with failure if any test failed.

---
+++ t/test-inception.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#  Run all Flux tests as Flux jobs using bulksubmit.
+#  This must be run from the sharness test directory ./t.
+#
+
+stats() {
+    while :; do
+       echo === $(flux jobs --stats-only) ===
+       sleep 5
+    done 
+}
+
+stats &
+PID=$!
+export FLUX_TESTS_LOGFILE=t
+flux mini bulksubmit --watch -q ./{} ::: t[0-9]*.t python/t*.py lua/t*.t
+RC=$?
+kill $PID
+wait
+
+# dump output of failed jobs to *.log
+for id in $(flux jobs -f failed -no {id}); do
+    name=$(flux jobs -no {name} ${id})
+    flux job attach $id > ${name/.t}.log 2>&1
+done
+ 
+# print failed jobs listing to stdout
+flux jobs -f failed
+
+# exit with failure if any tests failed
+exit $RC
