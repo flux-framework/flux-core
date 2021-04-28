fe3440d405c7edfcdbfe6cfca64261c80ff90d99
Jim Garlick <garlick.jim@gmail.com>
2021-04-28 06:37:22 -0700

    libczmqcontainers: rename czmq_rename.h
    Problem: we need to add additional container classes and adding
    a new "rename" header for each seems unnecessary.
    
    Move zhashx_map.h to czmq_rename.h.
    Update header guard.

--- src/common/libczmqcontainers/czmq_rename.h
+++ src/common/libczmqcontainers/czmq_rename.h
@@ -8,8 +8,8 @@
  * SPDX-License-Identifier: LGPL-3.0
 \************************************************************/
 
-#ifndef _ZHASHX_MAP_H
-#define _ZHASHX_MAP_H
+#ifndef _CZMQ_RENAME_H
+#define _CZMQ_RENAME_H
 
 #ifdef __cplusplus
 extern "C" {
