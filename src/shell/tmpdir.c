e51dcdcd2d5fe48186b5c15ae43fa4298ac7f530
Jim Garlick <garlick.jim@gmail.com>
2021-05-19 10:43:06 -0700

    shell: add tmpdir plugin for FLUX_JOB_TMPDIR
    Problem: MPI shell plugins and perhaps other future applications can
    benefit from the availability of job-specific temporary directories.
    
    Create a job-specific directory when the shell starts.
    Set FLUX_JOB_TMPDIR to the new directory in the job environment.
    In the shell's atexit handler, remove the directory recursively.
    In case a job spans multiple brokers on the same node, avoid cleanup
    races by including the shell rank in the directory name.
    
    Attempt to create the directory in the broker's rundir.  If that
    fails, as it would for guest users, fall back to ${TMPDIR:-/tmp}.
    
    Note that cleanup by the shell is not reliable since it may be
    terminated early.  However, that is mitigated in most cases by the
    fact that the broker rundir is cleaned up by the broker itself or
    by flux-start.  The system instance should employ an epilog script
    or similar to clean up directories in ${TMPDIR:-/tmp}.
    
    If TMPDIR was set in the job environment, ensure that it exists,
    replacing the tmpdir.lua shell plugin, which was added to address #3326.
    
    Since other plugins (such as the batch plugin) may depend on TMPDIR
    and/or FLUX_JOB_TMPDIR, place this plugin at the top of the stack.

---
+++ src/shell/tmpdir.c
@@ -0,0 +1,119 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "src/common/libutil/cleanup.h"
+
+#include "builtins.h"
+#include "internal.h"
+#include "info.h"
+
+static int mkdir_exist_ok (const char *path, bool quiet)
+{
+    if (mkdir (path, 0700) < 0) {
+        if (errno != EEXIST) {
+            if (!quiet)
+                shell_log_errno ("mkdir %s", path);
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static int make_job_path (flux_shell_t *shell,
+                          const char *parent,
+                          char *buf,
+                          size_t size)
+{
+    size_t n;
+
+    n = snprintf (buf, size, "%s/jobtmp-%d-", parent, shell->info->shell_rank);
+    if (n >= size) {
+        errno = EOVERFLOW;
+        return -1;
+    }
+    if (flux_job_id_encode (shell->jobid, "f58", buf + n, size - n) < 0)
+        return -1;
+    return 0;
+}
+
+static int mkjobtmp_rundir (flux_shell_t *shell, char *buf, size_t size)
+{
+    const char *rundir;
+
+    if (shell->standalone
+        || !(rundir = flux_attr_get (shell->h, "rundir"))
+        || make_job_path (shell, rundir, buf, size) < 0
+        || mkdir_exist_ok (buf, true) < 0)
+        return -1;
+    return 0;
+}
+
+static int mkjobtmp_tmpdir (flux_shell_t *shell, char *buf, size_t size)
+{
+    const char *tmpdir = flux_shell_getenv (shell, "TMPDIR");
+
+    if (make_job_path (shell, tmpdir ? tmpdir : "/tmp", buf, size) < 0
+        || mkdir_exist_ok (buf, false) < 0)
+        return -1;
+    return 0;
+}
+
+static int mktmpdir (flux_shell_t *shell)
+{
+    const char *tmpdir = flux_shell_getenv (shell, "TMPDIR");
+
+    if (tmpdir && mkdir_exist_ok (tmpdir, false) < 0)
+        return -1;
+    return 0;
+}
+
+static int tmpdir_init (flux_plugin_t *p,
+                        const char *topic,
+                        flux_plugin_arg_t *args,
+                        void *data)
+{
+    flux_shell_t *shell = flux_plugin_get_shell (p);
+    char jobtmp[1024];
+
+    /* Ensure TMPDIR exists if it is set in job environment
+     */
+    if (mktmpdir (shell) < 0)
+        shell_die_errno (1, "error creating TMPDIR");
+
+    /* Try to create jobtmp in broker rundir.
+     * Fall back to ${TMPDIR:-/tmp} if that fails (e.g. guest user).
+     */
+    if (mkjobtmp_rundir (shell, jobtmp, sizeof (jobtmp)) < 0
+        && mkjobtmp_tmpdir (shell, jobtmp, sizeof (jobtmp)) < 0)
+        shell_die_errno (1, "error creating FLUX_JOB_TMPDIR");
+    cleanup_push_string (cleanup_directory_recursive, jobtmp);
+
+    /* Set/change FLUX_JOB_TMPDIR to jobtmp.
+     * If TMPDIR is unset, set it to $FLUX_JOB_TMPDIR.
+     */
+    if (flux_shell_setenvf (shell, 1, "FLUX_JOB_TMPDIR", "%s", jobtmp) < 0
+        || flux_shell_setenvf (shell, 0, "TMPDIR", "%s", jobtmp) < 0)
+        shell_die_errno (1, "error updating job environment");
+
+    return 0;
+}
+
+struct shell_builtin builtin_tmpdir = {
+    .name = "tmpdir",
+    .init = tmpdir_init,
+};
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
