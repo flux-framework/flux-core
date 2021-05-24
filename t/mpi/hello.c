c050140861036ac0439f306d777113d74e3efb6b
Jim Garlick <garlick.jim@gmail.com>
2021-05-24 16:18:07 -0700

    t/mpi/hello: use monotime
    Problem: hello.c duplicates functionality in libutil/monotime.
    
    Include monotime.h and calla those functions.

--- t/mpi/hello.c
+++ t/mpi/hello.c
@@ -8,65 +8,45 @@
  * SPDX-License-Identifier: LGPL-3.0
 \************************************************************/
 
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <mpi.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
 
-static struct timespec diff (struct timespec start, struct timespec end)
-{
-    struct timespec temp;
-    if ((end.tv_nsec-start.tv_nsec)<0) {
-        temp.tv_sec = end.tv_sec-start.tv_sec-1;
-        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-    } else {
-        temp.tv_sec = end.tv_sec-start.tv_sec;
-        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-    }
-    return temp;
-}
-
-static double time_since (struct timespec t0)
-{
-    struct timespec ts, d;
-    clock_gettime (CLOCK_MONOTONIC, &ts);
-
-    d = diff (t0, ts);
-
-    return ((double) d.tv_sec * 1000 + (double) d.tv_nsec / 1000000);
-}
+#include "src/common/libutil/monotime.h"
 
 int main (int argc, char *argv[])
 {
     int id, ntasks;
     struct timespec t;
 
-    clock_gettime (CLOCK_MONOTONIC, &t);
+    monotime (&t);
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &id);
     MPI_Comm_size (MPI_COMM_WORLD, &ntasks);
-
     if (id == 0) {
         printf ("0: completed MPI_Init in %0.3fs.  There are %d tasks\n",
-                time_since (t)/1000, ntasks);
+                monotime_since (t) / 1000, ntasks);
         fflush (stdout);
     }
-	if (id == 0)
-		clock_gettime (CLOCK_MONOTONIC, &t);
+
+    monotime (&t);
     MPI_Barrier (MPI_COMM_WORLD);
     if (id == 0) {
         printf ("0: completed first barrier in %0.3fs\n",
-                time_since (t) / 1000);
+                monotime_since (t) / 1000);
         fflush (stdout);
     }
 
-    if (id == 0)
-        clock_gettime (CLOCK_MONOTONIC, &t);
+    monotime (&t);
     MPI_Finalize ();
     if (id == 0) {
         printf ("0: completed MPI_Finalize in %0.3fs\n",
-                time_since (t) / 1000);
+                monotime_since (t) / 1000);
         fflush (stdout);
     }
     return 0;
