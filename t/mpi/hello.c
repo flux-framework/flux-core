be5bfd62d9093271da7240c87da114776608d8c4
Jim Garlick <garlick.jim@gmail.com>
2021-05-24 16:28:36 -0700

    t/mpi/hello: conform to RFC 7
    Problem: hello.c often doesn't include a space between function
    name and open parenthesis, violating RFC 7.
    
    Add a space where it is missing.

--- t/mpi/hello.c
+++ t/mpi/hello.c
@@ -37,38 +37,37 @@ static double time_since (struct timespec t0)
     return ((double) d.tv_sec * 1000 + (double) d.tv_nsec / 1000000);
 }
 
-int
-main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
     int id, ntasks;
     struct timespec t;
 
     clock_gettime (CLOCK_MONOTONIC, &t);
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank (MPI_COMM_WORLD, &id);
+    MPI_Comm_size (MPI_COMM_WORLD, &ntasks);
 
     if (id == 0) {
-        printf("0: completed MPI_Init in %0.3fs.  There are %d tasks\n",
-               time_since (t)/1000, ntasks);
-        fflush(stdout);
+        printf ("0: completed MPI_Init in %0.3fs.  There are %d tasks\n",
+                time_since (t)/1000, ntasks);
+        fflush (stdout);
     }
 	if (id == 0)
 		clock_gettime (CLOCK_MONOTONIC, &t);
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier (MPI_COMM_WORLD);
     if (id == 0) {
-        printf("0: completed first barrier in %0.3fs\n",
-               time_since (t) / 1000);
-        fflush(stdout);
+        printf ("0: completed first barrier in %0.3fs\n",
+                time_since (t) / 1000);
+        fflush (stdout);
     }
 
     if (id == 0)
         clock_gettime (CLOCK_MONOTONIC, &t);
-    MPI_Finalize();
+    MPI_Finalize ();
     if (id == 0) {
-        printf("0: completed MPI_Finalize in %0.3fs\n",
-               time_since (t) / 1000);
-        fflush(stdout);
+        printf ("0: completed MPI_Finalize in %0.3fs\n",
+                time_since (t) / 1000);
+        fflush (stdout);
     }
     return 0;
 }
