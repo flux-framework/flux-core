7cea453e201fd88bf660cf86300480c0ce14b279
James Corbett <corbett8@llnl.gov>
2021-06-10 16:37:27 -0700

    libjob/jobspec1: add jobspec V1 library
    Add a set of public functions to libjob for generating
    and modifying jobspec V1s, per HPE/Cray use case with
    their Common Tools Interface. Add unit tests.

---
+++ src/common/libjob/jobspec1.h
@@ -0,0 +1,129 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef _FLUX_CORE_JOBSPEC1_H
+#define _FLUX_CORE_JOBSPEC1_H
+
+typedef struct flux_jobspec1 flux_jobspec1_t;
+
+/* Remove the value in the jobspec's 'attributes' section at the given path,
+ * where "." is treated as a path separator. Return 0 on success, -1 on error.
+ * It is not an error if 'path' does not exist.
+ */
+int flux_jobspec1_attr_del (flux_jobspec1_t *jobspec, const char *path);
+
+/* Set the value in the jobspec's 'attributes' section at the given path,
+ * where "." is treated as a path separator.
+ * 'fmt' should be a jansson pack-style string corresponding to the
+ * remaining arguments.
+ * Return 0 on success, -1 on error.
+ */
+int flux_jobspec1_attr_pack (flux_jobspec1_t *jobspec,
+                             const char *path,
+                             const char *fmt,
+                             ...);
+
+/* Unpack the values in the jobspec's 'attributes' section at the given path,
+ * where "." is treated as a path separator.
+ * 'fmt' should be a jansson unpack-style string corresponding to the
+ * remaining args.
+ * Return 0 on success, -1 on error.
+ */
+int flux_jobspec1_attr_unpack (flux_jobspec1_t *jobspec,
+                               const char *path,
+                               const char *fmt,
+                               ...);
+
+/* Check the validity of the 'attributes' section of a jobspec, writing an error
+ * message to 'errbuf'.
+ * Return 0 on success, -1 on error with an error message written.
+ * This function succeeding does not guarantee that the jobspec is valid.
+ */
+int flux_jobspec1_attr_check (flux_jobspec1_t *jobspec, char *errbuf, int errbufsz);
+
+/* Remove an entry in a Jobspec's environment.
+ * This function succeeds regardless of the presence of the variable.
+ * However, it will fail if the environment object does not exist.
+ * Return 0 on success, nonzero on error.
+ */
+int flux_jobspec1_unsetenv (flux_jobspec1_t *jobspec, const char *name);
+
+/* Add the variable 'name' to the environment
+ * with the value 'value', if name does not already exist.  If 'name'
+ * does exist in the environment, then its value is changed to 'value'
+ * if 'overwrite' is nonzero; if 'overwrite' is zero, then the value of
+ *'name' is not changed (and this function returns a success status).
+ * Return 0 on success, nonzero with errno set on error.
+ */
+int flux_jobspec1_setenv (flux_jobspec1_t *jobspec,
+                           const char *name,
+                           const char *value,
+                           int overwrite);
+
+/* Direct the stdin of a jobspec to a path given by
+ * 'stdin'. Return 0 on success, -1 on error.
+ */
+int flux_jobspec1_set_stdin (flux_jobspec1_t *jobspec, const char *path);
+
+/* Direct the stdout of a jobspec to a path given by
+ * 'stdout'. Return 0 on success, -1 on error.
+ */
+int flux_jobspec1_set_stdout (flux_jobspec1_t *jobspec, const char *path);
+
+/* Direct the stderr of a jobspec to a path given by
+ * 'stderr'. Return 0 on success, -1 on error.
+ */
+int flux_jobspec1_set_stderr (flux_jobspec1_t *jobspec, const char *path);
+
+/* Set the working directory of a jobspec.
+ * Return 0 on success, -1 on error.
+ */
+int flux_jobspec1_set_cwd (flux_jobspec1_t *jobspec, const char *cwd);
+
+/* Encode a jobspec to a string, e.g. for usage with ``flux_job_submit()``.
+ * 'flags' should be 0.
+ * Return NULL with errno set on error.
+ * The return value must be released with ``free()``.
+ */
+char *flux_jobspec1_encode (flux_jobspec1_t *jobspec, size_t flags);
+
+/* Create and return a minimum viable V1 Jobspec.
+ * The jobspec will have stdin, stdout, stderr all directed to the KVS,
+ * and the environment will be empty.
+ * The jobspec must be released with 'flux_jobspec1_free'.
+ * 'argc' and 'argv' should collectively define the command and its
+ * arguments.
+ * 'env' should be an 'environ(7)'-style array, where NULL indicates an
+ * empty environment.
+ * 'tasks' should be the number of tasks to launch
+ * 'cores_per_task' should be the number of cores per task to allocate
+ * 'gpus_per_task' should be the number of gpus per task to allocate
+ * 'nodes' should be the number of nodes to spread the allocated cores
+ * and gpus across. If 0, the scheduler will determine how to distribute
+ * the resources.
+ * Return NULL on error with errno set.
+ */
+flux_jobspec1_t *flux_jobspec1_from_command (int argc,
+                                             char **argv,
+                                             char **env,
+                                             int ntasks,
+                                             int cores_per_task,
+                                             int gpus_per_task,
+                                             int nnodes,
+                                             double duration);
+
+/* Free a jobspec. */
+void flux_jobspec1_destroy (flux_jobspec1_t *jobspec);
+
+#endif /* _FLUX_CORE_JOBSPEC1_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ src/common/libjob/test/jobspec1.c
@@ -0,0 +1,416 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <errno.h>
+#include <jansson.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "src/common/libjob/jobspec1.h"
+#include "src/common/libtap/tap.h"
+
+extern char **environ;
+
+int free_wrapper (void *ptr)
+{
+    free (ptr);
+    return 1;
+}
+
+int decref_wrapper (json_t *ptr)
+{
+    json_decref (ptr);
+    return 1;
+}
+
+void check_stdio_cwd (void)
+{
+    char *argv[] = {"this", "is", "a", "test"};
+    int argc = sizeof (argv) / sizeof (argv[0]);
+    flux_jobspec1_t *jobspec;
+    char *path;
+
+    if (!(jobspec = flux_jobspec1_from_command (argc, argv, NULL, 1, 1, 1, 0, 0.0))) {
+        BAIL_OUT ("flux_jobspec1_from_command failed");
+    }
+    ok (flux_jobspec1_set_cwd (NULL, "/foo/bar/baz") < 0 && errno == EINVAL,
+        "flux_jobspec1_set_cwd catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_set_cwd (jobspec, NULL) < 0 && errno == EINVAL,
+        "flux_jobspec1_set_cwd catches NULL cwd");
+    errno = 0;
+    ok (flux_jobspec1_set_cwd (jobspec, "/foo/bar/baz") == 0
+            && (flux_jobspec1_attr_unpack (jobspec, "system.cwd", "s", &path) == 0)
+
+            && (strcmp ("/foo/bar/baz", path) == 0),
+        "flux_jobspec1_set_cwd works");
+    ok (flux_jobspec1_set_stdin (NULL, "/foo/bar/baz") < 0 && errno == EINVAL,
+        "flux_jobspec1_set_stdin catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_set_stdin (jobspec, NULL) < 0 && errno == EINVAL,
+        "flux_jobspec1_set_stdin catches NULL path");
+    errno = 0;
+    ok (flux_jobspec1_set_stdin (jobspec, "/foo/bar/stdin.txt") == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.shell.options.input.stdin.path",
+                                           "s",
+                                           &path)
+                == 0)
+
+            && (strcmp ("/foo/bar/stdin.txt", path) == 0),
+        "flux_jobspec1_set_stdin sets right path");
+    ok ((flux_jobspec1_attr_unpack (jobspec,
+                                    "system.shell.options.input.stdin.type",
+                                    "s",
+                                    &path)
+         == 0)
+            && (strcmp ("file", path) == 0),
+        "flux_jobspec1_set_stdin sets right type");
+    ok (flux_jobspec1_set_stdout (NULL, "/foo/bar/baz") < 0 && errno == EINVAL,
+        "flux_jobspec1_set_stdout catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_set_stdout (jobspec, NULL) < 0 && errno == EINVAL,
+        "flux_jobspec1_set_stdout catches NULL path");
+    errno = 0;
+    ok (flux_jobspec1_set_stdout (jobspec, "/foo/bar/stdout.txt") == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.shell.options.output.stdout.path",
+                                           "s",
+                                           &path)
+                == 0)
+
+            && (strcmp ("/foo/bar/stdout.txt", path) == 0),
+        "flux_jobspec1_set_stdout sets right path");
+    ok ((flux_jobspec1_attr_unpack (jobspec,
+                                    "system.shell.options.output.stdout.type",
+                                    "s",
+                                    &path)
+         == 0)
+            && (strcmp ("file", path) == 0),
+        "flux_jobspec1_set_stdout sets right type");
+    ok (flux_jobspec1_set_stderr (NULL, "/foo/bar/baz") < 0 && errno == EINVAL,
+        "flux_jobspec1_set_stderr catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_set_stderr (jobspec, NULL) < 0 && errno == EINVAL,
+        "flux_jobspec1_set_stderr catches NULL path");
+    errno = 0;
+    ok (flux_jobspec1_set_stderr (jobspec, "/foo/bar/stderr.txt") == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.shell.options.output.stderr.path",
+                                           "s",
+                                           &path)
+                == 0)
+
+            && (strcmp ("/foo/bar/stderr.txt", path) == 0),
+        "flux_jobspec1_set_stderr sets right path");
+    ok ((flux_jobspec1_attr_unpack (jobspec,
+                                    "system.shell.options.output.stderr.type",
+                                    "s",
+                                    &path)
+         == 0)
+            && (strcmp ("file", path) == 0),
+        "flux_jobspec1_set_stderr sets right type");
+    flux_jobspec1_destroy (jobspec);
+}
+
+void check_env (void)
+{
+    char *argv[] = {"this", "is", "a", "test"};
+    int argc = sizeof (argv) / sizeof (argv[0]);
+    flux_jobspec1_t *jobspec;
+    char *val;
+
+    if (!(jobspec =
+              flux_jobspec1_from_command (argc, argv, environ, 1, 1, 1, 0, 0.0))) {
+        BAIL_OUT ("flux_jobspec1_from_command failed with environ");
+    }
+    ok (flux_jobspec1_setenv (NULL, "FOO", "BAR", 1) < 0 && errno == EINVAL,
+        "flux_jobspec1_setenv catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_setenv (jobspec, NULL, "BAR", 1) < 0 && errno == EINVAL,
+        "flux_jobspec1_setenv catches NULL variable name");
+    errno = 0;
+    ok (flux_jobspec1_setenv (jobspec, "FOO", NULL, 1) < 0 && errno == EINVAL,
+        "flux_jobspec1_setenv catches NULL variable value");
+    errno = 0;
+    ok (flux_jobspec1_unsetenv (NULL, "FOO") < 0 && errno == EINVAL,
+        "flux_jobspec1_unsetenv catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_unsetenv (jobspec, NULL) < 0 && errno == EINVAL,
+        "flux_jobspec1_unsetenv catches NULL variable");
+    errno = 0;
+    ok (flux_jobspec1_setenv (jobspec, "FOO1", "BAR1", 1) == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.environment.FOO1",
+                                           "s",
+                                           &val)
+                == 0)
+            && (strcmp ("BAR1", val) == 0),
+        "jobspec_setenv FOO1=BAR1 works");
+    ok (flux_jobspec1_setenv (jobspec, "FOO1", "BAZ1", 1) == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.environment.FOO1",
+                                           "s",
+                                           &val)
+                == 0)
+            && (strcmp ("BAZ1", val) == 0),
+        "jobspec_setenv FOO1=BAZ1 works (overwrite=1)");
+    ok (flux_jobspec1_setenv (jobspec, "FOO1", "BAZ2", 0) == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.environment.FOO1",
+                                           "s",
+                                           &val)
+                == 0)
+            && (strcmp ("BAZ1", val) == 0),
+        "jobspec_setenv FOO1=BAZ2 works (overwrite=0)");
+    ok (flux_jobspec1_unsetenv (jobspec, "FOO1") == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.environment.FOO1",
+                                           "s",
+                                           &val)
+                < 0),
+        "unset FOO1 works");
+    ok (flux_jobspec1_setenv (jobspec, "FOO2", "BAR2", 1) == 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.environment.FOO2",
+                                           "s",
+                                           &val)
+                == 0)
+            && (strcmp ("BAR2", val) == 0),
+        "jobspec_setenv FOO2=BAR2 works");
+
+    // test functions when environment object is deleted
+    ok (flux_jobspec1_attr_del (jobspec, "system.environment") == 0,
+        "deleting environment works");
+    ok (flux_jobspec1_setenv (jobspec, "FOO1", "BAR1", 1) < 0
+            && (flux_jobspec1_attr_unpack (jobspec,
+                                           "system.environment.FOO1",
+                                           "s",
+                                           &val)
+                < 0),
+        "can't set FOO1=BAR1 after deleting environment object");
+    ok (flux_jobspec1_unsetenv (jobspec, "FOO") < 0,
+        "flux_jobspec1_unsetenv fails after deleting environment");
+    flux_jobspec1_destroy (jobspec);
+}
+
+void check_attr (void)
+{
+    char *argv[] = {"this", "is", "a", "test"};
+    int argc = sizeof (argv) / sizeof (argv[0]);
+    flux_jobspec1_t *jobspec;
+    json_t *json_ptr;
+    int int_val;
+    char *char_ptr;
+
+    if (!(jobspec = flux_jobspec1_from_command (argc, argv, NULL, 1, 1, 1, 0, 0.0))) {
+        BAIL_OUT ("flux_jobspec1_from_command failed");
+    }
+    ok (flux_jobspec1_attr_pack (NULL, "foo.bar", "i", 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_pack catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_attr_pack (jobspec, NULL, "i", 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_pack catches NULL path");
+    errno = 0;
+    ok (flux_jobspec1_attr_pack (jobspec, "foo.bar", NULL, 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_pack catches NULL format string");
+    errno = 0;
+    ok (flux_jobspec1_attr_unpack (NULL, "foo.bar", "i", 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_unpack catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_attr_unpack (jobspec, NULL, "i", 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_unpack catches NULL path");
+    errno = 0;
+    ok (flux_jobspec1_attr_unpack (jobspec, "foo.bar", NULL, 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_unpack catches NULL format string");
+    errno = 0;
+    ok (flux_jobspec1_attr_del (jobspec, NULL) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_del catches NULL path");
+    errno = 0;
+    ok (flux_jobspec1_attr_del (NULL, "foo.bar") < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_del catches NULL jobspec");
+    errno = 0;
+
+    ok (flux_jobspec1_attr_pack (jobspec, "foo.bar", "s", "baz") == 0
+            && (flux_jobspec1_attr_unpack (jobspec, "foo.bar", "s", &char_ptr) == 0)
+            && strcmp ("baz", char_ptr) == 0,
+        "flux_jobspec1_attr_pack works on strings");
+    ok (flux_jobspec1_attr_pack (jobspec, "foo.bar", "i", 19) == 0
+            && (flux_jobspec1_attr_unpack (jobspec, "foo.bar", "i", &int_val) == 0)
+            && 19 == int_val,
+        "flux_jobspec1_attr_pack works on integers");
+    ok (flux_jobspec1_attr_pack (jobspec, "foo", "{s:s}", "bar", "baz") == 0
+            && (flux_jobspec1_attr_unpack (jobspec, "foo", "{s:s}", "bar", &char_ptr)
+                == 0)
+            && strcmp ("baz", char_ptr) == 0,
+        "flux_jobspec1_attr_pack works on objects");
+    ok (flux_jobspec1_attr_del (jobspec, "foo.bar.baz") == 0
+            && (flux_jobspec1_attr_unpack (jobspec, "foo.bar.baz", "o", &json_ptr) < 0),
+        "flux_jobspec1_attr_del works");
+    ok (flux_jobspec1_attr_del (jobspec, "foo") == 0
+            && (flux_jobspec1_attr_unpack (jobspec, "foo", "o", &json_ptr) < 0),
+        "flux_jobspec1_attr_del works");
+    flux_jobspec1_destroy (jobspec);
+}
+
+void check_jobspec (void)
+{
+    char *argv[] = {"this", "is", "a", "test"};
+    int argc = sizeof (argv) / sizeof (argv[0]);
+    flux_jobspec1_t *jobspec;
+    char errbuf[50];
+    char *str;
+    json_t *val;
+    double passed_duration = 5.0;
+    double duration;
+
+    if (!(jobspec = flux_jobspec1_from_command (argc,
+                                                argv,
+                                                NULL,
+                                                1,
+                                                1,
+                                                1,
+                                                0,
+                                                passed_duration))) {
+        BAIL_OUT ("flux_jobspec1_from_command failed");
+    }
+    ok (flux_jobspec1_attr_check (NULL, errbuf, 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_check catches NULL jobspec");
+    errno = 0;
+    ok (flux_jobspec1_attr_check (jobspec, NULL, 5) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_check catches NULL errbuf");
+    errno = 0;
+    ok (flux_jobspec1_attr_check (jobspec, errbuf, -1) < 0 && errno == EINVAL,
+        "flux_jobspec1_attr_check catches invalid errbuf size");
+    errno = 0;
+
+    ok (flux_jobspec1_attr_check (jobspec, errbuf, sizeof (errbuf)) == 0,
+        "flux_jobspec1_attr_check passed");
+    ok (flux_jobspec1_attr_unpack (jobspec, "system", "o", &val) == 0,
+        "jobspec has system attribute");
+    ok (flux_jobspec1_attr_unpack (jobspec, "system.duration", "f", &duration) == 0
+            && (duration - passed_duration < 0.001),
+        "jobspec has system.duration attribute set to correct value");
+    ok (flux_jobspec1_attr_unpack (jobspec, "system.environment", "o", &val) == 0
+            && json_is_object (val) && json_object_size (val) == 0,
+        "jobspec has system.environment object of size 0");
+    ok (flux_jobspec1_attr_unpack (jobspec, "foo.bar", "s", &str) < 0,
+        "jobspec has no foo.bar attribute");
+    flux_jobspec1_destroy (jobspec);
+    passed_duration = 0.0;
+    if (!(jobspec = flux_jobspec1_from_command (argc,
+                                                argv,
+                                                NULL,
+                                                1,
+                                                1,
+                                                1,
+                                                0,
+                                                passed_duration))) {
+        BAIL_OUT ("flux_jobspec1_from_command failed");
+    }
+    ok ((flux_jobspec1_attr_unpack (jobspec, "system.duration", "f", &duration) == 0)
+            && (duration == passed_duration),
+        "jobspec has system.duration attribute set to correct value");
+    flux_jobspec1_destroy (jobspec);
+    ok (flux_jobspec1_from_command (argc, argv, NULL, 1, 1, 1, 5, 0) == NULL,
+        "flux_jobspec1_from_command failed when nnodes > ntasks");
+    if (!(jobspec = flux_jobspec1_from_command (argc, argv, NULL, 5, 1, 1, 3, 0.0))) {
+        BAIL_OUT ("flux_jobspec1_from_command failed when nnodes < ntasks");
+    }
+    ok (flux_jobspec1_attr_check (jobspec, errbuf, sizeof (errbuf)) == 0,
+        "flux_jobspec1_attr_check passed when nnodes < ntasks");
+    ok (flux_jobspec1_attr_pack (jobspec, "system.duration", "s", "not a number") == 0
+            && flux_jobspec1_attr_unpack (jobspec, "system.duration", "f", &duration)
+                   < 0,
+        "deleting system.duration works");
+    ok (flux_jobspec1_attr_check (jobspec, errbuf, sizeof (errbuf)) < 0,
+        "flux_jobspec1_attr_check failed after changing system.duration to a string");
+    flux_jobspec1_destroy (jobspec);
+    if (!(jobspec = flux_jobspec1_from_command (argc, argv, NULL, 5, 1, 1, 5, 0.0))) {
+        BAIL_OUT ("flux_jobspec1_from_command failed when nnodes == ntasks");
+    }
+    ok (flux_jobspec1_attr_check (jobspec, errbuf, sizeof (errbuf)) == 0,
+        "flux_jobspec1_attr_check passed when nnodes == ntasks");
+    ok (flux_jobspec1_attr_pack (jobspec, "foo", "f", 19.5) == 0
+            && flux_jobspec1_attr_check (jobspec, errbuf, sizeof (errbuf)) < 0,
+        "attr_check failed after adding spurious attribute");
+    flux_jobspec1_destroy (jobspec);
+}
+
+void check_encoding (void)
+{
+    char *argv[] = {"this", "is", "a", "test"};
+    int argc = sizeof (argv) / sizeof (argv[0]);
+    flux_jobspec1_t *jobspec;
+    char *encoded;
+    json_t *dup = NULL;
+    json_error_t err;
+
+    if (!(jobspec = flux_jobspec1_from_command (argc, argv, NULL, 5, 3, 2, 0, 0.0))
+        || !(encoded = flux_jobspec1_encode (jobspec, 0))) {
+        BAIL_OUT ("flux_jobspec1_from_command or subsequent encoding failed");
+    };
+    ok ((dup = json_loads (encoded, 0, &err)) && free_wrapper (encoded)
+            && decref_wrapper (dup),
+        "decoding works");
+
+    errno = EINVAL;
+    flux_jobspec1_destroy (jobspec);
+    ok (errno == EINVAL, "flux_jobspec1_destroy preserves errno");
+    errno = 0;
+
+    ok (flux_jobspec1_encode (NULL, 0) == NULL && errno == EINVAL,
+        "flux_jobspec1_encode catches NULL jobspec");
+    errno = 0;
+}
+
+void check_bad_args (void)
+{
+    char *argv[] = {"this", "is", "a", "test"};
+    int argc = sizeof (argv) / sizeof (argv[0]);
+
+    ok (flux_jobspec1_from_command (-1, argv, NULL, 1, 1, 1, 0, 5.0) == NULL
+            && errno == EINVAL,
+        "flux_jobspec1_from_command catches bad argc");
+    errno = 0;
+    ok (flux_jobspec1_from_command (argc, NULL, NULL, 1, 1, 1, 0, 5.0) == NULL
+            && errno == EINVAL,
+        "flux_jobspec1_from_command catches bad argv");
+    errno = 0;
+    ok (flux_jobspec1_from_command (argc, argv, NULL, 1, 1, 1, 0, -1.5) == NULL
+            && errno == EINVAL,
+        "flux_jobspec1_from_command catches bad duration");
+    errno = 0;
+
+    flux_jobspec1_destroy (NULL);
+}
+
+int main (int argc, char *argv[])
+{
+    plan (NO_PLAN);
+
+    check_stdio_cwd ();
+    check_env ();
+    check_jobspec ();
+    check_attr ();
+    check_encoding ();
+    check_bad_args ();
+
+    done_testing ();
+    return 0;
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
