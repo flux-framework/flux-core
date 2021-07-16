6281dde25f395d2e8acadddbf1fed216c0a325bb
Mark A. Grondona <mark.grondona@gmail.com>
2021-07-15 20:29:46 -0700

    testsuite: add reproducer for binary io encoding
    Problem: No end-to-end test exists that the end-to-end job system
    can handle non-printable binary I/O.
    
    Add a reproducer for issue #3775 to t/issues.

---
+++ t/issues/t3775-binary-io.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+# job-shell properly encodes binary data on stdin/out/err
+
+dd if=/dev/urandom bs=1k count=1 > data
+cat data | flux mini run cat >data2
+cmp data data2
+cat data | flux mini run sh -c 'cat >&2' 2>data3
+cmp data data3
+exit 0
