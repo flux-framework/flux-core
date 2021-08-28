fbed550433dca070dc4f82a20585abbf5fe7a98d
Mark A. Grondona <mark.grondona@gmail.com>
2021-08-27 19:28:33 -0700

    docker: add a systest stage to the fluxorama Dockerfile
    Problem: The fluxorama "system test" docker container is not ideal
    for system testing since it includes and runs ttyd, unnecessarily
    exposing an external port for web browser shell.
    
    Add a 'systest' stage to the fluxorama Dockerfile which doesn't
    have ttyd enabled. This stage will be used as part of automated
    and interactive system testing outside of ttyd mechanism.
    
    Since ttyd is not necessarily running, make the requirement for
    FLUXUSER_PASSWORD dependent on the ttyd service in entrypoint.sh.

--- src/test/docker/fluxorama/entrypoint.sh
+++ src/test/docker/fluxorama/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-#  Set default password for fluxuser
-if test -z "$FLUXUSER_PASSWORD"; then
-   printf >&2 "ERROR: No password set for fluxuser\n"
-   printf >&2 "Please set via the FLUXUSER_PASSWORD environment variable\n"
-   printf >&2 "e.g. FLUXUSER_PASSWORD=xxzzy docker run -e FLUXUSER_PASSWORD ..\n"
+if systemctl is-enabled ttyd; then
+  #  Set default password for fluxuser
+  if test -z "$FLUXUSER_PASSWORD"; then
+     printf >&2 "ERROR: No password set for fluxuser\n"
+     printf >&2 "Please set via the FLUXUSER_PASSWORD environment variable\n"
+     printf >&2 \
+         "e.g. FLUXUSER_PASSWORD=xxzzyy docker run -e FLUXUSER_PASSWORD ..\n"
    exit 1
+  fi
+  printf >&2 "Setting requested password for fluxuser..\n"
+  echo fluxuser:${FLUXUSER_PASSWORD} | chpasswd
 fi
-printf >&2 "Setting requested password for fluxuser..\n"
-echo fluxuser:${FLUXUSER_PASSWORD} | chpasswd
 exec "$@"
