6ee1a3ced4ee1737472d3ea6661d3b49e59fd3de
Mark A. Grondona <mark.grondona@gmail.com>
2021-09-10 11:59:21 -0700

    shell: add a library of Lua functions to lua.d
    Problem: It would be useful to have some Lua functions shared among
    the job shell's Lua plugins, but there is currently no place to collect
    these functions.
    
    Add a new "plugin" caled 00-lib.lua, so that it is loaded before other
    plugins. Add some convenience functions to start, including:
    
     shell.getopt_with_version() - get an option by name from shell.options
      in jobspec which may include an option @version specification.
      The value and any version of the option are returned.
    
     shell.prepend_path() - Prepend a path to a colon-separated environment
      variable
    
     shell.env_strip() - Strip environment variables from the shell that
      match one or more supplied patterns.

---
+++ src/shell/lua.d/00-lib.lua
@@ -0,0 +1,70 @@
+-------------------------------------------------------------
+-- Copyright 2021 Lawrence Livermore National Security, LLC
+-- (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+--
+-- This file is part of the Flux resource manager framework.
+-- For details, see https://github.com/flux-framework.
+--
+-- SPDX-License-Identifier: LGPL-3.0
+-------------------------------------------------------------
+
+--  Useful functions shared by all subsequently loaded
+--   shell Lua plugins
+
+--- Get a top level option from the shell.options table which includes
+---  an optional "@version" specification.
+--
+--  @param name The name of the option to get
+--
+--  Returns val, version
+--   `val` will be nil if shell option was not set
+--   `version` will be nil if no version specified.
+--
+function shell.getopt_with_version (name)
+    if not name then return nil end
+    local opt = shell.options[name]
+    if opt then
+        return opt:match("^[^@]+"), opt:match("@(.+)$")
+    end
+    return nil
+end
+
+--- Prepend `path` to colon-separated path environment variable `env_var`
+--
+--  @param env_var The environment variable to which to prepend
+--  @param path    The path to prepend
+--
+function shell.prepend_path (env_var, path)
+    local val = shell.getenv (env_var)
+
+    -- If path is already in env_var, do nothing. We stick ":" on both
+    -- ends of the existing value so we can easily match exact paths
+    -- instead of possibly matching substrings of paths when trying
+    -- to match "zero or more" colons.
+    --
+    if val and ((":"..val..":"):match (":"..path..":")) then return end
+
+    if val == nil then
+       suffix = ''
+    else
+       suffix = ':'..val
+    end
+    shell.setenv (env_var, path..suffix)
+end
+
+--- Strip all environment variables from job environment that match one
+--   or more pattern arguments.
+--
+function shell.env_strip (...)
+    local env = shell.getenv ()
+    for k,v in pairs (env) do
+        for _,pattern in pairs ({...}) do
+            if k:match(pattern) then
+                shell.unsetenv (k)
+            end
+        end
+    end
+end
+
+-- vi: ts=4 sw=4 expandtab
+-- 
