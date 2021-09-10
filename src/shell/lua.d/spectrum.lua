cb94f9a0fa4f412070b86c797ae5197cf804da45
Mark A. Grondona <mark.grondona@gmail.com>
2021-09-10 12:07:47 -0700

    shell: update spectrum mpi personality to use library functions
    Problem: The spectrum.lua mpi personality plugin for the job shell
    duplicates code in 00-lib.lua.
    
    Use functionality provided by 00-lib.lua where possible. Use
    shell.getopt_with_version() to capture any MPI version provided
    by the user.

--- src/shell/lua.d/spectrum.lua
+++ src/shell/lua.d/spectrum.lua
@@ -10,45 +10,15 @@
 
 -- Set environment specific to spectrum_mpi (derived from openmpi)
 --
-if shell.options.mpi ~= "spectrum" then return end
+
+local mpi, version = shell.getopt_with_version ("mpi")
+
+if mpi ~= "spectrum" then return end
 
 local posix = require 'posix'
 
-function prepend_path (env_var, path)
-    local val = shell.getenv (env_var)
-
-    -- If path is already in env_var, do nothing. We stick ":" on both
-    -- ends of the existing value so we can easily match exact paths
-    -- instead of possibly matching substrings of paths when trying
-    -- to match "zero or more" colons.
-    --
-    if val and ((":"..val..":"):match (":"..path..":")) then return end
-
-    if val == nil then
-       suffix = ''
-    else
-       suffix = ':'..val
-    end
-    shell.setenv (env_var, path..suffix)
-end
-
-local function strip_env_by_prefix (env, prefix)
-    --
-    -- Have to call env:get() to translate env object to Lua table
-    --  in order to use pairs() to iterate environment keys:
-    --
-    for k,v in pairs (env) do
-        if k:match("^"..prefix) then
-            shell.unsetenv (k)
-        end
-    end
-end
-
-local env = shell.getenv()
-
 -- Clear all existing PMIX_ and OMPI_ values before setting our own
-strip_env_by_prefix (env, "PMIX_")
-strip_env_by_prefix (env, "OMPI_")
+shell.env_strip ("^PMIX_", "^OMPI_")
 
 -- Assumes the installation paths of Spectrum MPI on LLNL's Sierra
 shell.setenv ('OMPI_MCA_osc', "pt2pt")
@@ -57,8 +27,10 @@ shell.setenv ('OMPI_MCA_btl', "self")
 shell.setenv ('OMPI_MCA_coll_hcoll_enable', '0')
 
 -- Help find libcollectives.so
-prepend_path ('LD_LIBRARY_PATH', '/opt/ibm/spectrum_mpi/lib/pami_port')
-prepend_path ('LD_PRELOAD', '/opt/ibm/spectrum_mpi/lib/libpami_cudahook.so')
+shell.prepend_path ('LD_LIBRARY_PATH',
+                    '/opt/ibm/spectrum_mpi/lib/pami_port')
+shell.prepend_path ('LD_PRELOAD',
+                    '/opt/ibm/spectrum_mpi/lib/libpami_cudahook.so')
 
 plugin.register {
     name = "spectrum",
@@ -76,7 +48,6 @@ plugin.register {
         }
     }
 }
-
 
 -- vi: ts=4 sw=4 expandtab
 
