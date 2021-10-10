67708d5ca04999594c0c0362733de10ddb6e9b40
Jim Garlick <garlick.jim@gmail.com>
2021-10-10 15:14:58 -0700

    broker/content-cache: use inline comment in struct
    Problem: struct documentation is clunky with /* */ style
    quotes.
    
    Switch to // for struct documentation.

--- src/broker/content-cache.c
+++ src/broker/content-cache.c
@@ -56,9 +56,9 @@ struct cache_entry {
     void *data;
     int len;
     char *blobref;
-    uint8_t valid:1;                /* entry contains valid data */
-    uint8_t dirty:1;                /* entry needs to be stored upstream */
-                                    /*   or to backing store (rank 0) */
+    uint8_t valid:1;                // entry contains valid data
+    uint8_t dirty:1;                // entry needs to be stored upstream
+                                    //   or to backing store (rank 0)
     uint8_t load_pending:1;
     uint8_t store_pending:1;
     struct msgstack *load_requests;
@@ -75,13 +75,13 @@ struct content_cache {
     flux_future_t *f_sync;
     uint32_t rank;
     zhashx_t *entries;
-    uint8_t backing:1;              /* 'content.backing' service available */
+    uint8_t backing:1;              // 'content.backing' service available
     char *backing_name;
     const char *hash_name;
     struct msgstack *flush_requests;
 
-    struct list_head lru;           /* LRU is for valid, clean entries only */
-    struct list_head flush;         /* dirties queued due to batch limit */
+    struct list_head lru;           // LRU is for valid, clean entries only
+    struct list_head flush;         // dirties queued due to batch limit
 
     uint32_t blob_size_limit;
     uint32_t flush_batch_limit;
@@ -90,9 +90,9 @@ struct content_cache {
     uint32_t purge_target_size;
     uint32_t purge_old_entry;
 
-    uint32_t acct_size;             /* total size of all cache entries */
-    uint32_t acct_valid;            /* count of valid cache entries */
-    uint32_t acct_dirty;            /* count of dirty cache entries */
+    uint32_t acct_size;             // total size of all cache entries
+    uint32_t acct_valid;            // count of valid cache entries
+    uint32_t acct_dirty;            // count of dirty cache entries
 };
 
 static void flush_respond (struct content_cache *cache);
