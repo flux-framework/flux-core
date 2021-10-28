ca93eda49fd513de8268e3195b9104fd78efd8eb
Mark A. Grondona <mark.grondona@gmail.com>
2021-10-28 15:49:10 -0700

    testsuite: add test for issue #3920
    Problem: No test exists for the issue described in #3920: Running
    job count underflow when an event is posted in CLEANUP state after
    an exception in SCHED state.
    
    Add a reproducer for this issue and include a jobtap plugin to generate
    the problematic event.

---
+++ t/job-manager/plugins/cleanup-event.c
@@ -0,0 +1,35 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+/* cleanup-event.c - emit a test event in CLEANUP state
+ */
+
+#include <flux/jobtap.h>
+
+static int cleanup_cb (flux_plugin_t *p,
+                       const char *topic,
+                       flux_plugin_arg_t *args,
+                       void *arg)
+{
+    return flux_jobtap_event_post_pack (p,
+                                        FLUX_JOBTAP_CURRENT_JOB,
+                                       "test-event",
+                                       NULL);
+}
+
+
+int flux_plugin_init (flux_plugin_t *p)
+{
+    return flux_plugin_add_handler (p,
+                                    "job.state.cleanup",
+                                     cleanup_cb,
+                                     NULL);
+}
+
