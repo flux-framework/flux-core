7acb3e2c0c4d48a47c35aec7ca6d19b14cd1d73d
Mark A. Grondona <mark.grondona@gmail.com>
2021-11-16 07:15:44 -0800

    testsuite: add test for job exception on rank disconnect
    Problem: No test verifies that a broker disconnect will terminate a
    running job.
    
    Add a test for issue #3906 which verifies killing a broker results
    in a fatal job exception for a job running on that rank.

---
+++ t/issues/t3906-job-exec-exception.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+#
+#  Ensure a job exception is raised when broker rank disconnects
+#
+#  1. Submit job and ensure it has started
+#  2. Kill a leaf broker
+#  3. Ensure job exception is raised and includes appropriate error note
+#
+export startctl="flux python ${SHARNESS_TEST_SRCDIR}/scripts/startctl.py"
+SHELL=/bin/sh flux start -s 4 -o,-Stbon.fanout=4 --test-exit-mode=leader '\
+   id=$(flux mini submit -n4 -N4 sleep inf) \
+&& flux job wait-event $id start \
+&& $startctl kill 3 9 \
+&& flux job wait-event $id exception >t3906.output 2>&1'
+
+cat t3906.output
+
+grep 'lost contact with job shell on broker rank 3' t3906.output
