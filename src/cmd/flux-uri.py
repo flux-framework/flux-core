474b63c62e08c079986f5f297c4bcb6ccb9e2797
Mark A. Grondona <mark.grondona@gmail.com>
2021-12-08 21:40:09 -0800

    cmd: add flux-uri
    Problem: A command line interface for resolving Flux jobids and
    uri-resolver URIs to job URIs is not currently available.
    
    Add flux-uri.py for this purpose. The command can take a jobid
    or URI in any supported resolver scheme and will return a
    FLUX_URI for the target instance.
    
    For convenience, --local and --remote options are provided to
    attempt to convert a resolved job URI to its local or remote form.

---
+++ src/cmd/flux-uri.py
@@ -0,0 +1,67 @@
+##############################################################
+# Copyright 2021 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+#
+# This file is part of the Flux resource manager framework.
+# For details, see https://github.com/flux-framework.
+#
+# SPDX-License-Identifier: LGPL-3.0
+##############################################################
+
+import sys
+import logging
+import argparse
+
+import flux
+from flux.uri import FluxURIResolver
+
+LOGGER = logging.getLogger("flux-uri")
+
+
+@flux.util.CLIMain(LOGGER)
+def main():
+
+    sys.stdout = open(
+        sys.stdout.fileno(), "w", encoding="utf8", errors="surrogateescape"
+    )
+
+    resolver = FluxURIResolver()
+    plugins = [f"  {x}\t\t{y}" for x, y in resolver.plugins().items()]
+    plugin_list = "Supported resolver schemes:\n" + "\n".join(plugins)
+
+    parser = argparse.ArgumentParser(
+        prog="flux-uri",
+        description="Resolve TARGET to a Flux URI",
+        epilog=plugin_list,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "--remote",
+        action="store_true",
+        help="convert a local URI to remote",
+    )
+    parser.add_argument(
+        "--local",
+        action="store_true",
+        help="convert a remote URI to local",
+    )
+    parser.add_argument(
+        "uri",
+        metavar="TARGET",
+        help="A Flux jobid or URI in scheme:argument form (e.g. jobid:f1234)",
+    )
+
+    args = parser.parse_args()
+    uri = resolver.resolve(args.uri)
+    if args.remote:
+        print(uri.remote)
+    elif args.local:
+        print(uri.local)
+    else:
+        print(uri)
+
+
+if __name__ == "__main__":
+    main()
+
+# vi: ts=4 sw=4 expandtab
