31785e8318013bed451592fbd8c02bd441abc9d8
Mark A. Grondona <mark.grondona@gmail.com>
2021-12-10 15:54:54 -0800

    libutil: add uri_resolve()
    Problem: Multiple flux-core utilities could benefit from calling
    out to flux-uri to resolve high-level URIs to native URIs, but this
    requires a lot of boilerplate code to launch the external process
    and read its output, etc.
    
    Add a uri_resolve() convenience function to libutil which takes a
    possibly unresolved URI and calls `flux uri` on it, returning the
    resolved URI on success.

---
+++ src/common/libutil/uri.c
@@ -0,0 +1,64 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdlib.h>
+#include <string.h>
+
+#include "src/common/libyuarel/yuarel.h"
+#include "popen2.h"
+#include "read_all.h"
+#include "log.h"
+
+static void nullify_newline (char *str)
+{
+    int n;
+    if (str && (n = strlen (str)) > 0) {
+        if (str[n-1] == '\n')
+            str[n-1] = '\0';
+    }
+}
+
+char *uri_resolve (const char *uri)
+{
+    struct popen2_child *child = NULL;
+    struct yuarel yuri;
+    char *result = NULL;
+    char *argv[] = { "flux", "uri", (char *) uri, NULL };
+
+    char *cpy = strdup (uri);
+    if (!cpy)
+        return NULL;
+    if (yuarel_parse (&yuri, cpy) == 0) {
+        if (strcmp (yuri.scheme, "ssh") == 0
+            || strcmp (yuri.scheme, "local") == 0) {
+            free (cpy);
+            return strdup (uri);
+        }
+    }
+    free (cpy);
+
+    if (!(child = popen2 ("flux", argv))
+        || (read_all (popen2_get_fd (child), (void **)&result) < 0))
+        goto out;
+    nullify_newline (result);
+out:
+    if (pclose2 (child) < 0) {
+        /* flux-uri returned error */
+        free (result);
+        result = NULL;
+    }
+    return result;
+}
+
+
+// vi:ts=4 sw=4 expandtab
