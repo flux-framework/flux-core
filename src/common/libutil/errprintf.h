5191dc5af614f995c4e76bb2ffe33b6c8a71404b
Mark A. Grondona <mark.grondona@gmail.com>
2022-02-24 20:57:34 -0800

    libutil: add errprintf utility function
    Problem: Many flux-core interfaces that use an *error_t to capture
    function errors implement an internal errprintf() convenience function
    to fill in the error text. This has resulted in a bit of duplicated
    code.
    
    Add an errprintf() implementation to libutil which can be used in place
    of duplicated code once callers are updated to use flux_error_t.
    
    Also add a set of simple unit tests for the interface.

---
+++ src/common/libutil/errprintf.h
@@ -0,0 +1,29 @@
+/************************************************************\
+ * Copyright 2022 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef _UTIL_ERRPRINTF_H
+#define _UTIL_ERRPRINTF_H
+
+#include "src/common/libflux/types.h" /* flux_error_t */
+
+/*
+ *  Utility function for printing an error to a flux_error_t container.
+ *  This function always returns -1 as a convenience in error handling
+ *   functions, e.g.
+ *
+ *  return errprintf (errp, "Function failed");
+ *
+ */
+int errprintf (flux_error_t *errp, const char *fmt, ...)
+     __attribute__ ((format (printf, 2, 3)));
+
+#endif /* !_UTIL_ERRPRINTF_H */
+
+// vi:ts=4 sw=4 expandtab
