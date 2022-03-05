42c1073902b3951df80d1e204635c296cf46b7a3
Mark A. Grondona <mark.grondona@gmail.com>
2022-03-05 13:58:22 -0800

    testsuite: add reproducer for issue #4184
    Problem: No test in the testsuite includes a reproducer for issue
    4184 - where on restart some resources are marked down and allocated
    in sched-simple and the scheduler subsequently fails to schedule any
    new jobs.
    
    Add a reproducer to t/issues/t4184-sched-simple-restart.sh to cover
    this case.

---
+++ t/issues/t4184-sched-simple-restart.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+cat <<-EOF >t4184.sh
+#!/bin/sh -e
+
+which flux
+
+NCORES=\$(flux resource list -s up -no {ncores})
+jobids=\$(flux mini submit --cc=0-1 -n \$NCORES sleep 600)
+id=\$(echo \$jobids | cut -d ' ' -f1)
+id2=\$(echo \$jobids | cut -d ' ' -f2)
+flux job wait-event \$id start
+
+flux resource drain 0
+
+flux module reload sched-simple
+
+flux resource list
+flux resource undrain 0
+
+echo "t4184: waiting for resources to be back up:"
+while test \$(flux resource list -s up -no {ncores}) -ne \$NCORES; do
+  sleep 0.25
+done
+
+echo "t4184: canceling \$id"
+flux job cancel \${id}
+
+echo "t4184: waiting for \$id to end"
+flux job wait-event \$id clean
+
+echo "t4184: waiting for \$id2 to start..."
+
+flux job wait-event -t 15 \$id2 start
+
+echo "t4184: canceling \$id2..."
+flux job cancel \$id2
+
+echo "t4184: waiting for \$id2 to end..."
+flux job wait-event -t 15 \$id2 clean
+
+EOF
+
+chmod +x t4184.sh
+
+flux start -s 1 ./t4184.sh
