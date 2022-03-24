5fdccc0eed95e4a5b0f875631d822f9a01521c47
Jim Garlick <garlick.jim@gmail.com>
2022-03-24 12:31:57 -0700

    flux-dmesg: simplify code
    Problem: flux-dmesg code is hard to follow.
    
    Restructure code to avoid obscuring behavior with flags and
    callbacks.

--- src/cmd/builtin/dmesg.c
+++ src/cmd/builtin/dmesg.c
@@ -26,122 +26,66 @@ static struct optparse_option dmesg_opts[] = {
     OPTPARSE_TABLE_END,
 };
 
-enum {
-    DMESG_CLEAR = 1,
-    DMESG_FOLLOW = 2,
-};
-
-static int dmesg_clear (flux_t *h)
+void dmesg_print (const char *buf, int len)
 {
-    flux_future_t *f;
-    int rc = -1;
-
-    if (!(f = flux_rpc (h, "log.clear", NULL, FLUX_NODEID_ANY, 0)))
-        goto done;
-    if (flux_future_get (f, NULL) < 0)
-        goto done;
-    rc = 0;
-done:
-    flux_future_destroy (f);
-    return rc;
-}
-
-static flux_future_t *dmesg_rpc (flux_t *h, bool follow)
-{
-    return flux_rpc_pack (h, "log.dmesg", FLUX_NODEID_ANY, FLUX_RPC_STREAMING,
-                          "{s:b}", "follow", follow);
-}
-
-static int dmesg_rpc_get (flux_future_t *f, flux_log_f fun, void *arg)
-{
-    const char *buf;
-    int rc = -1;
-
-    if (flux_rpc_get (f, &buf) < 0)
-        goto done;
-    fun (buf, strlen (buf), arg);
-    rc = 0;
-done:
-    return rc;
-}
-
-int dmesg (flux_t *h, int flags, flux_log_f fun, void *arg)
-{
-    int rc = -1;
-    bool eof = false;
-    bool follow = false;
-
-    if (flags & DMESG_FOLLOW)
-        follow = true;
-    if (fun) {
-        flux_future_t *f;
-        if (!(f = dmesg_rpc (h, follow)))
-            goto done;
-        while (!eof) {
-            if (dmesg_rpc_get (f, fun, arg) < 0) {
-                if (errno != ENODATA) {
-                    flux_future_destroy (f);
-                    goto done;
-                }
-                eof = true;
-            }
-            flux_future_reset (f);
-        }
-        flux_future_destroy (f);
-    }
-    if ((flags & DMESG_CLEAR)) {
-        if (dmesg_clear (h) < 0)
-            goto done;
-    }
-    rc = 0;
-done:
-    return rc;
-}
-
-void dmesg_fprint (const char *buf, int len, void *arg)
-{
-    FILE *f = arg;
     struct stdlog_header hdr;
     const char *msg;
     int msglen, severity;
     uint32_t nodeid;
 
-    if (f) {
-        if (stdlog_decode (buf, len, &hdr, NULL, NULL, &msg, &msglen) < 0)
-            fprintf (f, "%.*s\n", len, buf);
-        else {
-            nodeid = strtoul (hdr.hostname, NULL, 10);
-            severity = STDLOG_SEVERITY (hdr.pri);
-            fprintf (f, "%s %s.%s[%" PRIu32 "]: %.*s\n",
-                     hdr.timestamp,
-                     hdr.appname,
-                     stdlog_severity_to_string (severity),
-                     nodeid,
-                     msglen, msg);
-        }
-        fflush (f);
+    if (stdlog_decode (buf, len, &hdr, NULL, NULL, &msg, &msglen) < 0)
+        printf ("%.*s\n", len, buf);
+    else {
+        nodeid = strtoul (hdr.hostname, NULL, 10);
+        severity = STDLOG_SEVERITY (hdr.pri);
+        printf ("%s %s.%s[%" PRIu32 "]: %.*s\n",
+                 hdr.timestamp,
+                 hdr.appname,
+                 stdlog_severity_to_string (severity),
+                 nodeid,
+                 msglen, msg);
     }
+    fflush (stdout);
 }
 
 static int cmd_dmesg (optparse_t *p, int ac, char *av[])
 {
     int n;
     flux_t *h;
-    int flags = 0;
-    flux_log_f print_cb = dmesg_fprint;
 
     if ((n = optparse_option_index (p)) != ac)
         log_msg_exit ("flux-dmesg accepts no free arguments");
 
     h = builtin_get_flux_handle (p);
-    if (optparse_hasopt (p, "read-clear") || optparse_hasopt (p, "clear"))
-        flags |= DMESG_CLEAR;
-    if (optparse_hasopt (p, "clear"))
-        print_cb = NULL;
-    if (optparse_hasopt (p, "follow"))
-        flags |= DMESG_FOLLOW;
-    if (dmesg (h, flags, print_cb, stdout) < 0)
-        log_err_exit ("log.dmesg");
+
+    if (!optparse_hasopt (p, "clear")) {
+        flux_future_t *f;
+        const char *buf;
+
+        if (!(f = flux_rpc_pack (h,
+                                 "log.dmesg",
+                                 FLUX_NODEID_ANY,
+                                 FLUX_RPC_STREAMING,
+                                 "{s:b}",
+                                 "follow", optparse_hasopt (p, "follow"))))
+            log_err_exit ("error sending log.dmesg request");
+        while (flux_rpc_get (f, &buf) == 0) {
+            dmesg_print (buf, strlen (buf));
+            flux_future_reset (f);
+        }
+        if (errno != ENODATA)
+            log_msg_exit ("log.dmesg: %s", future_strerror (f, errno));
+        flux_future_destroy (f);
+    }
+    if (optparse_hasopt (p, "read-clear") || optparse_hasopt (p, "clear")) {
+        flux_future_t *f;
+
+        if (!(f = flux_rpc (h, "log.clear", NULL, FLUX_NODEID_ANY, 0))
+            || flux_future_get (f, NULL) < 0)
+            log_msg_exit ("log.clear: %s", future_strerror (f, errno));
+        flux_future_destroy (f);
+    }
+
     flux_close (h);
     return (0);
 }
