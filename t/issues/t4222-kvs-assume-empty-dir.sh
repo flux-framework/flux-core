152f82f1b8d67098506250353aa5107fa22592c5
Albert Chu <chu11@llnl.gov>
2022-05-18 14:26:17 -0700

    testsuite: workaround issue4333 in tests
    Problem: As detailed in issue #4333, when the kvs module is reloaded,
    the kvs-watch module has to be reloaded at the moment.  This
    was not done in issues/t4222-kvs-assume-empty-dir.sh.  This lead
    to potential problems in follow on tests.
    
    Solution: Remove / reload the kvs-watch module as well as the
    kvs module in t4222-kvs-assume-empty-dir.sh.

--- t/issues/t4222-kvs-assume-empty-dir.sh
+++ t/issues/t4222-kvs-assume-empty-dir.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+flux module remove kvs-watch
 flux module remove kvs
 
 flux dump --checkpoint issue4222.tar
@@ -16,6 +17,7 @@ flux module load content-sqlite
 flux restore --checkpoint issue4222.tar
 
 flux module load kvs
+flux module load kvs-watch
 
 flux kvs namespace create issue4222ns
 flux kvs put --namespace=issue4222ns a=1
