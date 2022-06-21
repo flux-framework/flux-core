a9c44192c32a97c76708ca86da8725d5c4c641be
Albert Chu <chu11@llnl.gov>
2022-06-21 07:46:58 -0700

    testsuite: add content cache regression tests
    Problem: No tests in the testsuite ensure that content flush
    is forced after `flux content flush` nor that any dirty cache
    entries are flushed after a content backing module is loaded.
    (Issues #4378 and #4379)
    
    Add regression tests for these specific issues.

---
+++ t/issues/t4378-content-flush-force.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+flux content flush
+
+flux module remove content-sqlite
+
+# create a dirty cache entry in the content-cache
+flux kvs put issue4378=issue4378bug
+
+flux module load content-sqlite
+
+# Issue 4378 - without fix, this flux content flush would hang
+flux content flush
