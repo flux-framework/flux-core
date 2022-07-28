9b52545cdb6c97d9db0428ed12fd3d505b2f112f
Mark A. Grondona <mark.grondona@gmail.com>
2022-07-28 09:04:07 -0700

    libutil/test: test popen2() with POPEN2_CAPTURE_STDERR
    Problem: No tests exercise the popen2() call with the flag to capture
    stderr.
    
    Add a set of tests that exercise this interface.

--- src/common/libutil/test/popen2.c
+++ src/common/libutil/test/popen2.c
@@ -11,10 +11,40 @@
 #include <string.h>
 #include <errno.h>
 #include <stdint.h>
+#include <sys/wait.h>
 
 #include "src/common/libtap/tap.h"
 #include "src/common/libutil/popen2.h"
 #include "src/common/libutil/read_all.h"
+
+static void test_popen2_stderr ()
+{
+    struct popen2_child *p;
+    char *av[] = { "cat",  "/nosuchfile", NULL };
+    char *buf;
+    int efd;
+
+    ok (popen2 ("cat", av, 42) == NULL && errno == EINVAL,
+        "popen2() with invalid flags returns EINVAL");
+
+    ok ((p = popen2 ("cat", av, POPEN2_CAPTURE_STDERR)) != NULL,
+        "popen2() with POPEN2_CAPTURE_STDERR works");
+    ok (pclose2 (p) == 0x100,
+        "immediate pclose2 returns failed exit status of command");
+
+    ok ((p = popen2 ("cat", av, POPEN2_CAPTURE_STDERR)) != NULL,
+        "popen2() with POPEN2_CAPTURE_STDERR works");
+    ok ((efd = popen2_get_stderr_fd (p)) >= 0,
+        "popen2_get_stderr_fd() works");
+    ok (read_all (efd, (void **) &buf) > 0,
+        "read from stderr fd worked");
+    ok (pclose2 (p) == 0x100,
+        "pclose2 returns failed exit status of command");
+
+    like (buf, ".*: No such file or directory",
+        "stderr contained expected error");
+    free (buf);
+}
 
 int main(int argc, char** argv)
 {
@@ -69,6 +99,7 @@ int main(int argc, char** argv)
     ok (pclose2 (p) == 0x100,
         "pclose2 returns child exit code 1");
 
+    test_popen2_stderr ();
     done_testing();
 }
 
