db2536f34f086b8564f0c9087b15f0d009582f14
vsoch <vsoch@users.noreply.github.com>
2022-08-31 14:24:58 -0600

    rlist build is working
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- src/bindings/python/_flux/_rlist_build.py
+++ src/bindings/python/_flux/_rlist_build.py
@@ -13,60 +13,52 @@ sys.path.insert(0, here)
 from _hostlist_build import ffi as hostlist_ffi
 from _idset_build import ffi as idset_ffi
 
-
 ffi = FFI()
 
 ffi.include(hostlist_ffi)
 ffi.include(idset_ffi)
-
 
 ffi.set_source(
     "_flux._rlist",
     """
 #include <jansson.h>
 #include <src/include/flux/core.h>
+#include <src/include/flux/idset.h>
 #include <src/common/libczmqcontainers/czmq_containers.h>
 #include <src/common/libflux/types.h>
 #include <src/common/librlist/rlist.h>
-#include <src/common/libutil/cronodate.h>
-#include <src/include/flux/idset.h>
-#include <src/include/flux/hostlist.h>
-#include <src/common/libhostlist/hostlist.h>
-#include <src/common/libdebugged/debugged.h>
+#include <src/common/libidset/idset.h>
             """,
     libraries=[
+        "rlist",
+        "idset",
+        "flux-idset",
         "flux-core",
         "flux",
-        "flux-idset",
-        "hostlist",
-        "flux-hostlist",
         "flux-internal",
-        "rlist",
-        "util",
+        "hwloc",
+        "jansson",
     ],
     library_dirs=[
         "/code/src/common/libflux/.libs",
-        "/code/src/common/libhostlist/.libs",
         "/code/src/common/librlist/.libs",
-        "/code/src/common/libutil/.libs",
         "/code/src/common/libidset/.libs",
         "/code/src/common/.libs",
     ],
     include_dirs=[
         "/code",
         "/usr/include",
+        # hwloc
+        "/usr/lib/x86_64-linux-gnu",
         "/code/config",
         "/code/src/include",
-        "/code/src/common/libutil",
         "/code/src/common/libflux",
         "/code/src/common/librlist",
         "/code/src/common/libidset",
     ],
     extra_compile_args=[
         "-L/code/src/common/.libs",
-        "-L/code/src/common/libhostlist/.libs",
         "-L/code/src/common/librlist/.libs",
-        "-L/code/src/common/libutil/.libs",
         "-L/code/src/common/libflux/.libs",
         "-L/code/src/common/libidset/.libs",
     ],
