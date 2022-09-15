3a8d768b03f1114042c7c382b8cc7f08b0d906bc
vsoch <vsoch@users.noreply.github.com>
2022-09-15 16:31:21 -0600

    testing adding gitlab ci file
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- etc/gen-cmdhelp.py
+++ etc/gen-cmdhelp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ###############################################################
 # Copyright 2020 Lawrence Livermore National Security, LLC
 # (c.f. AUTHORS, NOTICE.LLNS, COPYING)
