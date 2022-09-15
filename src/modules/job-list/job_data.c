e2e7ee82046fc30a2f1d8296c0f0bbcabef9f251
Albert Chu <chu11@llnl.gov>
2022-09-15 16:29:46 -0700

    job-list: rebuild and store eventlog
    Problem: In the near future we will need access to the job's
    eventlog when a job goes inactive.
    
    Solution: Rebuild the job eventlog from the events journal and
    store it internally in struct job.

--- src/modules/job-list/job_data.c
+++ src/modules/job-list/job_data.c
@@ -40,6 +40,7 @@ void job_destroy (void *data)
         grudgeset_destroy (job->dependencies);
         json_decref (job->jobspec);
         json_decref (job->R);
+        free (job->eventlog);
         json_decref (job->exception_context);
         free (job);
         errno = save_errno;
