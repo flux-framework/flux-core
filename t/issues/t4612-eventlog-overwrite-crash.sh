f341232f7289b4da77acf3b72f1b834680570470
Albert Chu <chu11@llnl.gov>
2022-09-27 09:51:58 -0700

    testsuite: add eventlog overwrite regression test
    Problem: A regression test for issue #4612, where an overwrite
    of a job eventlog can lead to a broker crash, does not exist.
    
    Add a reproducer of the original bug.

---
+++ t/issues/t4612-eventlog-overwrite-crash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+waitfile=${SHARNESS_TEST_SRCDIR}/scripts/waitfile.lua
+
+jobid=$(flux mini submit --wait-event=start sh -c "echo foo; sleep 300")
+
+kvsdir=$(flux job id --to=kvs $jobid)
+
+# issue a command that will watch / monitor the job's eventlog
+flux job attach $jobid > t4612.out &
+
+# ensure backgrounded process has started to monitor eventlog
+$waitfile --count=1 --timeout=30 --pattern=foo t4612.out
+
+# now overwrite the eventlog without changing its length
+flux kvs get --raw ${kvsdir}.eventlog \
+	| sed -e s/submit/foobar/ \
+	| flux kvs put --raw ${kvsdir}.eventlog=-
+
+wait
+
+# if flux broker segfaulted, this won't work
+flux mini run hostname
