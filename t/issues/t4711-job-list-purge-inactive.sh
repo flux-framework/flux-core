5d4e802826afdbe645a4f415d9712b0139f4b67e
Albert Chu <chu11@llnl.gov>
2022-10-28 13:52:30 -0700

    testsuite: add job-list purge regression
    Problem: There is no coverage for the job-purge-inactive corner
    case where a job can be purged if it is not inactive.
    
    Solution: Add a regression test.

---
+++ t/issues/t4711-job-list-purge-inactive.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# test-prereqs: HAVE_JQ
+
+prejob=$(flux job stats | jq .job_states.run)
+
+jobid=$(flux mini submit --wait-event=start sleep 100 | flux job id)
+
+postjob=$(flux job stats | jq .job_states.run)
+
+if [ ${postjob} -ne $((prejob + 1)) ]
+then
+    echo "stat counts invalid: ${postjob}, ${prejob}"
+    exit 1
+fi
+
+flux event pub job-purge-inactive "{\"jobs\":[${jobid}]}"
+
+# if job-list module asserted this won't work
+
+# this is technically a bit racy, as we won't know for sure if the
+# event has been received / processed by the job-list module yet.  In
+# the rare event the racy bit is hit and our fix regresses, subsequent
+# flux actions should catch a failure.
+
+flux job stats
