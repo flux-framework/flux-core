0c3af6ee64b22cd9b43f30f4286c15b403488a02
Jim Garlick <garlick.jim@gmail.com>
2023-01-23 12:15:34 -0800

    flux-keygen: add --meta KEY=VAL,... option
    Problem: it may be useful to allow certificate meta values to
    be overridden, or for for arbitrary new metadata to be added
    to the certificate.
    
    Add a new --meta KEY=VAL option.  The option may be specified multiple
    times, or multiple KEY=VAL pairs may be specified with the same option,
    separated by commas.
    
    Fixes #4875

--- src/cmd/flux-keygen.c
+++ src/cmd/flux-keygen.c
@@ -21,6 +21,9 @@
 static struct optparse_option opts[] = {
     { .name = "name", .key = 'n', .has_arg = 1, .arginfo = "NAME",
       .usage = "Set certificate name (default: hostname)", },
+    { .name = "meta", .has_arg = 1, .arginfo = "KEYVALS",
+      .flags = OPTPARSE_OPT_AUTOSPLIT,
+      .usage = "Add/update comma-separated key=value metadata", },
     OPTPARSE_TABLE_END,
 };
 
@@ -44,7 +47,9 @@ int main (int argc, char *argv[])
     optparse_t *p;
     int optindex;
     zcert_t *cert;
-    char buf[64];
+    const char *name;
+    char now[64];
+    char hostname[64];
     char *path = NULL;
 
     log_init ("flux-keygen");
@@ -65,13 +70,38 @@ int main (int argc, char *argv[])
 
     if (!(cert = zcert_new ()))
         log_msg_exit ("zcert_new: %s", zmq_strerror (errno));
-
-    if (gethostname (buf, sizeof (buf)) < 0)
+    if (gethostname (hostname, sizeof (hostname)) < 0)
         log_err_exit ("gethostname");
-    zcert_set_meta (cert, "hostname", "%s", buf);
+    if (ctime_iso8601_now (now, sizeof (now)) == NULL)
+        log_err_exit ("localtime");
+
+    /* N.B. zcert_set_meta() silently ignores attempts to set the same
+     * value twice so the default settings come later and are ignored
+     * if values were provided on the command line.
+     */
+    if ((name = optparse_get_str (p, "name", NULL))) {
+        zcert_set_meta (cert, "name", "%s", name);
+    }
+    if (optparse_hasopt (p, "meta")) {
+        const char *arg;
+
+        optparse_getopt_iterator_reset (p, "meta");
+        while ((arg = optparse_getopt_next (p, "meta"))) {
+            char *key;
+            char *val;
+            if (!(key = strdup (arg)))
+                log_msg_exit ("out of memory");
+            if ((val = strchr (key, '=')))
+                *val++ = '\0';
+            zcert_set_meta (cert, key, "%s", val ? val : "");
+            free (key);
+        }
+    }
+
     // name is used in overlay logging
-    zcert_set_meta (cert, "name", "%s", optparse_get_str (p, "name", buf));
-    zcert_set_meta (cert, "time", "%s", ctime_iso8601_now (buf, sizeof (buf)));
+    zcert_set_meta (cert, "name", "%s", hostname);
+    zcert_set_meta (cert, "hostname", "%s", hostname);
+    zcert_set_meta (cert, "time", "%s", now);
     zcert_set_meta (cert, "userid", "%d", getuid ());
 
     if (path && zcert_save_secret (cert, path) < 0)
