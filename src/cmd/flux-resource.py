5ece6c92f25137eae7c8e06dbbc2f2165f2b4571
Albert Chu <chu11@llnl.gov>
2023-01-31 21:31:53 -0800

    flux-resource: increase width of queue field
    Problem: Unlike other tools such as flux-queue and flux-jobs, it is
    possible for multiple queues to be listed with the "flux resource list"
    "queue" field.  The default output width is 8 characters, which is a
    little short for multiple queues.
    
    Increase from 8 characters to 10 characters.
    
    Fixes #4904

--- src/cmd/flux-resource.py
+++ src/cmd/flux-resource.py
@@ -56,7 +56,7 @@ class FluxResourceConfig(UtilConfig):
         "default": {
             "description": "Default flux-resource list format string",
             "format": (
-                "{state:>10} ?:{queue:<8.8} ?:{propertiesx:<10.10+} {nnodes:>6} "
+                "{state:>10} ?:{queue:<10.10} ?:{propertiesx:<10.10+} {nnodes:>6} "
                 "{ncores:>8} {ngpus:>8} {nodelist}"
             ),
         },
