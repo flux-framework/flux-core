e26c0e84c303425f043f773ee9d4fef88ffb65ea
Jim Garlick <garlick.jim@gmail.com>
2023-02-17 08:55:34 -0800

    testsuite: improve unit testing for subprocess server
    Problem: there are no unit tests for remote subprocesses.
    
    Create a subprocess server on a back to back flux_t handle using
    libtestutil, then poke at it various ways.
    
    The "remote" unit test program just ensures the subprocess API
    works as advertised for remote processes.
    
    The "iostress" unit test program overflows I/O buffers to cover
    the error code.

---
+++ src/common/libsubprocess/test/rcmdsrv.h
@@ -0,0 +1,25 @@
+/************************************************************\
+ * Copyright 2023 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifndef _SUBPROCESS_TEST_RCMDSRV_H
+#define _SUBPROCESS_TEST_RCMDSRV_H
+
+/* Start subprocess server.  Returns one end of back-to-back flux_t test
+ * handle.  Call test_server_stop (h) when done to join with server thread.
+ */
+flux_t *rcmdsrv_create (void);
+
+#endif // !_SUBPROCESS_TEST_RCMDSRV_H
+
+// vi: ts=4 sw=4 expandtab
