72d4588bc71e39edbad40e503738f1b69399da89
Mark A. Grondona <mark.grondona@gmail.com>
2023-02-25 10:21:51 -0800

    test: update use of flux-mini in create-kvs-dumpfile.sh
    Problem: src/test/create-kvs-dumpfile.sh uses flux-mini but this
    command is deprecated.
    
    Switch to non mini versions of commands in
    src/test/create-kvs-dumpfile.sh.

--- src/test/create-kvs-dumpfile.sh
+++ src/test/create-kvs-dumpfile.sh
@@ -10,11 +10,11 @@ WORKLOAD="\
 #
 #  Basic workload used to create a set of varied eventlogs in KVS
 #
-flux mini bulksubmit --quiet --cc=1-4 {} ::: true false nocommand &&
-id=\$(flux mini submit --urgency=hold hostname) &&
+flux bulksubmit --quiet --cc=1-4 {} ::: true false nocommand &&
+id=\$(flux submit --urgency=hold hostname) &&
 flux job cancel \$id &&
-id=\$(flux mini submit --wait-event=start sleep 30) &&
-flux mini submit --quiet --dependency=afternotok:\$id true &&
+id=\$(flux submit --wait-event=start sleep 30) &&
+flux submit --quiet --dependency=afternotok:\$id true &&
 flux job cancel \$id &&
 flux queue drain &&
 flux jobs -a
