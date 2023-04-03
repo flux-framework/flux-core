37c3efa8701e36e8a6d551f61c8f8958326dda68
Albert Chu <chu11@llnl.gov>
2023-04-03 10:35:02 -0700

    flux-top: correct variable names in prototypes
    Problem: Several functions in top.h had cut and paste errors with
    their variable names.
    
    Correct the variable names.

--- src/cmd/top/top.h
+++ src/cmd/top/top.h
@@ -74,10 +74,10 @@ void summary_pane_heartbeat (struct summary_pane *sum);
 void summary_pane_toggle_details (struct summary_pane *sum);
 
 struct joblist_pane *joblist_pane_create (struct top *top);
-void joblist_pane_destroy (struct joblist_pane *sum);
-void joblist_pane_draw (struct joblist_pane *sum);
-void joblist_pane_refresh (struct joblist_pane *sum);
-void joblist_pane_query (struct joblist_pane *sum);
+void joblist_pane_destroy (struct joblist_pane *joblist);
+void joblist_pane_draw (struct joblist_pane *joblist);
+void joblist_pane_refresh (struct joblist_pane *joblist);
+void joblist_pane_query (struct joblist_pane *joblist);
 void joblist_pane_set_current (struct joblist_pane *joblist, bool next);
 void joblist_pane_enter (struct joblist_pane *joblist);
 
