136aa755ff7d67662e8189147bcf18b0d189ff75
Tom Scogland <scogland1@llnl.gov>
2023-04-25 18:28:42 -0700

    get off of unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "flux-core";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
     # flake-compat = {
     #   url = "github:edolstra/flake-compat";
