18781db4a2ebf11e247b4470a8adf6c0f263a614
Mark A. Grondona <mark.grondona@gmail.com>
2023-04-27 16:43:31 -0700

    libutil: add timestamp_parse()
    Problem: It would be useful to be able to parse the ISO 8601
    timestamp used by Flux logging, but no shared function exists to
    do so.
    
    Copy the parse_timestamp() function used by flux-dmesg(1) into
    libutil/timestamp.c, and rename it timestamp_parse().

--- src/common/libutil/timestamp.c
+++ src/common/libutil/timestamp.c
@@ -12,9 +12,33 @@
 #  include <config.h>
 #endif /* HAVE_CONFIG_H */
 
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "timestamp.h"
+
+/*
+ *  GNU libc has timegm(3), but the manual states:
+ *
+ *   These functions [timelocal(), timegm()] are nonstandard GNU extensions
+ *    that are also present on the BSDs.  Avoid their use.
+ *
+ *  This "portable" version was found on sourceware.org, and appears to work:
+ *
+ *   https://patchwork.sourceware.org/project/glibc/patch/20211011115406.11430-2-alx.manpages@gmail.com/
+ *
+ */
+static time_t portable_timegm (struct tm *tm)
+{
+    time_t t;
+
+    tm->tm_isdst = 0;
+    if ((t = mktime (tm)) == (time_t) -1)
+        return t;
+    return t - timezone;
+}
 
 int timestamp_tostr (time_t t, char *buf, int size)
 {
@@ -38,6 +62,44 @@ int timestamp_fromstr (const char *s, time_t *tp)
         *tp = t;
     return 0;
 }
+
+int timestamp_parse (const char *s,
+                     struct tm *tm,
+                     struct timeval *tv)
+{
+    char *extra;
+    struct tm gm_tm;
+    time_t t;
+
+    memset (tm, 0, sizeof (*tm));
+
+    if (!(extra = strptime (s, "%FT%T", &gm_tm)))
+        return -1;
+
+    if ((t = portable_timegm (&gm_tm)) == (time_t) -1)
+        return -1;
+
+    if (!(localtime_r (&t, tm)))
+        return -1;
+    tv->tv_sec = t;
+
+    if (extra[0] == '.') {
+        char *endptr;
+        double d;
+
+        errno = 0;
+        d = strtod (extra, &endptr);
+
+        /*  Note: in this implementation, there should be a "Z" after the
+         *  timestamp to indicate UTC or "Zulu" time.
+         */
+        if (errno != 0 || *endptr != 'Z')
+            return -1;
+        tv->tv_usec = d * 1000000;
+    }
+    return 0;
+}
+
 
 /*
  * vi:tabstop=4 shiftwidth=4 expandtab
--- src/common/libutil/timestamp.h
+++ src/common/libutil/timestamp.h
@@ -22,6 +22,16 @@ int timestamp_tostr (time_t t, char *buf, int size);
  */
 int timestamp_fromstr (const char *s, time_t *tp);
 
+/* Convert from ISO 8601 timestamp string, including optional
+ * microsecond precision, to struct tm, timeval pair.
+ *
+ * e.g. "2022-10-15T14:43:18.159009Z"
+ *
+ */
+int timestamp_parse (const char *s,
+                     struct tm *tm,
+                     struct timeval *tv);
+
 
 #endif /* !_UTIL_TIMESTAMP_H */
 
