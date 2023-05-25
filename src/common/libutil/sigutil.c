135c12a5c922060702d8ef406b62c9fcba01f8a1
Mark A. Grondona <mark.grondona@gmail.com>
2023-05-25 08:10:11 -0700

    libtuil: add signal name/number helper routines
    Problem: Conversion of signal name to number and vice versa will be
    useful in a couple places in flux-core, but there are no standard
    routines for this purpose.
    
    Add a sigutil interface with two helper routines:
    
     int sigutil_signum (const char *s);
    
    which converts a string to a signal number, and:
    
     const char * sigutil_signame (int signum);
    
    which converts a signal number to a name.
    
    The set of supported signals is limited to the most common via a
    statically defined list in sigutil.c.

---
+++ src/common/libutil/sigutil.c
@@ -0,0 +1,129 @@
+/************************************************************\
+ * Copyright 2023 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ccan/str/str.h"
+#include "ccan/array_size/array_size.h"
+
+#include "sigutil.h"
+
+struct signal_info {
+    int signum;
+    const char *name;
+};
+
+#define SIGDEF(x) { x, #x }
+
+/* Note:
+ *
+ * This list was generated on Debian 11 with the cmdline:
+ *
+ *  $ kill -l \
+ *      | sed 's/[0-9]*)//g' \
+ *      | xargs -n1 printf '    SIGDEF(%s),\n' \
+ *      | grep -v SIGRT
+ */
+static const struct signal_info signals[] = {
+    SIGDEF(SIGHUP),
+    SIGDEF(SIGINT),
+    SIGDEF(SIGQUIT),
+    SIGDEF(SIGILL),
+    SIGDEF(SIGTRAP),
+    SIGDEF(SIGABRT),
+    SIGDEF(SIGBUS),
+    SIGDEF(SIGFPE),
+    SIGDEF(SIGKILL),
+    SIGDEF(SIGUSR1),
+    SIGDEF(SIGSEGV),
+    SIGDEF(SIGUSR2),
+    SIGDEF(SIGPIPE),
+    SIGDEF(SIGALRM),
+    SIGDEF(SIGTERM),
+    SIGDEF(SIGSTKFLT),
+    SIGDEF(SIGCHLD),
+    SIGDEF(SIGCONT),
+    SIGDEF(SIGSTOP),
+    SIGDEF(SIGTSTP),
+    SIGDEF(SIGTTIN),
+    SIGDEF(SIGTTOU),
+    SIGDEF(SIGURG),
+    SIGDEF(SIGXCPU),
+    SIGDEF(SIGXFSZ),
+    SIGDEF(SIGVTALRM),
+    SIGDEF(SIGPROF),
+    SIGDEF(SIGWINCH),
+    SIGDEF(SIGIO),
+    SIGDEF(SIGPWR),
+    SIGDEF(SIGSYS),
+};
+
+static bool isnumber (const char *s, int *result)
+{
+    char *endptr;
+    long int l;
+
+    errno = 0;
+    l = strtol (s, &endptr, 10);
+    if (errno || *endptr != '\0') {
+        return false;
+    }
+    *result = (int) l;
+    return true;
+}
+
+int sigutil_signum (const char *s)
+{
+    int signum;
+    if (s == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+    if (isnumber (s, &signum)) {
+        if (signum <= 0) {
+            errno = EINVAL;
+            return -1;
+        }
+        return signum;
+    }
+    for (int i = 0; i < ARRAY_SIZE(signals); i++) {
+        if (streq (s, signals[i].name)
+            || streq (s, signals[i].name+3))
+            return signals[i].signum;
+    }
+    errno = ENOENT;
+    return -1;
+}
+
+const char *sigutil_signame (int signum)
+{
+    if (signum <= 0) {
+        errno = EINVAL;
+        return NULL;
+    }
+    for (int i = 0; i < ARRAY_SIZE(signals); i++) {
+        if (signals[i].signum == signum)
+            return signals[i].name;
+    }
+    errno = ENOENT;
+    return NULL;
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
