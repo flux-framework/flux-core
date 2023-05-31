b44570c70cf1ca964f2221cef52b255bbe778193
Mark A. Grondona <mark.grondona@gmail.com>
2023-05-31 15:24:16 -0700

    flux-filemap: use libfilemap routines
    Problem: The flux-filemap(1) command contains duplicated code that
    in the libfilemap convenience library.
    
    Update flux-filemap to use the filemap_mmap_list() and
    filemap_extract() functions from libfilemap instead of local functions.

--- src/cmd/builtin/filemap.c
+++ src/cmd/builtin/filemap.c
@@ -26,6 +26,7 @@
 #include "src/common/libutil/dirwalk.h"
 #include "src/common/libutil/errno_safe.h"
 #include "src/common/libcontent/content.h"
+#include "src/common/libfilemap/filemap.h"
 #include "src/common/libutil/fileref.h"
 
 static const int default_chunksize = 1048576;
@@ -79,52 +80,6 @@ done:
     return result;
 }
 
-/* Decode the raw data field a fileref object, setting the result in 'data'
- * and 'data_size'.  Caller must free.
- */
-static int decode_data (const char *s, void **data, size_t *data_size)
-{
-    if (s) {
-        int len = strlen (s);
-        size_t bufsize = base64_decoded_length (len);
-        void *buf;
-        ssize_t n;
-
-        if (!(buf = malloc (bufsize)))
-            return -1;
-        if ((n = base64_decode (buf, bufsize, s, len)) < 0) {
-            free (buf);
-            errno = EINVAL;
-            return -1;
-        }
-        *data = buf;
-        *data_size = n;
-    }
-    return 0;
-}
-
-static json_t *load_fileref (flux_t *h, const char *blobref)
-{
-    flux_future_t *f;
-    const void *buf;
-    int size;
-    json_t *o;
-    json_error_t error;
-
-    if (!(f = content_load_byblobref (h, blobref, 0))
-        || content_load_get (f, &buf, &size) < 0) {
-        log_msg_exit ("error loading fileref from %s: %s",
-                      blobref,
-                      future_strerror (f, errno));
-    }
-    if (!(o = json_loads (buf, 0, &error)))
-        log_msg_exit ("error decoding fileref object from %s: %s",
-                      blobref,
-                      error.text);
-    flux_future_destroy (f);
-    return o;
-}
-
 static flux_future_t *mmap_add (flux_t *h,
                                  const char *path,
                                  bool disable_mmap,
@@ -169,35 +124,6 @@ static flux_future_t *mmap_remove (flux_t *h, json_t *tags)
 {
     flux_future_t *f;
     f = flux_rpc_pack (h, "content.mmap-remove", 0, 0, "{s:O}", "tags", tags);
-    return f;
-}
-
-static flux_future_t *mmap_list (flux_t *h,
-                                 bool blobref,
-                                 json_t *tags,
-                                 const char *pattern)
-{
-    flux_future_t *f;
-
-    if (pattern) {
-        f = flux_rpc_pack (h,
-                           "content.mmap-list",
-                           0,
-                           FLUX_RPC_STREAMING,
-                           "{s:b s:s s:O}",
-                           "blobref", blobref ? 1 : 0,
-                           "pattern", pattern,
-                           "tags", tags);
-    }
-    else {
-        f = flux_rpc_pack (h,
-                           "content.mmap-list",
-                           0,
-                           FLUX_RPC_STREAMING,
-                           "{s:b s:O}",
-                           "blobref", blobref ? 1 : 0,
-                           "tags", tags);
-    }
     return f;
 }
 
@@ -330,7 +256,7 @@ static int subcmd_list (optparse_t *p, int ac, char *av[])
     }
     if (!(h = builtin_get_flux_handle (p)))
         log_err_exit ("flux_open");
-    if (!(f = mmap_list (h, blobref, tags, pattern)))
+    if (!(f = filemap_mmap_list (h, blobref, tags, pattern)))
         log_err_exit ("mmap-list");
     for (;;) {
         json_t *files;
@@ -366,170 +292,18 @@ static int subcmd_list (optparse_t *p, int ac, char *av[])
     return 0;
 }
 
-static void extract_blob (flux_t *h,
-                          struct archive *archive,
-                          const char *path,
-                          json_t *o)
+static void trace_fn (void *arg,
+                      json_t *fileref,
+                      const char *path,
+                      int mode,
+                      int64_t size,
+                      int64_t mtime,
+                      int64_t ctime,
+                      const char *encoding)
 {
-    struct {
-        json_int_t offset;
-        json_int_t size;
-        const char *blobref;
-    } entry;
-    flux_future_t *f;
-    const void *buf;
-    int size;
-
-    if (json_unpack (o,
-                     "[I,I,s]",
-                     &entry.offset,
-                     &entry.size,
-                     &entry.blobref) < 0)
-        log_msg_exit ("%s: error decoding blobvec entry", path);
-    if (!(f = content_load_byblobref (h, entry.blobref, 0))
-        || content_load_get (f, &buf, &size) < 0) {
-        log_msg_exit ("%s: error loading offset=%ju size=%ju from %s: %s",
-                      path,
-                      (uintmax_t)entry.offset,
-                      (uintmax_t)entry.size,
-                      entry.blobref,
-                      future_strerror (f, errno));
-    }
-    if (size != entry.size) {
-        log_msg_exit ("%s: error loading offset=%ju size=%ju from %s:"
-                      " unexpected size %ju",
-                      path,
-                      (uintmax_t)entry.offset,
-                      (uintmax_t)entry.size,
-                      entry.blobref,
-                      (uintmax_t)size);
-    }
-    if (archive_write_data_block (archive,
-                                  buf,
-                                  size,
-                                  entry.offset) != ARCHIVE_OK)
-        log_msg_exit ("%s: write: %s", path, archive_error_string (archive));
-    flux_future_destroy (f);
-}
-
-
-static void extract_file (flux_t *h,
-                          optparse_t *p,
-                          struct archive *archive,
-                          json_t *fileref)
-{
-    int verbose = optparse_get_int (p, "verbose", 0);
-    const char *path;
-    int mode;
-    json_int_t size = -1;
-    json_int_t ctime = -1;
-    json_int_t mtime = -1;
-    const char *encoding = NULL;
-    json_t *data = NULL;
-    size_t index;
-    json_t *o;
-    struct archive_entry *entry;
-    json_error_t error;
-
-    if (json_unpack_ex (fileref,
-                        &error,
-                        0,
-                        "{s:s s:i s?I s?I s?I s?s s?o}",
-                        "path", &path,
-                        "mode", &mode,
-                        "size", &size,
-                        "mtime", &mtime,
-                        "ctime", &ctime,
-                        "encoding", &encoding,
-                        "data", &data) < 0)
-        log_msg_exit ("error decoding fileref object: %s", error.text);
-
-    if (verbose > 0)
+    optparse_t *p = arg;
+    if (optparse_get_int (p, "verbose", 0) > 0)
         fprintf (stderr, "%s\n", path);
-
-    /* metadata
-     */
-    if (!(entry = archive_entry_new ()))
-        log_msg_exit ("%s: error creating libarchive entry", path);
-    archive_entry_set_pathname (entry, path);
-    archive_entry_set_mode (entry, mode);
-    if (mtime != -1)
-        archive_entry_set_mtime (entry, mtime, 0);
-    if (ctime != -1)
-        archive_entry_set_ctime (entry, ctime, 0);
-    if (S_ISREG (mode)) {
-        if (size != -1)
-            archive_entry_set_size (entry, size);
-    }
-    else if (S_ISLNK (mode)) {
-        const char *target;
-        if (!data || !(target = json_string_value (data)))
-            log_msg_exit ("%s: missing symlink data", path);
-        archive_entry_set_symlink (entry, target);
-    }
-    else if (!S_ISDIR (mode)) // nothing to do for directory
-        log_msg_exit ("%s: unknown file type (mode=0%o)", path, mode);
-    if (archive_write_header (archive, entry) != ARCHIVE_OK)
-        log_msg_exit ("%s: %s", path, archive_error_string (archive));
-
-    /* data
-     */
-    if (S_ISREG (mode) && data != NULL) {
-        if (!encoding) {
-          char *str;
-            if (!(str = json_dumps (data, JSON_ENCODE_ANY | JSON_COMPACT)))
-                log_msg_exit ("%s: could not encode JSON file data", path);
-            if (archive_write_data_block (archive,
-                                          str,
-                                          strlen (str),
-                                          0) != ARCHIVE_OK) {
-                log_msg_exit ("%s: write: %s",
-                              path,
-                              archive_error_string (archive));
-            }
-            free (str);
-        }
-        else if (streq (encoding, "base64")) {
-            const char *str = json_string_value (data);
-            void *buf;
-            size_t buf_size;
-
-            if (!str || decode_data (str, &buf, &buf_size) < 0)
-                log_msg_exit ("%s: could not decode base64 file data", path);
-            if (archive_write_data_block (archive,
-                                          buf,
-                                          buf_size,
-                                          0) != ARCHIVE_OK) {
-                log_msg_exit ("%s: write: %s",
-                              path,
-                              archive_error_string (archive));
-            }
-            free (buf);
-        }
-        else if (streq (encoding, "blobvec")) {
-            json_array_foreach (data, index, o) {
-                extract_blob (h, archive, path, o);
-            }
-        }
-        else if (streq (encoding, "utf-8")) {
-            const char *str = json_string_value (data);
-            if (!str)
-                log_msg_exit ("%s: unexpected data type for utf8", path);
-            if (archive_write_data_block (archive,
-                                          str,
-                                          strlen (str),
-                                          0) != ARCHIVE_OK) {
-                log_msg_exit ("%s: write: %s",
-                              path,
-                              archive_error_string (archive));
-            }
-        }
-        else {
-            log_msg_exit ("%s: unknown RFC 37 encoding %s", path, encoding);
-        }
-    }
-
-    archive_entry_free (entry);
 }
 
 static void extract (flux_t *h,
@@ -540,10 +314,9 @@ static void extract (flux_t *h,
     json_t *tags = get_list_option (p, "tags", "main");
     bool direct = optparse_hasopt (p, "direct");
     flux_future_t *f;
-    size_t index;
-    json_t *entry;
+    flux_error_t error;
 
-    if (!(f = mmap_list (h, !direct, tags, pattern)))
+    if (!(f = filemap_mmap_list (h, !direct, tags, pattern)))
         log_err_exit ("mmap-list");
     for (;;) {
         json_t *files;
@@ -553,15 +326,8 @@ static void extract (flux_t *h,
                 break; // end of stream
             log_msg_exit ("mmap-list: %s", future_strerror (f, errno));
         }
-        json_array_foreach (files, index, entry) {
-            if (direct)
-                extract_file (h, p, archive, entry);
-            else {
-                json_t *fileref = load_fileref (h, json_string_value (entry));
-                extract_file (h, p, archive, fileref);
-                json_decref (fileref);
-            }
-        }
+        if (filemap_extract (h, files, direct, &error, trace_fn, p) < 0)
+            log_msg_exit ("%s", error.text);
         flux_future_reset (f);
     }
     flux_future_destroy (f);
