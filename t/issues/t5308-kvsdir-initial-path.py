4e7c2f8e52378ec891674d5fca2f595ec082cbd1
Albert Chu <chu11@llnl.gov>
2023-07-09 22:32:54 -0700

    testsuite: add KVSDir regression test
    Problem: The KVSDir initial path write issue (#5308) was discovered
    via the job_kvs() function in the job/kvs.py module.  There is no regression
    test for this issue.
    
    Add a new regression test t5308-kvsdir-initial-path.py.

---
+++ t/issues/t5308-kvsdir-initial-path.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+#  Modifying a job data via object (KVSDir) returned via job_kvs()
+#    works.
+#
+import sys
+import flux
+import flux.job
+from flux.job import JobspecV1
+
+handle = flux.Flux()
+jobspec = JobspecV1.from_command(command=["/bin/true"], num_tasks=1, num_nodes=1)
+jobid = flux.job.submit(handle, jobspec, waitable=True)
+flux.job.wait(handle, jobid=jobid)
+jobdir = flux.job.job_kvs(handle, jobid)
+jobdir["foo"] = "bar"
+jobdir.commit()
+
+jobdir2 = flux.job.job_kvs(handle, jobid)
+if jobdir2["foo"] != "bar":
+    sys.exit(1)
+
