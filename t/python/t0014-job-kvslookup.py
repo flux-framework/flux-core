97209330a35516d20f98129930e198d5de3dc498
Albert Chu <chu11@llnl.gov>
2023-09-06 10:30:41 -0700

    testsuite: cleanup python job kvslookup tests
    Problem: The tests in t0014-job-kvslookup.py are numbered, making
    it annoying to add new tests and renumber all of the tests.
    
    To avoid excessive re-numbering of tests when new tests
    are added, logically group similar tests together and numerically
    number tests only within a group.

--- t/python/t0014-job-kvslookup.py
+++ t/python/t0014-job-kvslookup.py
@@ -86,149 +86,149 @@ class TestJob(unittest.TestCase):
             data["jobspec"]["attributes"]["system"]["environment"]["FOO"], "BAR"
         )
 
-    def test_00_job_info_lookup(self):
+    def test_info_00_job_info_lookup(self):
         rpc = flux.job.job_info_lookup(self.fh, self.jobid1)
         data = rpc.get()
         self.check_jobspec_str(data, self.jobid1)
         data = rpc.get_decode()
         self.assertEqual(data["id"], self.jobid1)
 
-    def test_01_job_info_lookup_keys(self):
+    def test_info_01_job_info_lookup_keys(self):
         rpc = flux.job.job_info_lookup(self.fh, self.jobid1, keys=["R", "J"])
         data = rpc.get()
         self.check_R_J_str(data, self.jobid1)
         data = rpc.get_decode()
         self.check_R_J_decoded(data, self.jobid1)
 
-    def test_02_job_info_lookup_badid(self):
+    def test_info_02_job_info_lookup_badid(self):
         rpc = flux.job.job_info_lookup(self.fh, 123456789)
         with self.assertRaises(FileNotFoundError):
             rpc.get()
 
-    def test_03_job_info_lookup_badkey(self):
+    def test_info_03_job_info_lookup_badkey(self):
         rpc = flux.job.job_info_lookup(self.fh, self.jobid1, keys=["foo"])
         with self.assertRaises(FileNotFoundError):
             rpc.get()
 
-    def test_04_job_kvs_lookup(self):
+    def test_lookup_01_job_kvs_lookup(self):
         data = flux.job.job_kvs_lookup(self.fh, self.jobid1)
         self.check_jobspec_decoded(data, self.jobid1)
 
-    def test_05_job_kvs_lookup_nodecode(self):
+    def test_lookup_02_job_kvs_lookup_nodecode(self):
         data = flux.job.job_kvs_lookup(self.fh, self.jobid1, decode=False)
         self.check_jobspec_str(data, self.jobid1)
 
-    def test_06_job_kvs_lookup_keys(self):
+    def test_lookup_03_job_kvs_lookup_keys(self):
         data = flux.job.job_kvs_lookup(self.fh, self.jobid1, keys=["R", "J"])
         self.check_R_J_decoded(data, self.jobid1)
 
-    def test_07_job_kvs_lookup_keys_nodecode(self):
+    def test_lookup_04_job_kvs_lookup_keys_nodecode(self):
         data = flux.job.job_kvs_lookup(
             self.fh, self.jobid1, keys=["R", "J"], decode=False
         )
         self.check_R_J_str(data, self.jobid1)
 
-    def test_08_job_kvs_lookup_badid(self):
+    def test_lookup_05_job_kvs_lookup_badid(self):
         data = flux.job.job_kvs_lookup(self.fh, 123456789)
         self.assertEqual(data, None)
 
-    def test_09_job_kvs_lookup_badkey(self):
+    def test_lookup_06_job_kvs_lookup_badkey(self):
         data = flux.job.job_kvs_lookup(self.fh, self.jobid1, keys=["foo"])
         self.assertEqual(data, None)
 
-    def test_10_job_kvs_lookup_jobspec_original(self):
+    def test_lookup_07_job_kvs_lookup_jobspec_original(self):
         data = flux.job.job_kvs_lookup(self.fh, self.jobid1, original=True)
         self.assertNotIn("J", data)
         self.check_jobspec_original_decoded(data, self.jobid1)
 
-    def test_11_job_kvs_lookup_jobspec_original_nodecode(self):
+    def test_lookup_08_job_kvs_lookup_jobspec_original_nodecode(self):
         data = flux.job.job_kvs_lookup(
             self.fh, self.jobid1, decode=False, original=True
         )
         self.assertNotIn("J", data)
         self.check_jobspec_original_str(data, self.jobid1)
 
-    def test_12_job_kvs_lookup_jobspec_original_multiple_keys(self):
+    def test_lookup_09_job_kvs_lookup_jobspec_original_multiple_keys(self):
         data = flux.job.job_kvs_lookup(
             self.fh, self.jobid1, keys=["jobspec", "J"], original=True
         )
         self.assertIn("J", data)
         self.check_jobspec_original_decoded(data, self.jobid1)
 
-    def test_13_job_kvs_lookup_original_no_jobspec(self):
+    def test_lookup_10_job_kvs_lookup_original_no_jobspec(self):
         data = flux.job.job_kvs_lookup(
             self.fh, self.jobid1, keys=["R", "J"], original=True
         )
         self.assertNotIn("jobspec", data)
         self.check_R_J_decoded(data, self.jobid1)
 
-    def test_14_job_kvs_lookup_list(self):
+    def test_list_00_job_kvs_lookup_list(self):
         ids = [self.jobid1]
         data = flux.job.JobKVSLookup(self.fh, ids).data()
         self.assertEqual(len(data), 1)
         self.check_jobspec_decoded(data[0], self.jobid1)
 
-    def test_15_job_kvs_lookup_list_multiple(self):
+    def test_list_01_job_kvs_lookup_list_multiple(self):
         ids = [self.jobid1, self.jobid2]
         data = flux.job.JobKVSLookup(self.fh, ids).data()
         self.assertEqual(len(data), 2)
         self.check_jobspec_decoded(data[0], self.jobid1)
         self.check_jobspec_decoded(data[1], self.jobid2)
 
-    def test_16_job_kvs_lookup_list_multiple_nodecode(self):
+    def test_list_02_job_kvs_lookup_list_multiple_nodecode(self):
         ids = [self.jobid1, self.jobid2]
         data = flux.job.JobKVSLookup(self.fh, ids, decode=False).data()
         self.assertEqual(len(data), 2)
         self.check_jobspec_str(data[0], self.jobid1)
         self.check_jobspec_str(data[1], self.jobid2)
 
-    def test_17_job_kvs_lookup_list_multiple_keys(self):
+    def test_list_03_job_kvs_lookup_list_multiple_keys(self):
         ids = [self.jobid1, self.jobid2]
         data = flux.job.JobKVSLookup(self.fh, ids, keys=["R", "J"]).data()
         self.assertEqual(len(data), 2)
         self.check_R_J_decoded(data[0], self.jobid1)
         self.check_R_J_decoded(data[1], self.jobid2)
 
-    def test_18_job_kvs_lookup_list_multiple_keys_nodecode(self):
+    def test_list_04_job_kvs_lookup_list_multiple_keys_nodecode(self):
         ids = [self.jobid1, self.jobid2]
         data = flux.job.JobKVSLookup(self.fh, ids, keys=["R", "J"], decode=False).data()
         self.assertEqual(len(data), 2)
         self.check_R_J_str(data[0], self.jobid1)
         self.check_R_J_str(data[1], self.jobid2)
 
-    def test_19_job_kvs_lookup_list_none(self):
+    def test_list_05_job_kvs_lookup_list_none(self):
         data = flux.job.JobKVSLookup(self.fh).data()
         self.assertEqual(len(data), 0)
 
-    def test_20_job_kvs_lookup_list_badid(self):
+    def test_list_06_job_kvs_lookup_list_badid(self):
         ids = [123456789]
         datalookup = flux.job.JobKVSLookup(self.fh, ids)
         data = datalookup.data()
         self.assertEqual(len(data), 0)
         self.assertEqual(len(datalookup.errors), 1)
 
-    def test_21_job_kvs_lookup_list_badkey(self):
+    def test_list_07_job_kvs_lookup_list_badkey(self):
         ids = [self.jobid1]
         datalookup = flux.job.JobKVSLookup(self.fh, ids, keys=["foo"])
         data = datalookup.data()
         self.assertEqual(len(data), 0)
         self.assertEqual(len(datalookup.errors), 1)
 
-    def test_22_job_kvs_lookup_list_jobspec_original(self):
+    def test_list_08_job_kvs_lookup_list_jobspec_original(self):
         ids = [self.jobid1]
         data = flux.job.JobKVSLookup(self.fh, ids, original=True).data()
         self.assertEqual(len(data), 1)
         self.assertNotIn("J", data[0])
         self.check_jobspec_original_decoded(data[0], self.jobid1)
 
-    def test_23_job_kvs_lookup_list_jobspec_original_nodecode(self):
+    def test_list_09_job_kvs_lookup_list_jobspec_original_nodecode(self):
         ids = [self.jobid1]
         data = flux.job.JobKVSLookup(self.fh, ids, decode=False, original=True).data()
         self.assertEqual(len(data), 1)
         self.assertNotIn("J", data[0])
         self.check_jobspec_original_str(data[0], self.jobid1)
 
-    def test_24_job_kvs_lookup_list_jobspec_original_multiple_keys(self):
+    def test_list_10_job_kvs_lookup_list_jobspec_original_multiple_keys(self):
         ids = [self.jobid1]
         data = flux.job.JobKVSLookup(
             self.fh, ids, keys=["jobspec", "J"], original=True
@@ -237,7 +237,7 @@ class TestJob(unittest.TestCase):
         self.assertIn("J", data[0])
         self.check_jobspec_original_decoded(data[0], self.jobid1)
 
-    def test_25_job_kvs_lookup_list_original_no_jobspec(self):
+    def test_list_11_job_kvs_lookup_list_original_no_jobspec(self):
         ids = [self.jobid1]
         data = flux.job.JobKVSLookup(
             self.fh, ids, keys=["R", "J"], original=True
