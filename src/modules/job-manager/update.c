5d7d055a2010426b9fd6d24040360bfe3ad036b7
Mark A. Grondona <mark.grondona@gmail.com>
2023-09-07 20:05:19 -0700

    job-manager: update: return ENOENT for invalid job id
    Problem: When a job update is requested for an invalid job id, the
    same errno is returned as a valid, inactive job id, but it may be
    useful to differentiate the two.
    
    Return ENOENT for an invalid job id. Continue to return EINVAL for
    inactive jobs.

--- src/modules/job-manager/update.c
+++ src/modules/job-manager/update.c
@@ -196,11 +196,14 @@ static void update_handle_request (flux_t *h,
     /*  Verify jobid exists and is not inactive
      */
     if (!(job = zhashx_lookup (ctx->active_jobs, &id))) {
-        if (!(job = zhashx_lookup (ctx->inactive_jobs, &id)))
+        if (!(job = zhashx_lookup (ctx->inactive_jobs, &id))) {
             errstr = "unknown job id";
-        else
+            errno = ENOENT;
+        }
+        else {
             errstr = "job is inactive";
-        errno = EINVAL;
+            errno = EINVAL;
+        }
         goto error;
     }
     /*  Fetch the credential from this message and ensure the user
