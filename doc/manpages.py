e35504a736fc737783f55ee98572dc2e7e5d566f
Jim Garlick <garlick.jim@gmail.com>
2023-10-13 07:15:36 -0700

    flux_send_new(3): add new manpage stub
    Problem: there is no man page for flux_send_new().
    
    Add it to flux_send.rst and configure a stub to be produced for
    flux_send_new().
    
    The previous synopsis function prototypes were not within a literal
    block and had to be escaped.  Fix that while adding the new function.

--- doc/manpages.py
+++ doc/manpages.py
@@ -215,6 +215,7 @@ man_pages = [
     ('man3/flux_rpc', 'flux_rpc_get_matchtag', 'perform a remote procedure call to a Flux service', [author], 3),
     ('man3/flux_rpc', 'flux_rpc_get_nodeid', 'perform a remote procedure call to a Flux service', [author], 3),
     ('man3/flux_send', 'flux_send', 'send message using Flux Message Broker', [author], 3),
+    ('man3/flux_send', 'flux_send_new', 'send message using Flux Message Broker', [author], 3),
     ('man3/flux_service_register', 'flux_service_register', 'Register service with flux broker', [author], 3),
     ('man3/flux_service_register', 'flux_service_unregister', 'Unregister service with flux broker', [author], 3),
     ('man3/flux_shell_add_completion_ref', 'flux_shell_remove_completion_ref', 'Manipulate conditions for job completion.', [author], 3),
