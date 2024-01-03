7aabacb00ad07ba4ca5d273bcc8d558cdc069764
Mark A. Grondona <mark.grondona@gmail.com>
2024-01-03 11:19:37 -0800

    testsuite: test rolemask support in Python msg_handler_create()
    Problem: There are no tests of the optional rolemask parameter to
    the Python msg_handler_create() method.
    
    Add tests.

--- t/python/t1000-service-add-remove.py
+++ t/python/t1000-service-add-remove.py
@@ -11,6 +11,7 @@
 ###############################################################
 
 import errno
+import os
 import sys
 import unittest
 
@@ -33,6 +34,10 @@ class TestServiceAddRemove(unittest.TestCase):
         rc = self.f.service_register("foo").get()
         self.assertEqual(rc, None)
 
+    def test_001_register_owner_service(self):
+        rc = self.f.service_register("owner").get()
+        self.assertEqual(rc, None)
+
     def test_002_service_add_eexist(self):
         with self.assertRaises(EnvironmentError) as e:
             self.f.service_register("foo").get()
@@ -44,10 +49,28 @@ class TestServiceAddRemove(unittest.TestCase):
             self.assertEqual(rc, 0)
 
         self.f.watcher = self.f.msg_watcher_create(
-            service_cb, FLUX_MSGTYPE_REQUEST, "foo.echo"
+            service_cb,
+            FLUX_MSGTYPE_REQUEST,
+            "foo.echo",
+            rolemask=flux.constants.FLUX_ROLE_ALL,
         )
         self.assertIsNotNone(self.f.watcher)
         self.f.watcher.start()
+
+    def test_003_add_owner_message_watcher(self):
+        def service_cb(f, t, msg, arg):
+            rc = f.respond(msg, msg.payload_str)
+            self.assertEqual(rc, 0)
+
+        # Note: default rolemask for handle.msg_watcher_create()
+        # should allow instance-owner only:
+        self.f.watcher2 = self.f.msg_watcher_create(
+            service_cb,
+            FLUX_MSGTYPE_REQUEST,
+            "owner.echo",
+        )
+        self.assertIsNotNone(self.f.watcher2)
+        self.f.watcher2.start()
 
     def test_004_service_rpc(self):
         cb_called = [False]  # So that cb_called[0] is mutable in inner function
@@ -68,6 +91,60 @@ class TestServiceAddRemove(unittest.TestCase):
         self.assertTrue(m is not None)
         ret = self.f.send(m)
         self.assertEqual(ret, 0)
+
+        ret = self.f.reactor_run()
+        self.assertTrue(ret >= 0)
+        self.assertTrue(cb_called[0])
+        w2.stop()
+        w2.destroy()
+
+    def send_user_request(self, topic, payload):
+        m = Message()
+        m.topic = topic
+        m.payload = payload
+        m.pimpl.set_rolemask(flux.constants.FLUX_ROLE_USER)
+        m.pimpl.set_userid(os.getuid() + 1)
+        self.assertTrue(m is not None)
+        ret = self.f.send(m)
+        self.assertEqual(ret, 0)
+
+    def test_004_service_rpc_user(self):
+        cb_called = [False]  # So that cb_called[0] is mutable in inner function
+        p = {"test": "bar"}
+
+        def cb(f, t, msg, arg):
+            cb_called[0] = True
+            self.assertEqual(msg.payload, p)
+            f.reactor_stop()
+
+        w2 = self.f.msg_watcher_create(cb, FLUX_MSGTYPE_RESPONSE, "foo.echo")
+        w2.start()
+        self.assertIsNotNone(w2, msg="msg_watcher_create response handler")
+
+        self.send_user_request("foo.echo", p)
+
+        ret = self.f.reactor_run()
+        self.assertTrue(ret >= 0)
+        self.assertTrue(cb_called[0])
+        w2.stop()
+        w2.destroy()
+
+    def test_004_service_rpc_user_eperm(self):
+        cb_called = [False]  # So that cb_called[0] is mutable in inner function
+        p = {"test": "baz"}
+
+        def cb(f, t, msg, arg):
+            cb_called[0] = True
+            with self.assertRaises(OSError) as exc:
+                msgtype, topic, data = msg.decode()
+            self.assertEqual(exc.exception.errno, errno.EPERM)
+            f.reactor_stop()
+
+        w2 = self.f.msg_watcher_create(cb, FLUX_MSGTYPE_RESPONSE, "owner.echo")
+        w2.start()
+        self.assertIsNotNone(w2, msg="msg_watcher_create response handler")
+
+        self.send_user_request("owner.echo", p)
 
         ret = self.f.reactor_run()
         self.assertTrue(ret >= 0)
@@ -105,8 +182,12 @@ class TestServiceAddRemove(unittest.TestCase):
         rc = self.f.service_unregister("foo").get()
         self.assertEqual(rc, None)
 
+        rc = self.f.service_unregister("owner").get()
+        self.assertEqual(rc, None)
+
         # done with message handler
         self.f.watcher.destroy()
+        self.f.watcher2.destroy()
         self.f.error_watcher.destroy()
 
     def test_006_unregister_service_enoent(self):
