7d11ecce3396c5ad811c47bb70da415d10bd8b7b
Mark A. Grondona <mark.grondona@gmail.com>
2024-02-02 11:44:45 -0800

    testsuite: add one-line MPI timing output mode to t/mpi/hello
    Problem: There is no way to easily collect a set of timing output from
    the MPI hello test in t/mpi/hello.c.
    
    Modify the MPI hello test to emit formatted timing output when
    FLUX_MPI_TEST_TIMING is set in the environment.

--- t/mpi/hello.c
+++ t/mpi/hello.c
@@ -11,6 +11,7 @@
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include <stdint.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -19,30 +20,56 @@
 
 #include "src/common/libutil/monotime.h"
 
+static inline void monotime_diff (struct timespec *t0,
+                                  struct timespec *result)
+{
+    struct timespec now;
+    monotime (&now);
+    result->tv_sec  = now.tv_sec - t0->tv_sec;
+    result->tv_nsec = now.tv_nsec - t0->tv_nsec;
+    if (result->tv_nsec < 0) {
+        --result->tv_sec;
+        result->tv_nsec += 1000000000L;
+    }
+}
+
+static void die (const char *msg)
+{
+    fprintf (stderr, "%s\n", msg);
+    exit (1);
+}
+
 int main (int argc, char *argv[])
 {
     int id, ntasks;
-    struct timespec t;
+    struct timespec t0, t, times[4];
     const char *label;
+    bool timing = getenv ("FLUX_MPI_TEST_TIMING");
 
     if (!(label = getenv ("FLUX_JOB_CC")))
         if (!(label = getenv ("FLUX_JOB_ID")))
             label = "0";
 
-    monotime (&t);
-    MPI_Init (&argc, &argv);
-    MPI_Comm_rank (MPI_COMM_WORLD, &id);
-    MPI_Comm_size (MPI_COMM_WORLD, &ntasks);
-    if (id == 0) {
-        printf ("%s: completed MPI_Init in %0.3fs.  There are %d tasks\n",
+    monotime (&t0);
+    if (MPI_Init (&argc, &argv) != MPI_SUCCESS)
+        die ("MPI_Init failed\n");
+    if (MPI_Comm_rank (MPI_COMM_WORLD, &id) != MPI_SUCCESS
+        || MPI_Comm_size (MPI_COMM_WORLD, &ntasks) != MPI_SUCCESS)
+        die ("MPI_Comm_rank/size failed");
+    monotime_diff (&t0, &times[0]);
+    if (!timing && id == 0) {
+        printf ("%s: completed MPI_Init in %0.3fs. There are %d tasks\n",
                 label,
-                monotime_since (t) / 1000, ntasks);
+                monotime_since (t0) / 1000,
+                ntasks);
         fflush (stdout);
     }
 
     monotime (&t);
-    MPI_Barrier (MPI_COMM_WORLD);
-    if (id == 0) {
+    if (MPI_Barrier (MPI_COMM_WORLD) != MPI_SUCCESS)
+        die ("MPI_Barrier failed");
+    monotime_diff (&t, &times[1]);
+    if (!timing && id == 0) {
         printf ("%s: completed first barrier in %0.3fs\n",
                 label,
                 monotime_since (t) / 1000);
@@ -51,10 +78,22 @@ int main (int argc, char *argv[])
 
     monotime (&t);
     MPI_Finalize ();
+    monotime_diff (&t, &times[2]);
+    monotime_diff (&t0, &times[3]);
+
     if (id == 0) {
-        printf ("%s: completed MPI_Finalize in %0.3fs\n",
-                label,
-                monotime_since (t) / 1000);
+        if (timing) {
+            printf ("%6s %8d", getenv ("FLUX_JOB_NNODES"), ntasks);
+            for (int i = 0; i < 4; i++)
+                printf (" %4ju.%.9ld",
+                        (uintmax_t) times[i].tv_sec,
+                        times[i].tv_nsec);
+            printf ("\n");
+        }
+        else
+            printf ("%s: completed MPI_Finalize in %0.3fs\n",
+                    label,
+                    monotime_since (t) / 1000);
         fflush (stdout);
     }
     return 0;
