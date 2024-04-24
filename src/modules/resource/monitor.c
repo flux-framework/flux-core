42852ea1516f148ded5ec5e87e5af55a57c6e5a8
Jim Garlick <garlick.jim@gmail.com>
2024-04-23 19:42:38 -0700

    resource: don't post online/offline events to KVS
    Problem: the online/offline events in the resource.eventlog
    contribute to KVS eventlog bloat, but are not used when the
    resource module replays the eventlog.
    
    Post these events with the EVENT_NO_COMMIT flag so they are still
    seen in the internal eventlog callback but do not reach the KVS.

--- src/modules/resource/monitor.c
+++ src/modules/resource/monitor.c
@@ -111,7 +111,7 @@ static void broker_online_cb (flux_future_t *f, void *arg)
                                  NULL,
                                  0.,
                                  "online",
-                                 0,
+                                 EVENT_NO_COMMIT,
                                  "{s:s}",
                                  "idset",
                                  online) < 0) {
@@ -125,7 +125,7 @@ static void broker_online_cb (flux_future_t *f, void *arg)
                                  NULL,
                                  0.,
                                  "offline",
-                                 0,
+                                 EVENT_NO_COMMIT,
                                  "{s:s}",
                                  "idset",
                                  offline) < 0) {
