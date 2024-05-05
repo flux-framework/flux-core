94555e8b64f12a01532479ede5d7e1fd66880380
Mark A. Grondona <mark.grondona@gmail.com>
2024-05-05 07:03:35 -0700

    shell: add hwloc.restrict option
    Problem: The HWLOC_XMLFILE provided by the hwloc.xmlfile job shell
    option is not restricted to the resources available to the job,
    but in some circumstances this would be useful.
    
    Add a hwloc.restrict option that, when used with hwloc.xmlfile,
    restricts the HWLOC_XMLFILE to the current process binding.

--- src/shell/hwloc.c
+++ src/shell/hwloc.c
@@ -25,14 +25,16 @@
 #include <flux/shell.h>
 
 #include "src/common/libutil/read_all.h"
+#include "src/common/librlist/rhwloc.h"
 #include "ccan/str/str.h"
 
 #include "builtins.h"
 
-static int create_xmlfile (flux_shell_t *shell)
+static int create_xmlfile (flux_shell_t *shell, int do_restrict)
 {
     int fd = -1;
     char *xmlfile = NULL;
+    char *restricted_xml = NULL;
     const char *hwloc_xml;
     const char *tmpdir;
 
@@ -42,6 +44,13 @@ static int create_xmlfile (flux_shell_t *shell)
     if (flux_shell_get_hwloc_xml (shell, &hwloc_xml) < 0) {
         shell_log_error ("failed to get shell hwloc xml");
         goto error;
+    }
+    if (do_restrict) {
+        if (!(restricted_xml = rhwloc_topology_xml_restrict (hwloc_xml))) {
+            shell_log_errno ("failed to restrict topology xml");
+            goto error;
+        }
+        hwloc_xml = restricted_xml;
     }
     if (asprintf (&xmlfile, "%s/hwloc.xml", tmpdir) < 0) {
         shell_log_error ("asprintf HWLOC_XMLFILE failed");
@@ -75,6 +84,7 @@ error:
     if (fd >= 0)
         close (fd);
     free (xmlfile);
+    free (restricted_xml);
     return -1;
 }
 
@@ -85,14 +95,16 @@ static int hwloc_post_init (flux_plugin_t *p,
 {
     flux_shell_t *shell = flux_plugin_get_shell (p);
     int xmlfile = 0;
+    int do_restrict = 0;
 
     if (flux_shell_getopt_unpack (shell,
                                   "hwloc",
-                                  "{s?i}",
-                                    "xmlfile", &xmlfile) < 0)
+                                  "{s?i s?i}",
+                                    "xmlfile", &xmlfile,
+                                    "restrict", &do_restrict) < 0)
         return shell_log_errno ("failed to unpack hwloc options");
 
-    if (xmlfile && create_xmlfile (shell) < 0)
+    if (xmlfile && create_xmlfile (shell, do_restrict) < 0)
         return shell_log_errno ("failed to write HWLOC_XMLFILE");
     return 0;
 }
