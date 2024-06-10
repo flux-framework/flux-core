a84cc8d83a624a4183176f4942edec0db2919e57
Albert Chu <chu11@llnl.gov>
2024-06-10 10:34:46 -0700

    job-list: remove unnecessary variable args
    Problem: Several helper functions take variable arguments, which are
    unnecessary.
    
    Remove the variable args to these helper functions.

--- src/modules/job-list/test/state_match.c
+++ src/modules/job-list/test/state_match.c
@@ -20,25 +20,18 @@
 #include "ccan/str/str.h"
 
 static void state_constraint_create_corner_case (const char *str,
-                                                 const char *fmt,
-                                                 ...)
+                                                 const char *msg)
 {
     struct state_constraint *c;
-    char buf[1024];
     flux_error_t error;
     json_error_t jerror;
     json_t *jc;
-    va_list ap;
 
     if (!(jc = json_loads (str, 0, &jerror)))
         BAIL_OUT ("json constraint invalid: %s", jerror.text);
 
-    va_start (ap, fmt);
-    vsnprintf(buf, sizeof (buf), fmt, ap);
-    va_end (ap);
-
     c = state_constraint_create (jc, &error);
-    ok (c == NULL, "state_constraint_create fails on %s", buf);
+    ok (c == NULL, "state_constraint_create fails on %s", msg);
     diag ("error: %s", error.text);
     json_decref (jc);
 }
