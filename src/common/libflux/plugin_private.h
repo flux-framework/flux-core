0c9158d20d9771524ed62f78d3fa9681ca8e3a7d
Tom Scogland <scogland1@llnl.gov>
2024-06-28 12:17:18 -0700

    plugins: add env-var to disable DEEPBIND for all loads
    problem: I recently needed to get a heap trace of flux to track down why
    fluxion was using so much memory, that's another sordid tale I'm afraid,
    but every tool I tried reported freeing unallocated pointers.  There is
    a bad interaction between recent versions of glibc, that removed the
    allocator hooks, and the RTLD_DEEPBIND flag.  Essentially it's nearly
    impossible to consistely replace the allocator interface in all the
    libraries consistently, whether using LD_PRELOAD or anything else.
    Thankfully just turning that off works, as long as we don't have symbol
    conflicts.
    
    solution: Replace use of FLUX_DEEPBIND with calls to `flux_deepbind ()`
    which checks for an environment variable, `FLUX_LOAD_WITH_DEEPBIND`, and
    if that variable is both set and contains 0 returns 0, otherwise it
    returns FLUX_DEEPBIND.  This allows for easy flipping to load flux using
    say jemalloc, tcmalloc or the interposer library for heaptrack without a
    rebuild.

---
+++ src/common/libflux/plugin_private.h
@@ -0,0 +1,16 @@
+/************************************************************\
+ * Copyright 2024 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef FLUX_CORE_PLUGIN_PRIVATE_H
+#define FLUX_CORE_PLUGIN_PRIVATE_H
+
+int plugin_deepbind (void);
+
+#endif /* FLUX_CORE_PLUGIN_PRIVATE_H */
