d11e6ce34856f6c4acdb79701602eb80912febf3
Mark A. Grondona <mark.grondona@gmail.com>
2024-09-03 23:57:31 +0000

    validator: add configurable job size for require-instance
    Problem: The require-instance validator plugin is an all-or-nothing
    proposition -- either all jobs must be instances of Flux or none are
    required to be. This is limiting, since there are times when admins
    may only want to limit large jobs to being new instances of Flux to
    lessen the impact on a system instance.
    
    Add two options to the require-instance validator to set a minimum
    size for instances to which the requirement applies. The default is
    0 so that the plugin applies to all instance by default, but with
    --require-instance-minnodes=N and --require-instance-mincores=N this
    default can be increased so that only jobs above this size will be
    rejected if they are not new instances of Flux.
    
    To avoid any issue when minnodes is set but not minncores, set
    conservative default cores-per-node of 16 when mincores is not
    set. This will likely limit cores-only jobs more than jobs that set
    a node count, but this should encourage admins to set both, or users
    to use -N, --nodes with their jobs.

--- src/bindings/python/flux/job/validator/plugins/require-instance.py
+++ src/bindings/python/flux/job/validator/plugins/require-instance.py
@@ -22,19 +22,94 @@ not directly via `flux broker` or `flux start`.
 import errno
 from os.path import basename
 
+from flux.job import JobspecV1
 from flux.job.validator import ValidatorPlugin
 
 
+class JobSize:
+
+    def __init__(self, nnodes=0, ncores=0):
+        self.nnodes = nnodes
+        self.ncores = ncores
+
+    @classmethod
+    def from_jobspec(cls, jobspec):
+        counts = JobspecV1(**jobspec).resource_counts()
+        return JobSize(counts.get("node", None), counts.get("core", None))
+
+    def ignore_by_size(self, jobspec):
+        """
+        Return True if jobspec size is less than the currently configured
+        minimum job size required for a require-instance check
+        """
+        size = JobSize.from_jobspec(jobspec)
+        if size.nnodes is not None and size.nnodes >= self.nnodes:
+            return False
+        if size.ncores is not None and size.ncores >= self.ncores:
+            return False
+        return True
+
+    def configured(self):
+        return self.nnodes > 0 or self.ncores > 0
+
+    @property
+    def errstr(self):
+        result = "Direct job submission disabled"
+        if self.nnodes == 0 and self.ncores == 0:
+            result += " for all jobs in this instance"
+        else:
+            result += " for jobs >="
+            if self.nnodes > 0:
+                result += f" {self.nnodes} nodes"
+            if self.ncores > 0:
+                if self.nnodes > 0:
+                    result += " or"
+                result += f" {self.ncores} cores"
+        result += ". Please use flux-batch(1) or flux-alloc(1)"
+        return result
+
+
 class Validator(ValidatorPlugin):
+    def __init__(self, parser):
+        parser.add_argument(
+            "--require-instance-minnodes",
+            help="minimum node count that requires flux-batch/alloc be used."
+            + "default: 0",
+            metavar="N",
+            type=int,
+            default=0,
+        )
+        parser.add_argument(
+            "--require-instance-mincores",
+            metavar="N",
+            help="minimum core count that requires flux-batch/alloc be used."
+            + "default: 0",
+            type=int,
+            default=0,
+        )
+
+    def configure(self, args):
+        nnodes = args.require_instance_minnodes
+        ncores = args.require_instance_mincores
+        if nnodes > 0 and ncores == 0:
+            # Default ncores to a multiple of nnodes to avoid leaving an
+            # accidental hole that allows a cores-only job to be admitted
+            # by this plugin:
+            ncores = 16 * nnodes
+
+        self.minsize = JobSize(nnodes, ncores)
+
     def validate(self, args):
+
+        #  Ignore this job if it falls below a configured minsize:
+        if self.minsize.ignore_by_size(args.jobspec):
+            return
+
+        #  Otherwise, ensure this job is a new instance of Flux:
         if "batch" in args.jobspec["attributes"]["system"]:
             return
         command = args.jobspec["tasks"][0]["command"]
         arg0 = basename(command[0])
         if arg0 == "flux" and command[1] in ["broker", "start"]:
             return
-        return (
-            errno.EINVAL,
-            "Direct job submission is disabled for this instance."
-            + " Please use flux-batch(1) or flux-alloc(1)",
-        )
+        return (errno.EINVAL, self.minsize.errstr)
