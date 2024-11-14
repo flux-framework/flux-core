2ea0d217f7c25b4104402259ec5e51a5f394326f
Mark A. Grondona <mark.grondona@gmail.com>
2024-11-14 21:31:57 +0000

    python: support `wait` parameter to jobid URI resolver
    Problem: It is inconvenient that a user has to ensure a job's
    memo event containing the subinstance URI has been posted before
    they can call `flux uri` or other URI resolver.
    
    Support a `wait` parameter for the jobid resolver which, when used,
    causes the resolver to wait for the `memo` event containing the
    URI before returning. This is only done if the job is not in
    the INACTIVE or CLEANUP states.

--- src/bindings/python/flux/uri/resolvers/jobid.py
+++ src/bindings/python/flux/uri/resolvers/jobid.py
@@ -20,13 +20,23 @@ def filter_slash(iterable):
     return list(filter(lambda x: "/" not in x, iterable))
 
 
+def wait_for_uri(flux_handle, jobid):
+    """Wait for memo event containing job uri, O/w finish event"""
+    for event in flux.job.event_watch(flux_handle, jobid):
+        if event.name == "memo" and "uri" in event.context:
+            return event.context["uri"]
+        if event.name == "finish":
+            return None
+    return None
+
+
 class URIResolver(URIResolverPlugin):
     """A URI resolver that attempts to fetch the remote_uri for a job"""
 
     def describe(self):
         return "Get URI for a given Flux JOBID"
 
-    def _do_resolve(self, uri, flux_handle, force_local=False):
+    def _do_resolve(self, uri, flux_handle, force_local=False, wait=False):
         #
         #  Convert a possible hierarchy of jobids to a list, dropping any
         #   extraneous '/' (e.g. //id0/id1 -> [ "id0", "id1" ]
@@ -39,18 +49,21 @@ class URIResolver(URIResolverPlugin):
         except OSError as exc:
             raise ValueError(f"{arg} is not a valid jobid")
 
-        #  Fetch the jobinfo object for this job
         try:
-            job = job_list_id(
-                flux_handle, jobid, attrs=["state", "annotations"]
-            ).get_jobinfo()
-            if job.state != "RUN":
-                raise ValueError(f"jobid {arg} is not running")
-            uri = job.user.uri
+            if wait:
+                uri = wait_for_uri(flux_handle, jobid)
+            else:
+                #  Fetch the jobinfo object for this job
+                job = job_list_id(
+                    flux_handle, jobid, attrs=["state", "annotations"]
+                ).get_jobinfo()
+                if job.state != "RUN":
+                    raise ValueError(f"jobid {arg} is not running")
+                uri = job.user.uri
         except FileNotFoundError as exc:
             raise ValueError(f"jobid {arg} not found") from exc
 
-        if str(uri) == "":
+        if uri is None or str(uri) == "":
             raise ValueError(f"URI not found for job {arg}")
 
         #  If there are more jobids in the hierarchy to resolve, resolve
@@ -67,6 +80,9 @@ class URIResolver(URIResolverPlugin):
 
     def resolve(self, uri):
         force_local = False
+        wait = False
         if "local" in uri.query_dict or "FLUX_URI_RESOLVE_LOCAL" in os.environ:
             force_local = True
-        return self._do_resolve(uri, flux.Flux(), force_local=force_local)
+        if "wait" in uri.query_dict:
+            wait = True
+        return self._do_resolve(uri, flux.Flux(), force_local=force_local, wait=wait)
