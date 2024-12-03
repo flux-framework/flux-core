961c539289d72aab861b142c578eb085a78d646e
Jim Garlick <garlick.jim@gmail.com>
2024-12-03 07:14:13 -0800

    testsuite: avoid non-portable pthreads dependency
    Problem: the intree unit test uses pthread barriers, an optional
    POSIX feature that is missing on macos.
    
    Provide a simple alternate implementation within the test
    if barriers are unavailable.

--- src/common/libutil/test/intree.c
+++ src/common/libutil/test/intree.c
@@ -20,6 +20,63 @@
 
 #define NTHREADS 16
 
+/* pthread_barrier_t is an optional POSIX feature and is not present
+ * on macos.  If unavailable, a simple replacement is provided below.
+ */
+#if !defined _POSIX_BARRIERS || _POSIX_BARRIERS <= 0
+
+typedef struct {
+    int max;
+    int count;
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+} pthread_barrier_t;
+
+#ifndef PTHREAD_BARRIER_SERIAL_THREAD
+#define PTHREAD_BARRIER_SERIAL_THREAD -1
+#endif
+
+int pthread_barrier_destroy (pthread_barrier_t *barrier)
+{
+    if (!barrier)
+        return EINVAL;
+    pthread_mutex_destroy (&barrier->lock);
+    pthread_cond_destroy (&barrier->cond);
+    return 0;
+}
+
+int pthread_barrier_init (pthread_barrier_t *barrier,
+                          void *barrier_attr,
+                          unsigned count)
+{
+    if (!barrier || barrier_attr != NULL)
+        return EINVAL;
+    barrier->max = count;
+    barrier->count = 0;
+    pthread_mutex_init (&barrier->lock, NULL);
+    pthread_cond_init (&barrier->cond, NULL);
+    return 0;
+}
+
+int pthread_barrier_wait (pthread_barrier_t *barrier)
+{
+    int ret = EINVAL;
+    if (barrier) {
+        pthread_mutex_lock (&barrier->lock);
+        if (++barrier->count == barrier->max) {
+            pthread_cond_broadcast (&barrier->cond);
+            ret = PTHREAD_BARRIER_SERIAL_THREAD;
+        }
+        else if (barrier->count < barrier->max) {
+            pthread_cond_wait (&barrier->cond, &barrier->lock);
+            ret = 0;
+        }
+        pthread_mutex_unlock (&barrier->lock);
+    }
+    return ret;
+}
+#endif
+
 static pthread_barrier_t barrier;
 
 static void *thd_intree (void *arg)
