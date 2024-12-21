45130519e5e0f1c261f7c4eadebef972fdcadfa3
Mark A. Grondona <mark.grondona@gmail.com>
2024-12-21 07:23:27 -0800

    testsuite: python: fix OutputFormat unit test
    Problem: A test in t/python/t0024-util.py doesn't specify the 'f'
    format type so OutputFormat will adjust width and precision as a
    string, which the test does not expect.
    
    Fix the test to use the 'f' presentation type.

--- t/python/t0024-util.py
+++ t/python/t0024-util.py
@@ -207,9 +207,9 @@ class TestOutputFormat(unittest.TestCase):
         self.assertEqual(fmt, "{i:>8} {f:8.2}")
 
         fmt = OutputFormat(
-            "?+:{i:>7} ?:{s:>6} ?+:{f:.2}", headings=self.headings
+            "?+:{i:>7} ?:{s:>6} ?+:{f:.2f}", headings=self.headings
         ).filter(items)
-        self.assertEqual(fmt, "{i:>8} {f:3.2}")
+        self.assertEqual(fmt, "{i:>8} {f:3.2f}")
 
     def test_sort(self):
         a = Item("a", 0, 2.2)
