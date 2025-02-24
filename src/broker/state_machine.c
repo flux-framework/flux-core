008f13a380b28536c4e5b1567a3c10768ad2ae44
Jim Garlick <garlick.jim@gmail.com>
2025-02-23 17:29:13 -0800

    broker: extend timeouts for systemctl stop flux
    Problem: if a system instance is shut down with 'systemctl stop flux',
    as opposed to flux-shutdown(1), the whole stop process is under a 90s
    timeout ending in SIGKILL, which sometimes is not enough time to stop a
    large system instance cleanly.
    
    To mitigate this, CLEANUP state has a 45s timeout, and in FINALIZE,
    flux-dump(1) periodically extends the systemd stop timeout by 10s.
    However, if CLEANUP uses up the entire 45s, only 45s is left to split
    among the remaining states, more or less.
    
    Experience has shown that 'systemctl stop flux' will be used, so we need
    to improve management of the systemd stop timeout, and perhaps even let
    it apply to flux-shutdown(1), so the behavior at shutdown is not dependent
    on the method used.
    
    As an initial step towards this goal, add a function that effectively
    resets the systemd stop timer by calling sd_notify() EXTEND_TIMEOUT_USEC
    with a value equal to TimeoutStopSec, which is passed in from the unit
    file via broker attribute.  Call this function on entry to each of the
    CLEANUP, SHUTDOWN, FINALIZE, and GOODBYE states.  The state transitions
    are indicative of progress, and we treat the systemd stop timeout as a
    timeout on progress rather than on the entire stop sequence.  This
    increases the maximum time allowed to flux for stopping, but ensures the
    broker won't be killed while making progress.

--- src/broker/state_machine.c
+++ src/broker/state_machine.c
@@ -68,6 +68,11 @@ struct monitor {
     unsigned int parent_error:1;
 };
 
+struct systemd {
+    bool timeout_is_active;
+    double stop_timeout;
+};
+
 struct state_machine {
     struct broker *ctx;
     broker_state_t state;
@@ -84,6 +89,8 @@ struct state_machine {
     struct quorum quorum;
     struct cleanup cleanup;
     struct shutdown shutdown;
+
+    struct systemd sd;
 
     struct flux_msglist *wait_requests;
 
@@ -167,6 +174,7 @@ static struct state_next nexttab[] = {
 static const double default_quorum_warn = 60; // log slow joiners
 static const double default_shutdown_warn = 60; // log slow shutdown
 static const double default_cleanup_timeout = -1;
+static const double default_sd_stop_timeout = -1;
 static const double goodbye_timeout = 60;
 
 static void state_action (struct state_machine *s, broker_state_t state)
@@ -200,6 +208,35 @@ static broker_state_t state_next (broker_state_t current, const char *event)
             return nexttab[i].next;
     }
     return current;
+}
+
+/* When systemd is tracking service stop, a SIGKILL will be sent to the broker
+ * if TimeoutStopSec is exceeded.  Call this function when stop begins, and
+ * each time the broker enters a new state thereafter, to extend the timeout.
+ * That way if a state consumes a bunch of time but eventually completes,
+ * the remaining states aren't running under diminishing time constraints.
+ */
+static void sd_timeout_reset (struct state_machine *s)
+{
+#if HAVE_LIBSYSTEMD
+    if (s->ctx->sd_notify) {
+        if (s->sd.timeout_is_active) {
+            if (s->sd.stop_timeout > 0) {
+                double timeout_usec = s->sd.stop_timeout * 1E6;
+                sd_notifyf (0, "EXTEND_TIMEOUT_USEC=%d", (int)timeout_usec);
+            }
+        }
+        else {
+            /* Uncomment to tell systemd to transition the unit to
+             * "deactivating" state and begin the stop timeout.
+             * For now, we don't inform systemd so flux-shutdown(1)
+             * escapes the timeout.
+             */
+            //sd_notify (0, "STOPPING=1");
+            s->sd.timeout_is_active = true;
+        }
+    }
+#endif
 }
 
 static void action_init (struct state_machine *s)
@@ -393,6 +430,7 @@ static void cleanup_timer_cb (flux_reactor_t *r,
 
 static void action_cleanup (struct state_machine *s)
 {
+    sd_timeout_reset (s);
     /* Prevent new downstream clients from saying hello, but
      * let existing ones continue to communicate so they can
      * shut down and disconnect.
@@ -419,6 +457,7 @@ static void action_cleanup (struct state_machine *s)
 
 static void action_finalize (struct state_machine *s)
 {
+    sd_timeout_reset (s);
     /* Now that all clients have disconnected, finalize all
      * downstream communication.
      */
@@ -462,6 +501,8 @@ static void shutdown_warn_timer_cb (flux_reactor_t *r,
 
 static void action_shutdown (struct state_machine *s)
 {
+    sd_timeout_reset (s);
+
     if (overlay_get_child_peer_count (s->ctx->overlay) == 0) {
         state_machine_post (s, "children-none");
         return;
@@ -496,6 +537,7 @@ static void goodbye_continuation (flux_future_t *f, void *arg)
 
 static void action_goodbye (struct state_machine *s)
 {
+    sd_timeout_reset (s);
     /* On rank 0, "goodbye" is posted by shutdown.c.
      * On other ranks, send a goodbye message and wait for a response
      * (with timeout) before continuing on.
@@ -1386,6 +1428,13 @@ struct state_machine *state_machine_create (struct broker *ctx)
                            &s->cleanup.timeout,
                            default_cleanup_timeout) < 0) {
         log_err ("error configuring cleanup timeout attribute");
+        goto error;
+    }
+    if (timeout_configure (s,
+                           "broker.sd-stop-timeout",
+                           &s->sd.stop_timeout,
+                           default_sd_stop_timeout) < 0) {
+        log_err ("error configuring systemd stop timeout attribute");
         goto error;
     }
     if (timeout_configure (s,
