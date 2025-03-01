15fdbf1162b7b8b6b02eb1563ccbc291c388a170
Mark A. Grondona <mark.grondona@gmail.com>
2025-02-28 16:12:04 -0800

    perilog: start timeout timer when launching procesess
    Problem: When a prolog/epilog timeout is configured, the perilog.so
    plugin delays the start of the timeout timer until the bulk-exec
    `on_start` callback is called. But this could result in the timer
    never being started if one or more tasks get stuck starting.
    
    Start the timeout timer immediately on launch of the prolog or
    epilog tasks. This removes the need for the start_cb(), so remove
    that function.
    
    Fixes #6644

--- src/modules/job-manager/plugins/perilog.c
+++ src/modules/job-manager/plugins/perilog.c
@@ -773,18 +773,8 @@ static void io_cb (struct bulk_exec *bulk_exec,
     }
 }
 
-
-static void start_cb (struct bulk_exec *exec, void *arg)
-{
-    struct perilog_proc *proc = bulk_exec_aux_get (exec, "perilog_proc");
-    /*  Start timeout timer when processes have started
-     */
-    if (proc && proc->timer)
-        flux_watcher_start (proc->timer);
-}
-
 static struct bulk_exec_ops ops = {
-        .on_start = start_cb,
+        .on_start = NULL,
         .on_exit = NULL,
         .on_complete = completion_cb,
         .on_error = error_cb,
@@ -889,8 +879,7 @@ static struct perilog_proc *procdesc_run (flux_t *h,
             goto error;
         }
         proc->timer = w;
-        /* Note: watcher will be started in bulk-exec start callback
-         */
+        flux_watcher_start (proc->timer);
     }
     proc->R = json_incref (R);
     proc->bulk_exec = bulk_exec;
