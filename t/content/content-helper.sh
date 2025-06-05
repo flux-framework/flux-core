b89dfab88e130699675998b717c6f62f168123f7
Albert Chu <chu11@llnl.gov>
2025-06-04 17:03:58 -0700

    t: update content checkpoint tests
    Problem: A recent change made it so checkpoint keys are no longer
    recognized.  Most checkpoint tests still test with checkpoint keys.
    
    Remove key from all checkpoint related tests and related helper
    functions.  Remove tests that are no longer relevant.

--- t/content/content-helper.sh
+++ t/content/content-helper.sh
@@ -4,43 +4,43 @@
 # content module test helper functions
 
 # Get RPC message contents for checkpoint put
-# Usage: checkpoint_put_msg key rootref
+# Usage: checkpoint_put_msg rootref
 checkpoint_put_msg() {
-	o="{key:\"$1\",value:{version:1,rootref:\"$2\",timestamp:2.2}}"
+	o="{value:{version:1,rootref:\"$1\",timestamp:2.2}}"
 	echo ${o}
 }
 
-# checkpoint rootref at specific key
-# Usage: checkpoint_put key rootref
+# checkpoint rootref
+# Usage: checkpoint_put rootref
 checkpoint_put() {
-	o=$(checkpoint_put_msg $1 $2)
+	o=$(checkpoint_put_msg $1)
 	jq -j -c -n ${o} | $RPC content.checkpoint-put
 }
 
 # Get RPC message contents for checkpoint get
-# Usage: checkpoint_get_msg key
+# Usage: checkpoint_get_msg
 checkpoint_get_msg() {
-	o="{key:\"$1\"}"
+	o="{}"
 	echo ${o}
 }
 
-# get checkpoint rootref at key
-# Usage: checkpoint_get key
+# get checkpoint rootref
+# Usage: checkpoint_get
 checkpoint_get() {
-	o=$(checkpoint_get_msg $1)
+	o=$(checkpoint_get_msg)
 	jq -j -c -n ${o} | $RPC content.checkpoint-get
 }
 
 # Identical to checkpoint_put(), but go directly to backing store
-# Usage: checkpoint_put key rootref
+# Usage: checkpoint_put rootref
 checkpoint_backing_put() {
-	o=$(checkpoint_put_msg $1 $2)
+	o=$(checkpoint_put_msg $1)
 	jq -j -c -n ${o} | $RPC content-backing.checkpoint-put
 }
 
 # Identical to checkpoint_get(), but go directly to backing store
-# Usage: checkpoint_get key
+# Usage: checkpoint_get
 checkpoint_backing_get() {
-	o=$(checkpoint_get_msg $1)
+	o=$(checkpoint_get_msg)
 	jq -j -c -n ${o} | $RPC content-backing.checkpoint-get
 }
