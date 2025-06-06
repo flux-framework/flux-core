3fed415f3bab62245690d9a2520b98583d27f919
Mark A. Grondona <mark.grondona@gmail.com>
2025-06-06 06:35:59 -0700

    testsuite: fix incorrect comment in t0010-job.py
    Problem: The comment for `test_23_from_nest_command` in t0010-job.py
    incorrectly mentions `from_batch_command`.
    
    Fix the comment.

--- t/python/t0010-job.py
+++ t/python/t0010-job.py
@@ -385,7 +385,7 @@ class TestJob(unittest.TestCase):
                 setattr(jobspec, stream, None)
 
     def test_22_from_batch_command(self):
-        """Test that `from_batch_command` produces a valid jobspec"""
+        """Test that `from_nest_command` produces a valid jobspec"""
         jobid = job.submit(
             self.fh, JobspecV1.from_batch_command("#!/bin/sh\nsleep 0", "nested sleep")
         )
