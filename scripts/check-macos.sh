c084a43b76f0aab165277f929ab90e3c80546f80
Tom Scogland <scogland1@llnl.gov>
2025-07-29 15:59:05 -0700

    check-macos.sh: run all checks in src and annotate
    problem: we were only running known working checks in src and were not
    annotating failures.
    
    solution: run all subdirectories of `src` and run check-annotate.sh on
    failure.

--- scripts/check-macos.sh
+++ scripts/check-macos.sh
@@ -12,12 +12,8 @@ CONF_SCRIPT=scripts/configure-macos.sh
 test -f $CONF_SCRIPT || die "please run from the top level of the source tree"
 test -f configure || die "please run $CONF_SCRIPT first"
 
-COMMON_WORKING="libtap libtestutil libyuarel libpmi liblsd libutil libflux libfluxutil libkvs libjob liboptparse libidset libtomlc99 libschedutil libeventlog libioencode librouter libdebugged libcontent libjob libhostlist libczmqcontainers libccan libzmqutil libtaskmap libfilemap libsdexec libmissing"
-COMMON_BROKEN="libsubprocess libterminus librlist"
-
-make -j4 -C src/common check SUBDIRS="$COMMON_WORKING" || die "check failed"
-
-cat >&2 <<-EOT
+if make -j4 -C src check ; then
+    cat >&2 <<-EOT
 =============================================
 * Well the unit tests that worked before on macos still work!
 * However, please note that the macos port of flux-core is incomplete.
@@ -25,3 +21,8 @@ cat >&2 <<-EOT
 * for portability issues that still need to be resolved.
 =============================================
 EOT
+else
+    ./src/test/checks-annotate.sh
+    die "check failed"
+fi
+
