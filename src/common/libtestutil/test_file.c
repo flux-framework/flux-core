2a2bc73f4d98f8710bf3305c34dad19e0a217f8f
Tom Scogland <scogland1@llnl.gov>
2025-08-23 09:51:00 -0700

    test_file: add get_test_dir for easier lifetimes
    problem: Managing the lifetime of test directories in some cases proved
    to be complicated in overlay.c, especially because it wasn't always
    clear when all users of files in the directory were done with the
    directory
    
    solution: create a thread-safe singleton test directory interface that
    cleans itself up on process exit

--- src/common/libtestutil/test_file.c
+++ src/common/libtestutil/test_file.c
@@ -1,7 +1,12 @@
 #include "test_file.h"
 
 #include <errno.h>
+#include <limits.h>
+#include <pthread.h>
+#include <stdio.h>
 #include <string.h>
+
+#include "src/common/libtap/tap.h"
 
 
 void
@@ -41,4 +46,32 @@ create_test_dir (char *dir, int dirlen)
         BAIL_OUT ("snprintf overflow");
     if (!mkdtemp (dir))
         BAIL_OUT ("mkdtemp %s: %s", dir, strerror (errno));
+}
+
+static pthread_once_t global_test_dir_once = PTHREAD_ONCE_INIT;
+static char global_test_dir[PATH_MAX];
+static void
+init_global_test_dir ()
+{
+  create_test_dir (global_test_dir, sizeof global_test_dir);
+}
+
+__attribute__ ((destructor))
+static void
+cleanup_global_test_dir ()
+{
+  // Skip if it was never created
+  if (global_test_dir[0] == 0)
+    return;
+  if (rmdir (global_test_dir) < 0) {
+    fprintf (stderr, "could not cleanup test dir %s: %s\n", global_test_dir, strerror (errno));
+    exit (1);
+  }
+}
+
+const char *
+get_test_dir ()
+{
+  pthread_once (&global_test_dir_once, init_global_test_dir);
+  return global_test_dir;
 }
