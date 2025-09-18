811c8bb401308e48b10992408786240f7ad3f6c4
Albert Chu <chu11@llnl.gov>
2025-09-18 15:05:36 -0700

    etc/modprobe: run flux-fsck in rc1
    Problem: It would be useful to run flux-fsck in rc1 on broker
    startup.  That way any corruption errors could be found on startup
    if prior shutdown was not clean.
    
    Add an "fsck" task to rc1.py that runs flux-fsck as needed.

--- etc/modprobe/rc1.py
+++ etc/modprobe/rc1.py
@@ -8,10 +8,12 @@
 # SPDX-License-Identifier: LGPL-3.0
 ##############################################################
 
+import errno
 import os
 from pathlib import Path
 from subprocess import Popen
 
+import flux.kvs
 from flux.modprobe import run_all_rc_scripts, task
 
 
@@ -105,6 +107,7 @@ def restore(context):
     if dumpfile.exists():
         cmd = f"flux restore --sd-notify --quiet --checkpoint --size-limit=100M {dumpfile}"
         context.bash(cmd)
+        context.set("dump_restored", True)
     if dumplink and dumplink.exists():
         dumplink.unlink()
 
@@ -135,6 +138,42 @@ def check_clean_shutdown(context):
     fi
     """
     )
+
+
+# The most common scenario we want to fsck is when the flux instance
+# was not shut down cleanly.  In other words, when:
+#
+# A) a checkpoint exists
+# B) a dump was not restored
+#
+# If a checkpoint does not exist, we can assume this is the first bootup
+# of an instance.
+#
+# If a dump was restored, we can assume the prior instance was shutdown
+# cleanly.
+@task(
+    "fsck",
+    ranks="0",
+    before=["kvs"],
+    after=["content-backing", "restore"],
+    requires=["content-backing"],
+    needs=["content-backing"],
+)
+def fsck(context):
+    try:
+        flux.kvs.kvs_checkpoint_lookup(context.handle)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+        return
+    if not context.get("dump_restored", False):
+        try:
+            # Limit output to only core errors with --quiet
+            context.bash("flux fsck --repair --quiet")
+        except RuntimeError:
+            # disable content.dump in rc3
+            context.handle.attr_remove("content.dump")
+            raise
 
 
 @task(
