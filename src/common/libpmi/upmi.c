65f4daeb46003c5188f2bff34cfac3c7552842e8
Jim Garlick <garlick.jim@gmail.com>
2025-11-12 10:51:31 -0800

    libpmi: add upmi config plugin
    Problem: broker bootstrap is overly complex with its two bootstrap
    methods that co-mingle multiple activities and duplicate code.
    
    Add a "config" PMI client (upmi) method that pre-populates the
    PMI KVS with business cards for every node based on [bootstrap]
    configuration.  This will allow the broker to combine bootstrap
    methods and abstract the way peer contact information is obtained
    behind libupmi.la in a future commit.

--- src/common/libpmi/upmi.c
+++ src/common/libpmi/upmi.c
@@ -58,16 +58,18 @@ static int upmi_preinit (struct upmi *upmi,
 int upmi_simple_init (flux_plugin_t *p);
 int upmi_libpmi2_init (flux_plugin_t *p);
 int upmi_libpmi_init (flux_plugin_t *p);
+int upmi_config_init (flux_plugin_t *p);
 int upmi_single_init (flux_plugin_t *p);
 
 static flux_plugin_init_f builtins[] = {
     &upmi_simple_init,
     &upmi_libpmi2_init,
     &upmi_libpmi_init,
+    &upmi_config_init,
     &upmi_single_init,
 };
 
-static const char *default_methods = "simple libpmi2 libpmi single";
+static const char *default_methods = "config simple libpmi2 libpmi single";
 
 void upmi_destroy (struct upmi *upmi)
 {
