b8faad4d4351bbc41179dac2607c3a18eb95c0b3
William Hobbs <hobbs17@llnl.gov>
2025-12-17 11:50:02 -0800

    t: clean up CLI plugins test and associated extra tests
    Problem: when t2717-python-cli-plugins.t was written, only a single
    path was supported for the FLUX_CLI_PLUGINPATH environment variable,
    which led to some redundant symlinks in the t/cli-plugins directory
    to be able to test failure cases.
    
    Update the test to use colon-delimited paths and eliminate the
    redundant paths.

--- t/cli-plugins/extras/doubleplugin.py
+++ t/cli-plugins/extras/doubleplugin.py
@@ -4,4 +4,4 @@ from flux.cli.plugin import CLIPlugin
 class GoodPlugin(CLIPlugin):
     def __init__(self, prog, prefix="diff"):
         super().__init__(prog, prefix=prefix)
-        self.add_option("--gpumode", dest="different_gpu", help="I am a valid plugin")
+        self.add_option("--gpumode", dest="different_gpu", help="This one is fine, because the dest is different")
