1f88f1e172de5c4b482fd8f3425a85a9ff69e64a
Jim Garlick <garlick.jim@gmail.com>
2026-02-05 15:58:41 -0800

    flux-shell(1): split into multiple pages
    Problem: the flux-shell(1) man page is rather long and contains info
    for admin, user, and developer audiences.
    
    Split into
      flux-shell(1)
      flux-shell-initrc(5)
      flux-shell-options(7)
      flux-shell-plugins(7)
    
    Convert section references into manual page references.
    Update other man pages that reference shell options.
    
    Fixes #7364

--- doc/manpages.py
+++ doc/manpages.py
@@ -378,7 +378,10 @@ man_pages = [
     ('man5/flux-config-job-manager', 'flux-config-job-manager', 'configure Flux job manager service', [author], 5),
     ('man5/flux-config-kvs', 'flux-config-kvs', 'configure Flux kvs service', [author], 5),
     ('man5/flux-config-heartbeat', 'flux-config-heartbeat', 'configure Flux heartbeat service', [author], 5),
+    ('man5/flux-shell-initrc', 'flux-shell-initrc', 'Flux shell config file', [author], 5),
     ('man7/flux-broker-attributes', 'flux-broker-attributes', 'overview Flux broker attributes', [author], 7),
     ('man7/flux-jobtap-plugins', 'flux-jobtap-plugins', 'overview Flux jobtap plugin API', [author], 7),
+    ('man7/flux-shell-plugins', 'flux-shell-plugins', 'Flux shell plugin interfaces', [author], 7),
+    ('man7/flux-shell-options', 'flux-shell-options', 'Flux shell built-in options', [author], 7),
     ('man7/flux-environment', 'flux-environment', 'Flux environment overview', [author], 7),
 ]
