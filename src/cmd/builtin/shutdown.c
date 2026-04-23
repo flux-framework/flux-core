0df8246575b87b0d1fc481462b897b709d7eccc2
Jim Garlick <garlick.jim@gmail.com>
2026-04-23 09:21:09 -0700

    flux-shutdown: tidy kvs dump/gc handling
    Problem: flux-shutdown's handling of the --dump, --gc, and --skip-gc
    options is a bit muddled, and human readable errors from attribute
    RPCs are not reported.
    
    Move KVS dump handling to a helper function.
    Prevent --gc from being specified with either --dump or --skip-gc.
    Print human readable errors from attr.rm and attr.set, if any.
    Notify that dump will occur, regardless of who arranged it.

--- src/cmd/builtin/shutdown.c
+++ src/cmd/builtin/shutdown.c
@@ -18,6 +18,8 @@
 #include "src/broker/state_machine.h"
 #include "src/common/libkvs/kvs_checkpoint.h"
 #include "src/common/libutil/uri.h"
+#include "src/common/libutil/errprintf.h"
+#include "ccan/str/str.h"
 
 #include "builtin.h"
 
@@ -108,20 +110,67 @@ static void process_updates (flux_future_t *f)
         log_msg_exit ("%s", future_strerror (f, errno));
 }
 
-static int rmattr (flux_t *h, const char *name)
+/* attr.rm is not currently exposed via the Flux C API.
+ */
+static int rmattr (flux_t *h, const char *name, flux_error_t *errp)
 {
     flux_future_t *f;
-    int rc = 0;
+    int rc = -1;
+
     if (!(f = flux_rpc_pack (h,
                              "attr.rm",
                              FLUX_NODEID_ANY,
                              0,
                              "{s:s}",
                              "name", name))
-        || flux_rpc_get (f, NULL) < 0)
-        rc = -1;
+        || flux_rpc_get (f, NULL) < 0) {
+        errprintf (errp, "%s", future_strerror (f, errno));
+        goto done;
+    }
+    rc = 0;
+done:
     flux_future_destroy (f);
     return rc;
+}
+
+/* Configure KVS dump in rc3 by manipulating the content.dump attribute.
+ * N.B. In a system instance, the systemd unit file currently sets
+ * -Scontent.dump=auto -Scontent.restore=auto on the command line.
+ */
+static void configure_kvs_dump (flux_t *h, optparse_t *p)
+{
+    if (optparse_hasopt (p, "gc")) {
+        if (optparse_hasopt (p, "skip-gc") || optparse_hasopt (p, "dump"))
+            log_msg_exit ("--gc is incompatible with --skip-gc or --dump");
+    }
+
+    const char *content_dump = flux_attr_get (h, "content.dump");
+    flux_error_t error;
+
+    if (optparse_hasopt (p, "skip-gc")) {
+        if (content_dump) {
+            if (rmattr (h, "content.dump", &error) < 0)
+                log_err_exit ("rmattr content.dump: %s", error.text);
+            content_dump = NULL;
+        }
+    }
+    else if (optparse_hasopt (p, "dump")) {
+        const char *optarg = optparse_get_str (p, "dump", NULL);
+        if (!content_dump || !streq (content_dump, optarg)) {
+            if (flux_attr_set_ex (h, "content.dump", optarg, false, &error) < 0)
+                log_msg_exit ("setattr content.dump: %s", error.text);
+            content_dump = optarg;
+        }
+    }
+    else if (optparse_hasopt (p, "gc") || gc_threshold_check (h, p)) {
+        if (!content_dump || !streq (content_dump, "auto")) {
+            if (flux_attr_set_ex (h, "content.dump", "auto", false, &error) < 0)
+                log_msg_exit ("setattr content.dump: %s", error.text);
+            content_dump = "auto";
+        }
+    }
+    if (content_dump)
+        log_msg ("shutdown will dump KVS (this may take some time)");
 }
 
 static int subcmd (optparse_t *p, int ac, char *av[])
@@ -161,21 +210,7 @@ static int subcmd (optparse_t *p, int ac, char *av[])
     if (optparse_hasopt (p, "background"))
         flags &= ~FLUX_RPC_STREAMING;
 
-    if (optparse_hasopt (p, "skip-gc")) {
-        if (rmattr (h, "content.dump") < 0)
-            log_err_exit ("error clearing content.dump attribute");
-    }
-
-    if (optparse_hasopt (p, "gc")
-        || optparse_hasopt (p, "dump")
-        || gc_threshold_check (h, p)) {
-        const char *val = optparse_get_str (p, "dump", "auto");
-
-        if (flux_attr_set (h, "content.dump", val) < 0)
-            log_err_exit ("error setting content.dump attribute");
-
-        log_msg ("shutdown will dump KVS (this may take some time)");
-    }
+    configure_kvs_dump (h, p);
 
     /* N.B. set nodeid=FLUX_NODEID_ANY so we get immediate error from
      * broker if run on rank > 0.
