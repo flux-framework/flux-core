053976b4a1dc7643c98db7ba7638335dc584442f
Mark A. Grondona <mark.grondona@gmail.com>
2026-04-25 08:06:08 -0700

    dependency-after: add afterstart scheme as replacement for after
    Problem: The dependency scheme "after" is ambiguous and does not
    clearly convey that it waits for a job to start (enter RUN state).
    
    Add "afterstart" as an alias for the AFTER_START dependency type.
    Register the "job.dependency.afterstart" handler so the new scheme
    name is recognized. Keep "after" working for backward compatibility.

--- src/modules/job-manager/plugins/dependency-after.c
+++ src/modules/job-manager/plugins/dependency-after.c
@@ -69,7 +69,7 @@ static const char * after_typestr (enum after_type type)
 
 static int after_type_parse (const char *s, enum after_type *tp)
 {
-    if (streq (s, "after"))
+    if (streq (s, "after") || streq (s, "afterstart"))
         *tp = AFTER_START;
     else if (streq (s, "afterany"))
         *tp = AFTER_ANY;
@@ -772,6 +772,7 @@ static int query_cb (flux_plugin_t *p,
 
 static const struct flux_plugin_handler tab[] = {
     { "job.dependency.after",      dependency_after_cb, NULL },
+    { "job.dependency.afterstart", dependency_after_cb, NULL },
     { "job.dependency.afterok",    dependency_after_cb, NULL },
     { "job.dependency.afterany",   dependency_after_cb, NULL },
     { "job.dependency.afternotok", dependency_after_cb, NULL },
