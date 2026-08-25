flux-framework/flux-core

commit f34751b87699ccf4db385854f86c130dc606d320

job-manager: use f58 encoding for jobids in log messages
Problem: The job manager logs jobids in their decimal representation,
which aren't easily identifiable as jobids at first glance and take
up more space in most cases.

Log jobids as f58 where possible in the job manager using idf58().

Update a couple tests in the testsuite that expect decimal jobids in
dmesg output.

src/modules/job-manager/restart.c
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <flux/core.h>
 
+#include "src/common/libjob/idf58.h"
 #include "src/common/libutil/fluid.h"
 #include "src/common/libutil/errprintf.h"
 #include "src/common/libczmqcontainers/czmq_containers.h"
@@ -63,8 +64,8 @@ static struct job *lookup_job (flux_t *h,
         || !(f1 = flux_kvs_lookup (h, NULL, 0, k1))
         || !(f2 = flux_kvs_lookup (h, NULL, 0, k2))) {
         errprintf (error,
-                   "cannot send lookup requests for job %ju: %s",
-                   (uintmax_t)id,
+                   "cannot send lookup requests for job %s: %s",
+                   idf58 (id),
                    strerror (errno));
         *fatal = true;
         goto done;
@@ -123,8 +124,8 @@ static int depthfirst_map_one (flux_t *h,
         }
         flux_log (h,
                   LOG_ERR,
-                  "job %ju not replayed: %s",
-                  (uintmax_t)id,
+                  "job %s not replayed: %s",
+                  idf58 (id),
                   lookup_error.text);
         return 0;
     }
@@ -222,8 +223,8 @@ static int restart_map_cb (struct job *job, void *arg, flux_error_t *error)
 
     if (zhashx_insert (ctx->active_jobs, &job->id, job) < 0) {
         errprintf (error,
-                   "could not insert job %ju into active job hash",
-                   (uintmax_t)job->id);
+                   "could not insert job %s into active job hash",
+                   idf58 (job->id));
         return -1;
     }
     if (ctx->max_jobid < job->id)
@@ -232,9 +233,9 @@ static int restart_map_cb (struct job *job, void *arg, flux_error_t *error)
         wait_notify_active (ctx->wait, job);
     if (event_job_action (ctx->event, job) < 0) {
         flux_log_error (ctx->h,
-                        "replay warning: %s action failed on job %ju",
+                        "replay warning: %s action failed on job %s",
                         flux_job_statetostr (job->state, "L"),
-                        (uintmax_t)job->id);
+                        idf58 (job->id));
     }
     return 0;
 }
@@ -346,8 +347,8 @@ int restart_from_kvs (struct job_manager *ctx)
                                            true,
                                            &errmsg) < 0) {
                 flux_log (ctx->h, LOG_ERR,
-                          "restart: id=%ju: dependency check failed: %s",
-                          (uintmax_t) job->id, errmsg);
+                          "restart: id=%s: dependency check failed: %s",
+                          idf58 (job->id), errmsg);
             }
             free (errmsg);
         }
@@ -363,11 +364,11 @@ int restart_from_kvs (struct job_manager *ctx)
          *   after this one. (job.priority, job.sched...)
          */
         if (jobtap_call (ctx->jobtap, job, "job.create", NULL) < 0)
-            flux_log_error (ctx->h, "jobtap_call (id=%ju, create)",
-                                (uintmax_t) job->id);
+            flux_log_error (ctx->h, "jobtap_call (id=%s, create)",
+                            idf58 (job->id));
         if (jobtap_call (ctx->jobtap, job, "job.new", NULL) < 0)
-            flux_log_error (ctx->h, "jobtap_call (id=%ju, new)",
-                                (uintmax_t) job->id);
+            flux_log_error (ctx->h, "jobtap_call (id=%s, new)",
+                            idf58 (job->id));
 
         if (job->state == FLUX_JOB_STATE_SCHED) {
             /*
@@ -383,8 +384,8 @@ int restart_from_kvs (struct job_manager *ctx)
                                      "flux-restart",
                                      0,
                                      NULL) < 0) {
-                flux_log_error (ctx->h, "%s: event_job_post_pack id=%ju",
-                                __FUNCTION__, (uintmax_t)job->id);
+                flux_log_error (ctx->h, "%s: event_job_post_pack id=%s",
+                                __FUNCTION__, idf58 (job->id));
             }
         }
         else if ((job->state & FLUX_JOB_STATE_RUNNING) != 0) {
@@ -396,9 +397,9 @@ int restart_from_kvs (struct job_manager *ctx)
                                          "debug.exec-reattach-start",
                                          0,
                                          "{s:I}",
-                                         "id", (uintmax_t)job->id) < 0)
-                    flux_log_error (ctx->h, "%s: event_job_post_pack id=%ju",
-                                    __FUNCTION__, (uintmax_t)job->id);
+                                         "id", idf58 (job->id)) < 0)
+                    flux_log_error (ctx->h, "%s: event_job_post_pack id=%s",
+                                    __FUNCTION__, idf58 (job->id));
             }
         }
         job = zhashx_next (ctx->active_jobs);
@@ -422,8 +423,8 @@ int restart_from_kvs (struct job_manager *ctx)
     }
     flux_log (ctx->h,
               LOG_DEBUG,
-              "restart: max_jobid=%ju",
-              (uintmax_t)ctx->max_jobid);
+              "restart: max_jobid=%s",
+              idf58 (ctx->max_jobid));
     return 0;
 }
 

src/modules/job-manager/alloc.c
@@ -25,6 +25,7 @@
 #include <assert.h>
 
 #include "src/common/libczmqcontainers/czmq_containers.h"
+#include "src/common/libjob/idf58.h"
 #include "ccan/str/str.h"
 
 #include "job.h"
@@ -130,14 +131,14 @@ static void free_response_cb (flux_t *h, flux_msg_handler_t *mh,
     if (flux_msg_unpack (msg, "{s:I}", "id", &id) < 0)
         goto teardown;
     if (!(job = zhashx_lookup (ctx->active_jobs, &id))) {
-        flux_log (h, LOG_ERR, "sched.free-response: id=%ju not active",
-                  (uintmax_t)id);
+        flux_log (h, LOG_ERR, "sched.free-response: id=%s not active",
+                  idf58 (id));
         errno = EINVAL;
         goto teardown;
     }
     if (!job->has_resources) {
-        flux_log (h, LOG_ERR, "sched.free-response: id=%ju not allocated",
-                  (uintmax_t)id);
+        flux_log (h, LOG_ERR, "sched.free-response: %s not allocated",
+                  idf58 (id));
         errno = EINVAL;
         goto teardown;
     }
@@ -184,7 +185,7 @@ int cancel_request (struct alloc *alloc, struct job *job)
                              "{s:I}",
                              "id",
                              job->id))) {
-        flux_log_error (h, "sending sched.cancel id=%ju", (uintmax_t)job->id);
+        flux_log_error (h, "sending sched.cancel id=%s", idf58 (job->id));
         return -1;
     }
     flux_future_destroy (f);
@@ -215,14 +216,14 @@ static void alloc_response_cb (flux_t *h, flux_msg_handler_t *mh,
                               "annotations", &annotations) < 0)
         goto teardown;
     if (!(job = zhashx_lookup (ctx->active_jobs, &id))) {
-        flux_log (h, LOG_ERR, "sched.alloc-response: id=%ju not active",
-                  (uintmax_t)id);
+        flux_log (h, LOG_ERR, "sched.alloc-response: id=%s not active",
+                  idf58 (id));
         errno = EINVAL;
         goto teardown;
     }
     if (!job->alloc_pending) {
-        flux_log (h, LOG_ERR, "sched.alloc-response: id=%ju not requested",
-                  (uintmax_t)id);
+        flux_log (h, LOG_ERR, "sched.alloc-response: id=%s not requested",
+                  idf58 (id));
         errno = EINVAL;
         goto teardown;
     }
@@ -236,13 +237,13 @@ static void alloc_response_cb (flux_t *h, flux_msg_handler_t *mh,
         if (job->has_resources) {
             flux_log (h,
                       LOG_ERR,
-                      "sched.alloc-response: id=%ju already allocated",
-                      (uintmax_t)id);
+                      "sched.alloc-response: id=%s already allocated",
+                      idf58 (id));
             errno = EEXIST;
             goto teardown;
         }
         if (annotations_update_and_publish (ctx, job, annotations) < 0)
-            flux_log_error (h, "annotations_update: id=%ju", (uintmax_t)id);
+            flux_log_error (h, "annotations_update: id=%s", idf58 (id));
 
         /*  Only modify job state after annotation event is published
          */
@@ -265,7 +266,7 @@ static void alloc_response_cb (flux_t *h, flux_msg_handler_t *mh,
             goto teardown;
         }
         if (annotations_update_and_publish (ctx, job, annotations) < 0)
-            flux_log_error (h, "annotations_update: id=%ju", (uintmax_t)id);
+            flux_log_error (h, "annotations_update: id=%s", idf58 (id));
         break;
     case FLUX_SCHED_ALLOC_DENY: // error
         alloc->alloc_pending_count--;
@@ -281,8 +282,9 @@ static void alloc_response_cb (flux_t *h, flux_msg_handler_t *mh,
                                      EVENT_NO_COMMIT,
                                      "{s:n}", "annotations") < 0)
                 flux_log_error (ctx->h,
-                                "%s: event_job_post_pack: id=%ju",
-                                __FUNCTION__, (uintmax_t)id);
+                                "%s: event_job_post_pack: id=%s",
+                                __FUNCTION__,
+                                idf58 (id));
         }
         if (event_job_post_pack (ctx->event, job, "exception", 0,
                                  "{ s:s s:i s:I s:s }",
@@ -310,14 +312,15 @@ static void alloc_response_cb (flux_t *h, flux_msg_handler_t *mh,
                                      EVENT_NO_COMMIT,
                                      "{s:n}", "annotations") < 0)
                 flux_log_error (ctx->h,
-                                "%s: event_job_post_pack: id=%ju",
-                                __FUNCTION__, (uintmax_t)id);
+                                "%s: event_job_post_pack: id=%s",
+                                __FUNCTION__,
+                                idf58 (id));
         }
         if (queue_started (alloc->ctx->queue, job)) {
             if (event_job_action (ctx->event, job) < 0) {
                 flux_log_error (h,
-                                "event_job_action id=%ju on alloc cancel",
-                                (uintmax_t)id);
+                                "event_job_action id=%s on alloc cancel",
+                                idf58 (id));
                 goto teardown;
             }
         }

src/modules/job-manager/event.c
@@ -44,6 +44,7 @@
 #include "src/common/libeventlog/eventlog.h"
 #include "src/common/libutil/errno_safe.h"
 #include "src/common/libutil/jpath.h"
+#include "src/common/libjob/idf58.h"
 #include "ccan/ptrint/ptrint.h"
 #include "ccan/str/str.h"
 
@@ -212,8 +213,8 @@ static void event_batch_destroy (struct event_batch *batch)
                 job->hold_events = 0;
                 if (event_job_post_deferred (batch->event, job) < 0)
                     flux_log_error (batch->event->ctx->h,
-                                    "%ju: error posting deferred events",
-                                    (uintmax_t) job->id);
+                                    "%s: error posting deferred events",
+                                    idf58 (job->id));
             }
             zlist_destroy (&batch->jobs);
         }
@@ -440,8 +441,8 @@ int event_job_action (struct event *event, struct job *job)
                 if (purge_enqueue_job (ctx->purge, job) < 0) {
                     flux_log (event->ctx->h,
                               LOG_ERR,
-                              "%ju: error adding inactive job to purge queue",
-                               (uintmax_t)job->id);
+                              "%s: error adding inactive job to purge queue",
+                               idf58 (job->id));
                 }
             }
             (void) jobtap_call (ctx->jobtap, job, "job.destroy", NULL);
@@ -802,9 +803,9 @@ static int event_jobtap_call (struct event *event,
                                    "{s:O}",
                                    "entry", entry) < 0)
             flux_log (event->ctx->h, LOG_ERR,
-                      "jobtap: event.%s callback failed for job %ju",
+                      "jobtap: event.%s callback failed for job %s",
                       name,
-                      (uintmax_t) job->id);
+                      idf58 (job->id));
 
     if (job->state != old_state) {
         /*

src/modules/job-manager/jobtap.c
@@ -30,6 +30,7 @@
 #include "src/common/libutil/errno_safe.h"
 #include "src/common/libutil/errprintf.h"
 #include "src/common/libutil/aux.h"
+#include "src/common/libjob/idf58.h"
 #include "ccan/str/str.h"
 
 #include "annotate.h"
@@ -190,8 +191,8 @@ static int plugin_check_dependencies (struct jobtap *jobtap,
     if (dependencies_unpack (jobtap, job, &error, &dependencies) < 0) {
         flux_log (jobtap->ctx->h,
                   LOG_ERR,
-                  "id=%ju: plugin_register_dependencies: %s",
-                  (uintmax_t) job->id,
+                  "id=%s: plugin_register_dependencies: %s",
+                  idf58 (job->id),
                   error);
         free (error);
         return -1;
@@ -280,8 +281,8 @@ static flux_plugin_t * jobtap_load_plugin (struct jobtap *jobtap,
         if (job->state == FLUX_JOB_STATE_DEPEND) {
             if (plugin_check_dependencies (jobtap, p, job, args) < 0)
                 errprintf (errp,
-                           "failed to check dependencies for job %ju",
-                           job->id);
+                           "failed to check dependencies for job %s",
+                           idf58 (job->id));
             (void) flux_plugin_call (p, "job.state.depend", args);
         }
 
@@ -696,8 +697,8 @@ int jobtap_get_priority (struct jobtap *jobtap,
              */
             if (job->state == FLUX_JOB_STATE_SCHED)
                 flux_log (jobtap->ctx->h, LOG_ERR,
-                          "jobtap: %ju: BUG: plugin didn't return priority",
-                          (uintmax_t) job->id);
+                          "jobtap: %s: BUG: plugin didn't return priority",
+                          idf58 (job->id));
         }
         /*
          *   O/w, plugin provided a new priority.
@@ -727,8 +728,8 @@ static void error_asprintf (struct jobtap *jobtap,
     va_start (ap, fmt);
     if (vasprintf (errp, fmt, ap) < 0)
         flux_log_error (jobtap->ctx->h,
-                        "id=%ju: failed to create error string: fmt=%s",
-                        (uintmax_t) job->id, fmt);
+                        "id=%s: failed to create error string: fmt=%s",
+                        idf58 (job->id), fmt);
     va_end (ap);
 }
 
@@ -955,8 +956,8 @@ int jobtap_check_dependencies (struct jobtap *jobtap,
                                   "%s (job may be stuck in DEPEND state)",
                                   *errp) < 0)
                 flux_log_error (jobtap->ctx->h,
-                                "id=%ju: failed to raise dependency exception",
-                                (uintmax_t) job->id);
+                                "id=%s: failed to raise dependency exception",
+                                idf58 (job->id));
             free (*errp);
             *errp = NULL;
         }
@@ -984,9 +985,9 @@ int jobtap_notify_subscribers (struct jobtap *jobtap,
 
     if (snprintf (topic, topiclen, "job.event.%s", name) >= topiclen) {
         flux_log (jobtap->ctx->h, LOG_ERR,
-                  "jobtap: %s: %ju: event topic name too long",
+                  "jobtap: %s: %s: event topic name too long",
                   name,
-                  (uintmax_t) job->id);
+                  idf58 (job->id));
         return -1;
     }
 
@@ -995,9 +996,9 @@ int jobtap_notify_subscribers (struct jobtap *jobtap,
     va_end (ap);
     if (!args) {
         flux_log (jobtap->ctx->h, LOG_ERR,
-                  "jobtap: %s: %ju: failed to create plugin args",
+                  "jobtap: %s: %s: failed to create plugin args",
                   topic,
-                  (uintmax_t) job->id);
+                  idf58 (job->id));
         return -1;
     }
 
@@ -1024,9 +1025,9 @@ int jobtap_call (struct jobtap *jobtap,
     va_start (ap, fmt);
     if (!(args = jobtap_args_vcreate (jobtap, job, fmt, ap))) {
         flux_log (jobtap->ctx->h, LOG_ERR,
-                  "jobtap: %s: %ju: failed to create plugin args",
+                  "jobtap: %s: %s: failed to create plugin args",
                   topic,
-                  (uintmax_t) job->id);
+                  idf58 (job->id));
     }
     va_end (ap);
 
@@ -1071,9 +1072,9 @@ int jobtap_call (struct jobtap *jobtap,
             rc = annotations_update_and_publish (jobtap->ctx, job, note);
         if (rc < 0)
             flux_log_error (jobtap->ctx->h,
-                            "jobtap: %s: %ju: annotations_update",
+                            "jobtap: %s: %s: annotations_update",
                             topic,
-                            (uintmax_t) job->id);
+                            idf58 (job->id));
     }
     if (priority >= FLUX_JOB_PRIORITY_MIN) {
         /*

src/modules/job-manager/plugins/alloc-bypass.c
@@ -24,6 +24,7 @@
 #include <flux/jobtap.h>
 
 #include "src/common/librlist/rlist.h"
+#include "src/common/libjob/idf58.h"
 
 static void alloc_continuation (flux_future_t *f, void *arg)
 {
@@ -56,8 +57,8 @@ static void alloc_continuation (flux_future_t *f, void *arg)
      */
     if (flux_jobtap_job_aux_set (p, *idptr, "alloc-bypass::free", p, NULL) < 0)
         flux_log_error (flux_jobtap_get_flux (p),
-                        "id=%ju: Failed to set alloc-bypass::free",
-                        *idptr);
+                        "id=%s: Failed to set alloc-bypass::free",
+                        idf58 (*idptr));
 
 done:
     flux_future_destroy (f);

src/modules/job-manager/plugins/begin-time.c
@@ -20,6 +20,8 @@
 #include <flux/core.h>
 #include <flux/jobtap.h>
 
+#include "src/common/libjob/idf58.h"
+
 struct begin_time_arg {
     flux_plugin_t *p;
     flux_watcher_t *w;
@@ -94,7 +96,7 @@ static int add_begin_time (flux_plugin_t *p,
     flux_watcher_start (arg->w);
 
     if (flux_jobtap_dependency_add (p, id, arg->desc) < 0) {
-        flux_log_error (h, "%ju: flux_jobtap_dependency_add", (uintmax_t) id);
+        flux_log_error (h, "%s: flux_jobtap_dependency_add", idf58 (id));
         goto error;
     }
 

src/modules/job-manager/plugins/dependency-after.c
@@ -20,6 +20,7 @@
 #include <flux/jobtap.h>
 
 #include "src/common/libutil/iterators.h"
+#include "src/common/libjob/idf58.h"
 #include "src/common/libczmqcontainers/czmq_containers.h"
 #include "ccan/str/str.h"
 
@@ -413,8 +414,8 @@ static int dependency_after_cb (flux_plugin_t *p,
         && flux_jobtap_job_subscribe (p, afterid) < 0) {
         after_info_destroy (after);
         after_ref_destroy (ref);
-        return flux_jobtap_reject_job (p, args, "failed to subscribe to %ju",
-                                       (uintmax_t) id);
+        return flux_jobtap_reject_job (p, args, "failed to subscribe to %s",
+                                       idf58 (id));
     }
 
     return 0;
@@ -437,8 +438,8 @@ static void remove_jobid_dependency (flux_plugin_t *p,
                                          "Failed to remove dependency %s",
                                          after->description) < 0) {
             flux_log_error (flux_jobtap_get_flux (p),
-                            "flux_jobtap_raise_exception: id=%ju",
-                            (uintmax_t) after->depid);
+                            "flux_jobtap_raise_exception: id=%s",
+                            idf58 (after->depid));
         }
     }
 }
@@ -486,8 +487,8 @@ static void raise_exceptions (flux_plugin_t *p, zlistx_t *l)
                                              "dependency",
                                              after->description) < 0)
                 flux_log_error (flux_jobtap_get_flux (p),
-                                "id=%ju: unable to raise exception for %s",
-                                (uintmax_t) after->depid,
+                                "id=%s: unable to raise exception for %s",
+                                idf58 (after->depid),
                                 after->description);
         }
         /*  N.B. = entry will be deleted at list destruction */

src/modules/job-manager/plugins/perilog.c
@@ -161,8 +161,8 @@ static void emit_finish_event (struct perilog_proc *proc, int status)
                                        "job-manager.prolog",
                                        status) < 0)
             flux_log_error (flux_jobtap_get_flux (proc->p),
-                            "flux_jobtap_prolog_finish: id=%ju status=%d",
-                            proc->id,
+                            "flux_jobtap_prolog_finish: id=%s status=%d",
+                            idf58 (proc->id),
                             status);
     }
     else {
@@ -233,8 +233,8 @@ static void io_cb (flux_subprocess_t *sp, const char *stream)
     int len;
 
     if (!(s = flux_subprocess_getline (sp, stream, &len))) {
-        flux_log_error (h, "%ju: %s: %s: flux_subprocess_getline",
-                        (uintmax_t) proc->id,
+        flux_log_error (h, "%s: %s: %s: flux_subprocess_getline",
+                        idf58 (proc->id),
                         proc->prolog ? "prolog": "epilog",
                         stream);
         return;
@@ -245,8 +245,8 @@ static void io_cb (flux_subprocess_t *sp, const char *stream)
             level = LOG_ERR;
         flux_log (h,
                   level,
-                  "%ju: %s: %s: %s",
-                  (uintmax_t) proc->id,
+                  "%s: %s: %s: %s",
+                  idf58 (proc->id),
                   proc->prolog ? "prolog" : "epilog",
                   stream,
                   s);
@@ -380,8 +380,8 @@ static void prolog_kill_cb (flux_future_t *f, void *arg)
 
     if (flux_future_get (f, NULL) < 0) {
         flux_log_error (h,
-                        "%ju: Failed to signal job prolog",
-                        (uintmax_t) proc->id);
+                        "%s: Failed to signal job prolog",
+                        idf58 (proc->id));
     }
 }
 
@@ -419,8 +419,8 @@ static void prolog_kill_timer_cb (flux_reactor_t *r,
 
     if (!(f = flux_subprocess_kill (proc->sp, SIGKILL))) {
         flux_log_error (h,
-                        "%ju: failed to send SIGKILL to prolog",
-                        (uintmax_t) proc->id);
+                        "%s: failed to send SIGKILL to prolog",
+                        idf58 (proc->id));
         return;
     }
     /* Do not wait for any response */
@@ -439,8 +439,8 @@ static int prolog_kill_timer_start (struct perilog_proc *proc, double timeout)
                                                       proc);
         if (!proc->kill_timer) {
             flux_log_error (h,
-                            "%ju: failed to start prolog kill timer",
-                            (uintmax_t) proc->id);
+                            "%s: failed to start prolog kill timer",
+                            idf58 (proc->id));
             return -1;
         }
         flux_watcher_start (proc->kill_timer);

commit fab7a667ca665db7a891f5771cc832b31446b50d

job-manager: fix jobid encoding in reattach event
Problem: the "debug.exec-reattach-start" event packs "id" with the "I"
(64-bit integer) specifier but passes idf58(job->id), which returns a
string.

Commit f34751b8 inadvertently changed the payload argument while
converting log messages to f58; the payload should remain the numeric
jobid, as in every other "id" event.

Assisted-by: claude:opus-4.8

src/modules/job-manager/restart.c
@@ -586,7 +586,7 @@ int restart_from_kvs (struct job_manager *ctx)
                                          "debug.exec-reattach-start",
                                          0,
                                          "{s:I}",
-                                         "id", idf58 (job->id)) < 0)
+                                         "id", job->id) < 0)
                     flux_log_error (ctx->h, "%s: event_job_post_pack id=%s",
                                     __FUNCTION__, idf58 (job->id));
             }